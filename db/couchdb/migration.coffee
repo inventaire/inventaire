inventaire/inventaire

commit 121b6898b61329828344c2712d880731842cab99

migration: add a silent mode (useful to log something during the update function without the noise) and log doc diffs also when the changes are applyied (instead of only in preview mode)

db/couchdb/migration.coffee
@@ -3,29 +3,37 @@ _ = __.require 'builders', 'utils'
 Promise = require 'bluebird'
 Promise.longStackTraces()
 fs = require 'fs'
-previewDocDiffs = require './preview_doc_diffs'
 
-module.exports = (dbName, designDocName, preview=true)->
+module.exports = (params)->
+  { dbName, designDocName, preview, silent } = params
+  preview ?= true
+  silent ?= false
+
   db = __.require('couch', 'cot_base')(dbName, designDocName)
   unless db? then throw new Error('bad dbName')
 
+  log = if silent then _.identity else _.log
+  Log = (label)-> (obj)-> log obj, label
+  docDiff = if silent then _.noop else require('./doc_diffs')
+
   console.log 'preview mode:', preview
+  console.log 'silent mode:', silent
 
   updater = (docsIdsPromise, updateFunction, label)->
     unless preview then logMigration dbName, updateFunction, label
 
     docsIdsPromise
     .then (ids)->
-      _.log ids, 'got ids'
+      log ids, 'got ids'
       promises = []
       ids.forEach (id)->
         unless isDesignDoc id
-          _.log id, 'id'
+          log id, 'id'
           promises.push updateIfNeeded(id, updateFunction)
 
       return Promise.all promises
 
-    .then -> _.log 'done updating !!'
+    .then -> log 'done updating !!'
     .catch _.ErrorRethrow('migration error')
 
   updateIfNeeded = (id, updateFn)->
@@ -34,10 +42,10 @@ module.exports = (dbName, designDocName, preview=true)->
       # use a clone of the doc to keep it unmuted
       update = updateFn _.extend({}, doc)
       if _.objDiff doc, update
-        if preview then previewDocDiffs doc, update
-        else return db.update id, updateFn
+        docDiff doc, update, preview
+        return db.update id, updateFn
       else
-        console.log 'no changes', id
+        log id, 'no changes'
         return
 
   API =
@@ -58,7 +66,7 @@ module.exports = (dbName, designDocName, preview=true)->
     getViewKeys: (viewName)->
       db.view designDocName, viewName
       .then (res)-> return res.rows.map _.property('id')
-      .then _.Log('view ids')
+      .then Log('view ids')
 
   return API
 

db/couchdb/doc_diffs.coffee
@@ -4,8 +4,9 @@ write = process.stdout.write.bind(process.stdout)
 { diffLines } = require 'diff'
 stringify = (obj)-> JSON.stringify obj, null, 2
 
-module.exports = (current, update)->
-  console.log 'PREVIEW'.cyan
+module.exports = (current, update, preview)->
+  if preview then console.log 'PREVIEW'.cyan
+  else console.log 'CHANGE'.yellow
   diffLines stringify(current), stringify(update)
   .forEach (part)->
     { added, removed, value } = part

scripts/migrator
@@ -9,13 +9,13 @@
 
 __ = require('config').universalPath
 
+# Set preview to false to apply the migration
 migrationParams = __.require 'couchdb', "migration_docs/#{fileName}"
 
-# set preview to false to apply the migration
-{ dbName, designDocName, viewName, preview, updateFunction } = migrationParams
+{ viewName, updateFunction } = migrationParams
 
 # the database port will be taken from the local config
-migration = __.require('couchdb', './migration')(dbName, designDocName, preview)
+migration = __.require('couchdb', './migration')(migrationParams)
 
 # will run the updateFunction on all documents emitted by the view
 migration.updateByView viewName, updateFunction

commit 10588a0c7f457cc49bc2fb1bff33b1b035609b78

migration: fix preview mode changes were applied whatever the state of preview mode issue introduced by 121b6898

db/couchdb/migration.coffee
@@ -43,7 +43,7 @@ module.exports = (params)->
       update = updateFn _.cloneDeep(doc)
       if _.objDiff doc, update
         docDiff doc, update, preview
-        return db.update id, updateFn
+        unless preview then db.update id, updateFn
       else
         log id, 'no changes'
         return