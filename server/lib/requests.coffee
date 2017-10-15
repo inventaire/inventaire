inventaire/inventaire

commit 57863f305805869fd16ecf3bda1ebaa2c6fb4281

time loggers: cleaning

server/lib/requests.coffee
@@ -42,7 +42,8 @@ startTimer = (verb, url)->
     .replace /\/\/\w+:[^@:]+@/, '//'
 
   key = "#{verb.toUpperCase()} #{url} [#{randomString()}]"
-  return _.startTimer key
+  _.startTimer key
+  return key
 
 module.exports =
   get: _.partial req, 'get'

server/lib/utils/logs.coffee
@@ -81,12 +81,8 @@ module.exports = (_)->
 
       setInterval counter.bind(@), 5000
 
-    startTimer: (key, color='magenta')->
-      key = chalk[color](key)
-      console.time key
-      return key
-
-    EndTimer: (key)-> ()-> console.timeEnd key
+    startTimer: (key)-> console.time chalk.magenta(key)
+    EndTimer: (key)-> ()-> console.timeEnd chalk.magenta(key)
 
   # The same as inv-loggers::errorRethrow but using customLoggers.error instead
   errorRethrow = (err, label)->

commit 94dae4f5cb3cf93c4437754c1c4619010e147d04

time loggers: make sure to return the non-formated key fixing issue introduced by 57863f3: server/controllers/entities/lib/search_by_text.coffee startTime function was returning undefined keys

server/lib/requests.coffee
@@ -41,9 +41,7 @@ startTimer = (verb, url)->
     # Prevent logging Basic Auth credentials
     .replace /\/\/\w+:[^@:]+@/, '//'
 
-  key = "#{verb.toUpperCase()} #{url} [#{randomString()}]"
-  _.startTimer key
-  return key
+  return _.startTimer "#{verb.toUpperCase()} #{url} [#{randomString()}]"
 
 module.exports =
   get: _.partial req, 'get'

server/lib/utils/logs.coffee
@@ -84,8 +84,13 @@ module.exports = (_)->
 
       setInterval counter.bind(@), 5000
 
-    startTimer: (key)-> console.time chalk.magenta(key)
-    EndTimer: (key)-> ()-> console.timeEnd chalk.magenta(key)
+    startTimer: (key)->
+      console.time chalk.magenta(key)
+      # Make sure to return the non-formated key
+      return key
+
+    EndTimer: (key)-> ()->
+      console.timeEnd chalk.magenta(key)
 
   # The same as inv-loggers::errorRethrow but using customLoggers.error instead
   errorRethrow = (err, label)->