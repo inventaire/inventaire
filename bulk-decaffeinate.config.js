20299446e90d9904494152abc53785a950e41e6c
jums <jums@inventaire.io>
2019-11-11 17:38:07 +0100

    decaffeinate: adding config files

---
+++ bulk-decaffeinate.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "decaffeinateArgs": [ "--loose" ],
+  "mochaEnvFilePattern": '^.test.js$',
+  "eslintPath": '~/.nvm/versions/node/v12.2.0/bin/eslint'
+  // "skipVerify": true
+}
+
