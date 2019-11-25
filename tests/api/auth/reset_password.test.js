aa06c3cc56969c944f6218d4ba4a75a72407eb4f
jums <jums@inventaire.io>
2019-11-25 20:58:34 +0100

    api-tests: add tests to reset-password endpoint

--- tests/api/auth/reset_password.test.js
+++ tests/api/auth/reset_password.test.js
@@ -13,6 +13,26 @@ describe('auth:reset-password', () => {
     .catch(done)
   })
 
+  it('should reject invalid email', done => {
+    const invalidEmail = 'foo'
+    nonAuthReq('post', endpoint, { email: invalidEmail })
+    .catch(err => {
+      err.body.status_verbose.should.startWith('invalid email')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should reject inexistant email', done => {
+    const wrongEmail = 'validBut@wrongEmail.org'
+    nonAuthReq('post', endpoint, { email: wrongEmail })
+    .catch(err => {
+      err.body.status_verbose.should.startWith('email not found')
+      done()
+    })
+    .catch(done)
+  })
+
   it('should send a reset password email', done => {
     getUser()
     .then(user => nonAuthReq('post', endpoint, { email: user.email }))
