e4885a0fe84fe04100b572074fe4a561eff73f07
jums <jums@inventaire.io>
2019-11-26 03:39:33 +0100

    api-tests:auth: test update password with reset timestamp

--- tests/api/auth/update_password.test.js
+++ tests/api/auth/update_password.test.js
@@ -1,10 +1,12 @@
 const CONFIG = require('config')
 const __ = CONFIG.universalPath
 require('should')
-const { authReq, getUser } = require('../utils/utils')
+const { authReq, customAuthReq, getUser, getUserGetter } = require('../utils/utils')
 const randomString = __.require('lib', 'utils/random_string')
 const endpoint = '/api/auth?action=update-password'
 const { createUser, createUserEmail } = require('../fixtures/users')
+const { BasicUpdater } = __.require('lib', 'doc_updates')
+const db = __.require('couch', 'base')('users')
 
 describe('auth:update-password', () => {
   it('should reject short new password', done => {
@@ -50,4 +52,64 @@ describe('auth:update-password', () => {
     })
     .catch(done)
   })
+
+  it('should reject if reset password timestamp is invalid', done => {
+    const email = createUserEmail()
+    const userPromise = getUserGetter(email, false)()
+    updateCustomUser(userPromise, 'resetPassword', 'invalid')
+    .then(() => {
+      customAuthReq(userPromise, 'post', endpoint, {
+        email,
+        'new-password': randomString(20)
+      })
+      .catch(err => {
+        err.body.status_verbose.should.equal('invalid resetPassword timestamp')
+        done()
+      })
+    })
+    .catch(done)
+  })
+
+  it('should reject if reset password timestamp is too old', done => {
+    const email = createUserEmail()
+    const userPromise = getUserGetter(email, false)()
+    updateCustomUser(userPromise, 'resetPassword', 1000)
+    .then(() => {
+      customAuthReq(userPromise, 'post', endpoint, {
+        email,
+        'new-password': randomString(20)
+      })
+      .catch(err => {
+        err.body.status_verbose.should.equal('reset password timespan experied')
+        done()
+      })
+    })
+    .catch(done)
+  })
+
+  it('should reset password timestamp is recent', done => {
+    const email = createUserEmail()
+    const userPromise = getUserGetter(email, false)()
+    const recentTime = Date.now() - 1000
+    updateCustomUser(userPromise, 'resetPassword', recentTime)
+    .then(() => {
+      customAuthReq(userPromise, 'post', endpoint, {
+        email,
+        'new-password': randomString(20)
+      })
+      .then(res => {
+        res.ok.should.be.true()
+        done()
+      })
+    })
+    .catch(done)
+  })
 })
+
+const updateCustomUser = (userPromise, userAttribute, value) => {
+  return userPromise
+  .then(user => {
+    db.update(user._id, BasicUpdater(userAttribute, value))
+  })
+  .delay(100)
+}
