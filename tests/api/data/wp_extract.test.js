1ad85db047115cb61d98e3ab01468bc6a1a86634
jums <jums@inventaire.io>
2019-11-27 20:08:56 +0100

    api-tests: improve wikipedia extract endpoint tests

--- tests/api/data/wp_extract.test.js
+++ tests/api/data/wp_extract.test.js
@@ -1,9 +1,43 @@
-require('should')
-const { nonAuthReq } = require('../utils/utils')
+const CONFIG = require('config')
+const __ = CONFIG.universalPath
+const should = require('should')
+const { nonAuthReq, undesiredRes } = require('../utils/utils')
+const endpoint = '/api/data?action=wp-extract'
+const randomString = __.require('lib', 'utils/random_string')
 
 describe('wikipedia:extract', () => {
-  it('should get an extract of a Wikipedia article', done => {
-    nonAuthReq('get', '/api/data?action=wp-extract&lang=fr&title=Gilbert_Simondon')
+  it('should reject without title', done => {
+    nonAuthReq('get', endpoint)
+    .then(undesiredRes(done))
+    .catch(err => {
+      err.body.status_verbose.should.equal('missing parameter in query: title')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should return empty response when no page is found', done => {
+    const randomTitle = randomString(15)
+    nonAuthReq('get', `${endpoint}&title=${randomTitle}`)
+    .then(res => {
+      should(res.extract).not.be.ok()
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should get english Wikipedia article by default', done => {
+    nonAuthReq('get', `${endpoint}&title=Gilbert_Simondon`)
+    .then(res => {
+      res.url.should.equal('https://en.wikipedia.org/wiki/Gilbert_Simondon')
+      res.extract.should.startWith('Gilbert Simondon')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should get an extract in the appropriate language', done => {
+    nonAuthReq('get', `${endpoint}&lang=fr&title=Gilbert_Simondon`)
     .then(res => {
       res.url.should.equal('https://fr.wikipedia.org/wiki/Gilbert_Simondon')
       res.extract.should.startWith('Gilbert Simondon')
