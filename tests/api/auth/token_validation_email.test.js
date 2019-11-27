24f5c67120662471addbedcffe5f9659ce7d7ff1
jums <jums@inventaire.io>
2019-11-27 19:54:57 +0100

    api-tests:auth: improve token tests

---
+++ tests/api/auth/token_validation_email.test.js
@@ -0,0 +1,96 @@
+const CONFIG = require('config')
+const __ = CONFIG.universalPath
+const host = CONFIG.fullHost()
+require('should')
+const { undesiredRes, getUserGetter } = require('../utils/utils')
+const { rawRequest } = require('../utils/request')
+const { createUserEmail } = require('../fixtures/users')
+const { BasicUpdater } = __.require('lib', 'doc_updates')
+const db = __.require('couch', 'base')('users')
+const endpoint = '/api/token?action=validation-email'
+const randomString = __.require('lib', 'utils/random_string')
+
+describe('token:validation-email', () => {
+  it('should reject requests without email', done => {
+    rawRequest('get', {
+      url: host + endpoint,
+      followRedirect: false
+    })
+    .then(res => {
+      res.headers.location.should.equal('/?validEmail=false')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should reject requests without token', done => {
+    const email = createUserEmail()
+    rawRequest('get', {
+      url: `${host}${endpoint}&email=${email}`,
+      followRedirect: false
+    })
+    .then(res => {
+      res.headers.location.should.equal('/?validEmail=false')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should reject if token is too short', done => {
+    const email = createUserEmail()
+    const token = randomString(31)
+    const userPromise = getUserGetter(email, false)()
+    userPromise
+    .then(() => {
+      rawRequest('get', {
+        url: `${host}${endpoint}&email=${email}&token=${token}`,
+        followRedirect: false
+      })
+      .then(res => {
+        res.headers.location.should.equal('/?validEmail=false')
+        done()
+      })
+    })
+    .catch(undesiredRes(done))
+  })
+
+  it('should reject if account is already validated', done => {
+    const email = createUserEmail()
+    const token = randomString(32)
+    const userPromise = getUserGetter(email, false)()
+    userPromise
+    .then(user => {
+      db.update(user._id, BasicUpdater('validEmail', true))
+    })
+    .delay(100)
+    .then(() => {
+      rawRequest('get', {
+        url: `${host}${endpoint}&email=${email}&token=${token}`,
+        followRedirect: false
+      })
+      .then(res => {
+        res.headers.location.should.equal('/?validEmail=false')
+        done()
+      })
+    })
+    .catch(undesiredRes(done))
+  })
+
+  it('should reject if invalid token', done => {
+    const email = createUserEmail()
+    const token = randomString(32)
+    const userPromise = getUserGetter(email, false)()
+    userPromise
+    .then(() => {
+      rawRequest('get', {
+        url: `${host}${endpoint}&email=${email}&token=${token}`,
+        followRedirect: false
+      })
+      .then(res => {
+        res.headers.location.should.equal('/?validEmail=false')
+        done()
+      })
+    })
+    .catch(undesiredRes(done))
+  })
+})
