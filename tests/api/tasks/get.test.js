0eafc38ee8b366fc756fcb6b3cbba602a9354dee
jums <jums@inventaire.io>
2019-11-29 00:36:40 +0100

    api-tests:tasks: remove ordering
    as not required by clients for the moment

--- tests/api/tasks/get.test.js
+++ tests/api/tasks/get.test.js
@@ -2,9 +2,8 @@ const CONFIG = require('config')
 const __ = CONFIG.universalPath
 require('should')
 const { undesiredErr } = __.require('apiTests', 'utils/utils')
-const { createSomeTasks } = require('../fixtures/tasks')
-const { createHuman, createWorkWithAuthor } = require('../fixtures/entities')
-const { getByScore, getBySuspectUris, getBySuggestionUris, update, checkEntities } = require('../utils/tasks')
+const { createHuman } = require('../fixtures/entities')
+const { getByScore, getBySuspectUris, getBySuggestionUris, update } = require('../utils/tasks')
 const { createTask } = require('../fixtures/tasks')
 
 // Tests dependency: having a populated ElasticSearch wikidata index
@@ -47,29 +46,6 @@ describe('tasks:byScore', () => {
           tasksA[1].should.deepEqual(tasksB[0])
           done()
         })
-      })
-    })
-    .catch(undesiredErr(done))
-  })
-
-  it('should return tasks in the right order', done => {
-    const humanLabel = 'Stanislas Lem' // has no homonyms
-    const workLabel = 'Solaris' // too short label to be automerged
-    createSomeTasks('Gilbert Simondon')
-    .then(() => createHuman({ labels: { en: humanLabel } }))
-    .then(human => {
-      return createWorkWithAuthor(human, workLabel)
-      .then(work => checkEntities(human.uri))
-      .then(() => getByScore())
-      .then(tasks => {
-        tasks.forEach((task, i) => {
-          const previousTask = tasks[i - 1]
-          if (previousTask == null) return
-          const prevOccurrencesCount = previousTask.externalSourcesOccurrences.length
-          const occurrencesCount = task.externalSourcesOccurrences.length
-          return prevOccurrencesCount.should.be.aboveOrEqual(occurrencesCount)
-        })
-        done()
       })
     })
     .catch(undesiredErr(done))
