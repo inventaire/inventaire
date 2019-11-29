69b340708ae50e650f614aba16dba316d44fb9b3
jums <jums@inventaire.io>
2019-11-29 02:57:26 +0100

    api:update-claim: catch error on entity not found
    and improve tests

--- server/controllers/entities/lib/update_inv_claim.js
+++ server/controllers/entities/lib/update_inv_claim.js
@@ -14,24 +14,22 @@ const inferredClaimUpdates = require('./inferred_claim_updates')
 const updateInvClaim = (user, id, property, oldVal, newVal) => {
   assert_.object(user)
   const { _id: userId, admin: userIsAdmin } = user
-
   return entities_.byId(id)
-  .then(currentDoc => {
-    if (currentDoc == null) {
+  .catch(err => {
+    if (err.statusCode === 404) {
       throw error_.new('entity not found', 400, { id, property, oldVal, newVal })
     }
-
+  })
+  .then(currentDoc => {
     // Known cases: entities turned into redirections or removed:placeholders
     if (currentDoc.claims == null) {
       const context = { id, property, oldVal, newVal }
       throw error_.new('this entity is obsolete', 400, context)
     }
-
     const type = getEntityType(currentDoc.claims['wdt:P31'])
     validateClaimProperty(type, property)
     return updateClaim({ type, property, oldVal, newVal, userId, currentDoc, userIsAdmin })
   })
-
   .then(updatedDoc => {
     radio.emit('entity:update:claim', updatedDoc, property, oldVal, newVal)
     // Wait for inferred updates
