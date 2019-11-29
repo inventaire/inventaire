974fa67c1ab4194d5be24605f7b0fbbcb80d5556
jums <jums@inventaire.io>
2019-11-29 13:49:50 +0100

    api-tests:contributions
    add test "should return an empty list of patch when user does not exist", but is this really the behaviour this endpoint should have ?
    Wouldn't it make more sense to return a user not found ?

--- tests/api/entities/contributions.test.js
+++ tests/api/entities/contributions.test.js
@@ -1,13 +1,35 @@
 const should = require('should')
-const { adminReq, getUser, undesiredErr } = require('../utils/utils')
+const { adminReq, getUser, undesiredErr, undesiredRes } = require('../utils/utils')
 const { createWork } = require('../fixtures/entities')
+const endpoint = '/api/entities?action=contributions'
 
-describe('entities:get:contributions', () => {
+describe('entities:contributions', () => {
+  it('should reject without user id', done => {
+    adminReq('get', endpoint)
+    .then(undesiredRes(done))
+    .catch(err => {
+      err.body.status_verbose.should.equal('missing parameter in query: user')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should return an empty list of patch when user does not exist', done => {
+    const id = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab'
+    adminReq('get', `${endpoint}&user=${id}`)
+    .then(res => {
+      res.patches.should.be.an.Array()
+      res.patches.length.should.equal(0)
+      done()
+    })
+    .catch(done)
+  })
+
   it('should return a list of patches', done => {
     getUser()
     .then(user => {
       const { _id } = user
-      return adminReq('get', `/api/entities?action=contributions&user=${_id}`)
+      adminReq('get', `${endpoint}&user=${_id}`)
       .then(res => {
         res.patches.should.be.an.Array()
         done()
@@ -21,7 +43,7 @@ describe('entities:get:contributions', () => {
     .delay(1000)
     .spread((workA, workB, user) => {
       const { _id } = user
-      return adminReq('get', `/api/entities?action=contributions&user=${_id}`)
+      adminReq('get', `${endpoint}&user=${_id}`)
       .then(res => {
         const { patches } = res
         const patchesIds = patches.map(getPatchEntityId);
@@ -39,7 +61,7 @@ describe('entities:get:contributions', () => {
     .delay(1000)
     .spread((workA, workB, user) => {
       const { _id } = user
-      return adminReq('get', `/api/entities?action=contributions&user=${_id}&limit=1`)
+      adminReq('get', `${endpoint}&user=${_id}&limit=1`)
       .then(res => {
         const { patches } = res
         patches.length.should.equal(1)
@@ -55,7 +77,7 @@ describe('entities:get:contributions', () => {
     .delay(1000)
     .spread((workA, workB, user) => {
       const { _id } = user
-      return adminReq('get', `/api/entities?action=contributions&user=${_id}&limit=1&offset=1`)
+      adminReq('get', `${endpoint}&user=${_id}&limit=1&offset=1`)
       .then(res => {
         const { patches } = res
         patches.length.should.equal(1)
@@ -71,24 +93,24 @@ describe('entities:get:contributions', () => {
     .delay(1000)
     .spread((workA, workB, user) => {
       const { _id } = user
-      return adminReq('get', `/api/entities?action=contributions&user=${_id}&limit=1`)
+      adminReq('get', `${endpoint}&user=${_id}&limit=1`)
       .then(res1 => {
         res1.total.should.be.a.Number()
         should(res1.total >= 2).be.true()
         res1.continue.should.be.a.Number()
         res1.continue.should.equal(1)
         getWorkId(res1.patches[0]._id).should.equal(workB._id)
-        return create2WorksAndGetUser()
+        create2WorksAndGetUser()
         .delay(1000)
         .spread((workC, workD) => {
-          return adminReq('get', `/api/entities?action=contributions&user=${_id}&limit=3`)
+          adminReq('get', `${endpoint}&user=${_id}&limit=3`)
           .then(res2 => {
             getWorkId(res2.patches[0]._id).should.equal(workD._id)
             getWorkId(res2.patches[1]._id).should.equal(workC._id)
             getWorkId(res2.patches[2]._id).should.equal(workB._id)
             res2.continue.should.equal(3)
             res2.total.should.equal(res1.total + 2)
-            return adminReq('get', `/api/entities?action=contributions&user=${_id}&offset=3`)
+            adminReq('get', `${endpoint}&user=${_id}&offset=3`)
             .then(res3 => {
               getWorkId(res3.patches[0]._id).should.equal(workA._id)
               done()
