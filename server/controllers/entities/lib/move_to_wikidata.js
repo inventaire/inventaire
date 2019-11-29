cfdc1e0ae0006cf6e14add67b762ab710d6013c6
jums <jums@inventaire.io>
2019-11-29 13:37:31 +0100

    api-tests:entities: add tests on move-to-wikidata
    only basic tests to avoid mocking wikidata response
    do not test create_wd_entity and merge_entities libs as it should be
    tested (hopefully) somewhere else in the test suite

--- server/controllers/entities/lib/move_to_wikidata.js
+++ server/controllers/entities/lib/move_to_wikidata.js
@@ -1,3 +1,5 @@
+const __ = require('config').universalPath
+const error_ = __.require('lib', 'error/error')
 const entities_ = require('./entities')
 const mergeEntities = require('./merge_entities')
 const { unprefixify } = require('./prefix')
@@ -9,6 +11,11 @@ module.exports = (user, invEntityUri) => {
   const entityId = unprefixify(invEntityUri)
 
   return entities_.byId(entityId)
+  .catch(err => {
+    if (err.statusCode === 404) {
+      throw error_.new('entity not found', 400, { invEntityUri })
+    }
+  })
   .then(entity => {
     const { labels, claims } = entity
     return createWdEntity({ labels, claims, user, isAlreadyValidated: true })
