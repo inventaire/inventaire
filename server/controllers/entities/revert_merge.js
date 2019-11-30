8c33ce3a94b4e7f50c4b9c5265b22623b5a4add1
jums <jums@inventaire.io>
2019-11-30 01:26:03 +0100

    api-tests:entities: minor tests improvments to revert-merge
    no courage to dig into refactoring code

--- server/controllers/entities/revert_merge.js
+++ server/controllers/entities/revert_merge.js
@@ -4,24 +4,26 @@ const responses_ = __.require('lib', 'responses')
 const error_ = __.require('lib', 'error/error')
 const revertMerge = require('./lib/revert_merge')
 const radio = __.require('lib', 'radio')
+const sanitize = __.require('lib', 'sanitize/sanitize')
+
+const sanitization = {
+  from: {}
+}
 
 module.exports = (req, res) => {
-  const { body } = req
-  const { from: fromUri } = body
-  const { _id: reqUserId } = req.user
+  sanitize(req, res, sanitization)
+  .then(params => {
+    const { from: fromUri, reqUserId } = params
+    const [ fromPrefix, fromId ] = fromUri.split(':')
 
-  if (!_.isNonEmptyString(fromUri)) {
-    return error_.bundleMissingBody(req, res, 'from')
-  }
+    if ((fromPrefix !== 'inv') || !_.isInvEntityId(fromId)) {
+      const message = `invalid 'from' uri domain: ${fromPrefix}. Accepted domains: inv`
+      return error_.bundle(req, res, message, 400, params)
+    }
 
-  const [ fromPrefix, fromId ] = fromUri.split(':')
-
-  if ((fromPrefix !== 'inv') || !_.isInvEntityId(fromId)) {
-    return error_.bundleInvalid(req, res, 'from')
-  }
-
-  return revertMerge(reqUserId, fromId)
-  .tap(() => radio.emit('entity:revert:merge', fromUri))
-  .then(responses_.Send(res))
+    return revertMerge(reqUserId, fromId)
+    .tap(() => radio.emit('entity:revert:merge', fromUri))
+    .then(responses_.Send(res))
+  })
   .catch(error_.Handler(req, res))
 }
