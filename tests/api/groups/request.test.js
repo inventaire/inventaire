83f347a1bca9c06c2cc9b9083ac9a7b9ee523c17
jums <jums@inventaire.io>
2019-12-04 20:33:42 +0100

    api:groups: refactor request
    and add tests

---
+++ tests/api/groups/request.test.js
@@ -0,0 +1,46 @@
+const CONFIG = require('config')
+const __ = CONFIG.universalPath
+require('should')
+const { authReq, authReqB, authReqC, undesiredErr } = require('../utils/utils')
+const { groupPromise, getGroup } = require('../fixtures/groups')
+const endpoint = '/api/groups?action=request'
+
+describe('groups:update:accept', () => {
+  it('should reject without group', done => {
+    authReq('put', `${endpoint}`, { user: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' })
+    .catch(err => {
+      err.body.status_verbose.should.equal('missing parameter in body: group')
+      err.statusCode.should.equal(400)
+      done()
+    })
+  })
+
+  it('should reject request from already member users', done => {
+    groupPromise
+    .then(group => {
+      authReqB('put', `${endpoint}`, { group: group._id })
+      .catch(err => {
+        err.body.status_verbose.should.startWith('user is already in group')
+        err.statusCode.should.equal(403)
+        done()
+      })
+      .catch(undesiredErr(done))
+    })
+  })
+
+  it('should add user to requesters list', done => {
+    groupPromise
+    .then(group => {
+      group.requested.length.should.equal(0)
+      authReqC('put', `${endpoint}`, { group: group._id })
+      .then(res => {
+        getGroup(group._id)
+        .then(group => {
+          group.requested.length.should.equal(1)
+          done()
+        })
+        .catch(undesiredErr(done))
+      })
+    })
+  })
+})
