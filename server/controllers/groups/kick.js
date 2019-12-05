04dc264edcbb7bfb7ab34707fd5f040380498904
jums <jums@inventaire.io>
2019-12-05 04:52:10 +0100

    api:groups: sanitize kick and leave endpoints

---
+++ server/controllers/groups/kick.js
@@ -0,0 +1,44 @@
+const CONFIG = require('config')
+const __ = CONFIG.universalPath
+const _ = __.require('builders', 'utils')
+const sanitize = __.require('lib', 'sanitize/sanitize')
+const groups_ = require('./lib/groups')
+const membershipActions = require('./lib/membership_actions')
+const { Track } = __.require('lib', 'track')
+const error_ = __.require('lib', 'error/error')
+const promises_ = __.require('lib', 'promises')
+
+module.exports = (req, res) => {
+  sanitize(req, res, { group: {}, user: {} })
+  .then(params => {
+    const { group: groupId, user: userId, reqUserId } = params
+    _.log(params, 'kick request group')
+
+    return validateAdminRightsWithoutAdminsConflict(reqUserId, groupId, userId)
+    .then(membershipActions.kick.bind(null, params, reqUserId))
+    .then(addUpdateData(res))
+    .then(Track(req, [ 'groups', 'kick request' ]))
+  })
+  .catch(error_.Handler(req, res))
+}
+
+// Allow to pass an update object, with key/values to be updated on the model
+// as the results of update hooks
+const addUpdateData = res => (data = {}) => {
+  res.json({ ok: true, update: data.update })
+}
+
+const validateAdminRightsWithoutAdminsConflict = (reqUserId, groupId, targetId) => {
+  return promises_.all([
+    groups_.userInAdmins(reqUserId, groupId),
+    groups_.userInAdmins(targetId, groupId)
+  ])
+  .spread((userIsAdmin, targetIsAdmin) => {
+    if (!userIsAdmin) {
+      throw error_.new('user isnt a group admin', 403, reqUserId, groupId)
+    }
+    if (targetIsAdmin) {
+      throw error_.new('target user is also a group admin', 403, reqUserId, groupId, targetId)
+    }
+  })
+}
---
+++ server/controllers/groups/leave.js
@@ -0,0 +1,45 @@
+const CONFIG = require('config')
+const __ = CONFIG.universalPath
+const _ = __.require('builders', 'utils')
+const sanitize = __.require('lib', 'sanitize/sanitize')
+const groups_ = require('./lib/groups')
+const membershipActions = require('./lib/membership_actions')
+const { Track } = __.require('lib', 'track')
+const error_ = __.require('lib', 'error/error')
+const promises_ = __.require('lib', 'promises')
+
+module.exports = (req, res) => {
+  sanitize(req, res, { group: {} })
+  .then(params => {
+    const { group: groupId, reqUserId } = params
+    _.log(params, 'leave group')
+
+    return validateRightToLeave(reqUserId, groupId)
+    .then(membershipActions.leave.bind(null, params, reqUserId))
+    .then(addUpdateData(res))
+    .then(Track(req, [ 'groups', 'leave' ]))
+  })
+  .catch(error_.Handler(req, res))
+}
+
+// Allow to pass an update object, with key/values to be updated on the model
+// as the results of update hooks
+const addUpdateData = res => (data = {}) => {
+  res.json({ ok: true, update: data.update })
+}
+
+const validateRightToLeave = (reqUserId, groupId) => {
+  return promises_.all([
+    groups_.userInGroup(reqUserId, groupId),
+    groups_.userCanLeave(reqUserId, groupId)
+  ])
+  .spread((userInGroup, userCanLeave) => {
+    if (!userInGroup) {
+      throw error_.new('user is not in the group', 403, reqUserId, groupId)
+    }
+    if (!userCanLeave) {
+      const message = "the last group admin can't leave before naming another admin"
+      throw error_.new(message, 403, reqUserId, groupId)
+    }
+  })
+}
