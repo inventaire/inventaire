ba816ffb6e9ef3aabc8fa7851f260415332226d1
jums <jums@inventaire.io>
2019-12-06 21:46:40 +0100

    api:groups: sanitize create enpoint

--- server/controllers/groups/create.js
+++ server/controllers/groups/create.js
@@ -4,22 +4,35 @@ const responses_ = __.require('lib', 'responses')
 const error_ = __.require('lib', 'error/error')
 const groups_ = require('./lib/groups')
 const { Track } = __.require('lib', 'track')
+const sanitize = __.require('lib', 'sanitize/sanitize')
+
+const sanitization = {
+  name: {},
+  description: { optional: true },
+  position: { optional: true },
+  searchable: {
+    optional: true,
+    generic: 'boolean'
+  }
+}
 
 module.exports = (req, res) => {
-  const { name, description, position } = req.body
-  if (name == null) return error_.bundleMissingBody(req, res, 'name')
+  sanitize(req, res, sanitization)
+  .then(params => {
+    const { name, description, position } = params
+    let { searchable } = params
 
-  let { searchable } = req.body
-  if (searchable == null) { searchable = true }
+    if (searchable == null) { searchable = true }
 
-  return groups_.create({
-    name,
-    description: description || '',
-    searchable,
-    position: position || null,
-    creatorId: req.user._id
+    return groups_.create({
+      name,
+      description: description || '',
+      searchable,
+      position: position || null,
+      creatorId: req.user._id
+    })
+    .then(responses_.Send(res))
+    .then(Track(req, [ 'groups', 'create' ]))
   })
-  .then(responses_.Send(res))
-  .then(Track(req, [ 'groups', 'create' ]))
   .catch(error_.Handler(req, res))
 }
