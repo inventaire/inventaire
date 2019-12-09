c5f4ced490f2892e7649e67cd476b1efd01183af
jums <jums@inventaire.io>
2019-12-09 20:30:02 +0100

    api-tests;transactions: add a test on get transaction

--- tests/api/transactions/get.test.js
+++ tests/api/transactions/get.test.js
@@ -2,12 +2,13 @@ const CONFIG = require('config')
 const __ = CONFIG.universalPath
 const _ = __.require('builders', 'utils')
 require('should')
-const { authReq, undesiredErr } = __.require('apiTests', 'utils/utils')
+const { authReq, authReqC, undesiredErr } = __.require('apiTests', 'utils/utils')
 const { createTransaction } = require('../fixtures/transactions')
+const transactionPromise = createTransaction()
 
 describe('transactions:get', () => {
   it('should get user transactions', done => {
-    createTransaction()
+    transactionPromise
     .then(res1 => {
       const { transaction } = res1
       return authReq('get', '/api/transactions')
@@ -15,6 +16,20 @@ describe('transactions:get', () => {
         res2.transactions.should.be.an.Array()
         const transactionsIds = _.map(res2.transactions, '_id')
         transactionsIds.should.containEql(transaction._id)
+        done()
+      })
+    })
+    .catch(undesiredErr(done))
+  })
+
+  it('should not get other users transactions', done => {
+    transactionPromise
+    .then(res1 => {
+      const { transaction } = res1
+      return authReqC('get', '/api/transactions')
+      .then(res2 => {
+        const transactionsIds = _.map(res2.transactions, '_id')
+        transactionsIds.should.not.containEql(transaction._id)
         done()
       })
     })
