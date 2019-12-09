6da34b140e3e4d1da1d90a7659f23e65f8d58994
jums <jums@inventaire.io>
2019-12-09 18:21:11 +0100

    api:transactions: sanitize mark-as-read controller

--- server/controllers/transactions/mark_as_read.js
+++ server/controllers/transactions/mark_as_read.js
@@ -5,15 +5,23 @@ const error_ = __.require('lib', 'error/error')
 const responses_ = __.require('lib', 'responses')
 const validations = __.require('models', 'validations/common')
 const transactions_ = require('./lib/transactions')
+const sanitize = __.require('lib', 'sanitize/sanitize')
+
+const sanitization = {
+  id: {}
+}
 
 module.exports = (req, res, next) => {
-  const { id } = req.body
-  validations.pass('transactionId', id)
-  const reqUserId = req.user._id
+  sanitize(req, res, sanitization)
+  .then(params => {
+    const { id } = params
+    validations.pass('transactionId', id)
+    const reqUserId = req.user._id
 
-  return transactions_.byId(id)
-  .then(transactions_.verifyRightToInteract.bind(null, reqUserId))
-  .then(transactions_.markAsRead.bind(null, reqUserId))
-  .then(responses_.Ok(res))
+    return transactions_.byId(id)
+    .then(transactions_.verifyRightToInteract.bind(null, reqUserId))
+    .then(transactions_.markAsRead.bind(null, reqUserId))
+    .then(responses_.Ok(res))
+  })
   .catch(error_.Handler(req, res))
 }
