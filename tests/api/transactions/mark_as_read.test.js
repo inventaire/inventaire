cbdcbb4b5771157a9cb6880cdbfc2ee9635fe206
jums <jums@inventaire.io>
2019-12-09 18:21:11 +0100

    api:transactions: sanitize mark-as-read controller

---
+++ tests/api/transactions/mark_as_read.test.js
@@ -0,0 +1,41 @@
+const CONFIG = require('config')
+const __ = CONFIG.universalPath
+require('should')
+const { authReq, authReqB, authReqC, shouldNotGetHere, rethrowShouldNotGetHereErrors } = __.require('apiTests', 'utils/utils')
+const { createTransaction } = require('../fixtures/transactions')
+
+const endpoint = '/api/transactions?action=mark-as-read'
+
+describe('transactions:update-state', () => {
+  it('should reject without id', async () => {
+    try {
+      const res = await authReq('put', endpoint, {})
+      shouldNotGetHere(res)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.equal('missing parameter in body: id')
+      err.statusCode.should.equal(400)
+    }
+  })
+
+  it('should update transaction read state', async () => {
+    const { transaction } = await createTransaction()
+    transaction.read.should.deepEqual({ requester: true, owner: false })
+    await authReqB('put', endpoint, { id: transaction._id })
+    const { transactions } = await authReq('get', '/api/transactions')
+    const updatedTransation = transactions[0]
+    updatedTransation.read.should.deepEqual({ requester: true, owner: true })
+  })
+
+  it('should not update when requested by a user not involved in transaction', async () => {
+    try {
+      const transactionRes = await createTransaction()
+      const { transaction } = transactionRes
+      const updateRes = await authReqC('put', endpoint, { id: transaction._id })
+      shouldNotGetHere(updateRes)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.equal('wrong user')
+    }
+  })
+})
