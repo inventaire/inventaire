2a8ffb68e2f9a049f7ca00d9cc32f0dd6006cc72
jums <jums@inventaire.io>
2019-12-09 18:37:16 +0100

    api-tests:transaction add tests to update state endpoint

--- tests/api/transactions/update-state.test.js
+++ tests/api/transactions/update-state.test.js
@@ -1,24 +1,49 @@
 const CONFIG = require('config')
 const __ = CONFIG.universalPath
 require('should')
-const { undesiredErr } = __.require('apiTests', 'utils/utils')
+const { authReqB, authReqC, shouldNotGetHere, rethrowShouldNotGetHereErrors } = __.require('apiTests', 'utils/utils')
 const { createTransaction } = require('../fixtures/transactions')
-const { authReqB } = __.require('apiTests', 'utils/utils')
+
+const endpoint = '/api/transactions?action=update-state'
 
 describe('transactions:update-state', () => {
-  it('should update state and apply side effects', done => {
-    createTransaction()
-    .then(transactionRes => {
+  it('should update state and apply side effects', async () => {
+    const transactionRes = await createTransaction()
+    const { transaction } = transactionRes
+    const updateRes = await authReqB('put', endpoint, {
+      transaction: transaction._id,
+      state: 'accepted'
+    })
+    updateRes.ok.should.be.true()
+  })
+
+  it('should not update unknown state', async () => {
+    try {
+      const transactionRes = await createTransaction()
       const { transaction } = transactionRes
-      return authReqB('put', '/api/transactions?action=update-state', {
+      const updateRes = await authReqB('put', endpoint, {
+        transaction: transaction._id,
+        state: 'random state'
+      })
+      shouldNotGetHere(updateRes)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.startWith('invalid state')
+    }
+  })
+
+  it('should not update when requested by a user not involved in transaction', async () => {
+    try {
+      const transactionRes = await createTransaction()
+      const { transaction } = transactionRes
+      const updateRes = await authReqC('put', endpoint, {
         transaction: transaction._id,
         state: 'accepted'
       })
-      .then(updateRes => {
-        updateRes.ok.should.be.true()
-        done()
-      })
-    })
-    .catch(undesiredErr(done))
+      shouldNotGetHere(updateRes)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.equal('wrong user')
+    }
   })
 })
