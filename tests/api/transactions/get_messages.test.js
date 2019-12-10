7158a77ce3cd4d79f672d8dc056de9c491b8e425
jums <jums@inventaire.io>
2019-12-10 13:53:20 +0100

    api-tests:transactions: add tests to post and get message endpoints

--- tests/api/transactions/get_messages.test.js
+++ tests/api/transactions/get_messages.test.js
@@ -1,26 +1,31 @@
 const CONFIG = require('config')
 const __ = CONFIG.universalPath
 const should = require('should')
-const { undesiredErr } = __.require('apiTests', 'utils/utils')
+const { shouldNotGetHere, rethrowShouldNotGetHereErrors } = __.require('apiTests', 'utils/utils')
 const { createTransaction, addMessage } = require('../fixtures/transactions')
 const { authReq } = __.require('apiTests', 'utils/utils')
 
+const endpoint = '/api/transactions?action=get-messages'
+
 describe('transactions:get:messages', () => {
-  it('should get a transaction messages', done => {
-    createTransaction()
-    .then(res1 => {
-      const { transaction } = res1
-      const { _id } = transaction
-      return addMessage(transaction)
-      .then(res2 => {
-        return authReq('get', `/api/transactions?action=get-messages&transaction=${_id}`)
-        .then(res3 => {
-          res3.messages.should.be.an.Array()
-          should(res3.messages.length > 0).be.true()
-          done()
-        })
-      })
-    })
-    .catch(undesiredErr(done))
+  it('should reject without id', async () => {
+    try {
+      const res = await authReq('get', endpoint, {})
+      shouldNotGetHere(res)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.equal('missing parameter in query: transaction')
+      err.statusCode.should.equal(400)
+    }
+  })
+
+  it('should get a transaction messages', async () => {
+    const res1 = await createTransaction()
+    const { transaction } = res1
+    const { _id } = transaction
+    await addMessage(transaction)
+    const res3 = await authReq('get', `${endpoint}&transaction=${_id}`)
+    res3.messages.should.be.an.Array()
+    should(res3.messages.length > 0).be.true()
   })
 })
--- tests/api/transactions/post_message.test.js
+++ tests/api/transactions/post_message.test.js
@@ -1,20 +1,92 @@
 const CONFIG = require('config')
 const __ = CONFIG.universalPath
 require('should')
-const { undesiredErr } = __.require('apiTests', 'utils/utils')
+const { authReq, authReqC, shouldNotGetHere, rethrowShouldNotGetHereErrors } = __.require('apiTests', 'utils/utils')
 const { createTransaction, addMessage } = require('../fixtures/transactions')
+const { Promise } = __.require('lib', 'promises')
+
+const endpoint = '/api/transactions?action=message'
+const transactionPromise = Promise.try(createTransaction)
 
 describe('transactions:post:message', () => {
-  it('should create a transaction', done => {
-    createTransaction()
-    .then(res1 => {
-      const { transaction } = res1
-      return addMessage(transaction)
-      .then(res2 => {
-        res2.ok.should.be.true()
-        done()
+  it('should create a transaction', async () => {
+    const res = await transactionPromise
+    const res2 = await addMessage(res.transaction)
+    res2.ok.should.be.true()
+  })
+
+  it('should reject without transaction', async () => {
+    try {
+      const res2 = await authReq('post', endpoint, {
+        action: 'message'
       })
-    })
-    .catch(undesiredErr(done))
+      shouldNotGetHere(res2)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.equal('missing parameter in body: transaction')
+    }
+  })
+
+  it('should reject without message', async () => {
+    try {
+      const res1 = await transactionPromise
+      const { transaction } = res1
+      const res2 = await authReq('post', endpoint, {
+        action: 'message',
+        transaction: transaction._id
+      })
+      shouldNotGetHere(res2)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.equal('missing parameter in body: message')
+    }
+  })
+
+  it('should reject an message empty message', async () => {
+    try {
+      const res1 = await transactionPromise
+      const { transaction } = res1
+      const res2 = await authReq('post', endpoint, {
+        action: 'message',
+        transaction: transaction._id,
+        message: ''
+      })
+      shouldNotGetHere(res2)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.startWith('invalid message')
+    }
+  })
+
+  it('should reject non-string messages', async () => {
+    try {
+      const res1 = await transactionPromise
+      const { transaction } = res1
+      const res2 = await authReq('post', endpoint, {
+        action: 'message',
+        transaction: transaction._id,
+        message: 1
+      })
+      shouldNotGetHere(res2)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.startWith('invalid message')
+    }
+  })
+
+  it('should reject when requested by a user not involved in transaction', async () => {
+    try {
+      const res1 = await transactionPromise
+      const { transaction } = res1
+      const res2 = await authReqC('post', endpoint, {
+        action: 'message',
+        transaction: transaction._id,
+        message: 'yo'
+      })
+      shouldNotGetHere(res2)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.equal('wrong user')
+    }
   })
 })
