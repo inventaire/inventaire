4dced51752fedd5f3e33784f8d7d51e82e48b888
jums <jums@inventaire.io>
2019-12-10 15:59:45 +0100

    api:users: sanitize get by id

--- server/controllers/users/by_ids.js
+++ server/controllers/users/by_ids.js
@@ -1,30 +1,16 @@
 const __ = require('config').universalPath
-const _ = __.require('builders', 'utils')
-const promises_ = __.require('lib', 'promises')
 const error_ = __.require('lib', 'error/error')
 const responses_ = __.require('lib', 'responses')
 const user_ = __.require('controllers', 'user/lib/user')
-const User = __.require('models', 'user')
+const sanitize = __.require('lib', 'sanitize/sanitize')
 
-module.exports = (req, res) => {
-  const { ids } = req.query
-  const reqUserId = req.user && req.user._id
-
-  return promises_.try(parseAndValidateIds.bind(null, ids))
-  .then(user_.getUsersIndexByIds(reqUserId))
-  .then(responses_.Wrap(res, 'users'))
+module.exports = (req, res, next) => {
+  sanitize(req, res, { ids: {} })
+  .then(params => {
+    const { ids, reqUserId } = params
+    if (reqUserId) { ids.push(reqUserId) }
+    return user_.getUsersIndexByIds(ids)
+    .then(responses_.Wrap(res, 'users'))
+  })
   .catch(error_.Handler(req, res))
 }
-
-const parseAndValidateIds = ids => {
-  if (!_.isNonEmptyString(ids)) throw error_.newMissingQuery('ids')
-
-  ids = ids.split('|')
-  if (ids && ids.length > 0 && validUsersIds(ids)) {
-    return ids
-  } else {
-    throw error_.newInvalid('ids', ids)
-  }
-}
-
-const validUsersIds = ids => _.every(ids, User.validations.userId)
--- tests/api/users/by_ids.test.js
+++ tests/api/users/by_ids.test.js
@@ -1,19 +1,36 @@
-require('should')
-const { nonAuthReq, getUser, undesiredErr } = require('../utils/utils')
+const CONFIG = require('config')
+const __ = CONFIG.universalPath
+const _ = __.require('builders', 'utils')
+const { shouldNotGetHere, rethrowShouldNotGetHereErrors } = __.require('apiTests', 'utils/utils')
+const { nonAuthReq, getUser, getUserB } = require('../utils/utils')
+
+const endpoint = '/api/users?action=by-ids'
 
 describe('users:by-ids', () => {
-  it('should get a user', done => {
-    getUser()
-    .then(user => {
-      const userId = user._id
-      return nonAuthReq('get', `/api/users?action=by-ids&ids=${userId}`)
-      .then(res => {
-        res.users.should.be.an.Object()
-        res.users[userId].should.be.an.Object()
-        res.users[userId]._id.should.equal(userId)
-        done()
-      })
-    })
-    .catch(undesiredErr(done))
+  it('should reject without id', async () => {
+    try {
+      const res = await nonAuthReq('get', endpoint, {})
+      shouldNotGetHere(res)
+    } catch (err) {
+      rethrowShouldNotGetHereErrors(err)
+      err.body.status_verbose.should.equal('missing parameter in query: ids')
+      err.statusCode.should.equal(400)
+    }
+  })
+
+  it('should get a user', async () => {
+    const user = await getUser()
+    const userId = user._id
+    const res = await nonAuthReq('get', `${endpoint}&ids=${userId}`)
+    res.users.should.be.an.Object()
+    res.users[userId].should.be.an.Object()
+    res.users[userId]._id.should.equal(userId)
+  })
+
+  it('should get several users', async () => {
+    const users = await Promise.all([ getUser(), getUserB() ])
+    const ids = users.map(_.property('_id'))
+    const res = await nonAuthReq('get', `${endpoint}&ids=${ids.join('|')}`)
+    _.keys(res.users).should.deepEqual(ids)
   })
 })
