b3315808e465c3dd593815245b3a1ce03d6f7dc1
maxlath <k@maxlath.eu>
2019-12-14 22:27:04 +0100

    remove decaffeination scripts

--- bulk-decaffeinate.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "decaffeinateArgs": [ "--loose" ],
-  "mochaEnvFilePattern": '^.test.js$',
-  "eslintPath": '~/.nvm/versions/node/v12.2.0/bin/eslint'
-  // "skipVerify": true
-}
-
