e4164b6f9c529a6b84e4a4e6f3ac016fc1e64729
maxlath <k@maxlath.eu>
2019-12-15 17:01:00 +0100

    couch: base: validate dbBaseName and designDocName
    to avoid building handlers on non-existing databases or design docs

--- server/db/couch/base.js
+++ server/db/couch/base.js
@@ -3,17 +3,30 @@ const __ = CONFIG.universalPath
 const _ = __.require('builders', 'utils')
 const getDbApi = require('./cot_base')
 const memoize = __.require('lib', 'utils/memoize')
+const list = require('./list')
 
 // If no designDocName is provided,
 // assumes it is the same as the dbBaseName
 module.exports = (dbBaseName, designDocName) => {
   const dbName = CONFIG.db.name(dbBaseName)
   if (!designDocName) designDocName = dbBaseName
-  return getHandler(dbName, designDocName)
+  return getHandler(dbBaseName, dbName, designDocName)
 }
 
-const getHandler = memoize((dbName, designDocName) => {
+const getHandler = memoize((dbBaseName, dbName, designDocName) => {
+  validate(dbBaseName, designDocName)
   const db = getDbApi(dbName, designDocName)
   const bundles = require('./bundles')(db, _)
   return Object.assign(db, bundles)
 })
+
+// Not using error_ as that would make hard to solve cirucular dependencies
+const validate = (dbBaseName, designDocName) => {
+  if (!list[dbBaseName]) {
+    throw new Error(`unknown dbBaseName: ${dbBaseName}`)
+  }
+
+  if (!(list[dbBaseName].includes(designDocName))) {
+    throw new Error(`unknown designDocName: ${designDocName}`)
+  }
+}
