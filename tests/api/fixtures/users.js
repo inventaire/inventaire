e66518cb969073f26900ef7fd7f32cdbb4b38c9d
maxlath <k@maxlath.eu>
2019-12-18 16:41:00 +0100

    api-tests: user: add tests for non-tested endpoints

--- tests/api/fixtures/users.js
+++ tests/api/fixtures/users.js
@@ -57,6 +57,9 @@ const API = module.exports = {
   },
 
   getRefreshedUser: userPromise => {
+    // Also accept already resolved user docs with their cookie
+    if (userPromise._id && userPromise.cookie) userPromise = Promise.resolve(userPromise)
+
     return userPromise
     // Get the up-to-date user doc while keeping the cookie
     // set by tests/api/fixtures/users
--- tests/api/user/delete.test.js
+++ tests/api/user/delete.test.js
@@ -1,32 +1,22 @@
-const CONFIG = require('config')
-const __ = CONFIG.universalPath
 const should = require('should')
-const { getUserGetter, customAuthReq } = require('../utils/utils')
+const { getReservedUser, customAuthReq } = require('../utils/utils')
 const { getRefreshedUser } = require('../fixtures/users')
-const randomString = __.require('lib', './utils/random_string')
 
 describe('user:delete', () => {
-  it('should delete the user', done => {
-    const userPromise = getUserGetter(randomString(6), false)()
-    userPromise
-    .then(user => customAuthReq(userPromise, 'delete', '/api/user')
-    .then(res => {
-      res.ok.should.be.true()
-      return getRefreshedUser(userPromise)
-      .then(deletedUser => {
-        deletedUser._id.should.equal(user._id)
-        const previousRevInteger = parseInt(user._rev.split('-')[0])
-        parseInt(deletedUser._rev.split('-')[0]).should.equal(previousRevInteger + 1)
-        deletedUser.username.should.equal(user.username)
-        should(deletedUser.password).not.be.ok()
-        should(deletedUser.email).not.be.ok()
-        should(deletedUser.settings).not.be.ok()
-        should(deletedUser.readToken).not.be.ok()
-        should(deletedUser.picture).not.be.ok()
-        should(deletedUser.snapshot).not.be.ok()
-        done()
-      })
-    })
-    .catch(done))
+  it('should delete the user', async () => {
+    const user = await getReservedUser()
+    const res = await customAuthReq(user, 'delete', '/api/user')
+    res.ok.should.be.true()
+    const deletedUser = await getRefreshedUser(user)
+    deletedUser._id.should.equal(user._id)
+    const previousRevInteger = parseInt(user._rev.split('-')[0])
+    parseInt(deletedUser._rev.split('-')[0]).should.equal(previousRevInteger + 1)
+    deletedUser.username.should.equal(user.username)
+    should(deletedUser.password).not.be.ok()
+    should(deletedUser.email).not.be.ok()
+    should(deletedUser.settings).not.be.ok()
+    should(deletedUser.readToken).not.be.ok()
+    should(deletedUser.picture).not.be.ok()
+    should(deletedUser.snapshot).not.be.ok()
   })
 })
