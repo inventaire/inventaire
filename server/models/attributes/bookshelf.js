225e4908199cf6fddac4e74ad71b76bbc1067742
jums <jums@inventaire.io>
2019-12-19 03:36:35 +0100

    simplify attributes

--- server/models/attributes/bookshelf.js
+++ server/models/attributes/bookshelf.js
@@ -1,11 +1,5 @@
-const __ = require('config').universalPath
-const _ = __.require('builders', 'utils')
-
-module.exports = {
-  description: _.isString,
-  listing: {
-    possibilities: [ 'private', 'network', 'public' ],
-    defaultValue: 'private'
-  },
-  name: _.isString
-}
+module.exports = [
+  'description',
+  'listing',
+  'name'
+]
--- server/models/validations/bookshelf.js
+++ server/models/validations/bookshelf.js
@@ -1,12 +1,10 @@
 const { pass, BoundedString } = require('./common')
 
-const attributes = require('../attributes/bookshelf')
-
 module.exports = {
   pass,
   description: BoundedString(0, 5000),
   listing: listing => {
-    return attributes.listing.possibilities.includes(listing)
+    return [ 'private', 'network', 'public' ].includes(listing)
   },
   name: BoundedString(0, 128)
 }
