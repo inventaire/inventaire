d10708d3d5234ffee32ee609a86a7de8f6311cc7
maxlath <k@maxlath.eu>
2020-02-16 20:36:30 +0100

    users: search-by-position: add tests for semi-private and private data

--- tests/api/users/search_by_position.test.js
+++ tests/api/users/search_by_position.test.js
@@ -1,25 +1,41 @@
-const CONFIG = require('config')
-const __ = CONFIG.universalPath
-const _ = __.require('builders', 'utils')
 const should = require('should')
-const { nonAuthReq } = require('../utils/utils')
+const { nonAuthReq, customAuthReq, getUser } = require('../utils/utils')
 const { createUser } = require('../fixtures/users')
+const { makeFriends } = require('../utils/relations')
 const qs = require('querystring')
+const someUserWithPosition = createUser({ position: [ 1, 1 ] }).delay(100)
+const bbox = qs.escape(JSON.stringify([ 0, 0, 2, 2 ]))
 
-describe('groups:search-by-position', () => {
-  it('should get groups by position', done => {
-    createUser({ position: [ 1, 1 ] })
-    .delay(100)
-    .then(user => {
-      const bbox = qs.escape(JSON.stringify([ 0, 0, 2, 2 ]))
-      return nonAuthReq('get', `/api/users?action=search-by-position&bbox=${bbox}`)
-      .then(res => {
-        res.users.should.be.an.Array()
-        const usersIds = _.map(res.users, '_id')
-        should(usersIds.includes(user._id)).be.true()
-        done()
-      })
-    })
-    .catch(done)
+describe('users:search-by-position', () => {
+  it('should get users by position', async () => {
+    const user = await someUserWithPosition
+    const { users } = await nonAuthReq('get', `/api/users?action=search-by-position&bbox=${bbox}`)
+    users.should.be.an.Array()
+    const foundUser = users.find(userDoc => userDoc._id === user._id)
+    should(foundUser).be.ok()
+    foundUser.snapshot.public.should.be.an.Object()
+  })
+
+  it('should get semi-private data if user is in network', async () => {
+    const user = await someUserWithPosition
+    const requestingUser = await getUser()
+    await makeFriends(user, requestingUser)
+    const { users } = await customAuthReq(requestingUser, 'get', `/api/users?action=search-by-position&bbox=${bbox}`)
+    users.should.be.an.Array()
+    const foundUser = users.find(userDoc => userDoc._id === user._id)
+    should(foundUser).be.ok()
+    foundUser.snapshot.public.should.be.an.Object()
+    foundUser.snapshot.network.should.be.an.Object()
+  })
+
+  it('should get private data if requested user is requester', async () => {
+    const user = await someUserWithPosition
+    const { users } = await customAuthReq(user, 'get', `/api/users?action=search-by-position&bbox=${bbox}`)
+    users.should.be.an.Array()
+    const foundUser = users.find(userDoc => userDoc._id === user._id)
+    should(foundUser).be.ok()
+    foundUser.snapshot.public.should.be.an.Object()
+    foundUser.snapshot.network.should.be.an.Object()
+    foundUser.snapshot.private.should.be.an.Object()
   })
 })
