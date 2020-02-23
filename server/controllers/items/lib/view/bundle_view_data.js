0a118d55f943263a3d2051102d796f3508c21d10
maxlath <k@maxlath.eu>
2020-02-23 19:48:08 +0100

    bundle_view_data: removing worksTree.owner
    as its not used by the client

--- server/controllers/items/lib/view/bundle_view_data.js
+++ server/controllers/items/lib/view/bundle_view_data.js
@@ -9,8 +9,6 @@ module.exports = items => entitiesData => {
   // and this present sorting operation is cached
   // See https://gist.github.com/maxlath/7d5a2740616fd05d6eee7225ec6da2d1
   const timestampedItemsIds = items.map(getTimestampItemId).sort(byTimestamp)
-  const worksByOwner = items.reduce(aggregateOwnersWorks(editionWorkMap), {})
-  worksTree.owner = worksByOwner
   return { worksTree, workUriItemsMap, timestampedItemsIds }
 }
 
@@ -22,15 +20,4 @@ const buildWorkUriItemsMap = editionWorkMap => (workUriItemsMap, item) => {
     workUriItemsMap[workUri].push(itemId)
   }
   return workUriItemsMap
-}
-
-const aggregateOwnersWorks = editionWorkMap => (index, item) => {
-  const { _id: itemId, owner: ownerId, entity: entityUri } = item
-  const worksUris = editionWorkMap[entityUri] || [ entityUri ]
-  index[ownerId] = index[ownerId] || {}
-  for (const workUri of worksUris) {
-    index[ownerId][workUri] = index[ownerId][workUri] || []
-    index[ownerId][workUri].push(itemId)
-  }
-  return index
 }
