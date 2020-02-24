b1fb631fe8b11bc314cb273a296c7eba6476b50c
maxlath <k@maxlath.eu>
2020-02-24 15:13:07 +0100

    utils: json: use fs.promises

--- server/builders/utils.js
+++ server/builders/utils.js
@@ -3,7 +3,6 @@ const __ = CONFIG.universalPath
 const _ = require('lodash')
 const server_ = __.require('utils', 'base')
 const logs_ = __.require('utils', 'logs')(_)
-const json_ = __.require('utils', 'json')
 const booleanValidations_ = __.require('lib', 'boolean_validations')
 
-module.exports = Object.assign(_, server_, logs_, json_, booleanValidations_)
+module.exports = Object.assign(_, server_, logs_, booleanValidations_)
