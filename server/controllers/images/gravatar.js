23fcd683e5bd42fc871d989c4548c478e7f2dbfd
maxlath <k@maxlath.eu>
2020-02-25 12:09:21 +0100

    + /api/images?action=gravatar
    An endpoint to get the request user email and convert it to a gravatar url
    so that a client can offer to import an existing avatar

---
+++ server/controllers/images/gravatar.js
@@ -0,0 +1,36 @@
+// An endpoint to get the request user email and convert it to a gravatar url
+// so that a client can offer to import an existing avatar
+
+const CONFIG = require('config')
+const __ = CONFIG.universalPath
+const error_ = __.require('lib', 'error/error')
+const responses_ = __.require('lib', 'responses')
+const sanitize = __.require('lib', 'sanitize/sanitize')
+const { md5 } = __.require('lib', 'crypto')
+
+const sanitization = {}
+
+// Get an image data-url from a URL
+module.exports = (req, res) => {
+  sanitize(req, res, sanitization)
+  .then(() => {
+    const { email } = req.user
+    return getGravatarUrl(email)
+  })
+  .then(responses_.Wrap(res, 'url'))
+  .catch(error_.Handler(req, res))
+}
+
+const getGravatarUrl = email => `${baseUrl}${getHash(email)}${queryString}`
+
+const baseUrl = 'https://www.gravatar.com/avatar/'
+
+// Default to a 404 error if no image exists
+// see https://fr.gravatar.com/site/implement/images/
+const queryString = '?d=404&s=500'
+
+// See https://fr.gravatar.com/site/implement/hash/
+const getHash = email => {
+  email = email.trim().toLowerCase()
+  return md5(email)
+}
