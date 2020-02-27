54c1779d245cc41b83e7443b12656592e305c2fb
maxlath <k@maxlath.eu>
2020-02-27 16:56:39 +0100

    api-tests: allow to customize tests timeout in config

--- config/tests.js
+++ config/tests.js
@@ -11,6 +11,11 @@ module.exports = {
   fullHost: function () {
     return `${this.protocol}://${this.host}:${this.port}`
   },
+  mocha: {
+    // Fit to match the needs of the slowest API,
+    // but can most of the time be overriden in ./local.js config with a lower value
+    timeout: 20000
+  },
   db: {
     suffix: 'tests',
     // debug: true
