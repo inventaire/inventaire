inventaire/inventaire

commit 1276693e801b6d1873b3c79c1d3d6f19fcc0e9c1

api:exists-or-create-from-seed blind refactor
warning: this refactor is untested
please double check its validity

server/controllers/entities/exists_or_create_from_seed.js
@@ -42,28 +42,28 @@ const findOrCreateEntity = seed => entityDoc => {
 
 const addImage = seed => {
   if (!dataseedEnabled) return promises_.resolve(seed)
-
   // Try to find an image from the seed ISBN
   return dataseed.getImageByIsbn(seed.isbn)
   .then(res => {
-    if (res.url) {
-      seed.image = res.url
-      return seed
+    const { url } = res
+    if (url) {
+      seed.image = url
     } else {
-      const { image } = seed
-      if (image == null) return seed
-
-      // Else, if an image was provided in the seed, try to use it
-      return dataseed.getImageByUrl(seed.image)
-      .then(res2 => {
-        if (res.url) seed.image = res2.url
-        else delete seed.image
-        return seed
-      })
+      // if an image was provided in the seed, try to use it
+      if (seed.image) assignSeedImage(seed, url)
     }
+    return seed
   })
   .catch(err => {
     _.error(err, 'add image err')
     return seed
+  })
+}
+
+const assignSeedImage = (seed, url) => {
+  dataseed.getImageByUrl(seed.image)
+  .then(res => {
+    if (url) seed.image = res.url
+    else delete seed.image
   })
 }

commit c6b4e4f1ebe6ae6e6976758f4f40be04061f5770

exists_or_create_from_seed: refactor and fix bug introduced in 1276693e
promises L53 and L64 were not returned, meaning that the seed.image was probably
never used as the call stack was not waiting for the request to return
(unless for some reason something was blocking the process downstream, and the seed
object was still around when the request returned)

server/controllers/entities/exists_or_create_from_seed.js
@@ -39,30 +39,24 @@ const findOrCreateEntity = seed => entityDoc => {
   return entityDoc || addImage(seed).then(scaffoldFromSeed)
 }
 
-const addImage = seed => {
-  if (!dataseedEnabled) return Promise.resolve(seed)
-  // Try to find an image from the seed ISBN
-  return dataseed.getImageByIsbn(seed.isbn)
-  .then(res => {
-    const { url } = res
-    if (url) {
-      seed.image = url
-    } else {
-      // if an image was provided in the seed, try to use it
-      if (seed.image) assignSeedImage(seed, url)
-    }
-    return seed
-  })
-  .catch(err => {
+const addImage = async seed => {
+  if (!dataseedEnabled) return seed
+
+  try {
+    seed.image = await findImageFromSeed(seed)
+  } catch (err) {
     _.error(err, 'add image err')
-    return seed
-  })
+  }
+
+  return seed
 }
 
-const assignSeedImage = (seed, url) => {
-  dataseed.getImageByUrl(seed.image)
-  .then(res => {
-    if (url) seed.image = res.url
-    else delete seed.image
-  })
+const findImageFromSeed = async seed => {
+  // Try to find an image from the seed ISBN
+  const { url: isbnImageUrl } = await dataseed.getImageByIsbn(seed.isbn)
+  if (isbnImageUrl) return isbnImageUrl
+
+  // Else, if an image was provided in the seed, try to use it
+  const { url: seedImageUrl } = await dataseed.getImageByUrl(seed.image)
+  if (seedImageUrl) return seedImageUrl
 }