e4e02e3cab7e352eed688f724df3563ff31396ab
maxlath <k@maxlath.eu>
2020-03-27 22:10:46 +0100

    isbn groups: update isbn-groups lib

--- server/lib/isbn/groups.js
+++ server/lib/isbn/groups.js
@@ -6,8 +6,7 @@ const groupsMap = module.exports = {}
 for (const gs1Prefix in groupsData) {
   const gs1PrefixData = groupsData[gs1Prefix]
   for (const groupId in gs1PrefixData) {
-    const groupData = gs1PrefixData[groupId]
-    const { lang } = groupData
+    const lang = gs1PrefixData[groupId]
     if (lang) {
       const wdId = `wd:${wdLang.byCode[lang].wd}`
       groupsMap[`${gs1Prefix}-${groupId}`] = { lang, wd: wdId }
