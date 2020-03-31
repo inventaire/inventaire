dc88223346f68a470a62c507e29c2f4c3d678672
jums <jums@inventaire.io>
2020-03-31 03:19:12 +0200

    filter visible shelves on by-ids endpoint
    could be improved by triggering the the filter after
    couch view result and before the items request

--- server/controllers/shelves/by_owners.js
+++ server/controllers/shelves/by_owners.js
@@ -3,8 +3,9 @@ const _ = __.require('builders', 'utils')
 const responses_ = __.require('lib', 'responses')
 const error_ = __.require('lib', 'error/error')
 const shelves_ = __.require('controllers', 'shelves/lib/shelves')
-const relations_ = __.require('controllers', 'relations/lib/queries')
 const sanitize = __.require('lib', 'sanitize/sanitize')
+const filterVisibleShelves = require('./lib/filter_visible_shelves')
+const { getNetworkIds } = __.require('controllers', 'user/lib/relations_status')
 
 const sanitization = {
   owners: {},
@@ -19,26 +20,9 @@ module.exports = (req, res, next) => {
     let { owners } = params
     owners = _.forceArray(owners)
     return Promise.all([ shelves_.byOwners(owners), getNetworkIds(reqUserId) ])
-    .then(filterAuthorisedShelves(reqUserId))
+    .then(filterVisibleShelves(reqUserId))
     .then(_.KeyBy('_id'))
     .then(responses_.Wrap(res, 'shelves'))
   })
   .catch(error_.Handler(req, res))
-}
-
-const filterAuthorisedShelves = reqUserId => ([ shelves, networkIds ]) => {
-  return shelves.filter(isAuthorised(networkIds, reqUserId))
-}
-
-const isAuthorised = (networkIds, reqUserId) => shelf => {
-  if (shelf.listing === 'private' && shelf.owner === reqUserId) return true
-  if (shelf.listing === 'public') return true
-  if (shelf.listing === 'network' && networkIds.includes(shelf.owner)) return true
-}
-const getNetworkIds = reqUserId => {
-  if (reqUserId) {
-    return relations_.getUserFriendsAndCoGroupsMembers(reqUserId)
-  } else {
-    return []
-  }
 }
