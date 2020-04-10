87fba4318488b69c4617987b4bde95bfbfad336f
maxlath <k@maxlath.eu>
2020-04-10 18:36:50 +0200

    user: update_snapshot_items_counts: refactor without reduce

--- server/controllers/user/lib/update_snapshot_items_counts.js
+++ server/controllers/user/lib/update_snapshot_items_counts.js
@@ -6,31 +6,28 @@ const User = __.require('models', 'user')
 const db = __.require('couch', 'base')('users')
 
 module.exports = userId => {
-  return getItemsCounts(userId)
+  return items_.byOwner(userId)
+  .then(getItemsCounts)
   .then(itemsCounts => db.update(userId, User.updateItemsCounts(itemsCounts)))
   .then(() => _.info(`${userId} items counts updated`))
   .catch(_.Error('user updateSnapshotItemsCounts err'))
 }
 
-const getItemsCounts = userId => {
-  return items_.byOwner(userId)
-  .then(items => items.reduce(aggregateCounts, itemsCountsBase()))
-}
-
-const aggregateCounts = (index, item) => {
-  const { listing, created } = item
-  index[listing]['items:count'] += 1
-
-  const lastAdd = index[listing]['items:last-add']
-  if ((lastAdd == null) || (created > lastAdd)) {
-    index[listing]['items:last-add'] = created
+const getItemsCounts = items => {
+  const counts = {
+    private: { 'items:count': 0 },
+    network: { 'items:count': 0 },
+    public: { 'items:count': 0 }
   }
 
-  return index
-}
+  items.forEach(item => {
+    const { listing, created } = item
+    counts[listing]['items:count'] += 1
+    const lastAdd = counts[listing]['items:last-add'] || 0
+    if (created > lastAdd) {
+      counts[listing]['items:last-add'] = created
+    }
+  })
 
-const itemsCountsBase = () => ({
-  private: { 'items:count': 0 },
-  network: { 'items:count': 0 },
-  public: { 'items:count': 0 }
-})
+  return counts
+}
