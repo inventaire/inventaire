0cb8352573511271f6f5590f0aa649c33d135554
maxlath <k@maxlath.eu>
2020-04-13 18:29:55 +0200

    export: add support for large inventories
    - by enriching items sequentially
    - sending responses as the enriched items come to avoid buffering the whole response

--- server/controllers/items/export.js
+++ server/controllers/items/export.js
@@ -2,11 +2,10 @@ const CONFIG = require('config')
 const __ = CONFIG.universalPath
 const error_ = __.require('lib', 'error/error')
 const sanitize = __.require('lib', 'sanitize/sanitize')
-const responses_ = __.require('lib', 'responses')
-const promises_ = __.require('lib', 'promises')
 const items_ = require('./lib/items')
 const addEntitiesData = require('./lib/export/add_entities_data')
-const formatItemRow = require('./lib/export/format_item_row')
+const FormatItemRow = require('./lib/export/format_item_row')
+const csvHeaderRow = require('./lib/export/csv_header_row')
 
 const sanitization = {
   format: {
@@ -18,42 +17,26 @@ module.exports = (req, res) => {
   sanitize(req, res, sanitization)
   .then(({ reqUserId }) => {
     const { lang } = req.user
+    res.write(csvHeaderRow + '\n')
     return items_.byOwner(reqUserId)
-    .then(promises_.map(addEntitiesData))
-    .then(formatCsv(lang))
+    .then(sendItemsSequentially(res, lang))
   })
-  .then(responses_.SendText(res))
   .catch(error_.Handler(req, res))
 }
 
-const header = [
-  'Item ID',
-  'Edition URI',
-  'ISBN-13',
-  'ISBN-10',
-  'Title',
-  'Subtitle',
-  'PublicationDate',
-  'Cover',
-  'Works URIs',
-  'Works labels',
-  'Works Series ordinals',
-  'Authors URIs',
-  'Authors labels',
-  'Series URIs',
-  'Series labels',
-  'Genres URIs',
-  'Genres labels',
-  'Subjects URIs',
-  'Subjects labels',
-  'Publisher URIs',
-  'Publisher label',
-  'Item details',
-  'Item notes',
-  'Item created'
-].join(',')
+const sendItemsSequentially = (res, lang) => items => {
+  const formatItemRow = FormatItemRow(lang)
 
-const formatCsv = lang => items => {
-  const rows = items.map(formatItemRow(lang))
-  return `${header}\n${rows.join('\n')}`
+  const sendNextBatch = async () => {
+    if (items.length === 0) return res.end()
+
+    // Using batches of 10 items to reduce stress on entities APIs
+    const nextBatch = items.splice(0, 10)
+    const enrichedItems = await Promise.all(nextBatch.map(addEntitiesData))
+    const formattedItemsRows = enrichedItems.map(formatItemRow).join('\n')
+    res.write(formattedItemsRows + '\n')
+    return sendNextBatch()
+  }
+
+  return sendNextBatch()
 }
