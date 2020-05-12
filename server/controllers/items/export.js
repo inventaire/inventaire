c635a4adf6e59573e82534510bfad4a95901178d
maxlath <k@maxlath.eu>
2020-05-12 15:04:45 +0200

    export: avoid sending response headers before we are sure there is no error
    the previous implementation started by sending headers with a 200 status code
    before even having built the csv, which then, when encountering an error,
    would crash internally

--- server/controllers/items/export.js
+++ server/controllers/items/export.js
@@ -6,6 +6,7 @@ const items_ = require('./lib/items')
 const addEntitiesData = require('./lib/export/add_entities_data')
 const FormatItemRow = require('./lib/export/format_item_row')
 const csvHeaderRow = require('./lib/export/csv_header_row')
+const responses_ = __.require('lib', 'responses')
 
 const sanitization = {
   format: {
@@ -17,24 +18,25 @@ module.exports = (req, res) => {
   sanitize(req, res, sanitization)
   .then(({ reqUserId }) => {
     const { lang } = req.user
-    res.write(csvHeaderRow + '\n')
+    const responseText = csvHeaderRow + '\n'
     return items_.byOwner(reqUserId)
-    .then(sendItemsSequentially(res, lang))
+    .then(buildItemsRowsSequentially(responseText, lang))
   })
+  .then(responses_.SendText(res))
   .catch(error_.Handler(req, res))
 }
 
-const sendItemsSequentially = (res, lang) => items => {
+const buildItemsRowsSequentially = (responseText, lang) => items => {
   const formatItemRow = FormatItemRow(lang)
 
   const sendNextBatch = async () => {
-    if (items.length === 0) return res.end()
+    if (items.length === 0) return responseText
 
     // Using batches of 10 items to reduce stress on entities APIs
     const nextBatch = items.splice(0, 10)
     const enrichedItems = await Promise.all(nextBatch.map(addEntitiesData))
     const formattedItemsRows = enrichedItems.map(formatItemRow).join('\n')
-    res.write(formattedItemsRows + '\n')
+    responseText += formattedItemsRows + '\n'
     return sendNextBatch()
   }
 
