d0cda67ce4c087c10db778e8fa2b706fd5c5366d
maxlath <k@maxlath.eu>
2020-05-23 15:49:24 +0200

    tests: resolver: async/await refactor

--- tests/api/entities/resolver/resolve_and_create.test.js
+++ tests/api/entities/resolver/resolve_and_create.test.js
@@ -2,7 +2,7 @@ const CONFIG = require('config')
 const __ = CONFIG.universalPath
 const _ = __.require('builders', 'utils')
 const should = require('should')
-const { authReq, undesiredRes } = __.require('apiTests', 'utils/utils')
+const { authReq, shouldNotBeCalled } = __.require('apiTests', 'utils/utils')
 const { getByUris, getHistory } = __.require('apiTests', 'utils/entities')
 const { randomLabel, humanName, generateIsbn13, someGoodReadsId, someLibraryThingsWorkId, createEditionWithIsbn } = __.require('apiTests', 'fixtures/entities')
 
@@ -12,24 +12,19 @@ const resolveAndCreate = entry => authReq('post', '/api/entities?action=resolve'
 })
 
 describe('entities:resolve:create-unresolved', () => {
-  it('should create unresolved edition, work and author (the trinity)', done => {
-    resolveAndCreate({
+  it('should create unresolved edition, work and author (the trinity)', async () => {
+    const { entries } = await resolveAndCreate({
       edition: { isbn: generateIsbn13() },
       works: [ { labels: { en: randomLabel() } } ],
       authors: [ { labels: { en: humanName() } } ]
     })
-    .then(({ entries }) => entries)
-    .then(entries => {
-      const result = entries[0]
-      result.edition.created.should.be.true()
-      result.authors[0].created.should.be.true()
-      result.works[0].created.should.be.true()
-      should(result.edition.uri).be.ok()
-      should(result.works[0].uri).be.ok()
-      should(result.authors[0].uri).be.ok()
-      done()
-    })
-    .catch(done)
+    const result = entries[0]
+    result.edition.created.should.be.true()
+    result.authors[0].created.should.be.true()
+    result.works[0].created.should.be.true()
+    should(result.edition.uri).be.ok()
+    should(result.works[0].uri).be.ok()
+    should(result.authors[0].uri).be.ok()
   })
 
   it('should resolve and not create an existing edition', async () => {
@@ -39,30 +34,22 @@ describe('entities:resolve:create-unresolved', () => {
     entries[0].edition.uri.should.equal(uri)
   })
 
-  it('should create edition with title and isbn', done => {
+  it('should create edition with title and isbn', async () => {
     const editionLabel = randomLabel()
-    resolveAndCreate({
+    const { entries } = await resolveAndCreate({
       edition: { isbn: generateIsbn13(), claims: { 'wdt:P1476': editionLabel } },
       works: [ { labels: { en: randomLabel() } } ]
     })
-    .then(({ entries }) => entries)
-    .then(entries => {
-      const result = entries[0]
-      should(result.edition.uri).be.ok()
-      const { edition } = result
+    const result = entries[0]
+    should(result.edition.uri).be.ok()
+    const { edition } = result
 
-      return getByUris(edition.uri)
-      .then(({ entities }) => entities)
-      .then(entities => {
-        const editionClaims = _.values(entities)[0].claims
-        const newEditionTitle = editionClaims['wdt:P1476'][0]
+    const { entities } = await getByUris(edition.uri)
+    const editionClaims = _.values(entities)[0].claims
+    const newEditionTitle = editionClaims['wdt:P1476'][0]
 
-        should(editionClaims['wdt:P212'][0]).be.ok()
-        newEditionTitle.should.equal(editionLabel)
-        done()
-      })
-    })
-    .catch(done)
+    should(editionClaims['wdt:P212'][0]).be.ok()
+    newEditionTitle.should.equal(editionLabel)
   })
 
   it('should ignore unresolved work from resolve edition', async () => {
@@ -76,152 +63,106 @@ describe('entities:resolve:create-unresolved', () => {
     entry.works[0].created.should.be.false()
   })
 
-  it('should add optional claims to created edition', done => {
+  it('should add optional claims to created edition', async () => {
     const frenchLang = 'wd:Q150'
-    resolveAndCreate({
+    const { entries } = await resolveAndCreate({
       edition: { isbn: generateIsbn13(), claims: { 'wdt:P407': [ frenchLang ] } },
       works: [ { labels: { en: randomLabel() } } ]
     })
-    .then(({ entries }) => entries)
-    .then(entries => {
-      const result = entries[0]
-      should(result.edition.uri).be.ok()
-      const { edition } = result
-      return getByUris(edition.uri)
-      .then(({ entities }) => entities)
-      .then(entities => {
-        const newWorkClaimValue = _.values(entities)[0].claims['wdt:P407'][0]
-        newWorkClaimValue.should.equal(frenchLang)
-        done()
-      })
-    })
-    .catch(done)
+    const result = entries[0]
+    should(result.edition.uri).be.ok()
+    const { edition } = result
+    const { entities } = await getByUris(edition.uri)
+    const newWorkClaimValue = _.values(entities)[0].claims['wdt:P407'][0]
+    newWorkClaimValue.should.equal(frenchLang)
   })
 
-  it('should add optional claims to created works', done => {
+  it('should add optional claims to created works', async () => {
     const libraryThingsWorkId = someLibraryThingsWorkId()
-    resolveAndCreate({
+    const { entries } = await resolveAndCreate({
       edition: { isbn: generateIsbn13() },
       works: [ { claims: { 'wdt:P1085': [ libraryThingsWorkId ] }, labels: { en: randomLabel() } } ]
     })
-    .then(({ entries }) => entries)
-    .then(entries => {
-      const result = entries[0]
-      should(result.edition.uri).be.ok()
-      const { works } = result
-      return getByUris(works.map(_.property('uri')))
-      .then(({ entities }) => entities)
-      .then(entities => {
-        const newWorkClaimValue = _.values(entities)[0].claims['wdt:P1085'][0]
-        newWorkClaimValue.should.equal(libraryThingsWorkId)
-        done()
-      })
-    })
-    .catch(done)
+    const result = entries[0]
+    should(result.edition.uri).be.ok()
+    const { works } = result
+    const { entities } = await getByUris(works.map(_.property('uri')))
+    const newWorkClaimValue = _.values(entities)[0].claims['wdt:P1085'][0]
+    newWorkClaimValue.should.equal(libraryThingsWorkId)
   })
 
-  it('should add optional claims to created authors', done => {
+  it('should add optional claims to created authors', async () => {
     const goodReadsId = someGoodReadsId()
-    resolveAndCreate({
+    const { entries } = await resolveAndCreate({
       edition: { isbn: generateIsbn13() },
       works: [ { labels: { en: randomLabel() } } ],
       authors: [ { claims: { 'wdt:P2963': [ goodReadsId ] }, labels: { en: humanName() } } ]
     })
-    .then(({ entries }) => entries)
-    .then(entries => {
-      const result = entries[0]
-      should(result.edition.uri).be.ok()
-      const { authors } = result
-      return getByUris(authors.map(_.property('uri')))
-      .then(({ entities }) => entities)
-      .then(entities => {
-        const newWorkClaimValue = _.values(entities)[0].claims['wdt:P2963'][0]
-        newWorkClaimValue.should.equal(goodReadsId)
-        done()
-      })
-    })
-    .catch(done)
+    const result = entries[0]
+    should(result.edition.uri).be.ok()
+    const { authors } = result
+    const { entities } = await getByUris(authors.map(_.property('uri')))
+    const newWorkClaimValue = _.values(entities)[0].claims['wdt:P2963'][0]
+    newWorkClaimValue.should.equal(goodReadsId)
   })
 
-  it('should add a batch timestamp to patches', done => {
+  it('should add a batch timestamp to patches', async () => {
     const startTime = Date.now()
     const entry = {
       edition: { isbn: generateIsbn13() },
       works: [ { claims: { 'wdt:P1085': [ someLibraryThingsWorkId() ] }, labels: { en: humanName() } } ]
     }
-    resolveAndCreate(entry)
-    .then(({ entries }) => entries)
-    .then(entries => {
-      const result = entries[0]
-      const { uri: editionUri } = result.edition
-      return getHistory(editionUri)
-      .then(patches => {
-        const patch = patches[0]
-        patch.batch.should.be.a.Number()
-        patch.batch.should.above(startTime)
-        patch.batch.should.below(Date.now())
-        done()
-      })
-    })
-    .catch(done)
+    const { entries } = await resolveAndCreate(entry)
+    const result = entries[0]
+    const { uri: editionUri } = result.edition
+    const patches = await getHistory(editionUri)
+    const patch = patches[0]
+    patch.batch.should.be.a.Number()
+    patch.batch.should.above(startTime)
+    patch.batch.should.below(Date.now())
   })
 
-  it('should add created authors to created works', done => {
-    resolveAndCreate({
+  it('should add created authors to created works', async () => {
+    const { entries } = await resolveAndCreate({
       edition: { isbn: generateIsbn13() },
       works: [ { labels: { en: randomLabel() } } ],
       authors: [ { labels: { en: humanName() } } ]
     })
-    .then(({ entries }) => entries)
-    .then(entries => {
-      const result = entries[0]
-      const workUri = result.works[0].uri
-      return getByUris(workUri)
-      .then(({ entities }) => entities)
-      .then(entities => {
-        const work = entities[workUri]
-        const workAuthors = work.claims['wdt:P50']
-        workAuthors.includes(result.authors[0].uri).should.be.true()
-        done()
-      })
-    })
-    .catch(done)
+    const result = entries[0]
+    const workUri = result.works[0].uri
+    const { entities } = await getByUris(workUri)
+    const work = entities[workUri]
+    const workAuthors = work.claims['wdt:P50']
+    workAuthors.includes(result.authors[0].uri).should.be.true()
   })
 
-  it('should create a work entity from the edition seed', done => {
-    let title = randomLabel()
-    title = randomLabel()
+  it('should create a work entity from the edition seed', async () => {
+    const title = randomLabel()
     const dutchLangUri = 'wd:Q7411'
     const dutchLangCode = 'nl'
-    resolveAndCreate({
+    const { entries } = await resolveAndCreate({
       edition: {
         isbn: generateIsbn13(),
         claims: { 'wdt:P1476': [ title ], 'wdt:P407': [ dutchLangUri ] }
       }
     })
-    .then(({ entries }) => entries)
-    .then(entries => {
-      const work = entries[0].works[0]
-      work.labels[dutchLangCode].should.equal(title)
-      done()
-    })
-    .catch(done)
+    const work = entries[0].works[0]
+    work.labels[dutchLangCode].should.equal(title)
   })
 
-  it('should not create works without labels', done => {
+  it('should not create works without labels', async () => {
     const title = randomLabel()
-    resolveAndCreate({
-      edition: {
-        isbn: generateIsbn13(),
-        claims: { 'wdt:P1476': [ title ] }
-      },
-      works: [ {} ]
-    })
-    .then(undesiredRes(done))
-    .catch(err => {
+    try {
+      await resolveAndCreate({
+        edition: {
+          isbn: generateIsbn13(),
+          claims: { 'wdt:P1476': [ title ] }
+        },
+        works: [ {} ]
+      })
+      .then(shouldNotBeCalled)
+    } catch (err) {
       err.body.status_verbose.should.startWith('invalid labels')
-      done()
-    })
-    .catch(done)
+    }
   })
 })
