9e3880f4e0236b9fee9f181a278dc151c3e11430
jums <jums@inventaire.io>
2020-05-27 12:27:24 +0200

    adds some error handling and default parameters
    default lang value set to english
    throw 400 if type is not passed or invalid

--- server/data/aliases.js
+++ server/data/aliases.js
@@ -1,20 +1,34 @@
 const __ = require('config').universalPath
 const _ = __.require('builders', 'utils')
-
-const { typesAliases } = __.require('lib', 'wikidata/aliases')
+const error_ = __.require('lib', 'error/error')
+const { typesAliases, typesNames } = __.require('lib', 'wikidata/aliases')
 const getEntitiesByUris = __.require('controllers', 'entities/lib/get_entities_by_uris')
 const getBestLangValue = __.require('lib', 'get_best_lang_value')
 const { unprefixify } = __.require('controllers', 'entities/lib/prefix')
+const sanitize = __.require('lib', 'sanitize/sanitize')
+
+const sanitization = {
+  type: {
+    whitelist: typesNames
+  },
+  lang: { optional: true }
+}
 
 module.exports = (req, res) => {
-  const { type, lang } = req.query
-  const aliasesUris = typesAliases[type]
-  return getEntitiesByUris({ uris: aliasesUris })
-  .then(resp => {
-    const entityLabels = _.mapValues(resp.entities, getBestLangValuePerEntity(lang))
-    const aliases = arrayifyingLabels(entityLabels)
-    res.json(aliases)
+  sanitize(req, res, sanitization)
+  .then(params => {
+    const { type } = params
+    let { lang } = params
+    if (!lang) { lang = 'en' }
+    const aliasesUris = typesAliases[type]
+    return getEntitiesByUris({ uris: aliasesUris })
+    .then(resp => {
+      const entityLabels = _.mapValues(resp.entities, getBestLangValuePerEntity(lang))
+      const aliases = arrayifyingLabels(entityLabels)
+      res.json(aliases)
+    })
   })
+  .catch(error_.Handler(req, res))
 }
 
 const arrayifyingLabels = entityLabels => {
--- tests/api/data/aliases.test.js
+++ tests/api/data/aliases.test.js
@@ -1,8 +1,33 @@
+const __ = require('config').universalPath
 require('should')
 const { nonAuthReq } = require('../utils/utils')
 const endpoint = '/api/data?action=aliases'
+const { shouldNotBeCalled, rethrowShouldNotBeCalledErrors } = __.require('apiTests', 'utils/utils')
 
 describe('data:aliases', () => {
+  it('should reject without param', async () => {
+    try {
+      const res = await nonAuthReq('get', endpoint)
+      shouldNotBeCalled(res)
+    } catch (err) {
+      rethrowShouldNotBeCalledErrors(err)
+      err.body.status_verbose.should.equal('missing parameter in query: type')
+      err.statusCode.should.equal(400)
+    }
+  })
+
+  it('should reject invalid type', async () => {
+    try {
+      const type = 'blu'
+      const res = await nonAuthReq('get', `${endpoint}&type=${type}&lang=fr`)
+      shouldNotBeCalled(res)
+    } catch (err) {
+      rethrowShouldNotBeCalledErrors(err)
+      err.body.status_verbose.should.startWith('invalid type')
+      err.statusCode.should.equal(400)
+    }
+  })
+
   it('should return aliases information', done => {
     const type = 'works'
     nonAuthReq('get', `${endpoint}&type=${type}&lang=fr`)
@@ -10,6 +35,18 @@ describe('data:aliases', () => {
       const firstAlias = res[0]
       firstAlias.uri.should.equal('wd:Q571')
       firstAlias.label.should.equal('livre')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should return default lang as english', done => {
+    const type = 'works'
+    nonAuthReq('get', `${endpoint}&type=${type}`)
+    .then(res => {
+      const firstAlias = res[0]
+      firstAlias.uri.should.equal('wd:Q571')
+      firstAlias.label.should.equal('book')
       done()
     })
     .catch(done)
