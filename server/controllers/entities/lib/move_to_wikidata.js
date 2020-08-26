cf4f71a174105f51136a8f5569ecf561af22efb2
maxlath <k@maxlath.eu>
2020-08-26 17:16:30 +0200

    move_to_wikidata: cache moved entity relations

--- server/controllers/entities/lib/move_to_wikidata.js
+++ server/controllers/entities/lib/move_to_wikidata.js
@@ -2,6 +2,7 @@ const __ = require('config').universalPath
 const error_ = __.require('lib', 'error/error')
 const entities_ = require('./entities')
 const mergeEntities = require('./merge_entities')
+const { cacheEntityRelations } = require('./temporarily_cache_relations')
 const { unprefixify } = require('./prefix')
 const createWdEntity = require('./create_wd_entity')
 
@@ -14,6 +15,11 @@ module.exports = async (user, invEntityUri) => {
 
   const { labels, claims } = entity
   const { uri: wdEntityUri } = await createWdEntity({ labels, claims, user, isAlreadyValidated: true })
+
+  // Caching relations for some hours, as Wikidata Query Service can take some time to update,
+  // at the very minimum some minutes, during which the data contributor might be confused
+  // by the absence of the entity they just moved to Wikidata in lists generated with the help of the WQS
+  await cacheEntityRelations(invEntityUri)
 
   await mergeEntities({ userId: reqUserId, fromUri: invEntityUri, toUri: wdEntityUri })
 
