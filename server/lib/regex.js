12f857e76a2c79c460902c133033b1701a624bbd
maxlath <k@maxlath.eu>
2020-09-03 14:37:10 +0200

    regex: order alphabetically

--- server/lib/regex.js
+++ server/lib/regex.js
@@ -12,27 +12,27 @@ const urlPattern = '^(https?:\\/\\/)' + // protocol
   '(\\#[-a-z\\d_]*)?$' // fragment?
 
 module.exports = {
+  AssetImg: /^\/img\/assets\/\w/,
   CouchUuid: /^[0-9a-f]{32}$/,
   // minimanlist email regex
   // cf http://davidcel.is/blog/2012/09/06/stop-validating-email-addresses-with-regex/
   Email: /^[^@]+@[^@]+\.[^@]+$/,
   EntityUri: /^(wd:Q\d+|inv:[0-9a-f]{32}|isbn:\w{10}(\w{3})?)$/,
+  Float: /^-?[\d.]+$/,
   ImageHash: /^[0-9a-f]{40}$/,
-  Url: new RegExp(urlPattern, 'i'),
-  Uuid: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
+  Integer: /^-?\d+$/,
   // Accepting second level languages (like es-AR)
   Lang: /^\w{2}(-\w{2})?$/,
   LocalImg: /^\/img\/(users|entities)\/[0-9a-f]{40}$/,
-  AssetImg: /^\/img\/assets\/\w/,
-  UserImg: /^\/img\/users\/[0-9a-f]{40}$/,
-  // all 1 letter strings are reserved for the application
-  Username: /^\w{2,20}$/,
+  PositiveInteger: /^\d+$/,
   PropertyUri: /^(wdt|invp):P\d+$/,
   // A year can't start by a 0
   Sha1: /^[0-9a-f]{40}$/,
   SimpleDay: /^-?([1-9]{1}[0-9]{0,3}|0)(-\d{2})?(-\d{2})?$/,
-  Integer: /^-?\d+$/,
-  PositiveInteger: /^\d+$/,
   StrictlyPositiveInteger: /^[1-9]\d*$/,
-  Float: /^-?[\d.]+$/
+  Url: new RegExp(urlPattern, 'i'),
+  UserImg: /^\/img\/users\/[0-9a-f]{40}$/,
+  // all 1 letter strings are reserved for the application
+  Username: /^\w{2,20}$/,
+  Uuid: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
 }
