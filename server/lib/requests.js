29a3220cced4f865428f5e946899522e03b7fffa
maxlath <k@maxlath.eu>
2020-09-15 12:41:32 +0200

    reset ban time after a successful request

--- server/lib/requests.js
+++ server/lib/requests.js
@@ -9,7 +9,7 @@ const { magenta } = require('chalk')
 const { repository } = __.require('root', 'package.json')
 const userAgent = `${CONFIG.name} (${repository.url})`
 const { getAgent, selfSignedHttpsAgent } = require('./requests_agent')
-const { throwIfTemporarilyBanned, declareTimeout } = require('./requests_temporary_host_ban')
+const { throwIfTemporarilyBanned, resetBanData, declareTimeout } = require('./requests_temporary_host_ban')
 const { URL } = require('url')
 // Setting the default timeout low
 const defaultTimeout = 10 * 1000
@@ -75,6 +75,8 @@ const req = method => async (url, options = {}) => {
     addContextToStack(err)
     throw err
   }
+
+  resetBanData(host)
 
   if (returnBodyOnly) {
     return body
