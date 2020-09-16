a250e283f2557ffef68b99df06e1103e6db78c43
maxlath <k@maxlath.eu>
2020-09-16 20:23:41 +0200

    tests: mocha: allow to activate growl notifications from config

--- config/tests-api-mocha.js
+++ config/tests-api-mocha.js
@@ -8,7 +8,9 @@ module.exports = {
   mocha: {
     // Fit to match the needs of the slowest API,
     // but can most of the time be overriden in ./local.js config with a lower value
-    timeout: 20000
+    timeout: 20000,
+    growl: false
   },
+
   logOutgoingRequests: true
 }
