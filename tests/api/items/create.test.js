4b44bcf07c2900050e24d6567d6602bd1a647a31
jums <jums@inventaire.io>
2020-09-21 21:20:06 +0200

    tests items cleanup

--- tests/api/items/create.test.js
+++ tests/api/items/create.test.js
@@ -60,58 +60,92 @@ describe('items:create', () => {
     .catch(done)
   })
 
-  it('should increment the user items counter', done => {
-    const userPromise = createUser()
-    const timestamp = Date.now()
-    createItem(userPromise, { listing: 'public' })
-    .then(Wait(debounceDelay))
-    .then(() => getRefreshedUser(userPromise))
-    .then(user => {
-      user.snapshot.public['items:count'].should.equal(1)
-      user.snapshot.public['items:last-add'].should.be.greaterThan(timestamp)
-      user.snapshot.network['items:count'].should.equal(0)
-      user.snapshot.private['items:count'].should.equal(0)
-      done()
+  describe('user:snapshot', () => {
+    it('should increment the user items counter', done => {
+      const userPromise = createUser()
+      const timestamp = Date.now()
+      createItem(userPromise, { listing: 'public' })
+      .then(Wait(debounceDelay))
+      .then(() => getRefreshedUser(userPromise))
+      .then(user => {
+        user.snapshot.public['items:count'].should.equal(1)
+        user.snapshot.public['items:last-add'].should.be.greaterThan(timestamp)
+        user.snapshot.network['items:count'].should.equal(0)
+        user.snapshot.private['items:count'].should.equal(0)
+        done()
+      })
+      .catch(done)
     })
-    .catch(done)
+
+    // Should not create edition conflicts on the user document
+    it('should keep the snapshot data updated even when created in bulk', done => {
+      const userPromise = createUser()
+      Promise.all([
+        createItem(userPromise, { listing: 'public' }),
+        createItem(userPromise, { listing: 'network' }),
+        createItem(userPromise, { listing: 'private' })
+      ])
+      .then(Wait(debounceDelay))
+      .then(() => getRefreshedUser(userPromise))
+      .then(user => {
+        user.snapshot.public['items:count'].should.equal(1)
+        user.snapshot.network['items:count'].should.equal(1)
+        user.snapshot.private['items:count'].should.equal(1)
+        done()
+      })
+      .catch(done)
+    })
   })
 
-  it('should deduce the title from an edition entity', async () => {
-    const edition = await createEditionWithIsbn()
-    const title = edition.claims['wdt:P1476'][0]
-    title.should.be.a.String()
-    const item = await authReq('post', '/api/items', { entity: edition.uri })
-    item.snapshot.should.be.an.Object()
-    item.snapshot['entity:title'].should.equal(title)
-  })
+  describe('item:snapshot', () => {
+    it('should deduce the title from an edition entity', async () => {
+      const edition = await createEditionWithIsbn()
+      const title = edition.claims['wdt:P1476'][0]
+      title.should.be.a.String()
+      const item = await authReq('post', '/api/items', { entity: edition.uri })
+      item.snapshot.should.be.an.Object()
+      item.snapshot['entity:title'].should.equal(title)
+    })
 
-  it('should deduce the author from a work entity', done => {
-    createHuman()
-    .then(author => {
-      return createWorkWithAuthor(author)
-      .then(workEntity => {
-        return authReq('post', '/api/items', { entity: workEntity.uri, lang: 'en' })
-        .then(item => {
-          item.snapshot.should.be.an.Object()
-          item.snapshot['entity:authors'].should.equal(author.labels.en)
-          done()
+    it('should deduce the author from a work entity', done => {
+      createHuman()
+      .then(author => {
+        return createWorkWithAuthor(author)
+        .then(workEntity => {
+          return authReq('post', '/api/items', { entity: workEntity.uri, lang: 'en' })
+          .then(item => {
+            item.snapshot.should.be.an.Object()
+            item.snapshot['entity:authors'].should.equal(author.labels.en)
+            done()
+          })
         })
       })
+      .catch(done)
     })
-    .catch(done)
-  })
 
-  it('should deduce the author from an edition entity', async () => {
-    const { uri: editionUri } = await createEditionWithWorkAndAuthor()
-    await wait(1000)
-    const { entities } = await getEntitiesByUris(editionUri, 'wdt:P629|wdt:P50')
-    const item = await authReq('post', '/api/items', { entity: editionUri })
-    const edition = entities[editionUri]
-    const work = entities[edition.claims['wdt:P629'][0]]
-    const author = entities[work.claims['wdt:P50'][0]]
-    const authorLabel = _.values(author.labels)[0]
-    item.snapshot.should.be.an.Object()
-    item.snapshot['entity:authors'].should.equal(authorLabel)
+    it('should deduce the author from an edition entity', async () => {
+      const { uri: editionUri } = await createEditionWithWorkAndAuthor()
+      await wait(1000)
+      const { entities } = await getEntitiesByUris(editionUri, 'wdt:P629|wdt:P50')
+      const item = await authReq('post', '/api/items', { entity: editionUri })
+      const edition = entities[editionUri]
+      const work = entities[edition.claims['wdt:P629'][0]]
+      const author = entities[work.claims['wdt:P50'][0]]
+      const authorLabel = _.values(author.labels)[0]
+      item.snapshot.should.be.an.Object()
+      item.snapshot['entity:authors'].should.equal(authorLabel)
+    })
+
+    it('should use the original language label for an item created from a work without specifying in which lang the title is', done => {
+      authReq('post', '/api/items', { entity: 'wd:Q3548806' })
+      .then(item => {
+        item.snapshot.should.be.an.Object()
+        item.snapshot['entity:title'].should.equal('Die Hochzeit von Lyon')
+        item.snapshot['entity:lang'].should.equal('de')
+        done()
+      })
+      .catch(done)
+    })
   })
 
   it('should reject an item created with an unknown entity', done => {
@@ -134,17 +168,6 @@ describe('items:create', () => {
     .catch(done)
   })
 
-  it('should use the original language label for an item created from a work without specifying in which lang the title is', done => {
-    authReq('post', '/api/items', { entity: 'wd:Q3548806' })
-    .then(item => {
-      item.snapshot.should.be.an.Object()
-      item.snapshot['entity:title'].should.equal('Die Hochzeit von Lyon')
-      item.snapshot['entity:lang'].should.equal('de')
-      done()
-    })
-    .catch(done)
-  })
-
   it('should reject an item created with an invalid URI', done => {
     authReq('post', '/api/items', { entity: 'isbn:9782800051922' })
     .catch(err => {
@@ -155,47 +178,30 @@ describe('items:create', () => {
     .catch(done)
   })
 
-  // Should not create edition conflicts on the user document
-  it('should keep the snapshot data updated even when created in bulk', done => {
-    const userPromise = createUser()
-    Promise.all([
-      createItem(userPromise, { listing: 'public' }),
-      createItem(userPromise, { listing: 'network' }),
-      createItem(userPromise, { listing: 'private' })
-    ])
-    .then(Wait(debounceDelay))
-    .then(() => getRefreshedUser(userPromise))
-    .then(user => {
-      user.snapshot.public['items:count'].should.equal(1)
-      user.snapshot.network['items:count'].should.equal(1)
-      user.snapshot.private['items:count'].should.equal(1)
-      done()
-    })
-    .catch(done)
-  })
-
-  it('should create an item with a shelf', async () => {
-    const editionUri = await editionUriPromise
-    const shelf = await createShelf()
-    const item = await authReq('post', '/api/items', {
-      entity: editionUri,
-      shelves: [ shelf._id ]
-    })
-    item.shelves.should.deepEqual([ shelf._id ])
-  })
-
-  it('should reject item with an invalid shelf', async () => {
-    const editionUri = await editionUriPromise
-    try {
+  describe('shelves', () => {
+    it('should create an item with a shelf', async () => {
+      const editionUri = await editionUriPromise
+      const shelf = await createShelf()
       const item = await authReq('post', '/api/items', {
         entity: editionUri,
-        shelves: [ 'not a shelf id' ]
+        shelves: [ shelf._id ]
       })
-      shouldNotBeCalled(item)
-    } catch (err) {
-      rethrowShouldNotBeCalledErrors(err)
-      err.statusCode.should.equal(400)
-      err.body.error_name.should.equal('invalid_shelves')
-    }
+      item.shelves.should.deepEqual([ shelf._id ])
+    })
+
+    it('should reject item with an invalid shelf', async () => {
+      const editionUri = await editionUriPromise
+      try {
+        const item = await authReq('post', '/api/items', {
+          entity: editionUri,
+          shelves: [ 'not a shelf id' ]
+        })
+        shouldNotBeCalled(item)
+      } catch (err) {
+        rethrowShouldNotBeCalledErrors(err)
+        err.statusCode.should.equal(400)
+        err.body.error_name.should.equal('invalid_shelves')
+      }
+    })
   })
 })
