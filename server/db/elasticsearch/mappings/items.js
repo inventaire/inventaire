bea2996365a5b1af072ffdcfd51feaf0726fd5d0
jums <jums@inventaire.io>
2020-09-25 01:02:16 +0200

    remove some types
    start upgrading to elasticsearch 7.9
    
    remove type in elastic urls in type_search.js and couch2elastic4sync/init.    js
    remove _type when indexing in bulk.js
    remove types in items mapping
    
    unexplicably update modified the files permission's, to 644 and 755
     => amend it back as it was before this commit ?

--- server/db/elasticsearch/mappings/items.js
+++ server/db/elasticsearch/mappings/items.js
@@ -1,22 +1,20 @@
 const { string, keyword, date } = require('./mappings_datatypes')
 
 module.exports = {
-  item: {
-    properties: {
-      _rev: keyword,
-      owner: keyword,
-      entity: keyword,
-      listing: keyword,
-      transaction: keyword,
-      created: date,
-      snapshot: {
-        properties: {
-          'entity:title': string,
-          'entity:subtitle': string,
-          'entity:authors': string,
-          'entity:series': string,
-          'entity:lang': keyword
-        }
+  properties: {
+    _rev: keyword,
+    owner: keyword,
+    entity: keyword,
+    listing: keyword,
+    transaction: keyword,
+    created: date,
+    snapshot: {
+      properties: {
+        'entity:title': string,
+        'entity:subtitle': string,
+        'entity:authors': string,
+        'entity:series': string,
+        'entity:lang': keyword
       }
     }
   }
