5172c6dc21452e57aa5850aac18dca9ddbca0387
maxlath <k@maxlath.eu>
2020-09-25 17:44:16 +0200

    item mapping: update

--- server/db/elasticsearch/mappings/mappings_datatypes.js
+++ server/db/elasticsearch/mappings/mappings_datatypes.js
@@ -1,6 +1,7 @@
 // See https://www.elastic.co/guide/en/elasticsearch/reference/2.4/mapping-types.html
 
 module.exports = {
+  text: { type: 'text' },
   keyword: { type: 'keyword' },
   date: { type: 'date' },
   flattened: { type: 'flattened' }
