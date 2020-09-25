d77560a63f5e3cfbb7b9097226e77b147461ec99
maxlath <k@maxlath.eu>
2020-09-25 19:26:22 +0200

    indexation: move group filter to reindex_on_change

--- server/db/elasticsearch/reindex_on_change.js
+++ server/db/elasticsearch/reindex_on_change.js
@@ -8,7 +8,9 @@ const isntDesignDoc = doc => !doc._id.startsWith('_design/')
 const filters = {
   // Do not filter-out doc.type=deletedUser so that deleted users can be unindexed
   users: doc => doc.type === 'user' || doc.type === 'deletedUser',
-  groups: isntDesignDoc,
+  // Do not filter-out doc.searchable=false so that toggling this settings does
+  // update the document in Elasticsearch and can then be filtered-out at search time
+  groups: doc => doc.type === 'group',
   items: isntDesignDoc,
   entities: isntDesignDoc,
 }
