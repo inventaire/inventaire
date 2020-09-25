017d89af45f839cc983386bea86cab4beb5e8135
maxlath <k@maxlath.eu>
2020-09-25 19:26:48 +0200

    indexation: remove filter from user formatter
    as that filter is already operated in reindex_on_change

--- server/db/elasticsearch/formatters/user.js
+++ server/db/elasticsearch/formatters/user.js
@@ -4,8 +4,4 @@ const publicAttributes = __.require('models', 'attributes/user').public
 // Omit snapshot as it contains private and semi priavte data
 const publicAttributesStrict = without(publicAttributes, 'snapshot')
 
-module.exports = doc => {
-  const data = pick(doc, publicAttributesStrict)
-  if (doc.type === 'deletedUser') data._deleted = true
-  return data
-}
+module.exports = doc => pick(doc, publicAttributesStrict)
