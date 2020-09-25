395504de3402129d3aaeb83aa59a15f3663ee719
maxlath <k@maxlath.eu>
2020-09-25 17:43:33 +0200

    desindex delete docs

--- server/db/elasticsearch/formatters/user.js
+++ server/db/elasticsearch/formatters/user.js
@@ -5,12 +5,7 @@ const publicAttributes = __.require('models', 'attributes/user').public
 const publicAttributesStrict = without(publicAttributes, 'snapshot')
 
 module.exports = doc => {
-  // Do not filter-out doc.type=deletedUser so that deleted users can be updated
-  // and filtered-out at search time
-  // It would be better to just unindex deleted users, but that's not something we can do with couch2elastic4sync
-  if (doc.type === 'user' || doc.type === 'deletedUser') {
-    const data = pick(doc, publicAttributesStrict)
-    if (doc.type === 'deletedUser') data.deleted = true
-    return data
-  }
+  const data = pick(doc, publicAttributesStrict)
+  if (doc.type === 'deletedUser') data._deleted = true
+  return data
 }
