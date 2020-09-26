f52b5d0dbcee4bac38eae4745de9b41b22491bf9
maxlath <k@maxlath.eu>
2020-09-26 09:33:41 +0200

    reindex_on_change: extract filters to make them accessible to the load script

--- server/db/elasticsearch/reindex_on_change.js
+++ server/db/elasticsearch/reindex_on_change.js
@@ -3,17 +3,7 @@ const __ = CONFIG.universalPath
 // const error_ = __.require('lib', 'error/error')
 const follow = __.require('lib', 'follow')
 const indexation = require('./indexation')
-const isntDesignDoc = doc => !doc._id.startsWith('_design/')
-
-const filters = {
-  // Do not filter-out doc.type=deletedUser so that deleted users can be unindexed
-  users: doc => doc.type === 'user' || doc.type === 'deletedUser',
-  // Do not filter-out doc.searchable=false so that toggling this settings does
-  // update the document in Elasticsearch and can then be filtered-out at search time
-  groups: doc => doc.type === 'group',
-  items: isntDesignDoc,
-  entities: isntDesignDoc,
-}
+const filters = require('./filters')
 
 module.exports = indexBaseName => {
   const index = CONFIG.db.name(indexBaseName)
