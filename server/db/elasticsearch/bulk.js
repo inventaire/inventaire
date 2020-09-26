4d2b3125c871891b3b85ffac869a4602b13a5389
maxlath <k@maxlath.eu>
2020-09-26 17:52:21 +0200

    load: rewrite to post sequentially
    to avoid the crash in the previous implementation

--- server/db/elasticsearch/bulk.js
+++ server/db/elasticsearch/bulk.js
@@ -2,14 +2,12 @@ const CONFIG = require('config')
 const __ = CONFIG.universalPath
 const _ = __.require('builders', 'utils')
 const requests_ = __.require('lib', 'requests')
-const { host: elasticHost, updateDelay } = CONFIG.elasticsearch
+const { host: elasticHost } = CONFIG.elasticsearch
 const { logBulkRes } = require('./helpers')
-const bulkThrottleDelay = updateDelay / 2
 const assert_ = __.require('utils', 'assert_types')
 const headers = { 'content-type': 'application/json' }
 
-let batch = []
-const addToNextBatch = (action, index, doc) => {
+const addToBatch = (batch, action, index, doc) => {
   const { _id } = doc
   assert_.string(_id)
   // Prevent triggering the error
@@ -17,15 +15,12 @@ const addToNextBatch = (action, index, doc) => {
   delete doc._id
   batch.push(`{"${action}":{"_index":"${index}","_id":"${_id}"}}`)
   if (action === 'index') batch.push(JSON.stringify(doc))
-  if (batch.length >= 1000) postBatch()
-  else lazyPostBatch()
 }
 
-const postBatch = async () => {
+const postBatch = async batch => {
+  if (batch.length === 0) return _.warn('empty batch')
   // It is required to end by a newline break
   const body = batch.join('\n') + '\n'
-  // Reset
-  batch = []
   try {
     const res = await requests_.post(`${elasticHost}/_doc/_bulk`, {
       headers,
@@ -37,6 +32,4 @@ const postBatch = async () => {
   }
 }
 
-const lazyPostBatch = _.throttle(postBatch, bulkThrottleDelay, { leading: false })
-
-module.exports = { addToNextBatch }
+module.exports = { addToBatch, postBatch }
