46f9f94255133a7fb61590675114748faa5ce9c6
jums <jums@inventaire.io>
2020-09-27 03:02:51 +0200

    replace obsolete query body builder
    as _all keys are not available anymore
    - add bool queries inside other bool query to include entity type
    which should match at least one of the types
    - add approximative attempts to boost results : mainly tested on authors names
      - boost results with labels in user language
      - reduce noise by not boosting descriptions much

--- server/controllers/search/lib/query_body_builder.js
+++ server/controllers/search/lib/query_body_builder.js
@@ -1,13 +1,58 @@
-const CONFIG = require('config')
-const __ = CONFIG.universalPath
-const _ = __.require('builders', 'utils')
-
-module.exports = (search, size) => {
-  const should = [
-    { match_phrase_prefix: { _all: { query: search, boost: 5 } } },
-    { match: { _all: { query: search, boost: 5 } } },
-    { prefix: { _all: _.last(search.split(' ')) } }
+module.exports = params => {
+  const { lang: userLang, search, limit: size } = params
+  let { types } = params
+  types = singularType(types)
+  // must match at least one of the types
+  const must = [
+    { bool: { should: matchType(types) } },
+    { bool: { should: matchSearch(search, userLang) } }
   ]
+  return {
+    query: {
+      bool: { must }
+    },
+    size,
+    min_score: 1
+  }
+}
 
-  return { query: { bool: { should } }, size, min_score: 0.1 }
+const singularType = types => types.map(type => type.replace(/s$/, ''))
+
+const matchType = types => types.map(type => ({ match: { type } }))
+
+const matchSearch = (search, userLang) => {
+  return [
+    {
+      multi_match: {
+        query: search,
+        operator: 'and',
+        fields: [
+          `labels.${userLang}^4`,
+          'labels.*^4',
+          'aliases.*^2',
+          'descriptions.*^2'
+        ],
+      }
+    },
+    {
+      multi_match: {
+        query: search,
+        fields: [
+          `labels.${userLang}^2`,
+          'labels.*^2',
+          'aliases.*',
+        ]
+      }
+    },
+    {
+      multi_match: {
+        query: search,
+        fields: [
+          'flattenedLabels',
+          'flattenedAliases',
+          'flattenedDescriptions'
+        ]
+      }
+    }
+  ]
 }
