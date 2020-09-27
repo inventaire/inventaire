c2c08fdbfcfbb4d57289226bcd3845f56271671d
jums <jums@inventaire.io>
2020-09-27 02:58:38 +0200

    create index on load and add an analyser in settings
    this is a first attempt to setup an analyser, based on the offical doc
    ways which could improve it: add tokenizers, refine min and max gram

--- server/db/elasticsearch/create_index.js
+++ server/db/elasticsearch/create_index.js
@@ -4,6 +4,7 @@ const _ = __.require('builders', 'utils')
 const requests_ = __.require('lib', 'requests')
 const { host } = CONFIG.elasticsearch
 const mappings = __.require('db', 'elasticsearch/mappings/mappings')
+const settings = __.require('db', 'elasticsearch/settings/settings')
 
 module.exports = async index => {
   const url = `${host}/${index}`
@@ -11,6 +12,8 @@ module.exports = async index => {
   const indexMappings = mappings[indexBaseName]
   let body
   if (indexMappings) body = { mappings: indexMappings }
+
+  body.settings = settings
   try {
     const res = await requests_.put(url, { body })
     _.success(res, `elasticsearch index created: ${url}`)
@@ -20,7 +23,7 @@ module.exports = async index => {
 }
 
 const ignoreAlreadyExisting = (url, err) => {
-  if (err.body && err.body.error.type === 'index_already_exists_exception') {
+  if (err.body && err.body.error.type === 'resource_already_exists_exception') {
     return _.warn(url, 'database already exist')
   } else {
     throw err
