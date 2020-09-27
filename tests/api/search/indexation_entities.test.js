b71ad4bfd8b9b3beef11b18728387ede0a3961d0
maxlath <k@maxlath.eu>
2020-09-27 13:31:26 +0200

    indexation: entities: recover works and series images from their associated editions

--- tests/api/search/indexation_entities.test.js
+++ tests/api/search/indexation_entities.test.js
@@ -2,8 +2,8 @@ const CONFIG = require('config')
 const __ = CONFIG.universalPath
 const should = require('should')
 const { wait } = __.require('lib', 'promises')
-const { createHuman } = require('../fixtures/entities')
-const { deleteByUris, merge } = require('../utils/entities')
+const { createHuman, createEdition, addSerie } = require('../fixtures/entities')
+const { deleteByUris, merge, updateLabel } = require('../utils/entities')
 const { updateDelay: elasticsearchUpdateDelay } = CONFIG.elasticsearch
 const { getIndexedDoc } = require('../utils/search')
 const { entities: entitiesIndex } = __.require('controllers', 'search/lib/indexes').indexes
@@ -16,6 +16,39 @@ describe('indexation:entities', () => {
     result.found.should.be.true()
     result._source.labels.should.be.an.Object()
     result._source.type.should.equal('human')
+  })
+
+  it('should index a work with its editions images per lang', async () => {
+    const lang = 'nl'
+    // Creates a work, then an edition linking to that work
+    // so the edition wasn't accessible when the work was indexed
+    // so we will need to trigger a reindexation later to get access
+    // to the edition data
+    const { claims } = await createEdition({ lang })
+    const editionImageHash = claims['invp:P2'][0]
+    const workId = claims['wdt:P629'][0].split(':')[1]
+    // Trigger a reindexation
+    await updateLabel(workId, 'es', 'foo')
+    await wait(elasticsearchUpdateDelay)
+    const result = await getIndexedDoc(entitiesIndex, workId)
+    result._source.images[lang][0].should.equal(editionImageHash)
+  })
+
+  it('should index a serie with its works editions images per lang', async () => {
+    const lang = 'nl'
+    // Creates a work, then an edition linking to that work
+    // so the edition wasn't accessible when the work was indexed
+    // so we will need to trigger a reindexation later to get access
+    // to the edition data
+    const { claims } = await createEdition({ lang })
+    const editionImageHash = claims['invp:P2'][0]
+    const workUi = claims['wdt:P629'][0].split(':')[1]
+    const { _id: serieId } = await addSerie(workUi)
+    // Trigger a reindexation
+    await updateLabel(serieId, 'es', 'foo')
+    await wait(elasticsearchUpdateDelay)
+    const result = await getIndexedDoc(entitiesIndex, serieId)
+    result._source.images[lang][0].should.equal(editionImageHash)
   })
 })
 
