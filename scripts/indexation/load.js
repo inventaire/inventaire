0f8f453de8ccfb34f63f17b2fda539d899bc20b4
jums <jums@inventaire.io>
2020-09-28 14:37:52 +0200

    integration of elasticsearch client-helper
    could be integrated once the lib is not at experimental stage anymore
    see https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/client-helpers.html

--- scripts/indexation/load.js
+++ scripts/indexation/load.js
@@ -1,24 +1,23 @@
 #!/usr/bin/env node
 const CONFIG = require('config')
 const __ = CONFIG.universalPath
-const _ = __.require('builders', 'utils')
 const { indexesList, indexes } = __.require('db', 'elasticsearch/list')
 const { logErrorAndExit } = require('../scripts_utils')
-const split = require('split')
+const split = require('split2')
 const { red } = require('chalk')
 const formatters = __.require('db', 'elasticsearch/formatters/formatters')
 const filters = __.require('db', 'elasticsearch/filters')
 const deindex = __.require('db', 'elasticsearch/deindex')
-const { addToBatch, postBatch } = __.require('db', 'elasticsearch/bulk')
 const createIndex = __.require('db', 'elasticsearch/create_index')
 const [ indexBaseName ] = process.argv.slice(2)
-
+const { addToBatch } = __.require('db', 'elasticsearch/bulk')
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({ node: 'http://localhost:9200' })
 if (!indexesList.includes(indexBaseName)) {
   logErrorAndExit(`invalid index base name: ${indexBaseName} (valid indexes: ${indexesList.join(', ')})`)
 }
 
 const { index } = indexes[indexBaseName]
-
 const format = formatters[indexBaseName]
 const shouldBeDeindexed = deindex[indexBaseName]
 const filter = filters[indexBaseName]
@@ -32,56 +31,26 @@ const parseLine = line => {
   }
 }
 
-let batch = []
-const post = async () => {
-  await postBatch(batch)
-  batch = []
-}
-
-let received = 0
-let indexed = 0
-
 const addLine = async line => {
   const doc = parseLine(line)
   if (doc == null) return
-  received++
   if (!filter(doc) || shouldBeDeindexed(doc)) return
   const formattedDoc = await format(doc)
   if (formattedDoc) {
-    addToBatch(batch, 'index', index, formattedDoc)
-    indexed++
+    return addToBatch('index', index, formattedDoc)
   }
-  if (batch.length >= 4000) await post()
 }
 
-const logStatusPeriodically = () => _.info({ received, indexed }, 'indexation:load status')
-const statusLogInterval = setInterval(logStatusPeriodically, 5000)
-const lastStatusLog = () => {
-  clearInterval(statusLogInterval)
-  logStatusPeriodically()
-}
-
-const stopLoading = logErrorAndExit.bind(null, 'loadFromStdin')
-
-const loadFromStdin = () => {
-  process.stdin
-  .pipe(split())
-  .on('data', async function (line) {
-    this.pause()
-    await addLine(line).catch(stopLoading)
-    this.resume()
-  })
-  .on('close', async () => {
-    _.info(`${indexBaseName} indexation:load stdin closed`)
-    await post().catch(stopLoading)
-    _.success(`${indexBaseName} indexation:load done`)
-    lastStatusLog()
-  })
-  .on('error', _.Error(`${indexBaseName} indexation:load err`))
-}
+const readStream = () => process.stdin.pipe(split(addLine))
 
 // Ensure index creation to load mappings and settings
 createIndex(index)
 // before starting to load
-.then(loadFromStdin)
-.catch(console.error)
+.then(() => {
+  client.helpers.bulk({
+    datasource: readStream(),
+    onDocument: () => {
+      return { index: { _index: index } }
+    }
+  })
+})
--- server/db/elasticsearch/bulk.js
+++ server/db/elasticsearch/bulk.js
@@ -7,15 +7,14 @@ const { logBulkRes } = require('./helpers')
 const assert_ = __.require('utils', 'assert_types')
 const headers = { 'content-type': 'application/x-ndjson' }
 
-const addToBatch = (batch, action, index, doc) => {
+const addToBatch = (action, index, doc) => {
   if (!doc) return _.warn('ignore empty doc')
   const { _id } = doc
   assert_.string(_id)
   // Prevent triggering the error
   // 'Field [_id] is a metadata field and cannot be added inside a document.'
   delete doc._id
-  batch.push(`{"${action}":{"_index":"${index}","_id":"${_id}"}}`)
-  if (action === 'index') batch.push(JSON.stringify(doc))
+  return JSON.stringify(doc)
 }
 
 const postBatch = async batch => {
