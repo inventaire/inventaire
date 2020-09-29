8c94cd17fda657cf074464d785a688d4cf14b81f
jums <jums@inventaire.io>
2020-09-29 02:11:58 +0200

    add type 'user' to document indexation
    and implement naively (with a big if) query body builder for users
    attributes, will need complexification per type

--- server/db/elasticsearch/formatters/user.js
+++ server/db/elasticsearch/formatters/user.js
@@ -4,4 +4,8 @@ const publicAttributes = __.require('models', 'attributes/user').public
 // Omit snapshot as it contains private and semi priavte data
 const publicAttributesStrict = without(publicAttributes, 'snapshot')
 
-module.exports = doc => pick(doc, publicAttributesStrict)
+module.exports = doc => {
+  const publicUserDoc = pick(doc, publicAttributesStrict)
+  publicUserDoc.type = 'user'
+  return publicUserDoc
+}
