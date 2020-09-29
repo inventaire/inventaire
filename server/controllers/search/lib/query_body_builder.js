b02dcd5f1e9ec9f021a6af6dd22eabc58e41c02b
jums <jums@inventaire.io>
2020-09-29 15:12:34 +0200

    integrate groups search to query body builder

--- server/controllers/search/lib/query_body_builder.js
+++ server/controllers/search/lib/query_body_builder.js
@@ -4,28 +4,72 @@ const _ = __.require('builders', 'utils')
 module.exports = params => {
   const { lang: userLang, search, limit: size } = params
   let { types } = params
+  types = singularTypes(types)
 
-  types = singularType(types)
-  // must match at least one of the types
-  const must = [
-    { bool: { should: matchType(types) } },
-    { bool: { should: matchSearchPerTypes(types, search, userLang) } }
-  ]
   return {
     query: {
-      bool: { must }
+      bool: {
+        must: [
+        // must match at least one of the types
+          { bool: { should: matchType(types) } },
+          { bool: { should: matchSearchPerTypes(types, search, userLang) } }
+        ]
+      }
     },
     size,
     min_score: 1
   }
 }
 
-const singularType = types => types.map(type => type.replace(/s$/, ''))
+const singularTypes = types => types.map(type => type.replace(/s$/, ''))
 
 const matchType = types => {
   return types.map(type => (
     { match: { type } }
   ))
+}
+
+const matchSearchPerTypes = (types, search, userLang) => {
+  const isSocialQuery = _.includes(types, 'user') || _.includes(types, 'group')
+
+  if (isSocialQuery) return matchSocial(search)
+  return matchEntities(search, userLang)
+}
+
+const matchSocial = search => {
+  return [
+    {
+      multi_match: {
+        query: search,
+        fields: [
+          'name^2',
+          'bio',
+          'description'
+        ],
+      }
+    },
+  ]
+}
+
+const matchEntities = (search, userLang) => {
+  return [
+    // strict (operator 'and'):
+    // match on all words in search, so descriptions are allowed
+    {
+      multi_match: {
+        query: search,
+        operator: 'and',
+        fields: defaultEntitiesFields(userLang),
+      }
+    },
+    // loose match some words in search
+    {
+      multi_match: {
+        query: search,
+        fields: flattenedTermsFields(userLang)
+      }
+    }
+  ]
 }
 
 const defaultEntitiesFields = userLang => {
@@ -44,40 +88,5 @@ const flattenedTermsFields = userLang => {
     'flattenedAliases', // text type
     'flattenedDescriptions', // text type
     ...defaultEntitiesFields(userLang)
-  ]
-}
-
-const matchSearchPerTypes = (types, search, userLang) => {
-  if (_.includes(types, 'user')) {
-    return [
-      {
-        multi_match: {
-          query: search,
-          fields: [
-            'name',
-            'bio'
-          ],
-        }
-      },
-    ]
-  }
-
-  return [
-    // strict (operator 'and'):
-    // match on all words in search, so descriptions are allowed
-    {
-      multi_match: {
-        query: search,
-        operator: 'and',
-        fields: defaultEntitiesFields(userLang),
-      }
-    },
-    // loose match some words in search
-    {
-      multi_match: {
-        query: search,
-        fields: flattenedTermsFields(userLang)
-      }
-    },
   ]
 }
