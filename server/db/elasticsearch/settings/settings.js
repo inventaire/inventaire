e4aba0948f862e87d0fde272b3ea21361bfbbd84
jums <jums@inventaire.io>
2020-10-05 17:46:09 +0200

    use an autocomplete analyzer at indexation
    but not at search time to not slow down the query
    since indexation broke terms into small pieces (ngrams) this as the
    davantage of doing all the work at indexation, it is more performant
    than a macth_phrase_prefix query and achieve the same objective

--- server/db/elasticsearch/settings/settings.js
+++ server/db/elasticsearch/settings/settings.js
@@ -1,9 +1,14 @@
 // See: https://www.elastic.co/guide/en/elasticsearch/reference/current/search-analyzer.html
 
 module.exports = {
+  // use number_of_shards in testing environment only
+  // See: https://www.elastic.co/guide/en/elasticsearch/guide/current/relevance-is-broken.html
+  // number_of_shards: 1,
   analysis: {
-    tokenizer: {
-      autocomplete: {
+    filter: {
+      // emits N-grams of each word
+      // See: https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-edgengram-tokenizer.html
+      autocomplete_filter: {
         type: 'edge_ngram',
         min_gram: 2,
         max_gram: 10
@@ -11,8 +16,14 @@ module.exports = {
     },
     analyzer: {
       autocomplete: {
-        tokenizer: 'autocomplete',
-        filter: [ 'lowercase' ]
+        type: 'custom',
+        // define standard stop words
+        // See https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-standard-tokenizer.html
+        tokenizer: 'standard',
+        filter: [
+          'lowercase',
+          'autocomplete_filter'
+        ]
       }
     }
   }
