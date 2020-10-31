b85824c3e507656736c47c9cc074dd03cd02ce78
Geoffroy Planquart <geoffroy@planquart.fr>
2020-10-31 14:00:45 +0100

    paginate notifications

--- server/controllers/notifications.js
+++ server/controllers/notifications.js
@@ -1,14 +1,42 @@
 const __ = require('config').universalPath
 const _ = __.require('builders', 'utils')
 const error_ = __.require('lib', 'error/error')
+const sanitize = __.require('lib', 'sanitize/sanitize')
 const responses_ = __.require('lib', 'responses')
 const notifs_ = __.require('lib', 'notifications')
+
+const sanitization = {
+  limit: { optional: true, default: 10 },
+  offset: { optional: true },
+}
+
+const Paginate = page => notifications => {
+  let { limit, offset } = page
+  const total = notifications.length
+  if (offset == null) offset = 0
+  const last = offset + limit
+
+  if (limit != null) {
+    notifications = notifications.slice(offset, last)
+    const data = { notifications, total, offset }
+    if (last < total) data.continue = last
+    return data
+  } else {
+    return { notifications, total, offset }
+  }
+}
+
+const getNotifications = page => {
+  return notifs_.byUserId(page.reqUserId)
+  .then(Paginate(page))
+}
 
 const get = (req, res) => {
   if (req.user == null) return error_.unauthorizedApiAccess(req, res)
 
-  return notifs_.byUserId(req.user._id)
-  .then(responses_.Wrap(res, 'notifications'))
+  return sanitize(req, res, sanitization)
+  .then(getNotifications)
+  .then(responses_.Send(res))
   .catch(error_.Handler(req, res))
 }
 
--- tests/api/notifications/get.test.js
+++ tests/api/notifications/get.test.js
@@ -5,7 +5,8 @@ const { authReq } = __.require('apiTests', 'utils/utils')
 
 describe('notifications:get', () => {
   it('should get user notifications', async () => {
-    const { notifications } = await authReq('get', '/api/notifications')
+    const { notifications, total } = await authReq('get', '/api/notifications')
     notifications.should.be.an.Array()
+    total.should.be.aboveOrEqual(0)
   })
 })
