27d4eb6cc79be65baf24f8572700ade6b6b9020b
jums <jums@inventaire.io>
2020-11-18 18:25:22 +0100

    async tests refactor

--- tests/api/groups/request.test.js
+++ tests/api/groups/request.test.js
@@ -1,41 +1,39 @@
 require('should')
-const { authReq, authReqB, authReqC } = require('../utils/utils')
+const { customAuthReq, authReq, shouldNotBeCalled, rethrowShouldNotBeCalledErrors } = require('../utils/utils')
 const { groupPromise, getGroup, createGroup } = require('../fixtures/groups')
+const { createUser } = require('../fixtures/users')
 const endpoint = '/api/groups?action=request'
 
 describe('groups:update:request', () => {
-  it('should reject without group', done => {
-    authReq('put', endpoint, { user: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' })
-    .catch(err => {
+  it('should reject without group', async () => {
+    try {
+      await authReq('put', endpoint, { user: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' }).then(shouldNotBeCalled)
+    } catch (err) {
+      rethrowShouldNotBeCalledErrors(err)
       err.body.status_verbose.should.equal('missing parameter in body: group')
       err.statusCode.should.equal(400)
-      done()
-    })
-    .catch(done)
+    }
   })
 
-  it('should reject request from already member users', done => {
-    groupPromise
-    .then(group => authReqB('put', endpoint, { group: group._id }))
-    .catch(err => {
+  it('should reject request from already member users', async () => {
+    try {
+      const group = await groupPromise
+      const userPromise = createUser()
+      await customAuthReq(userPromise, 'put', endpoint, { group: group._id })
+      await customAuthReq(userPromise, 'put', endpoint, { group: group._id }).then(shouldNotBeCalled)
+    } catch (err) {
+      rethrowShouldNotBeCalledErrors(err)
       err.body.status_verbose.should.startWith('user is already in group')
       err.statusCode.should.equal(403)
-      done()
-    })
-    .catch(done)
+    }
   })
 
-  it('should add user to requesters list', done => {
-    createGroup()
-    .then(group => {
-      const requestCount = group.invited.length
-      return authReqC('put', endpoint, { group: group._id })
-      .then(() => getGroup(group))
-      .then(group => {
-        group.requested.length.should.equal(requestCount + 1)
-        done()
-      })
-    })
-    .catch(done)
+  it('should add user to requesters list', async () => {
+    const group = await createGroup()
+    const userPromise = createUser()
+    const requestCount = group.invited.length
+    await customAuthReq(userPromise, 'put', endpoint, { group: group._id })
+    const resGroup = await getGroup(group)
+    resGroup.requested.length.should.equal(requestCount + 1)
   })
 })
