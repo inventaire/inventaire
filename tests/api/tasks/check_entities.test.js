142a99ac715b235d5268e9b0a698acf131b85693
jums <jums@inventaire.io>
2020-12-05 15:37:52 +0100

    test:refactor:async wait check_entities

--- tests/api/tasks/check_entities.test.js
+++ tests/api/tasks/check_entities.test.js
@@ -3,63 +3,48 @@ const __ = CONFIG.universalPath
 const _ = __.require('builders', 'utils')
 require('should')
 const { checkEntities, getBySuspectUri } = require('../utils/tasks')
-const { undesiredRes } = __.require('apiTests', 'utils/utils')
 const { findOrIndexEntities } = require('../utils/entities')
 const { createHuman, createWork } = require('../fixtures/entities')
+const { rethrowShouldNotBeCalledErrors } = __.require('apiTests', 'utils/utils')
 
-// Tests dependency: having a populated Elasticsearch wikidata index
 describe('tasks:check-entities', () => {
   before(async () => {
+    // Tests dependency: having a populated ElasticSearch wikidata index
     const wikidataUris = [ 'wd:Q237087' ]
     await findOrIndexEntities(wikidataUris)
   })
-  it('should refuse to check entities of non-allowlisted types', done => {
+  it('should refuse to check entities of non-allowlisted types', async () => {
     // Currently, only humans can be checked for duplicates,
     // or at least are the entrypoint for duplicate checks
-    createWork()
-    .then(work => {
-      return checkEntities(work.uri)
-      .then(undesiredRes(done))
-      .catch(err => {
-        err.statusCode.should.equal(400)
-        err.body.status_verbose.should.equal('unsupported type: work')
-        done()
-      })
-    })
-    .catch(done)
+    const work = await createWork()
+    try {
+      await checkEntities(work.uri)
+    } catch (err) {
+      rethrowShouldNotBeCalledErrors(err)
+      err.statusCode.should.equal(400)
+      err.body.status_verbose.should.equal('unsupported type: work')
+    }
   })
 
-  it('should create tasks for the requested URIs', done => {
-    createHuman({ labels: { en: 'Fred Vargas' } })
-    .then(human => {
-      return checkEntities(human.uri)
-      .then(tasks => {
-        tasks.should.be.an.Array()
-        const task = tasks[0]
-        task.suspectUri.should.equal(human.uri)
-        task.type.should.equal('deduplicate')
-        task.suggestionUri.should.startWith('wd')
-        task.lexicalScore.should.be.a.Number()
-        task.relationScore.should.be.a.Number()
-        task.externalSourcesOccurrences.should.be.an.Array()
-        done()
-      })
-    })
-    .catch(done)
+  it('should create tasks for the requested URIs', async () => {
+    const human = await createHuman({ labels: { en: 'Fred Vargas' } })
+    const tasks = await checkEntities(human.uri)
+    tasks.should.be.an.Array()
+    const task = tasks[0]
+    task.suspectUri.should.equal(human.uri)
+    task.type.should.equal('deduplicate')
+    task.suggestionUri.should.startWith('wd')
+    task.lexicalScore.should.be.a.Number()
+    task.relationScore.should.be.a.Number()
+    task.externalSourcesOccurrences.should.be.an.Array()
   })
 
-  it('should not re-create existing tasks', done => {
-    createHuman({ labels: { en: 'Fred Vargas' } })
-    .then(human => {
-      return checkEntities(human.uri)
-      .then(() => checkEntities(human.uri))
-      .then(() => getBySuspectUri(human.uri))
-    })
-    .then(tasks => {
-      const uniqSuggestiontUris = _.uniq(_.map(tasks, 'suggestionUri'))
-      tasks.length.should.equal(uniqSuggestiontUris.length)
-      done()
-    })
-    .catch(done)
+  it('should not re-create existing tasks', async () => {
+    const human = await createHuman({ labels: { en: 'Fred Vargas' } })
+    await checkEntities(human.uri)
+    await checkEntities(human.uri)
+    const tasks = await getBySuspectUri(human.uri)
+    const uniqSuggestiontUris = _.uniq(_.map(tasks, 'suggestionUri'))
+    tasks.length.should.equal(uniqSuggestiontUris.length)
   })
 })
