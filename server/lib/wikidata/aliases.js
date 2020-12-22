e4d9d11a3ae5afabba256e3b851058b532d3eb38
maxlath <k@maxlath.eu>
2020-12-22 11:09:42 +0100

    wikidata: aliases: remove unused P31=Q5 alias: writer (wd:Q36180)

--- server/lib/wikidata/aliases.js
+++ server/lib/wikidata/aliases.js
@@ -8,7 +8,6 @@ const typesAliases = module.exports = {
     'wd:Q5', // human
     'wd:Q10648343', // duo
     'wd:Q14073567', // sibling duo
-    'wd:Q36180', // writer
     'wd:Q19913602', // house name
     'wd:Q61002' // pseudonym
   ],
