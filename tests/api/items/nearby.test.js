4beb28196f1b290ac245c5f2c03c33622e5618db
maxlath <k@maxlath.eu>
2020-12-22 19:49:38 +0100

    tests: items: nearby: refactor

--- tests/api/items/nearby.test.js
+++ tests/api/items/nearby.test.js
@@ -2,39 +2,35 @@ const CONFIG = require('config')
 const __ = CONFIG.universalPath
 const _ = __.require('builders', 'utils')
 require('should')
-const { Wait } = __.require('lib', 'promises')
 const { getUserGetter, customAuthReq } = __.require('apiTests', 'utils/utils')
 const { createItem } = require('../fixtures/items')
-const geolocatedUser1Promise = getUserGetter('geo1', null, { position: [ 1, 1 ] })().then(Wait(1000))
-const geolocatedUser2Promise = getUserGetter('geo2', null, { position: [ 2, 2 ] })().then(Wait(1000))
+const { waitForIndexation } = require('../utils/search')
+const { getRandomPosition } = require('../fixtures/users')
+const positionUser1 = getRandomPosition()
+const positionUser2 = [
+  positionUser1[0] - 0.1,
+  positionUser1[1] - 0.1,
+]
+const geolocatedUser1Promise = getUserGetter('geo1', null, { position: positionUser1 })()
+const geolocatedUser2Promise = getUserGetter('geo2', null, { position: positionUser2 })()
 const endpoint = '/api/items?action=nearby'
 
 describe('items:nearby', () => {
-  it('should get items nearby', done => {
-    createItem(geolocatedUser1Promise)
-    .then(Wait(500))
-    .then(item => {
-      return customAuthReq(geolocatedUser2Promise, 'get', endpoint)
-      .then(res => {
-        const itemsIds = _.map(res.items, '_id')
-        itemsIds.includes(item._id).should.be.true()
-        done()
-      })
-    })
-    .catch(done)
+  it('should get items nearby', async () => {
+    const user = await geolocatedUser1Promise
+    await waitForIndexation('users', user._id)
+    const item = await createItem(user)
+    const { items } = await customAuthReq(geolocatedUser2Promise, 'get', endpoint)
+    const itemsIds = _.map(items, '_id')
+    itemsIds.includes(item._id).should.be.true()
   })
 
-  it('should accept a range', done => {
-    createItem(geolocatedUser1Promise)
-    .then(Wait(500))
-    .then(item => {
-      return customAuthReq(geolocatedUser2Promise, 'get', `${endpoint}&range=1&strict-range=true`)
-      .then(res => {
-        const itemsIds = _.map(res.items, '_id')
-        itemsIds.includes(item._id).should.be.false()
-        done()
-      })
-    })
-    .catch(done)
+  it('should accept a range', async () => {
+    const user = await geolocatedUser1Promise
+    await waitForIndexation('users', user._id)
+    const item = await createItem(user)
+    const { items } = await customAuthReq(geolocatedUser2Promise, 'get', `${endpoint}&range=1&strict-range=true`)
+    const itemsIds = _.map(items, '_id')
+    itemsIds.includes(item._id).should.be.false()
   })
 })
