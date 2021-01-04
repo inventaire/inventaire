c284b6271e9e64a712b6188e011a50dc266208f8
maxlath <k@maxlath.eu>
2021-01-04 12:10:38 +0100

    swift_client: remove unused functions and refactor getParams

--- server/controllers/images/lib/swift_client.js
+++ server/controllers/images/lib/swift_client.js
@@ -10,36 +10,19 @@ const { publicURL } = CONFIG.mediaStorage.swift
 const absoluteUrl = (container, filename) => `${publicURL}/${container}/${filename}`
 const relativeUrl = (container, filename) => `/img/${container}/${filename}`
 
-const getParams = async (container, filename, body, type = 'application/octet-stream') => {
-  const url = absoluteUrl(container, filename)
+const getParams = async (container, filename) => {
   const token = await getToken()
-  const headers = {
-    accept: 'application/json',
-    'content-type': type,
-    'x-auth-token': token
-  }
-  return { url, headers }
-}
-
-const action = verb => async (container, filename, body, type) => {
-  const { url, headers } = await getParams(container, filename, body, type)
-  try {
-    let resBody = await requests_[verb](url, { headers })
-    resBody = resBody || { ok: true }
-    _.log(resBody, `${verb} ${filename} body`)
-    return resBody
-  } catch (err) {
-    _.error(err, `swift ${verb} ${filename}`)
-    throw err
+  return {
+    url: absoluteUrl(container, filename),
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/octet-stream',
+      'x-auth-token': token
+    }
   }
 }
 
 module.exports = {
-  get: action('get'),
-  post: action('post'),
-  put: action('put'),
-  delete: action('delete'),
-
   // inspired by https://github.com/Automattic/knox/blob/master/lib/client.js
   putImage: async (container, path, filename) => {
     const [ params, contentLength ] = await Promise.all([
