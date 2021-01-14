64abca1e6b1011ddb3a0fa195a894af188c69a7d
maxlath <k@maxlath.eu>
2021-01-14 22:49:45 +0100

    init_express: propagate route and middleware initialization errors to the consumer

--- server/init_express.js
+++ server/init_express.js
@@ -7,37 +7,39 @@ const { port, host, name } = CONFIG
 const middlewares = require('./middlewares/middlewares')
 const routes = require('./controllers/routes')
 
-const app = express()
+module.exports = () => {
+  const app = express()
 
-for (const middleware of middlewares) {
-  if (_.isArray(middleware)) {
-    app.use.apply(app, middleware)
-  } else {
-    app.use(middleware)
-  }
-}
-
-for (const endpoint in routes) {
-  const controllers = routes[endpoint]
-  for (const verb in controllers) {
-    const controller = controllers[verb]
-    app[verb](`/${endpoint}`, controller)
-  }
-}
-
-// Should be used after all middlewares and routes
-// cf http://expressjs.com/fr/guide/error-handling.html
-app.use(require('./middlewares/error_handler'))
-
-app.disable('x-powered-by')
-
-module.exports = () => new Promise((resolve, reject) => {
-  app.listen(port, host, err => {
-    if (err) {
-      reject(err)
+  for (const middleware of middlewares) {
+    if (_.isArray(middleware)) {
+      app.use.apply(app, middleware)
     } else {
-      _.info(`${name} server is listening on port ${port}...`)
-      resolve(app)
+      app.use(middleware)
     }
+  }
+
+  for (const endpoint in routes) {
+    const controllers = routes[endpoint]
+    for (const verb in controllers) {
+      const controller = controllers[verb]
+      app[verb](`/${endpoint}`, controller)
+    }
+  }
+
+  // Should be used after all middlewares and routes
+  // cf http://expressjs.com/fr/guide/error-handling.html
+  app.use(require('./middlewares/error_handler'))
+
+  app.disable('x-powered-by')
+
+  return new Promise((resolve, reject) => {
+    app.listen(port, host, err => {
+      if (err) {
+        reject(err)
+      } else {
+        _.info(`${name} server is listening on port ${port}...`)
+        resolve(app)
+      }
+    })
   })
-})
+}
