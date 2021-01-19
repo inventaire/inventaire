c8322a237284197042197011b61c793918c9bd35
maxlath <k@maxlath.eu>
2021-01-19 20:13:34 +0100

    user: reject username updates that would use an existing stableUsername

--- tests/api/utils/oauth.js
+++ tests/api/utils/oauth.js
@@ -1,11 +1,11 @@
 const __ = require('config').universalPath
 const _ = __.require('builders', 'utils')
 const clientsDb = __.require('couch', 'base')('oauth_clients')
-const { rawAuthReq } = require('../utils/utils')
 const randomString = __.require('lib', 'utils/random_string')
 const { parse: parseQuery } = require('querystring')
 const { sha1 } = __.require('lib', 'crypto')
-const { postUrlencoded } = require('./request')
+const { postUrlencoded, rawCustomAuthReq } = require('./request')
+const { getUser } = require('./utils')
 const assert_ = __.require('utils', 'assert_types')
 
 const getClient = async (params = {}) => {
@@ -32,7 +32,10 @@ const getClient = async (params = {}) => {
 }
 
 const getClientWithAuthorization = async (params = {}) => {
-  const { scope = [ 'username' ] } = params
+  const {
+    scope = [ 'username' ],
+    user = getUser()
+  } = params
   const client = await getClient(params)
   const url = _.buildPath('/api/oauth/authorize', {
     client_id: client._id,
@@ -40,13 +43,13 @@ const getClientWithAuthorization = async (params = {}) => {
     response_type: 'code',
     scope: scope.join('+'),
   })
-  const { headers } = await rawAuthReq('get', url)
+  const { headers } = await rawCustomAuthReq(user, 'get', url)
   const authorizationData = parseQuery(headers.location.split('?')[1])
   return Object.assign(authorizationData, client)
 }
 
-const getToken = async ({ scope }) => {
-  const { _id: clientId, secret, code, redirectUris } = await getClientWithAuthorization({ scope })
+const getToken = async ({ user, scope }) => {
+  const { _id: clientId, secret, code, redirectUris } = await getClientWithAuthorization({ user, scope })
   const { body } = await postUrlencoded('/api/oauth/token', {
     client_id: clientId,
     client_secret: secret,
