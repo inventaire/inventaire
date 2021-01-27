609d377b2ea17ba35ec8b64c9ddf756517099317
maxlath <k@maxlath.eu>
2021-01-27 18:04:20 +0100

    distinguish between inv accepted P31 values and wikidata tolerated P31 values
    the idea being that the list of inv accepted P31 values is likely to progressively grow,
    but that some values that are tolerated from Wikidata entity for the sake of maximizing compatibility
    won't be suggested or accepted in Inventaire.
    
    Example: wd:Q571 or wd:Q20540385 are used to guess that a Wikidata entity is of type 'work'
    but we should not spread their use beyond that typing phase

--- server/data/property_values.js
+++ server/data/property_values.js
@@ -1,11 +1,14 @@
 const __ = require('config').universalPath
-const _ = __.require('builders', 'utils')
 const responses_ = __.require('lib', 'responses')
 const error_ = __.require('lib', 'error/error')
-const { typesAliases, typesNames, depreciatedAliases } = __.require('lib', 'wikidata/aliases')
+const { typesNames } = __.require('lib', 'wikidata/aliases')
 const sanitize = __.require('lib', 'sanitize/sanitize')
+const propertiesValuesLists = __.require('controllers', 'entities/lib/properties/properties_values_lists')
 
 const sanitization = {
+  property: {
+    allowlist: Object.keys(propertiesValuesLists)
+  },
   type: {
     allowlist: typesNames
   }
@@ -13,11 +16,13 @@ const sanitization = {
 
 module.exports = (req, res) => {
   sanitize(req, res, sanitization)
-  .then(params => {
-    const { type } = params
-    const allAliases = typesAliases[type]
-    return _.difference(allAliases, depreciatedAliases)
-  })
+  .then(getPropertyValues)
   .then(responses_.Wrap(res, 'values'))
   .catch(error_.Handler(req, res))
+}
+
+const getPropertyValues = ({ property, type }) => {
+  const list = propertiesValuesLists[property]
+  if (list[type]) return list[type]
+  else throw error_.new('unsupported type for this property', 400, { property, type })
 }
