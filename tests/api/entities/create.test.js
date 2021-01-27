3c450aa1f063301ce946011014958c8200403a97
maxlath <k@maxlath.eu>
2021-01-27 18:26:07 +0100

    tests: entities: some async/await refactor

--- tests/api/entities/create.test.js
+++ tests/api/entities/create.test.js
@@ -1,252 +1,218 @@
 require('should')
-const { authReq, undesiredRes, shouldNotBeCalled, rethrowShouldNotBeCalledErrors } = require('../utils/utils')
+const { authReq, shouldNotBeCalled } = require('../utils/utils')
 const { createEditionWithIsbn, humanName, randomLabel, someOpenLibraryId } = require('../fixtures/entities')
 const endpoint = '/api/entities?action=create'
 
 describe('entities:create', () => {
-  it('should reject without from claims', done => {
-    authReq('post', endpoint, {})
-    .then(undesiredRes(done))
+  it('should reject without from claims', async () => {
+    await authReq('post', endpoint, {})
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('missing parameter in body: claims')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject invalid claims', done => {
-    authReq('post', endpoint, { claims: 'foo' })
-    .then(undesiredRes(done))
+  it('should reject invalid claims', async () => {
+    await authReq('post', endpoint, { claims: 'foo' })
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.startWith('invalid claims')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
   it('should reject entities of unknown entity types', async () => {
-    try {
-      await authReq('post', '/api/entities?action=create', {
-        labels: {},
-        claims: { 'wdt:P31': [ 'wd:Q1' ] }
-      })
-      .then(shouldNotBeCalled)
-    } catch (err) {
+    await authReq('post', '/api/entities?action=create', {
+      labels: {},
+      claims: { 'wdt:P31': [ 'wd:Q1' ] }
+    })
+    .then(shouldNotBeCalled)
+    .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal("wdt:P31 value isn't a known value")
-    }
+    })
   })
 
   it('should reject entities of non-allowlisted entity types', async () => {
-    try {
-      await authReq('post', '/api/entities?action=create', {
-        labels: {
-          en: randomLabel()
-        },
-        // Is in server/lib/wikidata/aliases.js, but gives a type 'movement'
-        claims: { 'wdt:P31': [ 'wd:Q2198855' ] }
-      })
-      .then(shouldNotBeCalled)
-    } catch (err) {
-      rethrowShouldNotBeCalledErrors(err)
+    await authReq('post', '/api/entities?action=create', {
+      labels: {
+        en: randomLabel()
+      },
+      // Is in server/lib/wikidata/aliases.js, but gives a type 'movement'
+      claims: { 'wdt:P31': [ 'wd:Q2198855' ] }
+    })
+    .then(shouldNotBeCalled)
+    .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal("wdt:P31 value isn't a allowlisted value")
-    }
+    })
   })
 
-  it('should reject without a label (unless specific types)', done => {
-    authReq('post', endpoint, {
+  it('should reject without a label (unless specific types)', async () => {
+    await authReq('post', endpoint, {
       claims: { 'wdt:P31': [ 'wd:Q47461344' ] }
     })
-    .then(undesiredRes(done))
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('invalid labels')
-      done()
     })
-    .catch(done)
   })
 
-  it('should create a work entity', done => {
-    authReq('post', endpoint, {
+  it('should create a work entity', async () => {
+    const res = await authReq('post', endpoint, {
       labels: { fr: humanName() },
       claims: { 'wdt:P31': [ 'wd:Q47461344' ] }
     })
-    .then(res => {
-      res._id.should.be.a.String()
-      res._rev.should.be.a.String()
-      res.type.should.equal('work')
-      res.version.should.equal(2)
-      res.claims.should.deepEqual({ 'wdt:P31': [ 'wd:Q47461344' ] })
-      res.uri.should.be.a.String()
-      res.labels.should.be.an.Object()
-      done()
-    })
-    .catch(done)
+    res._id.should.be.a.String()
+    res._rev.should.be.a.String()
+    res.type.should.equal('work')
+    res.version.should.equal(2)
+    res.claims.should.deepEqual({ 'wdt:P31': [ 'wd:Q47461344' ] })
+    res.uri.should.be.a.String()
+    res.labels.should.be.an.Object()
   })
 
-  it('should create claim with a type specific validation', done => {
-    authReq('post', endpoint, {
+  it('should create claim with a type specific validation', async () => {
+    const { _id } = await authReq('post', endpoint, {
       labels: { fr: humanName() },
       claims: {
         'wdt:P31': [ 'wd:Q47461344' ],
         'wdt:P648': [ someOpenLibraryId('work') ]
       }
     })
-    .then(res => {
-      res._id.should.be.a.String()
-      done()
-    })
-    .catch(done)
+    _id.should.be.a.String()
   })
 
-  it('should reject multiple values for a property that take one', done => {
-    authReq('post', endpoint, {
+  it('should reject multiple values for a property that take one', async () => {
+    await authReq('post', endpoint, {
       labels: { fr: humanName() },
-      claims: { 'wdt:P31': [ 'wd:Q47461344', 'wd:Q572' ] }
+      claims: { 'wdt:P31': [ 'wd:Q47461344', 'wd:Q8274' ] }
     })
-    .then(undesiredRes(done))
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal('this property accepts only one value')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject invalid labels datatype', done => {
-    authReq('post', endpoint, {
+  it('should reject invalid labels datatype', async () => {
+    await authReq('post', endpoint, {
       labels: [],
       claims: {}
     })
-    .then(undesiredRes(done))
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.startWith('invalid labels')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject invalid claims datatype', done => {
-    authReq('post', endpoint, {
+  it('should reject invalid claims datatype', async () => {
+    await authReq('post', endpoint, {
       labels: {},
       claims: []
     })
-    .then(undesiredRes(done))
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.startWith('invalid claims')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject invalid claim property values', done => {
-    authReq('post', endpoint, {
+  it('should reject invalid claim property values', async () => {
+    await authReq('post', endpoint, {
       labels: { fr: humanName() },
       claims: {
         'wdt:P31': [ 'wd:Q47461344' ],
         'wdt:P50': 'wd:Q535'
       }
     })
-    .then(undesiredRes(done))
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('invalid property value array')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject invalid prefix properties', done => {
-    authReq('post', endpoint, {
+  it('should reject invalid prefix properties', async () => {
+    await authReq('post', endpoint, {
       labels: { fr: humanName() },
       claims: {
         'wdt:P31': [ 'wd:Q47461344' ],
         'wd:P50': [ 'wd:Q535' ]
       }
     })
-    .then(undesiredRes(done))
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('invalid property')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject invalid claim property value', done => {
-    authReq('post', endpoint, {
+  it('should reject invalid claim property value', async () => {
+    await authReq('post', endpoint, {
       labels: { fr: humanName() },
       claims: {
         'wdt:P31': [ 'wd:Q47461344' ],
         'wdt:P50': [ 'wd####Q535' ]
       }
     })
-    .then(undesiredRes(done))
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('invalid property value')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
   it('should reject when concurrent property value is already taken', async () => {
     const edition = await createEditionWithIsbn()
-    try {
-      await authReq('post', endpoint, {
-        claims: {
-          'wdt:P31': [ 'wd:Q3331189' ],
-          'wdt:P1476': [ randomLabel() ],
-          'wdt:P629': edition.claims['wdt:P629'],
-          // The concurrent property
-          'wdt:P212': edition.claims['wdt:P212']
-        }
-      })
-      .then(shouldNotBeCalled)
-    } catch (err) {
-      err.body.status_verbose.should.equal('this property value is already used')
+    await authReq('post', endpoint, {
+      claims: {
+        'wdt:P31': [ 'wd:Q3331189' ],
+        'wdt:P1476': [ randomLabel() ],
+        'wdt:P629': edition.claims['wdt:P629'],
+        // The concurrent property
+        'wdt:P212': edition.claims['wdt:P212']
+      }
+    })
+    .then(shouldNotBeCalled)
+    .catch(err => {
       err.statusCode.should.equal(400)
-    }
+      err.body.status_verbose.should.equal('this property value is already used')
+    })
   })
 
-  it('should reject creation with incorrect properties', done => {
-    authReq('post', endpoint, {
+  it('should reject creation with incorrect properties', async () => {
+    await authReq('post', endpoint, {
       labels: { fr: randomLabel() },
       claims: {
         'wdt:P31': [ 'wd:Q47461344' ], // work
         'wdt:P1104': [ 124 ] // edition pages counts
       }
     })
-    .then(undesiredRes(done))
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal("works can't have a property wdt:P1104")
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject invalid prefixes', done => {
-    authReq('post', endpoint, {
+  it('should reject invalid prefixes', async () => {
+    await authReq('post', endpoint, {
       prefix: 'foo',
       labels: {},
       claims: {}
     })
-    .then(undesiredRes(done))
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.startWith('invalid prefix: foo')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should create wikidata entities', done => {
-    authReq('post', endpoint, {
+  it('should create wikidata entities', async () => {
+    await authReq('post', endpoint, {
       prefix: 'wd',
       labels: { fr: humanName() },
       claims: {
@@ -254,15 +220,13 @@ describe('entities:create', () => {
         'wdt:P648': [ someOpenLibraryId('work') ]
       }
     })
-    .then(undesiredRes(done))
+    .then(shouldNotBeCalled)
     .catch(err => {
       // test oauth request sending by throwing error
       // as test env cannot have any wd tokens
       err.body.status_verbose.should.equal('missing wikidata oauth tokens')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 })
 
--- tests/api/entities/update_claims.test.js
+++ tests/api/entities/update_claims.test.js
@@ -1,242 +1,186 @@
-const CONFIG = require('config')
-const __ = CONFIG.universalPath
 const should = require('should')
-const { tap } = __.require('lib', 'promises')
-const { undesiredRes } = require('../utils/utils')
+const { shouldNotBeCalled } = require('../utils/utils')
 const { createWork, createEdition, createHuman, someOpenLibraryId, someFakeUri } = require('../fixtures/entities')
 const { getByUri, addClaim, updateClaim, removeClaim, merge } = require('../utils/entities')
 
 describe('entities:update-claims', () => {
-  it('should reject without uri', done => {
-    updateClaim()
-    .then(undesiredRes(done))
+  it('should reject without uri', async () => {
+    await updateClaim()
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('missing parameter in body: uri')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject without property', done => {
-    updateClaim(someFakeUri)
-    .then(undesiredRes(done))
+  it('should reject without property', async () => {
+    await updateClaim(someFakeUri)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('missing parameter in body: property')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject without old-value or new-value', done => {
+  it('should reject without old-value or new-value', async () => {
     const property = 'wdt:P1104'
-    updateClaim(someFakeUri, property)
-    .then(undesiredRes(done))
+    await updateClaim(someFakeUri, property)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('missing parameter in body: old-value or new-value')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject invalid uri prefix', done => {
+  it('should reject invalid uri prefix', async () => {
     const uri = 'invalidprefix:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
     const property = 'wdt:P1104'
     const oldValue = '1312'
-    updateClaim(uri, property, oldValue)
-    .then(undesiredRes(done))
+    await updateClaim(uri, property, oldValue)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.startWith('unsupported uri prefix')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject unfound entity', done => {
+  it('should reject unfound entity', async () => {
     const property = 'wdt:P1104'
     const oldValue = '1312'
-    updateClaim(someFakeUri, property, oldValue)
-    .then(undesiredRes(done))
+    await updateClaim(someFakeUri, property, oldValue)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('entity not found')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject an update with an inappropriate property', done => {
-    createWork()
+  it('should reject an update with an inappropriate property', async () => {
+    const work = await createWork()
     // A work entity should not have pages count
-    .then(work => addClaim(work.uri, 'wdt:P1104', 124))
-    .then(undesiredRes(done))
+    await addClaim(work.uri, 'wdt:P1104', 124)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal("works can't have a property wdt:P1104")
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject an update with an inappropriate property datatype', done => {
-    createWork()
-    .then(work => {
-      return addClaim(work.uri, 'wdt:P50', 124)
-      .then(undesiredRes(done))
-      .catch(err => {
-        err.body.status_verbose.should.equal('invalid value type: expected string, got number')
-        err.statusCode.should.equal(400)
-        done()
-      })
+  it('should reject an update with an inappropriate property datatype', async () => {
+    const work = await createWork()
+    await addClaim(work.uri, 'wdt:P50', 124)
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.body.status_verbose.should.equal('invalid value type: expected string, got number')
+      err.statusCode.should.equal(400)
     })
-    .catch(done)
   })
 
-  it('should reject adding an extra value to a property that accepts only one value', done => {
-    createEdition()
+  it('should reject adding an extra value to a property that accepts only one value', async () => {
+    const edition = await createEdition()
     // An edition entity should have only one date of publication
-    .then(edition => addClaim(edition.uri, 'wdt:P577', '2010'))
-    .then(undesiredRes(done))
+    await addClaim(edition.uri, 'wdt:P577', '2010')
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('this property accepts only one value')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject an update with an invalid property value', done => {
-    createEdition()
-    .then(edition => {
-      return addClaim(edition.uri, 'wdt:P123', 'not an entity uri')
-      .then(undesiredRes(done))
-      .catch(err => {
-        err.body.status_verbose.should.equal('invalid property value')
-        err.statusCode.should.equal(400)
-        done()
-      })
+  it('should reject an update with an invalid property value', async () => {
+    const edition = await createEdition()
+    await addClaim(edition.uri, 'wdt:P123', 'not an entity uri')
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.body.status_verbose.should.equal('invalid property value')
+      err.statusCode.should.equal(400)
     })
-    .catch(done)
   })
 
-  it('should reject an update removing a critical claim', done => {
-    createEdition()
-    .then(edition => {
-      const oldValue = edition.claims['wdt:P629'][0]
-      // An edition entity should always have at least one wdt:P629 claim
-      return removeClaim(edition.uri, 'wdt:P629', oldValue)
-    })
-    .then(undesiredRes(done))
+  it('should reject an update removing a critical claim', async () => {
+    const edition = await createEdition()
+    const oldValue = edition.claims['wdt:P629'][0]
+    // An edition entity should always have at least one wdt:P629 claim
+    await removeClaim(edition.uri, 'wdt:P629', oldValue)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('an edition should have an associated work (wdt:P629)')
       err.statusCode.should.equal(400)
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject an update on an unexisting claim (property with no claim)', done => {
-    createEdition()
-    .then(edition => updateClaim(edition.uri, 'wdt:P655', 'wd:Q23', 'wd:Q42'))
-    .then(undesiredRes(done))
+  it('should reject an update on an unexisting claim (property with no claim)', async () => {
+    const edition = await createEdition()
+    await updateClaim(edition.uri, 'wdt:P655', 'wd:Q23', 'wd:Q42')
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal('claim property value not found')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject an update on an unexisting claim (property with claims)', done => {
-    createEdition()
-    .then(tap(edition => addClaim(edition.uri, 'wdt:P655', 'wd:Q535')))
-    .then(edition => updateClaim(edition.uri, 'wdt:P655', 'wd:Q23', 'wd:Q42'))
-    .then(undesiredRes(done))
+  it('should reject an update on an unexisting claim (property with claims)', async () => {
+    const edition = await createEdition()
+    await addClaim(edition.uri, 'wdt:P655', 'wd:Q535')
+    await updateClaim(edition.uri, 'wdt:P655', 'wd:Q23', 'wd:Q42')
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal('claim property value not found')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject an update on an obsolete entity', done => {
-    Promise.all([
+  it('should reject an update on an obsolete entity', async () => {
+    const [ workA, workB ] = await Promise.all([
       createWork(),
       createWork()
     ])
-    .then(([ workA, workB ]) => merge(workA.uri, workB.uri)
-    .then(() => addClaim(workA.uri, 'wdt:P50', 'wd:Q535'))).then(undesiredRes(done))
+    await merge(workA.uri, workB.uri)
+    await addClaim(workA.uri, 'wdt:P50', 'wd:Q535')
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal('this entity is obsolete')
-      done()
     })
-    .catch(done)
   })
 
-  it('should accept rapid updates on the same entity', done => {
+  it('should accept rapid updates on the same entity', async () => {
     const authorsUris = [ 'wd:Q192214', 'wd:Q206685' ]
-    createWork()
-    .then(work => {
-      return Promise.all(authorsUris.map(uri => addClaim(work.uri, 'wdt:P50', uri)))
-      .then(responses => {
-        responses.forEach(res => should(res.ok).be.true())
-        return getByUri(work.uri)
-      })
-    })
-    .then(updatedWork => {
-      const addedAuthorsUris = updatedWork.claims['wdt:P50']
-      authorsUris.forEach(uri => should(addedAuthorsUris.includes(uri)).be.true())
-      done()
-    })
-    .catch(done)
+    const work = await createWork()
+    const responses = await Promise.all(authorsUris.map(uri => addClaim(work.uri, 'wdt:P50', uri)))
+    responses.forEach(res => should(res.ok).be.true())
+    const updatedWork = await getByUri(work.uri)
+    const addedAuthorsUris = updatedWork.claims['wdt:P50']
+    authorsUris.forEach(uri => should(addedAuthorsUris.includes(uri)).be.true())
   })
 
-  it('should accept a non-duplicated concurrent value', done => {
-    createHuman()
-    .then(human => addClaim(human._id, 'wdt:P648', someOpenLibraryId()))
-    .then(res => {
-      should(res.ok).be.true()
-      done()
-    })
-    .catch(done)
+  it('should accept a non-duplicated concurrent value', async () => {
+    const human = await createHuman()
+    const res = await addClaim(human._id, 'wdt:P648', someOpenLibraryId())
+    should(res.ok).be.true()
   })
 
-  it('should reject invalid value for type-specific value formats', done => {
-    createHuman()
-    .then(human => {
-      return updateClaim(human.uri, 'wdt:P648', null, someOpenLibraryId('work'))
-    })
-    .then(undesiredRes(done))
+  it('should reject invalid value for type-specific value formats', async () => {
+    const human = await createHuman()
+    await updateClaim(human.uri, 'wdt:P648', null, someOpenLibraryId('work'))
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal('invalid property value for entity type human')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject an update with a duplicated concurrent value', done => {
+  it('should reject an update with a duplicated concurrent value', async () => {
     const id = someOpenLibraryId()
-    createHuman()
-    .then(human => {
-      return addClaim(human.uri, 'wdt:P648', id)
-      .then(res => {
-        should(res.ok).be.true()
-        return createHuman()
-      })
-    })
-    .then(human2 => addClaim(human2.uri, 'wdt:P648', id))
-    .then(undesiredRes(done))
+    const human = await createHuman()
+    const res = await addClaim(human.uri, 'wdt:P648', id)
+    should(res.ok).be.true()
+    const human2 = await createHuman()
+    await addClaim(human2.uri, 'wdt:P648', id)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal('this property value is already used')
-      done()
     })
-    .catch(done)
   })
 })
