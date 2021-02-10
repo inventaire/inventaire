0a0f32314f386cf621f8f710eb3a048c61fcf909
maxlath <k@maxlath.eu>
2021-02-10 22:45:00 +0100

    feeds: add sanitization

--- server/controllers/feeds/feeds.js
+++ server/controllers/feeds/feeds.js
@@ -1,59 +1,75 @@
 const __ = require('config').universalPath
-const _ = __.require('builders', 'utils')
 const error_ = __.require('lib', 'error/error')
 const headers_ = __.require('lib', 'headers')
+const sanitize = __.require('lib', 'sanitize/sanitize')
 const getAuthentifiedUser = require('./lib/get_authentified_user')
 const userFeedData = require('./lib/user_feed_data')
 const groupFeedData = require('./lib/group_feed_data')
 const generateFeedFromFeedData = require('./lib/generate_feed_from_feed_data')
 
+const sanitization = {
+  user: {
+    optional: true
+  },
+  group: {
+    optional: true
+  },
+  requester: {
+    optional: true
+  },
+  token: {
+    optional: true
+  },
+  lang: {
+    optional: true,
+    // Set the defaults manually after having checked req.headers
+    default: null
+  }
+}
+
 module.exports = {
   get: (req, res) => {
-    const { query } = req
-    const { user: userId, group: groupId, requester, token } = query
-
-    if (requester) {
-      if (token == null) return error_.bundleMissingQuery(req, res, 'token')
-    } else {
-      if (token) return error_.bundleMissingQuery(req, res, 'requester')
-    }
-
-    // The reason to have this authentifying token system on a public endpoint
-    // is that relying on the general 'restrictApiAccess' middleware
-    // would have implyied creating a general token authentification strategy,
-    // but with a lower authorization level (only read operations), and for
-    // a limitied amount of allowlisted routes.
-    // It is way easier to simply have this ad-hoc token authentification strategy
-    // that we know opens only the limited rights we wish it to open.
-    const authentifiedUserPromise = getAuthentifiedUser(requester, token)
-
-    let feedDataPromise
-    if (userId != null) {
-      if (!_.isUserId(userId)) {
-        return error_.bundleInvalid(req, res, 'user', userId)
-      }
-
-      feedDataPromise = userFeedData(userId, authentifiedUserPromise)
-    } else if (groupId != null) {
-      if (!_.isGroupId(groupId)) {
-        return error_.bundleInvalid(req, res, 'group', groupId)
-      }
-
-      feedDataPromise = groupFeedData(groupId, authentifiedUserPromise)
-    } else {
-      return error_.bundleMissingQuery(req, res, 'user|group', 400)
-    }
-
-    // Guess the lang from the query string or from the request headers
-    // that might be passed by the feeds aggregator
-    const lang = req.query.lang || headers_.getLang(req.headers)
-
-    return feedDataPromise
-    .then(generateFeedFromFeedData(lang))
+    const headersLang = headers_.getLang(req.headers)
+    sanitize(req, res, sanitization)
+    .then(getFeed(headersLang))
     .then(xml => {
       res.set('content-type', 'application/rss+xml')
       res.send(xml)
     })
     .catch(error_.Handler(req, res))
   }
+}
+
+const getFeed = headersLang => async params => {
+  const { userId, groupId, requesterId, token } = params
+  // Guess the lang from the query string or from the request headers
+  // that might be passed by the feeds aggregator
+  const lang = params.lang || headersLang || 'en'
+
+  if (requesterId) {
+    if (token == null) throw error_.newMissingQuery('token')
+  } else {
+    if (token != null) throw error_.newMissingQuery('requester')
+  }
+
+  // The reason to have this authentifying token system on a public endpoint
+  // is that relying on the general 'restrictApiAccess' middleware
+  // would have implyied creating a general token authentification strategy,
+  // but with a lower authorization level (only read operations), and for
+  // a limitied amount of allowlisted routes.
+  // It is way easier to simply have this ad-hoc token authentification strategy
+  // that we know opens only the limited rights we wish it to open.
+  const authentifiedUserPromise = getAuthentifiedUser(requesterId, token)
+
+  let feedDataPromise
+  if (userId != null) {
+    feedDataPromise = userFeedData(userId, authentifiedUserPromise)
+  } else if (groupId != null) {
+    feedDataPromise = groupFeedData(groupId, authentifiedUserPromise)
+  } else {
+    throw error_.newMissingQuery('user|group', 400)
+  }
+
+  return feedDataPromise
+  .then(generateFeedFromFeedData(lang))
 }
