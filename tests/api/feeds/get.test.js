53aa742b032ee1811ed5da4a4303d564b6d58e95
maxlath <k@maxlath.eu>
2021-02-11 00:37:12 +0100

    tests: feeds: split tests in subsections

--- tests/api/feeds/get.test.js
+++ tests/api/feeds/get.test.js
@@ -6,69 +6,75 @@ const { groupPromise } = require('../fixtures/groups')
 const { createShelf } = require('../fixtures/shelves')
 
 describe('feeds:get', () => {
-  it('should return a user RSS feed', async () => {
-    const user = await getUser()
-    const userId = user._id
-    const { body } = await rawRequest('get', `/api/feeds?user=${userId}`)
-    body.startsWith('<?xml').should.be.true()
+  describe('user', () => {
+    it('should return a user RSS feed', async () => {
+      const user = await getUser()
+      const userId = user._id
+      const { body } = await rawRequest('get', `/api/feeds?user=${userId}`)
+      body.startsWith('<?xml').should.be.true()
+    })
+
+    it('should return a user RSS feed when the user has an item', async () => {
+      const userPromise = getUser()
+      const itemPromise = createItem(userPromise)
+
+      const [ user, item ] = await Promise.all([
+        userPromise,
+        itemPromise
+      ])
+      const userId = user._id
+      const { body } = await rawRequest('get', `/api/feeds?user=${userId}`)
+      body.includes(item._id).should.be.true()
+    })
+
+    it('should not return private items when not authorized', async () => {
+      const userPromise = getUser()
+      const itemAPromise = createItem(userPromise, { listing: 'private' })
+      const itemBPromise = createItem(userPromise, { listing: 'network' })
+
+      const [ user, itemA, itemB ] = await Promise.all([
+        userPromise,
+        itemAPromise,
+        itemBPromise
+      ])
+      const userId = user._id
+      const { body } = await rawRequest('get', `/api/feeds?user=${userId}`)
+      body.startsWith('<?xml').should.be.true()
+      body.includes(itemA._id).should.be.false()
+      body.includes(itemB._id).should.be.false()
+    })
+
+    it('should return private items when authorized', async () => {
+      const userPromise = getUser()
+      const itemAPromise = createItem(userPromise, { listing: 'private' })
+      const itemBPromise = createItem(userPromise, { listing: 'network' })
+
+      const [ user, itemA, itemB ] = await Promise.all([
+        userPromise,
+        itemAPromise,
+        itemBPromise
+      ])
+      const { _id: userId, readToken: token } = user
+      const { body } = await rawRequest('get', `/api/feeds?user=${userId}&requester=${userId}&token=${token}`)
+      body.startsWith('<?xml').should.be.true()
+      body.includes(itemA._id).should.be.true()
+      body.includes(itemB._id).should.be.true()
+    })
   })
 
-  it('should return a user RSS feed when the user has an item', async () => {
-    const userPromise = getUser()
-    const itemPromise = createItem(userPromise)
-
-    const [ user, item ] = await Promise.all([
-      userPromise,
-      itemPromise
-    ])
-    const userId = user._id
-    const { body } = await rawRequest('get', `/api/feeds?user=${userId}`)
-    body.includes(item._id).should.be.true()
+  describe('group', () => {
+    it('should return a group RSS feed', async () => {
+      const group = await groupPromise
+      const { body } = await rawRequest('get', `/api/feeds?group=${group._id}`)
+      body.startsWith('<?xml').should.be.true()
+    })
   })
 
-  it('should not return private items when not authorized', async () => {
-    const userPromise = getUser()
-    const itemAPromise = createItem(userPromise, { listing: 'private' })
-    const itemBPromise = createItem(userPromise, { listing: 'network' })
-
-    const [ user, itemA, itemB ] = await Promise.all([
-      userPromise,
-      itemAPromise,
-      itemBPromise
-    ])
-    const userId = user._id
-    const { body } = await rawRequest('get', `/api/feeds?user=${userId}`)
-    body.startsWith('<?xml').should.be.true()
-    body.includes(itemA._id).should.be.false()
-    body.includes(itemB._id).should.be.false()
-  })
-
-  it('should return private items when authorized', async () => {
-    const userPromise = getUser()
-    const itemAPromise = createItem(userPromise, { listing: 'private' })
-    const itemBPromise = createItem(userPromise, { listing: 'network' })
-
-    const [ user, itemA, itemB ] = await Promise.all([
-      userPromise,
-      itemAPromise,
-      itemBPromise
-    ])
-    const { _id: userId, readToken: token } = user
-    const { body } = await rawRequest('get', `/api/feeds?user=${userId}&requester=${userId}&token=${token}`)
-    body.startsWith('<?xml').should.be.true()
-    body.includes(itemA._id).should.be.true()
-    body.includes(itemB._id).should.be.true()
-  })
-
-  it('should return a group RSS feed', async () => {
-    const group = await groupPromise
-    const { body } = await rawRequest('get', `/api/feeds?group=${group._id}`)
-    body.startsWith('<?xml').should.be.true()
-  })
-
-  it('should return a shelf RSS feed', async () => {
-    const shelf = await createShelf(getUser(), { listing: 'public' })
-    const { body } = await rawRequest('get', `/api/feeds?shelf=${shelf._id}`)
-    body.startsWith('<?xml').should.be.true()
+  describe('shelf', () => {
+    it('should return a shelf RSS feed', async () => {
+      const shelf = await createShelf(getUser(), { listing: 'public' })
+      const { body } = await rawRequest('get', `/api/feeds?shelf=${shelf._id}`)
+      body.startsWith('<?xml').should.be.true()
+    })
   })
 })
