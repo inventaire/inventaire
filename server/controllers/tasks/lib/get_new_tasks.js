04b7f1fff6203b790d0e7fa6dcf6bd8a79f2f82d
jums <jums@inventaire.io>
2021-02-13 18:02:02 +0100

    add exact flag when searching for new tasks

--- server/controllers/tasks/lib/get_new_tasks.js
+++ server/controllers/tasks/lib/get_new_tasks.js
@@ -94,6 +94,7 @@ const searchEntityDuplicatesSuggestions = async entity => {
     search: name,
     types: [ 'humans' ],
     filter: 'wd',
+    exact: true,
     minScore: lowestSuggestionMatchScore
   })
 
