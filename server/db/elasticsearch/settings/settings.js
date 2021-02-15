7ab3cd1f2ffe1d5104e073c315ed16df24919ba6
maxlath <k@maxlath.eu>
2021-02-16 00:44:20 +0100

    elasticsearch: settings: document the need for the lowercase filter

--- server/db/elasticsearch/settings/settings.js
+++ server/db/elasticsearch/settings/settings.js
@@ -21,6 +21,10 @@ module.exports = {
         // See https://www.elastic.co/guide/en/elasticsearch/reference/7.10/analysis-standard-tokenizer.html
         tokenizer: 'standard',
         filter: [
+          // The 'standard' tokenizer does not imply lowercase
+          // https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-standard-tokenizer.html
+          // not to be confused with the 'standard' analyzer, which does
+          // https://www.elastic.co/guide/en/elasticsearch/reference/7.10/analysis-standard-analyzer.html
           'lowercase',
           'autocomplete_filter'
         ]
