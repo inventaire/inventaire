7acbf97642884ac607930776b284d426e5966da0
maxlath <k@maxlath.eu>
2021-02-16 23:03:15 +0100

    wikibase-edit: increase maxlag

--- server/lib/wikidata/edit.js
+++ server/lib/wikidata/edit.js
@@ -5,5 +5,7 @@ const { userAgent } = __.require('lib', 'requests')
 // Return an instance of wikibase-edit with the general config pre-set
 module.exports = require('wikibase-edit')({
   instance: 'https://www.wikidata.org',
-  userAgent
+  userAgent,
+  // Set an increased maxlag, as most edits are isolated edits from humans using the GUI
+  maxlag: 10,
 })
