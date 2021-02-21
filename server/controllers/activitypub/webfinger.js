de52f4df83f3e0b5071c21722a37a34d7008ec59
jums <jums@inventaire.io>
2021-02-21 14:30:16 +0100

    open a .well-known "endpoint"
    reject invalid requests
    reject non local actors
    reject unknown actor

---
+++ server/controllers/activitypub/webfinger.js
@@ -0,0 +1,50 @@
+const CONFIG = require('config')
+const __ = require('config').universalPath
+const error_ = __.require('lib', 'error/error')
+const assert_ = __.require('utils', 'assert_types')
+const user_ = __.require('controllers', 'user/lib/user')
+const couch_ = __.require('lib', 'couch')
+
+module.exports = {
+  get: (req, res) => {
+    const params = customSanitize(req, res)
+    const { resource } = params
+    findUser(resource)
+    .then(user => {
+      if (!user) throw error_.new('unknown actor', 400, resource)
+    })
+    .catch(error_.Handler(req, res))
+  }
+}
+
+const customSanitize = (req, res) => {
+  if (req.query.resource === null) {
+    throw error_.new('missing parameter in query: resource', 400)
+  }
+  const { resource } = req.query
+  if (!isValid(resource)) {
+    throw error_.new('invalid resource', 400, resource)
+  }
+  return req.query
+}
+
+const isValid = resource => {
+  assert_.string(resource)
+  if (resource.startsWith('acct:') === false) return false
+  const actorParts = getActorParts(resource)
+  if (actorParts.length !== 2) return false
+
+  const host = actorParts[1]
+  return host === CONFIG.publicHost
+}
+
+const getActorParts = resource => {
+  const actorWithHost = resource.substr(5)
+  return actorWithHost.split('@')
+}
+
+const findUser = async resource => {
+  const username = getActorParts(resource)[0]
+  return user_.byUsername(username)
+  .then(couch_.firstDoc)
+}
---
+++ tests/api/activitypub/webfinger.js
@@ -0,0 +1,44 @@
+const CONFIG = require('config')
+require('should')
+const { publicReq } = require('../utils/utils')
+const { shouldNotBeCalled, rethrowShouldNotBeCalledErrors } = require('../utils/utils')
+const { createUsername } = require('../fixtures/users')
+
+const endpoint = '/.well-known/webfinger?resource='
+
+describe('activitypub:webfinger', () => {
+  it('should reject invalid host', async () => {
+    try {
+      const invalidResource = 'bar.org'
+      invalidResource.should.not.equal(CONFIG.host)
+      const actor = `acct:foo@${invalidResource}`
+      await publicReq('get', `${endpoint}${actor}`).then(shouldNotBeCalled)
+    } catch (err) {
+      rethrowShouldNotBeCalledErrors(err)
+      err.statusCode.should.equal(400)
+      err.body.status_verbose.should.startWith('invalid resource')
+    }
+  })
+
+  it('should reject invalid actor', async () => {
+    try {
+      const invalidActorResource = 'acct:foobar.org'
+      await publicReq('get', `${endpoint}${invalidActorResource}`).then(shouldNotBeCalled)
+    } catch (err) {
+      rethrowShouldNotBeCalledErrors(err)
+      err.statusCode.should.equal(400)
+      err.body.status_verbose.should.startWith('invalid resource')
+    }
+  })
+
+  it('should reject unknown actor', async () => {
+    try {
+      const unknownLocalActorResource = `acct:${createUsername()}@${CONFIG.publicHost}`
+      await publicReq('get', `${endpoint}${unknownLocalActorResource}`).then(shouldNotBeCalled)
+    } catch (err) {
+      rethrowShouldNotBeCalledErrors(err)
+      err.statusCode.should.equal(400)
+      err.body.status_verbose.should.equal('unknown actor')
+    }
+  })
+})
