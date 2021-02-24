03901c24c4ac5e52f185478d15ef4534f3e4efa6
maxlath <k@maxlath.eu>
2021-02-25 00:07:45 +0100

    couch: list: remove obsolete design_doc

--- server/db/couch/list.js
+++ server/db/couch/list.js
@@ -13,7 +13,7 @@ module.exports = {
   comments: [ 'comments' ],
   entities: [ 'entities', 'entities_deduplicate' ],
   patches: [ 'patches' ],
-  notifications: [ 'notifications', 'groups_notifications' ],
+  notifications: [ 'notifications' ],
   tasks: [ 'tasks' ],
   shelves: [ 'shelves' ]
 }
