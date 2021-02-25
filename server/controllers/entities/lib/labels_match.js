58f0b4f2317a160c763e13bd65bc68081a6968f8
maxlath <k@maxlath.eu>
2021-02-25 18:54:57 +0100

    labels_match: refactor
    _.someMatch exits as soon as a match is found,
    while _.intersection tries to find all the matches,
    which is less performant, with no benefit

--- server/controllers/entities/lib/labels_match.js
+++ server/controllers/entities/lib/labels_match.js
@@ -4,7 +4,7 @@ const _ = __.require('builders', 'utils')
 const haveExactMatch = (labels1, labels2) => {
   const formattedLabels1 = labels1.map(formatLabel)
   const formattedLabels2 = labels2.map(formatLabel)
-  return _.intersection(formattedLabels1, formattedLabels2).length > 0
+  return _.someMatch(formattedLabels1, formattedLabels2)
 }
 
 const formatLabel = label => label.toLowerCase()
