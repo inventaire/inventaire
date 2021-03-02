ada05cc6477031bec7fa9ac243847fdeb18e7119
maxlath <k@maxlath.eu>
2021-03-02 21:41:10 +0100

    open public contributions to non-admin users

--- server/controllers/entities/contributions.js
+++ server/controllers/entities/contributions.js
@@ -1,5 +1,9 @@
 // An endpoint to list entities edits made by a user
+const error_ = require('lib/error/error')
 const patches_ = require('./lib/patches')
+const user_ = require('controllers/user/lib/user')
+const { shouldBeAnonymized } = require('models/user')
+const anonymizePatches = require('./lib/anonymize_patches')
 
 const sanitization = {
   user: { optional: true },
@@ -7,12 +11,24 @@ const sanitization = {
   offset: { default: 0 }
 }
 
-const controller = ({ userId, limit, offset }) => {
-  if (userId != null) {
-    return patches_.byUserId(userId, limit, offset)
-  } else {
-    return patches_.byDate(limit, offset)
+const getContributions = async ({ userId, limit, offset, reqUserHasAdminAccess }) => {
+  if (userId && !reqUserHasAdminAccess) {
+    const user = await user_.byId(userId)
+    if (shouldBeAnonymized(user)) {
+      throw error_.new('non-public contributions', 403)
+    }
   }
+
+  let patchesPage
+  if (userId != null) {
+    patchesPage = await patches_.byUserId(userId, limit, offset)
+  } else {
+    patchesPage = await patches_.byDate(limit, offset)
+  }
+
+  if (!reqUserHasAdminAccess) await anonymizePatches(patchesPage.patches)
+
+  return patchesPage
 }
 
-module.exports = { sanitization, controller }
+module.exports = { sanitization, controller: getContributions }
