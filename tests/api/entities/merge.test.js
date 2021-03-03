6fb3f2442de0fd1fea35c0ceee18d8a14ca50f13
maxlath <k@maxlath.eu>
2021-03-03 15:59:02 +0100

    tests: entities: merge: async/await refactor

--- tests/api/entities/merge.test.js
+++ tests/api/entities/merge.test.js
@@ -1,7 +1,7 @@
 const CONFIG = require('config')
 const __ = CONFIG.universalPath
 require('should')
-const { authReq, dataadminReq, undesiredRes, shouldNotBeCalled } = require('../utils/utils')
+const { authReq, dataadminReq, shouldNotBeCalled } = require('../utils/utils')
 const randomString = __.require('lib', './utils/random_string')
 const { getByUris, merge, getHistory, addClaim } = require('../utils/entities')
 const { getByIds: getItemsByIds } = require('../utils/items')
@@ -61,21 +61,15 @@ describe('entities:merge', () => {
     })
   })
 
-  it('should merge two entities with an inv URI', done => {
-    Promise.all([
+  it('should merge two entities with an inv URI', async () => {
+    const [ workA, workB ] = await Promise.all([
       createWork(),
       createWork()
     ])
-    .then(([ workA, workB ]) => {
-      return merge(workA.uri, workB.uri)
-      .then(() => getByUris(workA.uri))
-      .then(res => {
-        res.redirects[workA.uri].should.equal(workB.uri)
-        res.entities[workB.uri].should.be.ok()
-        done()
-      })
-    })
-    .catch(done)
+    await merge(workA.uri, workB.uri)
+    const { entities, redirects } = await getByUris(workA.uri)
+    redirects[workA.uri].should.equal(workB.uri)
+    entities[workB.uri].should.be.ok()
   })
 
   it('should merge entities with inv and isbn URIs', async () => {
@@ -86,13 +80,13 @@ describe('entities:merge', () => {
     const item = await createItemFromEntityUri(editionA.uri)
     item.entity.should.equal(editionA.uri)
     await merge(editionA.uri, editionB.uri)
-    const [ entitiesRes, itemsRes ] = await Promise.all([
+    const [ { entities, redirects }, { items } ] = await Promise.all([
       getByUris(editionA.uri),
       getItemsByIds(item._id)
     ])
-    entitiesRes.redirects[editionA.uri].should.equal(editionB.uri)
-    entitiesRes.entities[editionB.uri].should.be.ok()
-    itemsRes.items[0].entity.should.equal(editionB.uri)
+    redirects[editionA.uri].should.equal(editionB.uri)
+    entities[editionB.uri].should.be.ok()
+    items[0].entity.should.equal(editionB.uri)
   })
 
   it('should merge an entity with an ISBN', async () => {
@@ -102,16 +96,14 @@ describe('entities:merge', () => {
     ])
     const item = await createItemFromEntityUri(editionB.uri)
     await merge(editionA.uri, editionB.uri)
-    const [ entitiesRes, itemsRes ] = await Promise.all([
+    const [ { entities, redirects }, { items } ] = await Promise.all([
       getByUris(editionB.uri),
       getItemsByIds(item._id)
     ])
-    const { entities, redirects } = entitiesRes
     const updatedEditionB = entities[redirects[editionB.uri]]
-    updatedEditionB.claims['wdt:P212']
-    .should.deepEqual(editionA.claims['wdt:P212'])
+    updatedEditionB.claims['wdt:P212'].should.deepEqual(editionA.claims['wdt:P212'])
     const isbnUri = editionA.uri
-    itemsRes.items[0].entity.should.equal(isbnUri)
+    items[0].entity.should.equal(isbnUri)
   })
 
   it('should reject merge with different ISBNs', async () => {
@@ -119,161 +111,115 @@ describe('entities:merge', () => {
       createEditionWithIsbn(),
       createEditionWithIsbn()
     ])
-    try {
-      await merge(editionA.uri, editionB.uri).then(shouldNotBeCalled)
-    } catch (err) {
+    await merge(editionA.uri, editionB.uri)
+    .then(shouldNotBeCalled)
+    .catch(err => {
       err.body.status_verbose.should.equal("can't merge editions with different ISBNs")
       err.statusCode.should.equal(400)
-    }
+    })
   })
 
-  it('should transfer claims', done => {
-    Promise.all([
+  it('should transfer claims', async () => {
+    const [ workA, workB ] = await Promise.all([
       createWork(),
       createWork()
     ])
-    .then(([ workA, workB ]) => {
-      return addClaim(workA.uri, 'wdt:P50', 'wd:Q535')
-      .then(() => merge(workA.uri, workB.uri))
-      .then(() => getByUris(workB.uri))
-      .then(res => {
-        const authorsUris = res.entities[workB.uri].claims['wdt:P50']
-        authorsUris.should.deepEqual([ 'wd:Q535' ])
-        done()
-      })
-    })
-    .catch(done)
+    await addClaim(workA.uri, 'wdt:P50', 'wd:Q535')
+    await merge(workA.uri, workB.uri)
+    const { entities } = await getByUris(workB.uri)
+    const authorsUris = entities[workB.uri].claims['wdt:P50']
+    authorsUris.should.deepEqual([ 'wd:Q535' ])
   })
 
-  it('should transfer labels', done => {
+  it('should transfer labels', async () => {
     const label = randomString(6)
-    Promise.all([
+    const [ workA, workB ] = await Promise.all([
       createWork({ labels: { zh: label } }),
       createWork()
     ])
-    .then(([ workA, workB ]) => {
-      return merge(workA.uri, workB.uri)
-      .then(() => getByUris(workB.uri))
-      .then(res => {
-        res.entities[workB.uri].labels.zh.should.equal(label)
-        done()
-      })
-    })
-    .catch(done)
+    await merge(workA.uri, workB.uri)
+    const { entities } = await getByUris(workB.uri)
+    entities[workB.uri].labels.zh.should.equal(label)
   })
 
-  it('should keep track of the patch context', done => {
-    Promise.all([
+  it('should keep track of the patch context', async () => {
+    const [ workA, workB ] = await Promise.all([
       createWork(),
       createWork()
     ])
-    .then(([ workA, workB ]) => {
-      return addClaim(workA.uri, 'wdt:P50', 'wd:Q535')
-      .then(() => merge(workA.uri, workB.uri))
-      .then(() => getHistory(workB._id))
-      .then(patches => {
-        patches[1].context.mergeFrom.should.equal(workA.uri)
-        done()
-      })
-    })
-    .catch(done)
+    await addClaim(workA.uri, 'wdt:P50', 'wd:Q535')
+    await merge(workA.uri, workB.uri)
+    const patches = await getHistory(workB._id)
+    patches[1].context.mergeFrom.should.equal(workA.uri)
   })
 
-  it('should redirect claims', done => {
-    Promise.all([
+  it('should redirect claims', async () => {
+    const [ humanA, humanB, work ] = await Promise.all([
       createHuman(),
       createHuman(),
       createWork()
     ])
-    .then(([ humanA, humanB, work ]) => {
-      return addClaim(work.uri, 'wdt:P50', humanA.uri)
-      .then(() => merge(humanA.uri, humanB.uri))
-      .then(() => getByUris(work.uri))
-      .then(res => {
-        const authorsUris = res.entities[work.uri].claims['wdt:P50']
-        authorsUris.should.deepEqual([ humanB.uri ])
-      })
-      .then(() => getHistory(work._id))
-      .then(patches => {
-        // patch 0: create the work entity
-        // patch 1: add a wdt:P50 claim pointing to to humanA
-        // patch 2: redirect to humanB
-        patches[2].context.redirectClaims
-        .should.deepEqual({ fromUri: humanA.uri })
-        done()
-      })
-    })
-    .catch(done)
+    await addClaim(work.uri, 'wdt:P50', humanA.uri)
+    await merge(humanA.uri, humanB.uri)
+    const { entities } = await getByUris(work.uri)
+    const authorsUris = entities[work.uri].claims['wdt:P50']
+    authorsUris.should.deepEqual([ humanB.uri ])
+    const patches = await getHistory(work._id)
+    // patch 0: create the work entity
+    // patch 1: add a wdt:P50 claim pointing to to humanA
+    // patch 2: redirect to humanB
+    patches[2].context.redirectClaims.should.deepEqual({ fromUri: humanA.uri })
   })
 
-  it('should reject a merge from a redirection', done => {
-    Promise.all([
+  it('should reject a merge from a redirection', async () => {
+    const [ workA, workB, workC ] = await Promise.all([
       createWork(),
       createWork(),
       createWork()
     ])
-    .then(([ workA, workB, workC ]) => {
-      return merge(workA.uri, workB.uri)
-      .then(() => merge(workA.uri, workC.uri))
-      .then(undesiredRes(done))
-      .catch(err => {
-        err.statusCode.should.equal(400)
-        err.body.status_verbose.should.equal("'from' entity is already a redirection")
-        done()
-      })
+    await merge(workA.uri, workB.uri)
+    await merge(workA.uri, workC.uri)
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.statusCode.should.equal(400)
+      err.body.status_verbose.should.equal("'from' entity is already a redirection")
     })
-    .catch(done)
   })
 
-  it('should reject a merge to a redirection', done => {
-    Promise.all([
+  it('should reject a merge to a redirection', async () => {
+    const [ workA, workB, workC ] = await Promise.all([
       createWork(),
       createWork(),
       createWork()
     ])
-    .then(([ workA, workB, workC ]) => {
-      return merge(workA.uri, workB.uri)
-      .then(() => merge(workC.uri, workA.uri))
-      .then(undesiredRes(done))
-      .catch(err => {
-        err.statusCode.should.equal(400)
-        err.body.status_verbose.should.equal("'to' entity is already a redirection")
-        done()
-      })
+    await merge(workA.uri, workB.uri)
+    await merge(workC.uri, workA.uri)
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.statusCode.should.equal(400)
+      err.body.status_verbose.should.equal("'to' entity is already a redirection")
     })
-    .catch(done)
   })
 
-  it('should reject a circular merge', done => {
-    createWork()
-    .then(work => {
-      return merge(work.uri, work.uri)
-      .then(undesiredRes(done))
-      .catch(err => {
-        err.statusCode.should.equal(400)
-        err.body.status_verbose
-        .should.equal("can't merge an entity into itself")
-        done()
-      })
+  it('should reject a circular merge', async () => {
+    const work = await createWork()
+    await merge(work.uri, work.uri)
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.statusCode.should.equal(400)
+      err.body.status_verbose.should.equal("can't merge an entity into itself")
     })
-    .catch(done)
   })
 
-  it('should remove isolated human "placeholders" entities on works merge', done => {
-    Promise.all([
+  it('should remove isolated human "placeholders" entities on works merge', async () => {
+    const [ workA, workB ] = await Promise.all([
       createWorkWithAuthor(),
       createWorkWithAuthor()
     ])
-    .then(([ workA, workB ]) => {
-      const humanAUri = workA.claims['wdt:P50'][0]
-      return merge(workA.uri, workB.uri)
-      .then(() => getByUris(humanAUri))
-      .then(res => {
-        const entity = res.entities[humanAUri]
-        entity._meta_type.should.equal('removed:placeholder')
-        done()
-      })
-    })
-    .catch(done)
+    const humanAUri = workA.claims['wdt:P50'][0]
+    await merge(workA.uri, workB.uri)
+    const { entities } = await getByUris(humanAUri)
+    const entity = entities[humanAUri]
+    entity._meta_type.should.equal('removed:placeholder')
   })
 })
