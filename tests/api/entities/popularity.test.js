e69c628e3847fc35e0bb98ea36c50587b4bdd5d1
maxlath <k@maxlath.eu>
2021-03-03 16:34:15 +0100

    tests: entities: popularity: async/await refactor

--- tests/api/entities/popularity.test.js
+++ tests/api/entities/popularity.test.js
@@ -1,98 +1,79 @@
 require('should')
-const { undesiredRes } = require('../utils/utils')
+const { shouldNotBeCalled } = require('../utils/utils')
 const { addClaim, getRefreshedPopularityByUri } = require('../utils/entities')
 const { createEdition, createWork, createItemFromEntityUri, createSerie, createHuman } = require('../fixtures/entities')
 
 describe('entities:popularity', () => {
   describe('edition', () => {
-    it('should reject invalid uri', done => {
+    it('should reject invalid uri', async () => {
       const invalidUri = 'inv:aliduri'
-      getRefreshedPopularityByUri(invalidUri)
-      .then(undesiredRes(done))
+      await getRefreshedPopularityByUri(invalidUri)
+      .then(shouldNotBeCalled)
       .catch(err => {
         err.body.status_verbose.should.startWith('invalid ')
-        done()
       })
-      .catch(done)
     })
 
-    it('should default to 0', done => {
-      createEdition()
-      .then(edition => scoreShouldEqual(edition.uri, 0, done))
-      .catch(done)
+    it('should default to 0', async () => {
+      const edition = await createEdition()
+      await scoreShouldEqual(edition.uri, 0)
     })
 
-    it('should equal the amount of instances in inventories', done => {
-      createEdition()
-      .then(edition => {
-        const { uri } = edition
-        return scoreShouldEqual(uri, 0)
-        .then(() => createItemFromEntityUri(uri))
-        .then(() => scoreShouldEqual(uri, 1, done))
-      })
-      .catch(done)
+    it('should equal the amount of instances in inventories', async () => {
+      const { uri } = await createEdition()
+      await scoreShouldEqual(uri, 0)
+      await createItemFromEntityUri(uri)
+      await scoreShouldEqual(uri, 1)
     })
 
-    it('should count only one instance per owner', done => {
-      createEdition()
-      .then(edition => {
-        const { uri } = edition
-        return createItemFromEntityUri(uri, { details: '1' })
-        .then(() => createItemFromEntityUri(uri, { details: '2' }))
-        .then(() => scoreShouldEqual(uri, 1, done))
-      })
-      .catch(done)
+    it('should count only one instance per owner', async () => {
+      const { uri } = await createEdition()
+      await createItemFromEntityUri(uri, { details: '1' })
+      await createItemFromEntityUri(uri, { details: '2' })
+      await scoreShouldEqual(uri, 1)
     })
   })
 
   describe('work', () => {
-    it('should default to 0', done => {
-      createWork()
-      .then(work => scoreShouldEqual(work.uri, 0, done))
-      .catch(done)
+    it('should default to 0', async () => {
+      const work = await createWork()
+      await scoreShouldEqual(work.uri, 0)
     })
 
-    it('should be incremented by every instances of editions', done => {
-      createEdition()
-      .then(edition => {
-        const workUri = edition.claims['wdt:P629'][0]
-        return scoreShouldEqual(workUri, 1)
-        .then(createItemFromEntityUri.bind(null, edition.uri))
-        .then(() => scoreShouldEqual(workUri, 2, done))
-      })
-      .catch(done)
+    it('should be incremented by every instances of editions', async () => {
+      const edition = await createEdition()
+      const workUri = edition.claims['wdt:P629'][0]
+      await scoreShouldEqual(workUri, 1)
+      await createItemFromEntityUri(edition.uri)
+      await scoreShouldEqual(workUri, 2)
     })
   })
 
   describe('serie', () => {
-    it('should be made of the sum of its parts scores + number of parts', done => {
-      createSerieWithAWorkWithAnEditionWithAnItem()
+    it('should be made of the sum of its parts scores + number of parts', async () => {
+      const [ serie ] = await createSerieWithAWorkWithAnEditionWithAnItem()
       // 1: item
       // 1: edition
       // 1: work
-      .then(([ serie ]) => scoreShouldEqual(serie.uri, 3, done))
-      .catch(done)
+      await scoreShouldEqual(serie.uri, 3)
     })
   })
 
   describe('human', () => {
-    it('should be made of the sum of its works scores + number of works and series', done => {
-      createHumanWithAWorkWithAnEditionWithAnItem()
+    it('should be made of the sum of its works scores + number of works and series', async () => {
+      const [ human ] = await createHumanWithAWorkWithAnEditionWithAnItem()
       // 1: item
       // 1: edition
       // 1: work
       // 1: serie
-      .then(([ human ]) => scoreShouldEqual(human.uri, 4, done))
-      .catch(done)
+      await scoreShouldEqual(human.uri, 4)
     })
   })
 })
 
-const scoreShouldEqual = async (uri, value, done) => {
+const scoreShouldEqual = async (uri, value) => {
   const score = await getRefreshedPopularityByUri(uri)
   score.should.equal(value)
-  if (typeof done === 'function') done()
-  return score
 }
 
 const createSerieWithAWorkWithAnEditionWithAnItem = async () => {
