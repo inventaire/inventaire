9bfdde5b321041213696d7b4a8578883c600e790
maxlath <k@maxlath.eu>
2021-03-10 18:52:01 +0100

    tests: images: async/await refactor

--- tests/api/images/data_url.test.js
+++ tests/api/images/data_url.test.js
@@ -1,63 +1,51 @@
 const CONFIG = require('config')
 require('should')
-const { authReq, undesiredRes } = require('../utils/utils')
+const { authReq, shouldNotBeCalled } = require('../utils/utils')
 const imageUrl = encodeURIComponent('https://raw.githubusercontent.com/inventaire/inventaire-client/master/app/assets/icon/32.png')
 const dataUrlStart = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYA'
 const endpoint = '/api/images?action=data-url'
 
 describe('images:data-url', () => {
-  it('should reject without URL', done => {
-    authReq('get', endpoint)
-    .then(undesiredRes(done))
+  it('should reject without URL', async () => {
+    await authReq('get', endpoint)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal('missing parameter in query: url')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject with an invalid URL', done => {
-    authReq('get', `${endpoint}&url=bla`)
-    .then(undesiredRes(done))
+  it('should reject with an invalid URL', async () => {
+    await authReq('get', `${endpoint}&url=bla`)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal('invalid url: bla')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject with an invalid content type', done => {
+  it('should reject with an invalid content type', async () => {
     const invalidContentTypeUrl = encodeURIComponent('http://maxlath.eu/data.json')
-    authReq('get', `${endpoint}&url=${invalidContentTypeUrl}`)
-    .then(undesiredRes(done))
+    await authReq('get', `${endpoint}&url=${invalidContentTypeUrl}`)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal('invalid content type')
-      done()
     })
-    .catch(done)
   })
 
-  it('should return a data-url', done => {
-    authReq('get', `${endpoint}&url=${imageUrl}`)
-    .then(res => {
-      res['data-url'].should.be.a.String()
-      res['data-url'].should.startWith(dataUrlStart)
-      done()
-    })
-    .catch(done)
+  it('should return a data-url', async () => {
+    const { 'data-url': dataUrl } = await authReq('get', `${endpoint}&url=${imageUrl}`)
+    dataUrl.should.be.a.String()
+    dataUrl.should.startWith(dataUrlStart)
   })
 
-  it('should format to add host to a local url', done => {
+  it('should format to add host to a local url', async () => {
     const imageUrl = '/some/invalid/local/url'
-    authReq('get', `${endpoint}&url=${encodeURIComponent(imageUrl)}`)
-    .then(undesiredRes(done))
+    await authReq('get', `${endpoint}&url=${encodeURIComponent(imageUrl)}`)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.context.url.should.equal(`${CONFIG.fullPublicHost()}${imageUrl}`)
-      done()
     })
-    .catch(done)
   })
 })
