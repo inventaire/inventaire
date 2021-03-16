9d8a90bdbb9efe6cf307a65e2a6273aefbe8bd10
maxlath <k@maxlath.eu>
2021-03-16 18:59:42 +0100

    tests: images: add upload test
    using a new dependency to generate the form data, as generating this manually proved to be challenging

--- tests/api/images/upload.test.js
+++ tests/api/images/upload.test.js
@@ -1,7 +1,15 @@
 require('should')
-const { authReq } = require('../utils/utils')
+const CONFIG = require('config')
+const __ = CONFIG.universalPath
+const host = CONFIG.fullPublicHost()
+const someJpegPath = __.path('client', 'public/images/small/brittanystevens.jpg')
+const { authReq, getUser } = require('../utils/utils')
 const { shouldNotBeCalled } = require('root/tests/unit/utils')
 const endpoint = '/api/images?action=upload'
+const { createReadStream } = require('fs')
+const fetch = require('node-fetch')
+const FormData = require('form-data')
+const { isImageHash } = require('lib/boolean_validations')
 
 describe('images:upload', () => {
   it('reject uploads on assets containers', async () => {
@@ -18,5 +26,22 @@ describe('images:upload', () => {
     .catch(err => {
       err.body.status_verbose.should.startWith('invalid container')
     })
+  })
+
+  it('should upload an image', async () => {
+    const { cookie } = await getUser()
+    const container = 'entities'
+    const form = new FormData()
+    form.append('somefile', createReadStream(someJpegPath))
+    const res = await fetch(`${host}${endpoint}&container=${container}`, {
+      method: 'post',
+      headers: { cookie, ...form.getHeaders() },
+      body: form,
+    })
+    res.status.should.equal(200)
+    const { somefile } = await res.json()
+    somefile.should.startWith(`/img/${container}/`)
+    const imageHash = somefile.split('/').slice(3)[0]
+    isImageHash(imageHash).should.be.true()
   })
 })
