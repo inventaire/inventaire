1f6d8590cec42cd632dc1569054cdc2a41296eae
maxlath <k@maxlath.eu>
2021-04-12 16:54:00 +0200

    tests: items: update: async/await refactor

--- tests/api/items/update.test.js
+++ tests/api/items/update.test.js
@@ -1,134 +1,101 @@
 const CONFIG = require('config')
-const { Wait } = require('lib/promises')
-const should = require('should')
+const { wait } = require('lib/promises')
+require('should')
 const { authReq, authReqB, getUser, getUserB } = require('../utils/utils')
 const { newItemBase, CountChange } = require('./helpers')
 const { createShelf } = require('../fixtures/shelves')
 const debounceDelay = CONFIG.itemsCountDebounceTime + 500
-const { shouldNotBeCalled, rethrowShouldNotBeCalledErrors } = require('apiTests/utils/utils')
+const { shouldNotBeCalled } = require('apiTests/utils/utils')
 
 describe('items:update', () => {
   it('should reject invalid item id', async () => {
     const item = await authReq('post', '/api/items', newItemBase())
     item._id = 'invalidid'
-    try {
-      const newItem = await authReq('put', '/api/items', item)
-      shouldNotBeCalled(newItem)
-    } catch (err) {
-      rethrowShouldNotBeCalledErrors(err)
+    await authReq('put', '/api/items', item)
+    .then(shouldNotBeCalled)
+    .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.startWith('invalid _id')
-    }
+    })
   })
 
   it('should reject invalid entity', async () => {
     const item = await authReq('post', '/api/items', newItemBase())
     item.entity = 'inv:aliduri'
-    try {
-      const newItem = await authReq('put', '/api/items', item)
-      shouldNotBeCalled(newItem)
-    } catch (err) {
-      rethrowShouldNotBeCalledErrors(err)
+    await authReq('put', '/api/items', item)
+    .then(shouldNotBeCalled)
+    .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.startWith('invalid entity')
-    }
-  })
-
-  it('should update an item', done => {
-    authReq('post', '/api/items', newItemBase())
-    .then(item => {
-      let newDetails, newTransaction
-      item.transaction = (newTransaction = 'lending')
-      item.details = (newDetails = 'hello')
-      return authReq('put', '/api/items', item)
-      .then(updatedItem => {
-        updatedItem.transaction.should.equal(newTransaction)
-        updatedItem.details.should.equal(newDetails)
-        done()
-      })
     })
-    .catch(done)
   })
 
-  it('should not be able to update non updatable attributes', done => {
-    authReq('post', '/api/items', newItemBase())
-    .then(item => {
-      item.busy = true
-      return authReq('put', '/api/items', item)
-      .then(updatedItem => {
-        should(updatedItem.busy).not.be.ok()
-        done()
-      })
-    })
-    .catch(done)
+  it('should update an item', async () => {
+    const item = await authReq('post', '/api/items', newItemBase())
+    const newTransaction = item.transaction = 'lending'
+    const newDetails = item.details = 'hello'
+    const updatedItem = await authReq('put', '/api/items', item)
+    updatedItem.transaction.should.equal(newTransaction)
+    updatedItem.details.should.equal(newDetails)
   })
 
-  it('should trigger an update of the users items counters', done => {
-    authReq('post', '/api/items', newItemBase())
+  it('should not be able to update non updatable attributes', async () => {
+    const item = await authReq('post', '/api/items', newItemBase())
+    const { created: originalCreatedTimestamp } = item
+    item.created = Date.now() - 10000
+    const updatedItem = await authReq('put', '/api/items', item)
+    updatedItem.created.should.equal(originalCreatedTimestamp)
+  })
+
+  it('should trigger an update of the users items counters', async () => {
+    const item = await authReq('post', '/api/items', newItemBase())
     // Delay to let the time to the item counter to be updated
-    .then(Wait(debounceDelay))
-    .then(item => {
-      return getUser()
-      .then(userBefore => {
-        let newListing
-        item.listing.should.equal('private')
-        item.listing = (newListing = 'public')
-        return authReq('put', '/api/items', item)
-        // Delay to request the user after its items count was updated
-        .then(Wait(debounceDelay))
-        .then(updatedItem => {
-          updatedItem.listing.should.equal(newListing)
-          return getUser()
-          .then(userAfter => {
-            const countChange = CountChange(userBefore.snapshot, userAfter.snapshot)
-            countChange('private').should.equal(-1)
-            countChange('network').should.equal(0)
-            countChange('public').should.equal(1)
-            done()
-          })
-        })
-      })
-    })
-    .catch(done)
+    await wait(debounceDelay)
+    const userBefore = await getUser()
+    item.listing.should.equal('private')
+    const newListing = item.listing = 'public'
+    const updatedItem = await authReq('put', '/api/items', item)
+    // Delay to request the user after its items count was updated
+    await wait(debounceDelay)
+    updatedItem.listing.should.equal(newListing)
+    const userAfter = await getUser()
+    const countChange = CountChange(userBefore.snapshot, userAfter.snapshot)
+    countChange('private').should.equal(-1)
+    countChange('network').should.equal(0)
+    countChange('public').should.equal(1)
   })
 
   it('should reject item from another owner', async () => {
     const item = await authReqB('post', '/api/items', newItemBase())
     item.listing = 'public'
-    try {
-      const newItem = await authReq('put', '/api/items', item)
-      shouldNotBeCalled(newItem)
-    } catch (err) {
-      rethrowShouldNotBeCalledErrors(err)
+    await authReq('put', '/api/items', item)
+    .then(shouldNotBeCalled)
+    .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.startWith('user is not item owner')
-    }
+    })
   })
 
   it('should reject item with an invalid shelf', async () => {
     const item = await authReq('post', '/api/items', newItemBase())
     item.shelves = [ 'not a shelf id' ]
-    try {
-      const newItem = await authReq('put', '/api/items', item)
-      shouldNotBeCalled(newItem)
-    } catch (err) {
-      rethrowShouldNotBeCalledErrors(err)
+    await authReq('put', '/api/items', item)
+    .then(shouldNotBeCalled)
+    .catch(err => {
       err.statusCode.should.equal(400)
       err.body.error_name.should.equal('invalid_shelves')
-    }
+    })
   })
 
   it('should reject item with a shelf from another owner', async () => {
     const item = await authReq('post', '/api/items', newItemBase())
     const shelf = await createShelf(getUserB())
     item.shelves = [ shelf._id ]
-    try {
-      const newItem = await authReq('put', '/api/items', item)
-      shouldNotBeCalled(newItem)
-    } catch (err) {
-      rethrowShouldNotBeCalledErrors(err)
+    await authReq('put', '/api/items', item)
+    .then(shouldNotBeCalled)
+    .catch(err => {
       err.statusCode.should.equal(400)
       err.body.status_verbose.should.equal('invalid owner')
-    }
+    })
   })
 })
--- tests/unit/utils.js
+++ tests/unit/utils.js
@@ -11,7 +11,9 @@ module.exports = {
   shouldNotBeCalled: res => {
     warn(util.inspect(res, false, null), 'undesired positive res')
     const err = new Error('function was expected not to be called')
-    err.name = 'shouldNotBeCalled'
+    // Give 'shouldNotBeCalled' more chance to appear in the red text of the failing test
+    err.name = err.statusCode = 'shouldNotBeCalled'
+    err.body = { status_verbose: 'shouldNotBeCalled' }
     err.context = { res }
     throw err
   },
