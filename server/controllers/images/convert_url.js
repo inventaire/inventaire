34980115268ae24459e53f167e9d41955a98e1e8
maxlath <k@maxlath.eu>
2021-04-17 18:56:00 +0200

    convert_url: refactor with sanitize

--- server/controllers/images/convert_url.js
+++ server/controllers/images/convert_url.js
@@ -1,22 +1,25 @@
 const responses_ = require('lib/responses')
 const error_ = require('lib/error/error')
+const sanitize = require('lib/sanitize/sanitize')
 const { getImageByUrl } = require('data/dataseed/dataseed')
 const { enabled: dataseedEnabled } = require('config').dataseed
 
-module.exports = (req, res) => {
-  const { url } = req.body
+const sanitization = {
+  url: {}
+}
 
+module.exports = (req, res) => {
+  sanitize(req, res, sanitization)
+  .then(convertUrl)
+  .then(responses_.Send(res))
+  .catch(error_.Handler(req, res))
+}
+
+const convertUrl = async ({ url }) => {
   // If dataseed is disabled, we simply return the same url,
   // instead of converting it to an image hash
-  if (!dataseedEnabled) {
-    res.json({ url, converted: false })
-    return
-  }
-
-  return getImageByUrl(url)
-  .then(data => {
-    data.converted = true
-    responses_.send(res, data)
-  })
-  .catch(error_.Handler(req, res))
+  if (!dataseedEnabled) return { url, converted: false }
+  const data = await getImageByUrl(url)
+  data.converted = true
+  return data
 }
