9f4d6a7a760d7ab742b0f9e4b1d63439f7388496
maxlath <k@maxlath.eu>
2021-04-18 19:30:56 +0200

    syntax
    as suggested by https://github.com/inventaire/inventaire/pull/522#discussion_r615429213

--- server/controllers/images/lib/containers.js
+++ server/controllers/images/lib/containers.js
@@ -1,7 +1,8 @@
 const images_ = require('lib/images')
 const putImage = require('./put_image')
 
-const containerPutImage = (container, fnName) => ({ id = 0, path }) => {
+const containerPutImage = (container, fnName) => fileData => {
+  const { id = 0, path } = fileData
   return images_[fnName](path)
   .then(() => images_.getHashFilename(path))
   .then(filename => putImage(container, path, id, filename))
