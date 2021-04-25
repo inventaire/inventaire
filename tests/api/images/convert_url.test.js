809ad28b92410e54fc0ce707fbfe5c023bbfad89
maxlath <k@maxlath.eu>
2021-04-25 11:57:26 +0200

    images: convert url: accept to upload to users and groups containers
    additionally to the entities container
    
    This should be useful for tests, but also could lead to adding that
    option to the client for users and groups image upload

--- tests/api/images/convert_url.test.js
+++ tests/api/images/convert_url.test.js
@@ -1,31 +1,63 @@
 require('should')
 const { authReq } = require('../utils/utils')
 const endpoint = '/api/images?action=convert-url'
-const { isImageHash, isLocalImg } = require('lib/boolean_validations')
+const { isImageHash, isEntityImg, isUserImg, isGroupImg } = require('lib/boolean_validations')
 const { shouldNotBeCalled } = require('../utils/utils')
 
-const convertUrl = url => authReq('post', endpoint, { url })
+const convertUrl = (container, url) => authReq('post', endpoint, { container, url })
 
 describe('images:convert-url', () => {
   it('should reject an invalid URL', async () => {
     const imageUrl = 'upload.wikimedia.org/wikipedia/commons/thumb/6/64/foo.jpg'
-    await convertUrl(imageUrl)
+    await convertUrl('entities', imageUrl)
     .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(400)
     })
   })
 
-  it('should convert a URL', async () => {
+  it('should upload an image to the entities container from a URL', async () => {
     const imageUrl = 'https://raw.githubusercontent.com/inventaire/inventaire-client/master/app/assets/icon/32.png'
-    const { url, hash } = await convertUrl(imageUrl)
+    const { url, hash } = await convertUrl('entities', imageUrl)
     isImageHash(hash).should.be.true()
-    isLocalImg(url).should.be.true()
+    isEntityImg(url).should.be.true()
+  })
+
+  it('should upload an image to the users container from a URL', async () => {
+    const imageUrl = 'https://raw.githubusercontent.com/inventaire/inventaire-client/master/app/assets/icon/32.png'
+    const { url, hash } = await convertUrl('users', imageUrl)
+    isImageHash(hash).should.be.true()
+    isUserImg(url).should.be.true()
+  })
+
+  it('should upload an image to the groups container from a URL', async () => {
+    const imageUrl = 'https://raw.githubusercontent.com/inventaire/inventaire-client/master/app/assets/icon/32.png'
+    const { url, hash } = await convertUrl('groups', imageUrl)
+    isImageHash(hash).should.be.true()
+    isGroupImg(url).should.be.true()
+  })
+
+  it('should reject an image being uploaded to the assets container', async () => {
+    const imageUrl = 'https://raw.githubusercontent.com/inventaire/inventaire-client/master/app/assets/icon/32.png'
+    await convertUrl('assets', imageUrl)
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.statusCode.should.equal(400)
+    })
+  })
+
+  it('should reject an image being uploaded to an invalid container', async () => {
+    const imageUrl = 'https://raw.githubusercontent.com/inventaire/inventaire-client/master/app/assets/icon/32.png'
+    await convertUrl('foo', imageUrl)
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.statusCode.should.equal(400)
+    })
   })
 
   it('should reject an URL returning a 404', async () => {
     const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/foo.jpg'
-    await convertUrl(imageUrl)
+    await convertUrl('entities', imageUrl)
     .then(shouldNotBeCalled)
     .catch(err => {
       err.statusCode.should.equal(404)
@@ -34,13 +66,13 @@ describe('images:convert-url', () => {
 
   it('should support URLs with unescaped characters', async () => {
     const imageUrl = 'https://lafabrique.fr/wp-content/uploads/2017/10/Israël-Palestine_couv.jpg'
-    const { hash } = await convertUrl(imageUrl)
+    const { hash } = await convertUrl('entities', imageUrl)
     hash.should.equal('12f6bc6121725a1b28f57bdc10443db459119140')
   })
 
   it('should support URLs with escaped characters', async () => {
     const imageUrl = 'https://lafabrique.fr/wp-content/uploads/2017/10/Israe%CC%88l-Palestine_couv.jpg'
-    const { hash } = await convertUrl(imageUrl)
+    const { hash } = await convertUrl('entities', imageUrl)
     hash.should.equal('12f6bc6121725a1b28f57bdc10443db459119140')
   })
 })
