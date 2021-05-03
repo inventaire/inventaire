inventaire/inventaire

commit 2a394c4a7eba79392a1b70d5709fa30afea6bf2a

convert_url: locally import an image URL to the media storage
instead of relying on a dataseed service, allowing instances without such a service
to still import images from URLs

contributing to https://github.com/inventaire/inventaire/issues/481

server/controllers/images/convert_url.js
@@ -1,8 +1,10 @@
+const _ = require('builders/utils')
 const responses_ = require('lib/responses')
 const error_ = require('lib/error/error')
 const sanitize = require('lib/sanitize/sanitize')
-const { getImageByUrl } = require('data/dataseed/dataseed')
+const { getImageByUrl: rewriteImageUrl } = require('data/dataseed/dataseed')
 const { enabled: dataseedEnabled } = require('config').dataseed
+const convertImageUrl = require('./lib/convert_image_url')
 
 const sanitization = {
   url: {}
@@ -16,10 +18,15 @@ module.exports = (req, res) => {
 }
 
 const convertUrl = async ({ url }) => {
-  // If dataseed is disabled, we simply return the same url,
-  // instead of converting it to an image hash
-  if (!dataseedEnabled) return { url, converted: false }
-  const data = await getImageByUrl(url)
-  data.converted = true
+  const originalUrl = url
+  if (dataseedEnabled) {
+    const res = await rewriteImageUrl(url)
+    url = res.url
+  }
+  if (!_.isUrl(url)) {
+    throw error_.new('invalid image url', 400, { url, originalUrl })
+  }
+  const data = await convertImageUrl(url)
+  _.log({ originalUrl, cleanedUrl: url, ...data }, 'convert url')
   return data
 }

server/controllers/images/lib/containers.js
@@ -1,9 +1,7 @@
 const images_ = require('lib/images')
 const putImage = require('./put_image')
 
-const containerPutImage = (container, fnName) => fileData => {
-  const { id, path } = fileData
-
+const containerPutImage = (container, fnName) => ({ id = 0, path }) => {
   return images_[fnName](path)
   .then(() => images_.getHashFilename(path))
   .then(filename => putImage(container, path, id, filename))

server/controllers/images/lib/convert_image_url.js
@@ -1,0 +1,30 @@
+const _ = require('builders/utils')
+const error_ = require('lib/error/error')
+const db = require('db/couchdb/base')('images')
+const importImage = require('./import_image')
+
+module.exports = sourceImageUrl => importAndAddImage(sourceImageUrl)
+
+const importAndAddImage = async sourceImageUrl => {
+  const { url } = await importImage(sourceImageUrl)
+  const hash = url.split('/').slice(-1)[0]
+
+  if (!_.isImageHash(hash)) {
+    throw error_.new('invalid hash', 500, { sourceImageUrl, hash })
+  }
+
+  await saveImageSource(sourceImageUrl, hash)
+
+  return { url, hash }
+}
+
+const saveImageSource = async (sourceImageUrl, imageHash) => {
+  await db.update(imageHash, doc => {
+    doc.sources = doc.sources || []
+    doc.sources.push(sourceImageUrl)
+    doc.sources = _.uniq(doc.sources)
+    doc.updated = Date.now()
+    return doc
+  }, { createIfMissing: true })
+  _.log(sourceImageUrl, `adding source for ${imageHash}`)
+}

server/controllers/images/lib/download_file.js
@@ -1,0 +1,55 @@
+const fetch = require('node-fetch')
+const AbortController = require('abort-controller')
+const error_ = require('lib/error/error')
+const { encodeURL } = require('builders/utils')
+const fs = require('fs')
+const isValidImageContentType = require('./is_valid_image_content_type')
+const oneMB = Math.pow(1024, 2)
+
+module.exports = async (url, path) => {
+  url = encodeURL(url)
+  const controller = new AbortController()
+  const response = await fetch(url, { timeout: 30000, signal: controller.signal })
+  validateResponse(response, controller, url, path)
+  return saveFile(response.body, path)
+}
+
+const validateResponse = (response, controller, url, path) => {
+  let { status: statusCode, statusText } = response
+  let { 'content-type': contentType, 'content-length': contentLength } = response.headers.raw()
+  contentType = contentType.join(';')
+
+  if (contentLength != null) contentLength = parseInt(contentLength)
+
+  let errMessage
+  if (statusCode >= 400) {
+    errMessage = `Remote response: ${statusCode} ${statusText}`
+  } else if (!contentLength) {
+    errMessage = 'content-length is missing'
+    statusCode = 400
+  } else if (contentLength > (10 * oneMB)) {
+    errMessage = `image is too large: ${contentLength}`
+    statusCode = 400
+  } else if (contentLength < 1000) {
+    errMessage = `image is too small: ${contentLength}`
+    statusCode = 400
+  } else if (!isValidImageContentType(contentType)) {
+    errMessage = `invalid image content-type: ${contentType}`
+    statusCode = 400
+  }
+
+  if (errMessage != null) {
+    controller.abort()
+    statusCode = statusCode || 400
+    throw error_.new(errMessage, statusCode, { url, path })
+  }
+}
+
+const saveFile = (bodyStream, path) => new Promise((resolve, reject) => {
+  const destinationFile = fs.createWriteStream(path)
+
+  bodyStream
+  .pipe(destinationFile)
+  .on('close', resolve)
+  .on('error', reject)
+})

server/controllers/images/lib/import_image.js
@@ -1,0 +1,11 @@
+const { md5 } = require('lib/crypto')
+const downloadFile = require('./download_file')
+const { putImage } = require('controllers/images/lib/containers').entities
+
+module.exports = async sourceUrl => {
+  const tmpFilename = md5(sourceUrl)
+  const tmpPath = `/tmp/${tmpFilename}`
+
+  await downloadFile(sourceUrl, tmpPath)
+  return putImage({ path: tmpPath })
+}

server/controllers/images/lib/is_valid_image_content_type.js
@@ -1,0 +1,9 @@
+// Accepting image/*
+// Accepting application/octet-stream (known case: media storages 'dumb' content type)
+const imageContentTypePattern = /^(image\/[\w+-.]+|application\/octet-stream)$/
+
+module.exports = contentType => {
+  if (!contentType) return false
+  contentType = contentType.replace(';charset=UTF-8', '')
+  return imageContentTypePattern.test(contentType)
+}

server/db/couchdb/list.js
@@ -15,5 +15,6 @@ module.exports = {
   patches: [ 'patches' ],
   notifications: [ 'notifications' ],
   tasks: [ 'tasks' ],
-  shelves: [ 'shelves' ]
+  shelves: [ 'shelves' ],
+  images: [ 'images' ],
 }

server/lib/crypto.js
@@ -27,7 +27,16 @@ const createHexHash = algo => input => {
   .digest('hex')
 }
 
+const createHexHashFromStream = algo => stream => new Promise((resolve, reject) => {
+  const sum = crypto.createHash(algo)
+  return stream
+  .on('data', sum.update.bind(sum))
+  .on('end', () => resolve(sum.digest('hex')))
+  .on('error', reject)
+})
+
 exports.sha1 = createHexHash('sha1')
 exports.md5 = createHexHash('md5')
+exports.sha1FromStream = createHexHashFromStream('sha1')
 
 exports.getRandomBytesBuffer = length => crypto.randomBytes(length)

commit e1cb20255e255f21011663e42c453d7b44b60926

resolver: fix image url convertion
was broken by 2a394c4a

server/controllers/entities/lib/resolver/create_entity_from_seed.js
@@ -2,7 +2,8 @@ const _ = require('builders/utils')
 const properties = require('../properties/properties_values_constraints')
 const createInvEntity = require('../create_inv_entity')
 const isbn_ = require('lib/isbn/isbn')
-const { getImageByUrl, getImageByIsbn } = require('data/dataseed/dataseed')
+const { getImageByIsbn } = require('data/dataseed/dataseed')
+const convertAndCleanupImageUrl = require('controllers/images/lib/convert_and_cleanup_image_url')
 
 const createAuthor = (userId, batchId) => author => {
   if (author.uri != null) return author
@@ -24,7 +25,8 @@ const createWork = (userId, batchId, authors) => work => {
 const createEdition = async (edition, works, userId, batchId, enrich) => {
   if (edition.uri != null) return
 
-  const { isbn, image: imageUrl } = edition
+  const { isbn } = edition
+  let { image: imageUrl } = edition
   const worksUris = _.compact(_.map(works, 'uri'))
   const claims = {}
 
@@ -45,11 +47,13 @@ const createEdition = async (edition, works, userId, batchId, enrich) => {
   // garantee that an edition shall not have label
   edition.labels = {}
 
+  if (!imageUrl && enrich === true && isbn != null) {
+    const { url } = await getImageByIsbn(isbn)
+    imageUrl = url
+  }
+
   if (imageUrl) {
-    const { url: imageHash } = await getImageByUrl(imageUrl)
-    if (imageHash) claims['invp:P2'] = [ imageHash ]
-  } else if (enrich === true && isbn != null) {
-    const { url: imageHash } = await getImageByIsbn(isbn)
+    const { hash: imageHash } = await convertAndCleanupImageUrl(imageUrl)
     if (imageHash) claims['invp:P2'] = [ imageHash ]
   }
 

server/controllers/entities/lib/resolver/update_resolved_entry.js
@@ -1,6 +1,6 @@
 const _ = require('builders/utils')
 const entities_ = require('../entities')
-const { getImageByUrl } = require('data/dataseed/dataseed')
+const convertAndCleanupImageUrl = require('controllers/images/lib/convert_and_cleanup_image_url')
 
 module.exports = ({ reqUserId, batchId }) => async entry => {
   const { edition, works, authors } = entry
@@ -56,7 +56,7 @@ const addImageClaim = async (entity, imageUrl, newClaims) => {
   if (!imageUrl) return
   const imageClaims = entity.claims['invp:P2']
   if (imageClaims) return
-  const { url: imageHash } = await getImageByUrl(imageUrl)
+  const { hash: imageHash } = await convertAndCleanupImageUrl(imageUrl)
   newClaims['invp:P2'] = [ imageHash ]
 }
 

server/controllers/images/convert_url.js
@@ -1,10 +1,7 @@
-const _ = require('builders/utils')
 const responses_ = require('lib/responses')
 const error_ = require('lib/error/error')
 const sanitize = require('lib/sanitize/sanitize')
-const { getImageByUrl: rewriteImageUrl } = require('data/dataseed/dataseed')
-const { enabled: dataseedEnabled } = require('config').dataseed
-const convertImageUrl = require('./lib/convert_image_url')
+const convertAndCleanupImageUrl = require('./lib/convert_and_cleanup_image_url')
 
 const sanitization = {
   url: {}
@@ -12,21 +9,7 @@ const sanitization = {
 
 module.exports = (req, res) => {
   sanitize(req, res, sanitization)
-  .then(convertUrl)
+  .then(({ url }) => convertAndCleanupImageUrl(url))
   .then(responses_.Send(res))
   .catch(error_.Handler(req, res))
-}
-
-const convertUrl = async ({ url }) => {
-  const originalUrl = url
-  if (dataseedEnabled) {
-    const res = await rewriteImageUrl(url)
-    url = res.url
-  }
-  if (!_.isUrl(url)) {
-    throw error_.new('invalid image url', 400, { url, originalUrl })
-  }
-  const data = await convertImageUrl(url)
-  _.log({ originalUrl, cleanedUrl: url, ...data }, 'convert url')
-  return data
 }

server/controllers/images/lib/convert_and_cleanup_image_url.js
@@ -1,0 +1,19 @@
+const _ = require('builders/utils')
+const error_ = require('lib/error/error')
+const { cleanupImageUrl } = require('data/dataseed/dataseed')
+const { enabled: dataseedEnabled } = require('config').dataseed
+const convertImageUrl = require('./convert_image_url')
+
+module.exports = async url => {
+  const originalUrl = url
+  if (dataseedEnabled) {
+    const res = await cleanupImageUrl(url)
+    url = res.url
+  }
+  if (!_.isUrl(url)) {
+    throw error_.new('invalid image url', 400, { url, originalUrl })
+  }
+  const data = await convertImageUrl(url)
+  _.log({ originalUrl, cleanedUrl: url, ...data }, 'convert url')
+  return data
+}

server/data/dataseed/dataseed.js
@@ -35,8 +35,7 @@ module.exports = {
     return requests_.get(url, reqOptions)
   },
 
-  // Converts the url to an image hash
-  getImageByUrl: imageUrl => {
+  cleanupImageUrl: imageUrl => {
     const url = _.buildPath(`${host}/images`, { url: encodeURIComponent(imageUrl) })
     return requests_.get(url, reqOptions)
   }