614f1b471ac15e776c8a32702d6b3a5da1a57506
maxlath <k@maxlath.eu>
2021-05-05 11:00:34 +0200

    activity_summary: last_network_books: async/await refactor

--- server/lib/emails/activity_summary/last_network_books.js
+++ server/lib/emails/activity_summary/last_network_books.js
@@ -2,29 +2,25 @@ const _ = require('builders/utils')
 const items_ = require('controllers/items/lib/items')
 const getItemsByAccessLevel = require('controllers/items/lib/get_by_access_level')
 const relations_ = require('controllers/relations/lib/queries')
-const promises_ = require('lib/promises')
 const user_ = require('controllers/user/lib/user')
 const { getLastItems, formatData, embedUsersData, getHighlightedItems } = require('./last_books_helpers')
 
-module.exports = (userId, lang, limitDate = 0) => {
-  // Get network ids
-  return relations_.getUserFriendsAndCoGroupsMembers(userId)
-  // Get last network items available for a transaction
-  .then(getItemsByAccessLevel.network)
-  .then(promises_.map(items_.serializeData))
-  .then(getLastItems.bind(null, limitDate))
-  .then(extractHighlightedItems(lang))
-  .catch(_.ErrorRethrow('last network items'))
+module.exports = async (userId, lang, limitDate = 0) => {
+  const networkUsersIds = await relations_.getUserFriendsAndCoGroupsMembers(userId)
+  const networkItems = await getItemsByAccessLevel.network(networkUsersIds)
+  const serializedNetworkItems = await Promise.all(networkItems.map(items_.serializeData))
+  const lastNetworkItems = getLastItems(limitDate, serializedNetworkItems)
+  return extractHighlightedItems(lastNetworkItems, lang)
 }
 
-const extractHighlightedItems = lang => lastItems => {
-  const highlighted = getHighlightedItems(lastItems, 10)
-  return attachUsersData(highlighted)
-  .then(formatData.bind(null, lastItems, 'network', lang))
+const extractHighlightedItems = async (lastItems, lang) => {
+  let highlightedItems = getHighlightedItems(lastItems, 10)
+  highlightedItems = await attachUsersData(highlightedItems)
+  return formatData(lastItems, 'network', lang, highlightedItems)
 }
 
-const attachUsersData = (items, lang) => {
+const attachUsersData = async items => {
   const usersIds = _.uniq(items.map(_.property('owner')))
-  return user_.byIds(usersIds)
-  .then(embedUsersData.bind(null, items))
+  const users = await user_.byIds(usersIds)
+  return embedUsersData(items, users)
 }
