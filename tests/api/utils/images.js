67098e90608a08026f54876e3fdca1b608e8af79
maxlath <k@maxlath.eu>
2021-05-05 13:42:47 +0200

    tests: utils: images: rename uploadSomeImage -> importSomeImage
    to make room for a uploadSomeImage function that actually uses the upload endpoint
    (and not the convert-url endpoint)

--- tests/api/utils/images.js
+++ tests/api/utils/images.js
@@ -1,14 +1,22 @@
 require('should')
-const { mediaStorage } = require('config')
+const CONFIG = require('config')
+const { mediaStorage } = CONFIG
+const host = CONFIG.fullPublicHost()
 mediaStorage.mode.should.equal('local')
 const localStorageFolder = mediaStorage.local.folder()
-const { authReq } = require('./utils')
 const randomString = require('lib/utils/random_string')
 const fs = require('fs')
+const downloadFile = require('controllers/images/lib/download_file')
+const { authReq, getUser } = require('../utils/utils')
+const { createReadStream } = require('fs')
+const fetch = require('node-fetch')
+const FormData = require('form-data')
 
 const uploadImageFromUrl = async ({ container, url }) => {
   return authReq('post', '/api/images?action=convert-url', { container, url })
 }
+
+const someImageUrl = () => `https://via.placeholder.com/1000x1000.jpg?text=${randomString(10)}`
 
 module.exports = {
   getImageDataUrl: async url => {
@@ -17,15 +25,32 @@ module.exports = {
     return dataUrl
   },
 
-  uploadSomeImage: async ({ container }) => {
-    const text = randomString(10)
+  importSomeImage: async ({ container }) => {
     return uploadImageFromUrl({
       container,
-      url: `https://via.placeholder.com/1000x1000.jpg?text=${text}`,
+      url: someImageUrl()
     })
   },
 
-  localContainerHasImage: ({ container, hash }) => {
+  uploadSomeImage: async ({ container }) => {
+    const tmpPath = `/tmp/${randomString(10)}.jpg`
+    await downloadFile(someImageUrl(), tmpPath)
+    const { cookie } = await getUser()
+    const form = new FormData()
+    form.append('somefile', createReadStream(tmpPath))
+    const res = await fetch(`${host}/api/images?action=upload&container=${container}`, {
+      method: 'post',
+      headers: { cookie, ...form.getHeaders() },
+      body: form,
+    })
+    return {
+      statusCode: res.status,
+      body: await res.json()
+    }
+  },
+
+  localContainerHasImage: ({ container, hash, url }) => {
+    if (url) hash = url.split('/')[3]
     const localImagePath = `${localStorageFolder}/${container}/${hash}`
     try {
       // Using the sync method so that consumers can chain this function with ".shoud.be.true()"
