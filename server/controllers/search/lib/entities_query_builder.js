90caf250575ef33076f0d244460611c2ec9e4180
maxlath <k@maxlath.eu>
2021-05-18 15:35:02 +0200

    entities_query_builder: naming

--- server/controllers/search/lib/entities_query_builder.js
+++ server/controllers/search/lib/entities_query_builder.js
@@ -16,7 +16,7 @@ module.exports = params => {
     { terms: { type: types } }
   ]
 
-  if (claim) filters.push(...getClaimFilter(claim))
+  if (claim) filters.push(...getClaimFilters(claim))
 
   if (!search) minScore = 0
 
@@ -106,7 +106,7 @@ const entitiesFields = (userLang, exact) => {
   return fields
 }
 
-const getClaimFilter = claimParameter => {
+const getClaimFilters = claimParameter => {
   return claimParameter
   .split(' ')
   .map(andCondition => {
