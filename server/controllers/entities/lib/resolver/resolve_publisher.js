160f3c59f72283ed062c9a1f9b9692cf554cd971
maxlath <k@maxlath.eu>
2021-05-18 18:21:32 +0200

    resolve_publisher: more strict matching

--- server/controllers/entities/lib/resolver/resolve_publisher.js
+++ server/controllers/entities/lib/resolver/resolve_publisher.js
@@ -3,16 +3,22 @@ const parseIsbn = require('lib/isbn/parse')
 const reverseClaims = require('controllers/entities/lib/reverse_claims')
 const getEntitiesList = require('controllers/entities/lib/get_entities_list')
 const leven = require('leven')
+// Arbitrary tolerance threshold to accept, for instance, accents differences in publishers names
+const maximumNameDistance = 3
 
 const resolvePublisher = async (isbn, publisherLabel) => {
   const { publisherPrefix } = parseIsbn(isbn)
   const claims = await reverseClaims({ property: 'wdt:P3035', value: publisherPrefix })
   if (claims.length === 0) return
-  const possiblePublishers = await getEntitiesList(claims)
-  const publisher = possiblePublishers
-    .map(getPublisherClosestTerm(publisherLabel))
-    .sort(byDistance)[0]
-  return publisher.uri
+  const isbnPrefixPublishers = await getEntitiesList(claims)
+  const matchingPublishers = getMatchingPublishers(publisherLabel, isbnPrefixPublishers)
+  if (matchingPublishers.length === 1) return matchingPublishers[0].uri
+}
+
+const getMatchingPublishers = (publisherLabel, isbnPrefixPublishers) => {
+  return isbnPrefixPublishers
+  .map(getPublisherClosestTerm(publisherLabel))
+  .filter(publisher => publisher.distance <= maximumNameDistance)
 }
 
 const getPublisherClosestTerm = publisherLabel => entity => {
