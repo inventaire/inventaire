2824d2eae1d1304d6de3286e8edee0868d97f73c
maxlath <k@maxlath.eu>
2021-05-20 22:00:56 +0200

    get_bne_author_works_titles: fix protocol

--- server/data/bne/get_bne_author_works_titles.js
+++ server/data/bne/get_bne_author_works_titles.js
@@ -1,6 +1,6 @@
 const fetchExternalAuthorWorksTitles = require('data/lib/fetch_external_author_works_titles')
 
-const endpoint = 'http://datos.bne.es/sparql'
+const endpoint = 'https://datos.bne.es/sparql'
 
 const getQuery = bneId => `SELECT ?work ?title WHERE {
 <http://datos.bne.es/resource/${bneId}> <http://datos.bne.es/def/OP5001> ?work .
