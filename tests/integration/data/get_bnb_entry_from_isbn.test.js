d726aece916df1a4fd0db979da43489114a3d4ac
maxlath <k@maxlath.eu>
2021-06-11 17:02:29 +0200

    tests: get_bnb_entry_from_isbn: fix

--- tests/integration/data/get_bnb_entry_from_isbn.test.js
+++ tests/integration/data/get_bnb_entry_from_isbn.test.js
@@ -5,7 +5,7 @@ describe('get_bnb_entry_from_isbn', () => {
   it('should get an entry from a known ISBN', async () => {
     const entry = await getBnbEntryFromIsbn('9781903765234')
     entry.edition.claims['wdt:P5199'].should.equal('012938091')
-    entry.edition.claims['wdt:407'].should.equal('Q1860')
+    entry.edition.claims['wdt:P407'].should.equal('wd:Q1860')
     entry.authors[0].claims['wdt:P5361'].should.equal('OBrienJim1950-')
     entry.authors[0].claims['wdt:P213'].should.equal('0000000067818190')
     entry.authors[0].claims['wdt:P214'].should.equal('75843307')
