f96681a72fc1dcaf76d0572b757255c6b48aae36
maxlath <k@maxlath.eu>
2021-06-11 16:23:30 +0200

    harmonize entry/seed naming

---
+++ server/data/dataseed/get_authorities_aggregated_entry.js
@@ -0,0 +1,52 @@
+const _ = require('builders/utils')
+const { isNotEmpty, objLength } = require('lib/utils/base')
+
+const authorities = {
+  bnb: require('data/bnb/get_bnb_entry_from_isbn'),
+  bne: require('data/bne/get_bne_entry_from_isbn'),
+  bnf: require('data/bnf/get_bnf_entry_from_isbn'),
+}
+
+const authoritiesNames = Object.keys(authorities)
+
+module.exports = async isbn => {
+  const entries = await Promise.all(authoritiesNames.map(wrap(isbn)))
+  // TODO: aggregate resolved data, rather than just returning the best one
+  return getBestEntry(entries)
+}
+
+const wrap = isbn => async name => {
+  try {
+    return await authorities[name](isbn)
+  } catch (err) {
+    _.error(err, `${name} entry error`)
+  }
+}
+
+const getBestEntry = entries => {
+  entries = entries.filter(isNotEmpty)
+  if (entries.length === 0) return
+  if (entries.length === 1) return entries[0]
+
+  const scoredEntriesByResolvedEntities = entries
+    .map(getEntryResolutionScore)
+    .sort(byScore)
+
+  return scoredEntriesByResolvedEntities[0].entry
+}
+
+const getEntryResolutionScore = entry => {
+  let score = 0
+  if (entry.edition?.claims) score += objLength(entry.edition.claims)
+  entry.works?.forEach(work => {
+    if (work.uri) score += 50
+    if (work.claims) score += objLength(work.claims)
+  })
+  entry.authors?.forEach(author => {
+    if (author.uri) score += 50
+    if (author.claims) score += objLength(author.claims)
+  })
+  return { entry, score }
+}
+
+const byScore = (a, b) => b.score - a.score
