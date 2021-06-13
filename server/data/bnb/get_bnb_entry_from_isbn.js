0b1b3a23958f1bdea871cbb1737c2c38b52f3365
maxlath <k@maxlath.eu>
2021-06-13 08:51:05 +0200

    get_*_entry_from_isbn: remove caching
    it was convenient for development, but those requests are typically not run more than once,
    as they will be used to autocreate entities, making unlikely any cache hit

--- server/data/bnb/get_bnb_entry_from_isbn.js
+++ server/data/bnb/get_bnb_entry_from_isbn.js
@@ -2,20 +2,19 @@ const qs = require('querystring')
 const parseIsbn = require('lib/isbn/parse')
 const requests_ = require('lib/requests')
 const { sparqlResults: simplifySparqlResults } = require('wikidata-sdk').simplify
-const cache_ = require('lib/cache')
-const { hashCode } = require('lib/utils/base')
 const { parseSameasMatches } = require('data/lib/external_ids')
 const wdIdByIso6393Code = require('wikidata-lang/mappings/wd_id_by_iso_639_3_code.json')
 const { buildEntryFromFormattedRows } = require('data/lib/build_entry_from_formatted_rows')
 const { prefixifyWd } = require('controllers/entities/lib/prefix')
 
+const headers = {
+  'content-type': 'application/sparql-query',
+  accept: 'application/sparql-results+json',
+}
+
 module.exports = async isbn => {
-  const queryHash = hashCode(getQuery(isbn)) + Math.random()
-  const key = `bnb-seed:${isbn}:${queryHash}`
-  const response = await cache_.get({
-    key,
-    fn: get.bind(null, isbn)
-  })
+  const url = `https://bnb.data.bl.uk/sparql?format=json&query=${getQuery(isbn)}`
+  const response = await requests_.get(url, { headers })
   const simplifiedResults = simplifySparqlResults(response)
   const { bindings: rawResults } = response.results
   const rows = await Promise.all(simplifiedResults.map((result, i) => {
@@ -25,16 +24,6 @@ module.exports = async isbn => {
 }
 
 const getSourceId = entity => entity.claims?.['wdt:P5199'] || entity.claims?.['wdt:P5361']
-
-const headers = {
-  'content-type': 'application/sparql-query',
-  accept: 'application/sparql-results+json',
-}
-
-const get = async isbn => {
-  const url = `https://bnb.data.bl.uk/sparql?format=json&query=${getQuery(isbn)}`
-  return requests_.get(url, { headers })
-}
 
 const getQuery = isbn => {
   const isbnData = parseIsbn(isbn)
