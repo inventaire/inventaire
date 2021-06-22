521c038125d85da817490d582f92750e7d1eece6
jums <jums@inventaire.io>
2021-06-22 19:45:15 +0200

    deboilertificate actor and inbox endpoints

--- server/controllers/activitypub/actor.js
+++ server/controllers/activitypub/actor.js
@@ -1,17 +1,16 @@
-const error_ = require('lib/error/error')
 const getActor = require('controllers/activitypub/lib/get_actor')
-const { sanitizeAsync } = require('lib/sanitize/sanitize')
 
 const sanitization = {
   name: {}
 }
 
-module.exports = async (req, res) => {
-  sanitizeAsync(req, res, sanitization)
-  .then(params => {
-    const { name } = params
-    return getActor(name)
-  })
-  .then(res.json.bind(res))
-  .catch(error_.Handler(req, res))
+const controller = async params => {
+  const { name } = params
+  return getActor(name)
+}
+
+module.exports = {
+  sanitization,
+  controller,
+  track: [ 'activitypub', 'actor' ]
 }
