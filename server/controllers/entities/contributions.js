e5993f4bf0c6fc274e9de44bd3eeaee9bd34d566
jums <jums@inventaire.io>
2021-07-01 00:18:10 +0200

    merge activity endpoint in contributions controller
    the idea is to get rid of the confustion between a contributions
    activity and an activity-stream activity
    
    it makes the choice of having a more complex contributions controller,
    but the semantics makes more sense (an activity was actually a
    contribution)

--- server/controllers/entities/contributions.js
+++ server/controllers/entities/contributions.js
@@ -4,10 +4,26 @@ const patches_ = require('./lib/patches')
 const sanitization = {
   user: { optional: true },
   limit: { default: 100, max: 1000 },
-  offset: { default: 0 }
+  offset: { default: 0 },
+  users: {
+    generic: 'boolean',
+    optional: true
+  },
+  period: {
+    generic: 'positiveInteger',
+    optional: true
+  }
 }
 
-const controller = ({ userId, limit, offset }) => {
+const controller = async ({ userId, limit, offset, period, users }) => {
+  if (users) {
+    if (period != null) {
+      return patches_.getContributionsFromLastDay(period)
+    } else {
+      const contributions = await patches_.getGlobalContributions()
+      return { contributions }
+    }
+  }
   if (userId != null) {
     return patches_.byUserId(userId, limit, offset)
   } else {
