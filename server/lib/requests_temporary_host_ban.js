6784cd0977f8b6c3e0c18cefeb9637d8c3274ace
jums <jums@inventaire.io>
2021-07-15 12:23:10 +0200

    do not temporary ban localhost hosts

--- server/lib/requests_temporary_host_ban.js
+++ server/lib/requests_temporary_host_ban.js
@@ -30,6 +30,7 @@ const restoreNonExpiredBans = data => {
 }
 
 const throwIfTemporarilyBanned = host => {
+  if (host.startsWith('localhost')) return
   const hostBanData = banData[host]
   if (hostBanData != null && Date.now() < hostBanData.expire) {
     throw error_.new(`temporary ban: ${host}`, 500, { host, hostBanData })
