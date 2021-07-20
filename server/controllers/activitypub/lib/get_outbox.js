4627983e8370fe16a7803d49f47eb01976a682c7
jums <jums@inventaire.io>
2021-07-20 16:40:24 +0200

    add paginagted outbox
    for public items only
    
    if query params do not have an offset, return a generic outbox (with a first key URL to query)
    if there is an offset, then return an OrderedCollectionPage,
      with orderedItems list (of type 'Create', and an object with some
      html content)
    
    also add some rudimendarity paging: orderedItems elements do not have an id
    offset will be used to query a specific activity

--- server/controllers/activitypub/lib/get_outbox.js
+++ server/controllers/activitypub/lib/get_outbox.js
@@ -1,21 +1,29 @@
-const error_ = require('lib/error/error')
-const user_ = require('controllers/user/lib/user')
-const couch_ = require('lib/couch')
+const items_ = require('controllers/items/lib/items')
 const makeUrl = require('./make_url')
+const formatActivities = require('./format_activities')
 
-module.exports = async name => {
-  const user = await user_.byUsername(name)
-  .then(couch_.firstDoc)
-  if (!user) throw error_.notFound(name)
-  if (!user.fediversable) throw error_.new('user is not on the fediverse', 404, name)
-  const { username } = user
-  const outboxUrl = makeUrl({ params: { action: 'outbox', name: username } })
-  const totalItems = user.snapshot.public['items:count']
-
-  return {
-    id: outboxUrl,
-    type: 'OrderedCollectionPage',
-    totalItems,
-    first: `${outboxUrl}&page=1`
+module.exports = async (params, targetUser) => {
+  const { name, offset } = params
+  const fullOutboxUrl = makeUrl({ params: { action: 'outbox', name } })
+  const totalPublicItems = targetUser.snapshot.public['items:count']
+  const baseOutbox = {
+    id: fullOutboxUrl,
+    type: 'OrderedCollection',
+    totalItems: totalPublicItems,
+    first: `${fullOutboxUrl}&offset=0`
   }
+  if (offset != null && offset >= 0) {
+    return buildPaginatedOutbox(targetUser, offset, baseOutbox)
+  }
+  return baseOutbox
+}
+
+const buildPaginatedOutbox = async (targetUser, offset, outbox) => {
+  const { id: fullOutboxUrl } = outbox
+  outbox.type = 'OrderedCollectionPage'
+  outbox.partOf = fullOutboxUrl
+  outbox.next = `${fullOutboxUrl}&offset=${offset + 10}`
+  const items = await items_.byOwnerAndListing(targetUser._id, 'public')
+  outbox.orderedItems = await formatActivities(items, targetUser)
+  return outbox
 }
