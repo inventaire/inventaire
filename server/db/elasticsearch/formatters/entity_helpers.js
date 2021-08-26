6d5cf4cb5aba5dde980d777ae227c40ae7fc4cca
maxlath <k@maxlath.eu>
2021-08-26 09:52:25 +0200

    indexation: entity_helpers: remove unused functions

--- server/db/elasticsearch/formatters/entity_helpers.js
+++ server/db/elasticsearch/formatters/entity_helpers.js
@@ -1,16 +1,4 @@
-const wdk = require('wikidata-sdk')
-
-const helpers = module.exports = {
-  removeTrailingComma: line => line.replace(/,$/, ''),
-
-  isJsonLine: line => (line[0] === '{') && (line.slice(-1)[0] === '}'),
-
-  logCount: counter => {
-    // only log once every 100 calls to avoid slowing the process
-    // by just logging things
-    if ((counter % 100) === 0) return console.log(counter)
-  },
-
+module.exports = {
   getEntityId: entity => {
     // Working around differences in entities formatting between
     // - Wikidata entities from a dump or from Wikidata API (entity.id)
@@ -20,19 +8,4 @@ const helpers = module.exports = {
     if (entity.uri) return entity.uri.split(':')[1]
     else return entity.id || entity._id
   },
-
-  getEntityUri: entity => {
-    // Wikidata and Inventaire entities coming from the Inventaire API
-    // have a URI already defined
-    if (entity.uri) return entity.uri
-    // Wikidata entities coming from a Wikidata dump or the Wikidata API
-    // have only an id defined
-    const id = helpers.getEntityId(entity)
-    if (!id) throw new Error(`couldn't find entity URI: ${JSON.stringify(entity)}`)
-
-    if (wdk.isItemId(id)) return 'wd:' + id
-    else return 'inv:' + id
-  },
-
-  getEntityDomain: entity => helpers.getEntityUri(entity).split(':')[0]
 }
