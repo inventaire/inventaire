65ffc90303908eb1c46e6a45d589af84079d0346
maxlath <k@maxlath.eu>
2021-09-11 18:20:26 +0200

    webfinger: add support for entities actors

--- server/controllers/activitypub/webfinger.js
+++ server/controllers/activitypub/webfinger.js
@@ -3,17 +3,23 @@ const error_ = require('lib/error/error')
 const user_ = require('controllers/user/lib/user')
 const { ControllerWrapper } = require('lib/controller_wrapper')
 const makeUrl = require('controllers/activitypub/lib/make_url')
+const { isEntityUri, isUsername } = require('lib/boolean_validations')
+const getEntityByUri = require('controllers/entities/lib/get_entity_by_uri')
 
 const sanitization = {
   resource: {}
 }
 
 const controller = async ({ resource }) => {
-  const username = getActorParts(resource)[0]
-  const user = await user_.findOneByUsername(username)
-  if (!user) throw error_.new('not found', 404, resource)
-  if (!user.fediversable) throw error_.new('user is not on the fediverse', 404, resource)
-  return formatWebfinger(user.stableUsername)
+  const name = getActorName(resource)
+  if (isEntityUri(name)) {
+    const entity = await getEntityByUri({ uri: name })
+    if (entity) return formatWebfinger(entity.uri)
+  } else if (isUsername(name)) {
+    const user = await user_.findOneByUsername(name)
+    if (user.fediversable) return formatWebfinger(user.stableUsername)
+  }
+  throw error_.notFound({ resource, name })
 }
 
 module.exports = {
@@ -24,16 +30,16 @@ module.exports = {
   })
 }
 
-const getActorParts = resource => {
+const getActorName = resource => {
   const actorWithHost = resource.substr(5)
-  return actorWithHost.split('@')
+  return actorWithHost.split('@')[0]
 }
 
-const formatWebfinger = stableUsername => {
-  const actorUrl = makeUrl({ params: { action: 'actor', name: stableUsername } })
+const formatWebfinger = name => {
+  const actorUrl = makeUrl({ params: { action: 'actor', name } })
 
   return {
-    subject: `acct:${stableUsername}@${publicHost}`,
+    subject: `acct:${name}@${publicHost}`,
     aliases: [ actorUrl ],
     links: [
       {
