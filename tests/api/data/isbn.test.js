f267d065305bde1b08f49cce8ef8d187e651e59a
maxlath <k@maxlath.eu>
2021-09-13 15:58:09 +0200

    tests: data: async/await refactor

--- tests/api/data/isbn.test.js
+++ tests/api/data/isbn.test.js
@@ -1,38 +1,32 @@
 require('should')
-const { publicReq, undesiredRes } = require('../utils/utils')
+const { publicReq, shouldNotBeCalled } = require('../utils/utils')
 const endpoint = '/api/data?action=isbn'
 const randomString = require('lib/utils/random_string')
 const { generateIsbn13 } = require('../fixtures/entities')
 
 describe('data:isbn', () => {
-  it('should reject requests without isbn', done => {
-    publicReq('get', endpoint)
-    .then(undesiredRes(done))
+  it('should reject requests without isbn', async () => {
+    await publicReq('get', endpoint)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('missing parameter in query: isbn')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject requests with invalid isbn', done => {
-    publicReq('get', `${endpoint}&isbn=${randomString(10)}`)
-    .then(undesiredRes(done))
+  it('should reject requests with invalid isbn', async () => {
+    await publicReq('get', `${endpoint}&isbn=${randomString(10)}`)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.startWith('invalid isbn')
-      done()
     })
-    .catch(done)
   })
 
-  it('should return isbn information', done => {
+  it('should return isbn information', async () => {
     const isbn = generateIsbn13()
-    publicReq('get', `${endpoint}&isbn=${isbn}`)
+    await publicReq('get', `${endpoint}&isbn=${isbn}`)
     .then(res => {
       res.isValid.should.be.true()
       res.isbn13.should.equal(isbn)
-      done()
     })
-    .catch(done)
   })
 })
--- tests/api/data/wp_extract.test.js
+++ tests/api/data/wp_extract.test.js
@@ -1,46 +1,38 @@
 const should = require('should')
-const { publicReq, undesiredRes } = require('../utils/utils')
+const { publicReq, shouldNotBeCalled } = require('../utils/utils')
 const endpoint = '/api/data?action=wp-extract'
 const randomString = require('lib/utils/random_string')
 
 describe('wikipedia:extract', () => {
-  it('should reject without title', done => {
-    publicReq('get', endpoint)
-    .then(undesiredRes(done))
+  it('should reject without title', async () => {
+    await publicReq('get', endpoint)
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('missing parameter in query: title')
-      done()
     })
-    .catch(done)
   })
 
-  it('should return empty response when no page is found', done => {
+  it('should return empty response when no page is found', async () => {
     const randomTitle = randomString(15)
-    publicReq('get', `${endpoint}&title=${randomTitle}`)
+    await publicReq('get', `${endpoint}&title=${randomTitle}`)
     .then(res => {
       should(res.extract).not.be.ok()
-      done()
     })
-    .catch(done)
   })
 
-  it('should get english Wikipedia article by default', done => {
-    publicReq('get', `${endpoint}&title=Gilbert_Simondon`)
+  it('should get english Wikipedia article by default', async () => {
+    await publicReq('get', `${endpoint}&title=Gilbert_Simondon`)
     .then(res => {
       res.url.should.equal('https://en.wikipedia.org/wiki/Gilbert_Simondon')
       res.extract.should.startWith('Gilbert Simondon')
-      done()
     })
-    .catch(done)
   })
 
-  it('should get an extract in the appropriate language', done => {
-    publicReq('get', `${endpoint}&lang=fr&title=Gilbert_Simondon`)
+  it('should get an extract in the appropriate language', async () => {
+    await publicReq('get', `${endpoint}&lang=fr&title=Gilbert_Simondon`)
     .then(res => {
       res.url.should.equal('https://fr.wikipedia.org/wiki/Gilbert_Simondon')
       res.extract.should.startWith('Gilbert Simondon')
-      done()
     })
-    .catch(done)
   })
 })
