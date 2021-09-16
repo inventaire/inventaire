71f790c1554f3f6ecca700bfb6a10f65bbea2006
jums <jums@inventaire.io>
2021-09-16 17:44:01 +0200

    rename activity -> activityDoc

--- server/controllers/activitypub/outbox.js
+++ server/controllers/activitypub/outbox.js
@@ -3,7 +3,7 @@ const error_ = require('lib/error/error')
 const user_ = require('controllers/user/lib/user')
 const { byUsername } = require('controllers/activitypub/lib/activities')
 const makeUrl = require('./lib/make_url')
-const formatActivities = require('./lib/format_activities')
+const formatActivitiesDocs = require('./lib/format_activities_docs')
 
 const sanitization = {
   name: {},
@@ -26,10 +26,10 @@ module.exports = {
   track: [ 'activitypub', 'outbox' ]
 }
 
-const getOutbox = async (params, targetUser) => {
-  const { name, offset } = params
-  const fullOutboxUrl = makeUrl({ params: { action: 'outbox', name } })
-  const totalPublicItems = targetUser.snapshot.public['items:count']
+const getOutbox = async (params, user) => {
+  const { offset } = params
+  const fullOutboxUrl = makeUrl({ params: { action: 'outbox', name: user.stableUsername } })
+  const totalPublicItems = user.snapshot.public['items:count']
   const baseOutbox = {
     id: fullOutboxUrl,
     type: 'OrderedCollection',
@@ -37,19 +37,19 @@ const getOutbox = async (params, targetUser) => {
     first: `${fullOutboxUrl}&offset=0`
   }
   if (offset != null && offset >= 0) {
-    return buildPaginatedOutbox(targetUser, offset, baseOutbox)
+    return buildPaginatedOutbox(user, offset, baseOutbox)
   }
   return baseOutbox
 }
 
-const buildPaginatedOutbox = async (targetUser, offset, outbox) => {
+const buildPaginatedOutbox = async (user, offset, outbox) => {
   const { id: fullOutboxUrl } = outbox
   outbox.type = 'OrderedCollectionPage'
   outbox.partOf = fullOutboxUrl
   outbox.next = `${fullOutboxUrl}&offset=${offset + 10}`
-  const { username } = targetUser
-  const rawActivities = await byUsername(username)
-  const activities = await formatActivities(rawActivities, targetUser)
+  const { username } = user
+  const activitiesDocs = await byUsername(username)
+  const activities = await formatActivitiesDocs(activitiesDocs, user)
   _.extend(outbox, {
     totalItems: activities.length,
     orderedItems: activities
