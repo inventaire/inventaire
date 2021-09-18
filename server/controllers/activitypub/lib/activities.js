0793111a65fbf9989b12d79f270c91259e66e562
maxlath <k@maxlath.eu>
2021-09-18 21:28:12 +0200

    activities byUsername: remove obsolete filter

--- server/controllers/activitypub/lib/activities.js
+++ server/controllers/activitypub/lib/activities.js
@@ -1,7 +1,5 @@
 const Activity = require('models/activity')
 const db = require('db/couchdb/base')('activities')
-const { expired } = require('lib/time')
-const { activitiesDebounceTime } = require('config')
 
 // activities are stored as documents in order to allow
 // grouping items (and entities) under the same activity, this
@@ -17,12 +15,7 @@ module.exports = {
   },
   byUsername: async username => {
     return db.viewByKey('byUsername', username)
-    .then(docs => docs.filter(oldEnough))
   },
   byId: db.get,
   byIds: db.byIds,
 }
-
-const oldEnough = doc => !isRecent(doc.updated)
-
-const isRecent = date => date && !expired(date, activitiesDebounceTime)
