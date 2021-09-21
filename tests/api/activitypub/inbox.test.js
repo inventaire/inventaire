a5a4b86a598eea709726aa125134f9ff1a47c2d9
maxlath <k@maxlath.eu>
2021-09-21 17:28:41 +0200

    activitypub inbox: implement undo follow
    Co-authored-by: Jums <jums@inventaire.io>

--- tests/api/activitypub/inbox.test.js
+++ tests/api/activitypub/inbox.test.js
@@ -107,7 +107,7 @@ describe('activitypub:post:inbox', () => {
   })
 })
 
-describe('activitypub:follow', () => {
+describe('activitypub:Follow', () => {
   it('should create a Follow activity', async () => {
     const emitterUser = await createRemoteActivityPubServerUser()
     const { username } = await createUserOnFediverse()
@@ -156,5 +156,68 @@ describe('activitypub:follow', () => {
     activity.type.should.equal('Accept')
     activity.actor.should.equal(actorUrl)
     activity.object.should.startWith(remoteHost)
+  })
+})
+
+describe('activitypub:Undo', () => {
+  it('should repond ok if already not following', async () => {
+    const { username } = await createUserOnFediverse()
+    const inboxUrl = makeUrl({ params: { action: 'inbox', name: username } })
+    const emitterUser = await createRemoteActivityPubServerUser()
+    const res = await signedReq({
+      emitterUser,
+      url: inboxUrl,
+      type: 'Undo',
+      object: 'foo',
+    })
+    res.statusCode.should.equal(200)
+  })
+
+  it('should reject to undo from another actor', async () => {
+    const { username } = await createUserOnFediverse()
+    const actorUrl = makeUrl({ params: { action: 'actor', name: username } })
+    const inboxUrl = makeUrl({ params: { action: 'inbox', name: username } })
+    const emitterUserA = await createRemoteActivityPubServerUser()
+    const emitterUserB = await createRemoteActivityPubServerUser()
+    await signedReq({
+      emitterUser: emitterUserA,
+      object: actorUrl,
+      url: inboxUrl
+    })
+    const activities = await getFollowActivitiesByObject(username)
+    const activity = activities[0]
+    await signedReq({
+      emitterUser: emitterUserB,
+      url: inboxUrl,
+      type: 'Undo',
+      object: activity.externalId,
+    })
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.statusCode.should.equal(403)
+    })
+  })
+
+  it('should delete activity', async () => {
+    const { username } = await createUserOnFediverse()
+    const actorUrl = makeUrl({ params: { action: 'actor', name: username } })
+    const inboxUrl = makeUrl({ params: { action: 'inbox', name: username } })
+    const emitterUser = await createRemoteActivityPubServerUser()
+    await signedReq({
+      emitterUser,
+      object: actorUrl,
+      url: inboxUrl
+    })
+    const activities = await getFollowActivitiesByObject(username)
+    const activity = activities[0]
+    await signedReq({
+      emitterUser,
+      url: inboxUrl,
+      type: 'Undo',
+      object: activity.externalId,
+    })
+    await wait(500)
+    const activities2 = await getFollowActivitiesByObject(username)
+    activities2.length.should.equal(0)
   })
 })
