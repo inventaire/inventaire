ef9d413e9dc3ba0e586bc4581051f4ff5283dd04
maxlath <k@maxlath.eu>
2021-09-28 16:47:22 +0200

    tests: activity: reorganize

--- tests/api/activitypub/activity.test.js
+++ tests/api/activitypub/activity.test.js
@@ -9,24 +9,26 @@ const { makeUrl } = require('../utils/activitypub')
 const requests_ = require('lib/requests')
 
 describe('activity', () => {
-  it('should get an activity', async () => {
-    const user = await createUser({ fediversable: true })
-    const { username } = user
-    const actorUrl = makeUrl({ params: { action: 'actor', name: username } })
-    const inboxUrl = makeUrl({ params: { action: 'inbox', name: username } })
-    const { remoteHost, remoteUsername } = await signedReq({ object: actorUrl, url: inboxUrl, type: 'Follow' })
-    await createItem(user)
-    await wait(debounceTime + 500)
-    await publicReq('get', `/api/activitypub?action=outbox&name=${username}&offset=0`)
-    const { inbox } = await requests_.get(`${remoteHost}/inbox_inspection?username=${remoteUsername}`)
-    const [ createActivity ] = inbox
-    const activityUrl = createActivity.object.id
-    const activityId = new URL(activityUrl).searchParams.get('id')
-    const activity = await publicReq('get', `/api/activitypub?action=activity&id=${activityId}`)
-    activity.id.should.equal(`${activityUrl}#create`)
-    activity.type.should.equal('Create')
-    activity.object.id.should.equal(activityUrl)
-    activity.object.type.should.equal('Note')
-    activity.object.content.should.be.a.String()
+  describe('users', () => {
+    it('should get an activity', async () => {
+      const user = await createUser({ fediversable: true })
+      const { username } = user
+      const actorUrl = makeUrl({ params: { action: 'actor', name: username } })
+      const inboxUrl = makeUrl({ params: { action: 'inbox', name: username } })
+      const { remoteHost, remoteUsername } = await signedReq({ object: actorUrl, url: inboxUrl, type: 'Follow' })
+      await createItem(user)
+      await wait(debounceTime + 500)
+      await publicReq('get', `/api/activitypub?action=outbox&name=${username}&offset=0`)
+      const { inbox } = await requests_.get(`${remoteHost}/inbox_inspection?username=${remoteUsername}`)
+      const [ createActivity ] = inbox
+      const activityUrl = createActivity.object.id
+      const activityId = new URL(activityUrl).searchParams.get('id')
+      const activity = await publicReq('get', `/api/activitypub?action=activity&id=${activityId}`)
+      activity.id.should.equal(`${activityUrl}#create`)
+      activity.type.should.equal('Create')
+      activity.object.id.should.equal(activityUrl)
+      activity.object.type.should.equal('Note')
+      activity.object.content.should.be.a.String()
+    })
   })
 })
