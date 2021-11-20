d6cb2e2d9a707f768b90ae81ae58205daa1232a5
maxlath <k@maxlath.eu>
2021-11-20 22:43:53 +0100

    images: rename download_file -> download_image

--- server/controllers/images/lib/import_image.js
+++ server/controllers/images/lib/import_image.js
@@ -1,6 +1,6 @@
 const error_ = require('lib/error/error')
 const { md5 } = require('lib/crypto')
-const downloadFile = require('./download_file')
+const downloadImage = require('./download_image')
 const { containers } = require('controllers/images/lib/containers')
 
 module.exports = async (container, sourceUrl) => {
@@ -11,6 +11,6 @@ module.exports = async (container, sourceUrl) => {
   const tmpFilename = md5(sourceUrl)
   const tmpPath = `/tmp/${tmpFilename}`
 
-  await downloadFile(sourceUrl, tmpPath)
+  await downloadImage(sourceUrl, tmpPath)
   return containers[container].putImage({ path: tmpPath })
 }
