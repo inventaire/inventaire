cb902cfe188df755c84ffc5e3f0c4f1cd2003eac
maxlath <k@maxlath.eu>
2021-11-25 23:29:32 +0100

    tests: update_password: async/await refactor

--- tests/api/auth/update_password.test.js
+++ tests/api/auth/update_password.test.js
@@ -1,114 +1,94 @@
 require('should')
 const { authReq, customAuthReq, getUser, getUserGetter } = require('../utils/utils')
 const randomString = require('lib/utils/random_string')
-const { Wait } = require('lib/promises')
+const { wait } = require('lib/promises')
 const endpoint = '/api/auth?action=update-password'
 const { createUser, createUserEmail } = require('../fixtures/users')
 const { BasicUpdater } = require('lib/doc_updates')
+const { shouldNotBeCalled } = require('root/tests/unit/utils')
 const db = require('db/couchdb/base')('users')
 
 describe('auth:update-password', () => {
-  it('should reject short new password', done => {
-    getUser()
-    .then(user => {
-      return authReq('post', endpoint, {
-        email: user.email,
-        'new-password': randomString(7)
-      })
-      .catch(err => {
-        err.body.status_verbose.should.startWith('invalid new-password')
-        done()
-      })
+  it('should reject short new password', async () => {
+    const user = await getUser()
+    await authReq('post', endpoint, {
+      email: user.email,
+      'new-password': randomString(7)
     })
-    .catch(done)
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.body.status_verbose.should.startWith('invalid new-password')
+    })
   })
 
-  it('should reject short old password', done => {
-    authReq('post', endpoint, {
+  it('should reject short old password', async () => {
+    await authReq('post', endpoint, {
       email: createUserEmail(),
       'new-password': randomString(20),
       'current-password': randomString(7)
     })
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.startWith('invalid current-password')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject if current password is incorrect', done => {
-    createUser()
-    .then(user => {
-      return authReq('post', endpoint, {
-        email: user.email,
-        'current-password': randomString(20),
-        'new-password': randomString(20)
-      })
+  it('should reject if current password is incorrect', async () => {
+    const user = await createUser()
+    await authReq('post', endpoint, {
+      email: user.email,
+      'current-password': randomString(20),
+      'new-password': randomString(20)
     })
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.startWith('invalid current-password')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject if reset password timestamp is invalid', done => {
+  it('should reject if reset password timestamp is invalid', async () => {
     const email = createUserEmail()
     const userPromise = getUserGetter(email)()
-    updateCustomUser(userPromise, 'resetPassword', 'invalid')
-    .then(() => {
-      return customAuthReq(userPromise, 'post', endpoint, {
-        email,
-        'new-password': randomString(20)
-      })
+    await updateCustomUser(userPromise, 'resetPassword', 'invalid')
+    await customAuthReq(userPromise, 'post', endpoint, {
+      email,
+      'new-password': randomString(20)
     })
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('invalid resetPassword timestamp')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reject if reset password timestamp is too old', done => {
+  it('should reject if reset password timestamp is too old', async () => {
     const email = createUserEmail()
     const userPromise = getUserGetter(email)()
-    updateCustomUser(userPromise, 'resetPassword', 1000)
-    .then(() => {
-      return customAuthReq(userPromise, 'post', endpoint, {
-        email,
-        'new-password': randomString(20)
-      })
+    await updateCustomUser(userPromise, 'resetPassword', 1000)
+    await customAuthReq(userPromise, 'post', endpoint, {
+      email,
+      'new-password': randomString(20)
     })
+    .then(shouldNotBeCalled)
     .catch(err => {
       err.body.status_verbose.should.equal('reset password timespan experied')
-      done()
     })
-    .catch(done)
   })
 
-  it('should reset password timestamp is recent', done => {
+  it('should reset password timestamp is recent', async () => {
     const email = createUserEmail()
     const userPromise = getUserGetter(email)()
     const recentTime = Date.now() - 1000
-    updateCustomUser(userPromise, 'resetPassword', recentTime)
-    .then(() => {
-      return customAuthReq(userPromise, 'post', endpoint, {
-        email,
-        'new-password': randomString(20)
-      })
+    await updateCustomUser(userPromise, 'resetPassword', recentTime)
+    const res = await customAuthReq(userPromise, 'post', endpoint, {
+      email,
+      'new-password': randomString(20)
     })
-    .then(res => {
-      res.ok.should.be.true()
-      done()
-    })
-    .catch(done)
+    res.ok.should.be.true()
   })
 })
 
-const updateCustomUser = (userPromise, userAttribute, value) => {
-  return userPromise
-  .then(user => {
-    db.update(user._id, BasicUpdater(userAttribute, value))
-  })
-  .then(Wait(100))
+const updateCustomUser = async (userPromise, userAttribute, value) => {
+  const user = await userPromise
+  await db.update(user._id, BasicUpdater(userAttribute, value))
+  await wait(100)
 }
