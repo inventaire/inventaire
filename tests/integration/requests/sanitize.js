17fe1496ca6a3f145dd0b3aea7eb263e7584c647
maxlath <k@maxlath.eu>
2021-11-27 12:09:03 +0100

    fixup! activitypub: sanitize the actor url before fetching the public key

--- tests/integration/requests/sanitize.js
+++ tests/integration/requests/sanitize.js
@@ -10,8 +10,16 @@ describe('requests:sanitize', () => {
     })
   })
 
-  it('should reject ip addresses with port', async () => {
+  it('should reject ipv4 addresses with port', async () => {
     await requests_.get('http://192.168.178.247:3006/someimage.jpg', { sanitize: true })
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.error_name.should.equal('invalid_url')
+    })
+  })
+
+  it('should reject ipv6 addresses with port', async () => {
+    await requests_.get('http://[::1]:3006/someimage.jpg', { sanitize: true })
     .then(shouldNotBeCalled)
     .catch(err => {
       err.error_name.should.equal('invalid_url')
@@ -20,6 +28,14 @@ describe('requests:sanitize', () => {
 
   it('should reject localhost', async () => {
     await requests_.get('http://localhost/someimage.jpg', { sanitize: true })
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.error_name.should.equal('invalid_url')
+    })
+  })
+
+  it('should reject localhost with port', async () => {
+    await requests_.get('http://localhost:3006/someimage.jpg', { sanitize: true })
     .then(shouldNotBeCalled)
     .catch(err => {
       err.error_name.should.equal('invalid_url')
