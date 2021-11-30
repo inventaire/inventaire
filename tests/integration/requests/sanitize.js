e8316a05ea4a144a8cbd96767b3feeff92747c0e
maxlath <k@maxlath.eu>
2021-11-30 18:22:39 +0100

    activitypub: sanitize the actor url before fetching a public key or posting an activity
    to prevent server-side request forgery

---
+++ tests/integration/requests/sanitize.js
@@ -0,0 +1,13 @@
+const CONFIG = require('config')
+const requests_ = require('lib/requests')
+const { shouldNotBeCalled } = require('tests/api/utils/utils')
+
+describe('requests:sanitize', () => {
+  it('should reject private URLs', async () => {
+    await requests_.get(CONFIG.elasticsearch.host, { sanitize: true })
+    .then(shouldNotBeCalled)
+    .catch(err => {
+      err.error_name.should.equal('invalid_url')
+    })
+  })
+})
