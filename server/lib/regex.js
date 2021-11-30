8a188b6466a0523f7bc9b8821562f42ac6a31e73
maxlath <k@maxlath.eu>
2021-11-30 14:17:33 +0100

    boolean_validations: simplify isUrl implementation

--- server/lib/regex.js
+++ server/lib/regex.js
@@ -1,16 +1,5 @@
 // Keep in sync with client/lib/regex
 
-// Adapted from http://stackoverflow.com/a/14582229/3324977
-const urlPattern = '^(https?:\\/\\/)' + // protocol
-  '(\\w+:\\w+@)?' + // auth?
-  '((([a-z\\d]([a-z\\d-_]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-  '((\\d{1,3}\\.){3}\\d{1,3}))|' + // OR ip (v4) address
-  '(localhost)' + // OR localhost
-  '(\\:\\d+)?' + // port?
-  '(\\/[-a-z\\d%_.~+]*)*' + // path
-  '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string?
-  '(\\#[-a-z\\d_]*)?$' // fragment?
-
 module.exports = {
   AssetImg: /^\/img\/assets\/\w/,
   CouchUuid: /^[0-9a-f]{32}$/,
@@ -32,7 +21,6 @@ module.exports = {
   Sha1: /^[0-9a-f]{40}$/,
   SimpleDay: /^-?([1-9]{1}[0-9]{0,3}|0)(-\d{2})?(-\d{2})?$/,
   StrictlyPositiveInteger: /^[1-9]\d*$/,
-  Url: new RegExp(urlPattern, 'i'),
   UserImg: /^\/img\/users\/[0-9a-f]{40}$/,
   // all 1 letter strings are reserved for the application
   Username: /^\w{2,20}$/,
