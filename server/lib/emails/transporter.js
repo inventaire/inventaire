76eaca8638ddf1a73688d8b216daa586b4259e09
maxlath <k@maxlath.eu>
2021-12-08 17:27:29 +0100

    sendMail: log debug url

--- server/lib/emails/transporter.js
+++ server/lib/emails/transporter.js
@@ -1,11 +1,11 @@
 const CONFIG = require('config')
 const __ = CONFIG.universalPath
 const _ = require('builders/utils')
-
-const nodemailer = require('nodemailer')
+const { createTransport, getTestMessageUrl } = require('nodemailer')
 const hbs = require('nodemailer-express-handlebars')
 const handlebarsHelpers = require('./handlebars_helpers')
 const viewsPath = __.path('lib', 'emails/views')
+const debugMode = CONFIG.mailer.nodemailer.host === 'smtp.ethereal.email'
 
 const options = {
   viewEngine: {
@@ -22,7 +22,7 @@ const options = {
 const { defaultFrom, nodemailer: nodemailerOptions } = CONFIG.mailer
 const defaults = { from: defaultFrom }
 
-const transporter = nodemailer.createTransport(nodemailerOptions, defaults)
+const transporter = createTransport(nodemailerOptions, defaults)
 
 transporter.use('compile', hbs(options))
 
@@ -30,8 +30,9 @@ module.exports = {
   sendMail: async email => {
     const { template, subject } = email
     try {
-      const res = await transporter.sendMail(email)
-      _.success(res, `email sent (template="${template}" subject="${subject}")`)
+      const info = await transporter.sendMail(email)
+      const inspectUrl = debugMode ? ` inspect="${getTestMessageUrl(info)}"` : ''
+      _.success(info, `email sent (template="${template}" subject="${subject}"${inspectUrl})`)
     } catch (err) {
       _.error(err, `email error (template="${template}" subject="${subject}")`)
       return _.warn(email, 'associated email')
