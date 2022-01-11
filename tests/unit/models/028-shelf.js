4a06e71f26321e571cb51137bd1d7d8e7c3aa961
maxlath <k@maxlath.eu>
2022-01-11 14:51:18 +0100

    tests: shelves: refactor without done function

--- tests/unit/models/028-shelf.js
+++ tests/unit/models/028-shelf.js
@@ -21,7 +21,7 @@ const extendShelf = data => Object.assign({}, validShelf, data)
 
 describe('shelf model', () => {
   describe('create', () => {
-    it('should return an object', done => {
+    it('should return an object', () => {
       const shelf = create(validShelf)
       shelf.should.be.an.Object()
       shelf.name.should.equal(fakeName)
@@ -29,88 +29,77 @@ describe('shelf model', () => {
       shelf.owner.should.equal(someUserId)
       shelf.listing.should.equal('private')
       shelf.created.should.be.a.Number()
-      done()
     })
 
-    it('should throw when passed an invalid attributes', done => {
+    it('should throw when passed an invalid attributes', () => {
       const shelf = extendShelf({ authors: 'Abel Paz' })
       const creator = () => Shelf.create(shelf)
       creator.should.throw()
-      done()
     })
 
     describe('mandatory attributes', () => {
-      it('should throw on missing owner', done => {
+      it('should throw on missing owner', () => {
         const invalidShelf = _.cloneDeep(validShelf)
         delete invalidShelf.owner
         const creator = () => Shelf.create(invalidShelf)
         creator.should.throw()
-        done()
       })
 
-      it('should throw on missing name', done => {
+      it('should throw on missing name', () => {
         const invalidShelf = _.cloneDeep(validShelf)
         delete invalidShelf.name
         const creator = () => Shelf.create(invalidShelf)
         creator.should.throw()
-        done()
       })
     })
 
     describe('listing', () => {
-      it('should use a default listing value', done => {
+      it('should use a default listing value', () => {
         const shelf = create(extendShelf({ listing: null }))
         shelf.listing.should.equal('private')
-        done()
       })
 
-      it('should override a bad listing with default value', done => {
+      it('should override a bad listing with default value', () => {
         const shelf = () => create(extendShelf({ listing: 'notalist' }))
         shelf.should.throw()
-        done()
       })
     })
 
     describe('owner', () => {
-      it('should return an object with an owner', done => {
+      it('should return an object with an owner', () => {
         const shelf = create(validShelf)
         shelf.owner.should.equal(someUserId)
-        done()
       })
     })
 
     describe('created', () => {
-      it('should return an object with a created time', done => {
+      it('should return an object with a created time', () => {
         const shelf = create(validShelf)
         expired(shelf.created, 100).should.be.false()
-        done()
       })
     })
   })
 
   describe('update', () => {
-    it('should update when passing a valid attribute', done => {
+    it('should update when passing a valid attribute', () => {
       const shelf = create(validShelf)
       const updateAttributesData = { listing: 'public' }
       const res = update(shelf, updateAttributesData, someUserId)
       res.listing.should.equal('public')
-      done()
     })
 
-    it('should throw when passing an invalid attribute', done => {
+    it('should throw when passing an invalid attribute', () => {
       const doc = create(validShelf)
       const updateAttributesData = { foo: '123' }
       const updater = () => update(doc, updateAttributesData, someUserId)
       updater.should.throw('invalid attribute: foo')
-      done()
     })
 
-    it('should throw when passing an invalid attribute value', done => {
+    it('should throw when passing an invalid attribute value', () => {
       const doc = create(validShelf)
       const updateAttributesData = { listing: 'kikken' }
       const updater = () => update(doc, updateAttributesData, someUserId)
       updater.should.throw('invalid listing: kikken')
-      done()
     })
   })
 })
