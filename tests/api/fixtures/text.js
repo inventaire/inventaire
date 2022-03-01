913b9a12d14bf2d840bf4e76ac2e1163588e29aa
maxlath <k@maxlath.eu>
2022-03-01 21:38:33 +0100

    tests: increase text fixtures sample
    to decrease risks of having a many results when searching any word
    of that sample, which makes some tests fail. Typically tests
    relying on relatives terms (ex: "should find a user item by serie")
    can only pass if not too many results can be found we those words
    in direct labels or aliases
    
    * 'words' file was generated by taking a sample from https://en.wikipedia.org/w/api.php?format=json&action=query&titles=Philosophy&prop=extracts&explaintext=true
    * 'names' file was generated by requesting first names from Wikidata:
       wd query -p P31 -o Q202444 -a --limit 2000 | jq '.[] | .subject.label' -cr | grep -E '^[A-Z][a-z]+$' | shuffle | head -n 1000 | sort

--- tests/api/fixtures/text.js
+++ tests/api/fixtures/text.js
@@ -1,54 +1,15 @@
 const _ = require('builders/utils')
+const __ = require('config').universalPath
 const randomString = require('lib/utils/random_string')
-
-const lorem = [
-  'comitatus', 'bos', 'volubilis', 'cibus',
-  'caelestis', 'supplanto', 'aqua', 'tergum',
-  'trepide', 'tergeo', 'tam', 'curiositas',
-  'acervus', 'beneficium', 'sui', 'conor',
-  'audax', 'verus', 'celo', 'tibi',
-  'antepono', 'bellum', 'deludo', 'adinventitias',
-  'commodo', 'trado', 'talus', 'casso',
-  'aegrus', 'cimentarius', 'sortitus', 'vinculum',
-  'congregatio', 'aiunt', 'conicio', 'terga',
-  'vomer', 'creo', 'utrimque', 'dedico',
-  'tristis', 'antiquus', 'cicuta', 'defetiscor',
-  'timidus', 'sollers', 'vir', 'arbustum',
-  'tracto', 'talio', 'strues', 'comedo',
-  'dens', 'combibo', 'stabilis', 'thesis',
-  'clam', 'vilis', 'delinquo', 'absque',
-  'desidero', 'auxilium', 'substantia', 'sumo',
-  'avarus', 'cerno', 'deleo', 'abduco',
-  'conventus', 'cum', 'vinum', 'bibo',
-  'defungo', 'colligo', 'arceo', 'charisma',
-  'adipiscor', 'corroboro', 'triumphus', 'ascisco',
-  'turpis', 'ustulo', 'vicinus', 'supellex',
-  'carus', 'cunctatio', 'sollicito', 'creptio',
-  'amissio', 'cognatus', 'cupiditas', 'armarium',
-  'utpote', 'sto', 'curis', 'ancilla',
-  'tenuis', 'cursus', 'termes', 'sublime'
-]
-
+const { readFileSync } = require('fs')
+const getFixtureWords = filename => {
+  return readFileSync(__.path('tests', `api/fixtures/${filename}`))
+  .toString()
+  .split('\n')
+}
+const lorem = getFixtureWords('words')
 // no name should match on regex /\W/
-const firstNames = [
-  'Nikki', 'Rey', 'Miracle', 'Amanda', 'Betty', 'Stephania',
-  'Julie', 'Davonte', 'Lilian', 'Kaylin', 'Rocio', 'Nash',
-  'Phyllis', 'Simeon', 'Elsie', 'Ben', 'Aubrey', 'Jaime',
-  'Carolyne', 'Sam', 'Edmond', 'Abraham', 'Francisco', 'Queen',
-  'Sheila', 'Lora', 'Jed', 'Torrance', 'Lamont', 'Cleta',
-  'Petra', 'Renee', 'Sadie', 'Ernesto', 'Earlene', 'Allison',
-  'Constance', 'Ellie', 'Henri', 'Maegan', 'Janis', 'Oren',
-  'Otis', 'Jaleel', 'Federico', 'Katarina', 'Isom', 'Brenda',
-  'Opal', 'Orrin', 'Rene', 'Murl', 'Lela', 'Alisa',
-  'Cecil', 'Zaria', 'Richie', 'Aric', 'Vivienne', 'Zora',
-  'Chelsey', 'Frida', 'Rachael', 'Enrique', 'Eldridge', 'Cullen',
-  'Estevan', 'Gilberto', 'Virginie', 'Paige', 'Noemi', 'Nellie',
-  'Matt', 'Marilie', 'Alexanne', 'Abdiel', 'Maya', 'Timmothy',
-  'Bette', 'Adan', 'Charley', 'Kaden', 'Shirley', 'Vivien',
-  'Maximo', 'Liliane', 'Morris', 'Rickey', 'Vincenza', 'Ivy',
-  'Elwin', 'Okey', 'Lincoln', 'Harvey', 'Amir', 'Callie',
-  'Jerod', 'Mercedes', 'Hilma', 'Aiyana'
-]
+const firstNames = getFixtureWords('names')
 
 const capitalize = word => word[0].toUpperCase() + word.slice(1).toLowerCase()
 
