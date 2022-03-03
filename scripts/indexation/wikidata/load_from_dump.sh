6e8e05f8e82e052d157c10826a73e27f6cf95343
maxlath <k@maxlath.eu>
2022-03-03 10:27:18 +0100

    indexation: wikidata: load_from_dump: split pipeline in two steps
    which can be done on separated machines.
    
    By doing the first step on another, less ressource constrained, machine,
    the prod server is left with a having to deal with a much smaller file:
    from the >100GB gzipped dump file to a less than 2GB filtered simplified gzipped file

--- scripts/indexation/wikidata/load_from_dump.sh
+++ scripts/indexation/wikidata/load_from_dump.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # Dependencies:
+# - an access to the Elasticsearch wikidata index to load
 # - CouchDB entities database should exist and be populated to be able to fetch works' editions
 # - LevelDB popularities should have been transfered
 
@@ -8,21 +9,31 @@
 # This implementation reflects the current state of the getEntityType function (server/controllers/entities/lib/get_entity_type.js)
 # which only lets in entities that have a P31 or P279 value identified in server/lib/wikidata/aliases
 
+indexed_types_ids=$(mktemp)
+
 node --print "require('module-alias/register') ; JSON.stringify(require('./server/lib/wikidata/aliases'))" |
   # Get uris used as P31 from indexed types
   jq '.typesAliases | [ .humans, .series, .works, .genres, .publishers, .collections, .movements  ] | flatten[]' -cr |
   # Get the id, wrapped between double quotes
-  sed -E 's/wd:(Q.*)/"\1"/' > indexed_types_ids
+  sed -E 's/wd:(Q.*)/"\1"/' > "$indexed_types_ids"
 
+# This pipeline can be done on any machine. For performance reasons,
+# it may be done on a machine that is not the production server as it
+# does not need access to either the prod CouchDB, LevelDB, or Elasticsearch
 curl --silent https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.gz |
   gzip --decompress |
   # Prefilter to keep only entities that refer to a valid P31 value in some way
-  grep --file indexed_types_ids |
+  grep --file "$indexed_types_ids" |
   # Drop end-of-line comma to produce valid ndjson
   sed 's/,$//' |
-  ./scripts/indexation/load.js wikidata
+  ndjson-apply ./scripts/indexation/wikidata/format_dump_entity.js |
+  gzip --best > entities.filtered.simplified.ndjson.gz
 
-rm indexed_types_ids
+# This pipeline should ideally be done on a machine with access to
+# - the Elasticsearch wikidata index
+# - the entities-prod database (CouchDB)
+# - popularity scores (LevelDB)
+gzip -d < entities.filtered.simplified.ndjson.gz | ./scripts/indexation/load.js wikidata
 
 # ## Tip ##
 # If importing the dump fails at some point, rather than re-starting from 0,
