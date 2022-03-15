aaaa5a9c5b493385e403a94be8c025300d2328e2
maxlath <k@maxlath.eu>
2022-03-15 12:59:37 +0100

    add couchdb:preload-design-docs-changes script

--- server/db/couchdb/init.js
+++ server/db/couchdb/init.js
@@ -9,22 +9,26 @@ import initHardCodedDocuments from './init_hard_coded_documents.js'
 const dbBaseUrl = CONFIG.db.getOrigin()
 const formattedList = []
 
+const setPreloadSuffix = preload => designDocsName => preload ? `${designDocsName}_preload` : designDocsName
 const setJsExtension = filename => `${filename}.js`
-
-// Adapt the list to couch-init2 needs
-for (const dbName in databases) {
-  const designDocsNames = databases[dbName]
-  formattedList.push({
-    // Adding a suffix if needed
-    name: CONFIG.db.name(dbName),
-    designDocs: designDocsNames.map(setJsExtension),
-  })
-}
 
 const designDocFolder = absolutePath('db', 'couchdb/design_docs')
 
-const init = async () => {
+const init = async ({ preload }) => {
   try {
+    // Adapt the list to couch-init2 needs
+    for (const dbName in databases) {
+      const designDocsNames = databases[dbName]
+        .map(setPreloadSuffix(preload))
+        .map(setJsExtension)
+
+      formattedList.push({
+        // Adding a suffix if needed
+        name: CONFIG.db.name(dbName),
+        designDocs: designDocsNames,
+      })
+    }
+
     const res = await couchInit(dbBaseUrl, formattedList, designDocFolder)
     if (_.objLength(res.operations) !== 0) log(res, 'couch init')
     await initHardCodedDocuments()
@@ -41,8 +45,8 @@ const init = async () => {
 
 let _waitForCouchInit
 
-export async function waitForCouchInit () {
+export async function waitForCouchInit (options = {}) {
   // Return the same promises to all consumers
-  _waitForCouchInit = _waitForCouchInit || init()
+  _waitForCouchInit = _waitForCouchInit || init(options)
   return _waitForCouchInit
 }
