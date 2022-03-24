9f58245175adc5ac9da7ab78c137c6598544f4bd
maxlath <k@maxlath.eu>
2022-03-24 19:34:53 +0100

    waitForCPUsLoadToBeBelow: wait longer before retrying when the load is high

--- server/lib/os.js
+++ server/lib/os.js
@@ -2,18 +2,22 @@ const { cpus, loadavg } = require('os')
 const { wait } = require('./promises')
 const assert_ = require('./utils/assert_types')
 const cpusCount = cpus().length
+const checkIntervalBase = 10000
 
 const getCPUsAverageLoad = () => {
   const [ last5MinutesAverageLoad ] = loadavg()
   return last5MinutesAverageLoad / cpusCount
 }
 
-const waitForCPUsLoadToBeBelow = async ({ threshold, checkInterval = 10000 }) => {
+const waitForCPUsLoadToBeBelow = async ({ threshold }) => {
   assert_.number(threshold)
-  assert_.number(checkInterval)
-  if (getCPUsAverageLoad() > threshold) {
-    await wait(checkInterval)
-    return waitForCPUsLoadToBeBelow({ threshold, checkInterval })
+  const load = getCPUsAverageLoad()
+  if (load > threshold) {
+    // The more the load is far from the threshold,
+    // the more we wait before retrying
+    const factor = (load / threshold) ** 3
+    await wait(checkIntervalBase * factor)
+    return waitForCPUsLoadToBeBelow({ threshold, checkIntervalBase })
   }
 }
 
