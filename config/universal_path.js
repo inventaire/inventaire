6e5aa78600a590339e2b154b6e68305ef0cb6099
maxlath <k@maxlath.eu>
2022-05-05 15:28:50 +0200

    universal_path: check arguments
    without using lib/assert as that would create circular dependencies

--- config/universal_path.js
+++ config/universal_path.js
@@ -5,6 +5,7 @@ require('module-alias/register')
 const { _moduleAliases } = require('../package.json')
 
 const path = (route, name) => {
+  if (!(route && name)) throw new Error(`needs route and name (got route: ${route} / name: ${name})`)
   const path = _moduleAliases[route]
   if (name != null) return `${path}/${name}`
   else return path
