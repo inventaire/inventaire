3e0be1d49150ac213ff554e101ba6f9a0e802961
jums <jums@inventaire.io>
2022-06-09 09:21:30 +0200

    remove list filter pagination
    no need to filter yet as no group visibility is possible yet

--- server/controllers/lists/by_creators.js
+++ server/controllers/lists/by_creators.js
@@ -1,5 +1,4 @@
 const _ = require('builders/utils')
-const { validFilters } = require('controllers/items/lib/queries_commons')
 const lists_ = require('controllers/lists/lib/lists')
 const filterVisibleDocs = require('lib/filter_visible_docs')
 const { Paginate } = require('controllers/items/lib/queries_commons')
@@ -8,15 +7,11 @@ const sanitization = {
   users: {},
   limit: { optional: true },
   offset: { optional: true },
-  filter: {
-    allowlist: validFilters,
-    optional: true
-  },
 }
-const controller = async ({ users, offset, limit, filter, reqUserId }) => {
+const controller = async ({ users, offset, limit, reqUserId }) => {
   const foundLists = await lists_.byCreators(users)
   const { items: authorizedLists } = await filterVisibleDocs(foundLists, reqUserId)
-    .then(Paginate({ offset, limit, filter }))
+    .then(Paginate({ offset, limit }))
   const lists = _.keyBy(authorizedLists, '_id')
   return { lists }
 }
