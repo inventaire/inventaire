f3968ee770d82b6804cd4bd9a337132880e095ae
maxlath <k@maxlath.eu>
2022-06-15 15:21:37 +0200

    tests: items: update: refactor

--- tests/api/items/update.test.js
+++ tests/api/items/update.test.js
@@ -1,15 +1,16 @@
 const CONFIG = require('config')
 const { wait } = require('lib/promises')
 require('should')
-const { authReq, authReqB, getUser, getUserB } = require('../utils/utils')
-const { newItemBase, CountChange } = require('./helpers')
+const { authReq, getUser, getUserB } = require('../utils/utils')
+const { CountChange } = require('./helpers')
 const { createShelf } = require('../fixtures/shelves')
 const debounceDelay = CONFIG.itemsCountDebounceTime + 500
 const { shouldNotBeCalled } = require('tests/api/utils/utils')
+const { createItem } = require('tests/api/fixtures/items')
 
 describe('items:update', () => {
   it('should reject invalid item id', async () => {
-    const item = await authReq('post', '/api/items', newItemBase())
+    const item = await createItem()
     item._id = 'invalidid'
     await authReq('put', '/api/items', item)
     .then(shouldNotBeCalled)
@@ -20,7 +21,7 @@ describe('items:update', () => {
   })
 
   it('should reject invalid entity', async () => {
-    const item = await authReq('post', '/api/items', newItemBase())
+    const item = await createItem()
     item.entity = 'inv:aliduri'
     await authReq('put', '/api/items', item)
     .then(shouldNotBeCalled)
@@ -31,7 +32,7 @@ describe('items:update', () => {
   })
 
   it('should update an item', async () => {
-    const item = await authReq('post', '/api/items', newItemBase())
+    const item = await createItem()
     const newTransaction = item.transaction = 'lending'
     const newDetails = item.details = 'hello'
     const updatedItem = await authReq('put', '/api/items', item)
@@ -40,7 +41,7 @@ describe('items:update', () => {
   })
 
   it('should not be able to update non updatable attributes', async () => {
-    const item = await authReq('post', '/api/items', newItemBase())
+    const item = await createItem()
     const { created: originalCreatedTimestamp } = item
     item.created = Date.now() - 10000
     const updatedItem = await authReq('put', '/api/items', item)
@@ -48,7 +49,7 @@ describe('items:update', () => {
   })
 
   it('should trigger an update of the users items counters', async () => {
-    const item = await authReq('post', '/api/items', newItemBase())
+    const item = await createItem(null, { visibility: [] })
     // Delay to let the time to the item counter to be updated
     await wait(debounceDelay)
     const userBefore = await getUser()
@@ -66,8 +67,8 @@ describe('items:update', () => {
   })
 
   it('should reject item from another owner', async () => {
-    const item = await authReqB('post', '/api/items', newItemBase())
-    item.visibility = [ 'public' ]
+    const item = await createItem(getUserB())
+    item.details = 'yo'
     await authReq('put', '/api/items', item)
     .then(shouldNotBeCalled)
     .catch(err => {
@@ -77,7 +78,7 @@ describe('items:update', () => {
   })
 
   it('should reject item with an invalid shelf', async () => {
-    const item = await authReq('post', '/api/items', newItemBase())
+    const item = await createItem()
     item.shelves = [ 'not a shelf id' ]
     await authReq('put', '/api/items', item)
     .then(shouldNotBeCalled)
@@ -88,7 +89,7 @@ describe('items:update', () => {
   })
 
   it('should reject item with a shelf from another owner', async () => {
-    const item = await authReq('post', '/api/items', newItemBase())
+    const item = await createItem()
     const { shelf } = await createShelf(getUserB())
     item.shelves = [ shelf._id ]
     await authReq('put', '/api/items', item)
