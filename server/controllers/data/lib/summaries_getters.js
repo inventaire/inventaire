01f7156f4fc51e9b988c68f1a5fbe2f4e2d27ae8
maxlath <k@maxlath.eu>
2022-06-28 18:00:57 +0200

    fetch summary from wdt:P268 claims on BNF SPARQL endpoint

--- server/controllers/data/lib/summaries_getters.js
+++ server/controllers/data/lib/summaries_getters.js
@@ -1,14 +1,34 @@
 const requests_ = require('lib/requests')
 const cache_ = require('lib/cache')
 const timeout = 10 * 1000
+const { fixedEncodeURIComponent } = require('lib/utils/url')
+const { sparqlResults: simplifySparqlResults } = require('wikidata-sdk').simplify
 
 const summaryGettersByClaimProperty = {
+  'wdt:P268': async claimValues => {
+    const id = claimValues[0]
+    const sparql = `SELECT * {
+      <http://data.bnf.fr/ark:/12148/cb${id}#about> <http://purl.org/dc/terms/abstract> ?summary .
+    }`
+    const source = 'wdt:P268'
+    const headers = { accept: '*/*' }
+    const url = `https://data.bnf.fr/sparql?default-graph-uri=&format=json&timeout=${timeout}&query=${fixedEncodeURIComponent(sparql)}`
+    const text = await cache_.get({
+      key: `summary:${source}:${id}`,
+      fn: async () => {
+        const response = await requests_.get(url, { headers, timeout })
+        const simplifiedResults = simplifySparqlResults(response)
+        return simplifiedResults[0]?.summary
+      }
+    })
+    if (text) return { source, text }
+  },
   'wdt:P648': async claimValues => {
-    const olId = claimValues[0]
-    const url = `https://openlibrary.org/works/${olId}.json`
+    const id = claimValues[0]
+    const url = `https://openlibrary.org/works/${id}.json`
     const source = 'wdt:P648'
     const text = await cache_.get({
-      key: `summary:${source}:${olId}`,
+      key: `summary:${source}:${id}`,
       fn: async () => {
         const { description } = await requests_.get(url, { timeout })
         if (!description) return
--- tests/api/data/summaries.test.js
+++ tests/api/data/summaries.test.js
@@ -1,5 +1,5 @@
-require('should')
-const { createWork } = require('tests/api/fixtures/entities')
+const should = require('should')
+const { createWork, createEdition } = require('tests/api/fixtures/entities')
 const { getByUri } = require('tests/api/utils/entities')
 const { publicReq, shouldNotBeCalled } = require('../utils/utils')
 const requests_ = require('lib/requests')
@@ -14,42 +14,71 @@ describe('summaries', () => {
     })
   })
 
-  it('should return summaries', async () => {
-    const work = await existsOrCreate({
-      claims: {
-        'wdt:P648': [ 'OL45883W' ]
+  describe('openlibrary', () => {
+    it('should return summaries', async () => {
+      const work = await existsOrCreate({
+        claims: {
+          'wdt:P648': [ 'OL45883W' ]
+        }
+      })
+      const { uri } = work
+      const res = await publicReq('get', `${endpoint}&uri=${uri}`)
+      res.summaries['wdt:P648'].text.startsWith('The main character')
+    })
+
+    it('should return empty summaries when no description is provided', async () => {
+      const olId = 'OL4104668W'
+      const work = await existsOrCreate({
+        claims: {
+          'wdt:P648': [ olId ]
+        }
+      })
+      const { uri } = work
+      const res = await publicReq('get', `${endpoint}&uri=${uri}`)
+      Object.keys(res.summaries).should.not.containEql('wdt:P648')
+
+      if (res.summaries['wdt:P648']) {
+        const url = `https://openlibrary.org/works/${olId}.json`
+        const { description } = await requests_.get(url, { timeout: 10 * 1000 })
+        if (description) {
+          throw new Error(`This test is obsolete: the Openlibrary response contains a description (${olId})`)
+        }
       }
     })
-    const { uri } = work
-    const res = await publicReq('get', `${endpoint}&uri=${uri}`)
-    res.summaries['wdt:P648'].text.startsWith('The main character')
   })
 
-  it('should return empty summaries when no description is provided', async () => {
-    const olId = 'OL4104668W'
-    const work = await existsOrCreate({
-      claims: {
-        'wdt:P648': [ olId ]
-      }
+  describe('bnf', () => {
+    it('should return summaries', async () => {
+      const edition = await existsOrCreate({
+        createFn: createEdition,
+        claims: {
+          'wdt:P268': [ '458412245' ]
+        }
+      })
+      const { uri } = edition
+      const res = await publicReq('get', `${endpoint}&uri=${uri}`)
+      res.summaries['wdt:P268'].text.startsWith("C'est au beau milieu de la steppe")
     })
-    const { uri } = work
-    const res = await publicReq('get', `${endpoint}&uri=${uri}`)
-    Object.keys(res.summaries).should.not.containEql('wdt:P648')
 
-    if (res.summaries['wdt:P648']) {
-      const url = `https://openlibrary.org/works/${olId}.json`
-      const { description } = await requests_.get(url, { timeout: 10 * 1000 })
-      if (description) {
-        throw new Error(`This test is obsolete: the Openlibrary response contains a description (${olId})`)
-      }
-    }
+    it('should not return a summary when none exists', async () => {
+      const invalidBnfId = '00004444z'
+      const edition = await existsOrCreate({
+        createFn: createEdition,
+        claims: {
+          'wdt:P268': [ invalidBnfId ]
+        }
+      })
+      const { uri } = edition
+      const res = await publicReq('get', `${endpoint}&uri=${uri}`)
+      should(res.summaries['wdt:P268']).not.be.ok()
+    })
   })
 })
 
-const existsOrCreate = async ({ claims }) => {
+const existsOrCreate = async ({ claims, createFn = createWork }) => {
   try {
-    const work = await createWork({ claims })
-    return work
+    const entity = await createFn({ claims })
+    return entity
   } catch (err) {
     const existingEntityUri = err.body.context.entity
     return getByUri(existingEntityUri)
