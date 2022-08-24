6a29f429dc8864eef9aa46fce12281a00e830b17
maxlath <k@maxlath.eu>
2022-08-24 18:14:35 +0200

    tests: wp_extract: refactor

--- tests/api/data/wp_extract.test.js
+++ tests/api/data/wp_extract.test.js
@@ -22,26 +22,20 @@ describe('wikipedia:extract', () => {
   })
 
   it('should get english Wikipedia article by default', async () => {
-    await publicReq('get', `${endpoint}&title=Gilbert_Simondon`)
-    .then(res => {
-      res.url.should.equal('https://en.wikipedia.org/wiki/Gilbert_Simondon')
-      res.extract.should.startWith('Gilbert Simondon')
-    })
+    const res = await publicReq('get', `${endpoint}&title=Gilbert_Simondon`)
+    res.url.should.equal('https://en.wikipedia.org/wiki/Gilbert_Simondon')
+    res.extract.should.startWith('Gilbert Simondon')
   })
 
   it('should get an extract in the appropriate language', async () => {
-    await publicReq('get', `${endpoint}&lang=fr&title=Gilbert_Simondon`)
-    .then(res => {
-      res.url.should.equal('https://fr.wikipedia.org/wiki/Gilbert_Simondon')
-      res.extract.should.startWith('Gilbert Simondon')
-    })
+    const res = await publicReq('get', `${endpoint}&lang=fr&title=Gilbert_Simondon`)
+    res.url.should.equal('https://fr.wikipedia.org/wiki/Gilbert_Simondon')
+    res.extract.should.startWith('Gilbert Simondon')
   })
 
   it("should correctly encode title's special characters", async () => {
     const title = encodeURIComponent("Qu'est-ce que la vie ?")
-    await publicReq('get', `${endpoint}&lang=fr&title=${title}`)
-    .then(res => {
-      res.url.should.equal('https://fr.wikipedia.org/wiki/Qu%27est-ce_que_la_vie_%3F')
-    })
+    const res = await publicReq('get', `${endpoint}&lang=fr&title=${title}`)
+    res.url.should.equal('https://fr.wikipedia.org/wiki/Qu%27est-ce_que_la_vie_%3F')
   })
 })
