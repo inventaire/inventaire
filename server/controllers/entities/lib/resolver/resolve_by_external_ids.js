d59331666ff70d6bd0a5e2cb24c0b073043be96d
maxlath <k@maxlath.eu>
2022-11-23 17:55:38 +0100

    resolve_by_external_ids: handle case where resolveExternalIds returns several results

--- server/controllers/entities/lib/resolver/resolve_by_external_ids.js
+++ server/controllers/entities/lib/resolver/resolve_by_external_ids.js
@@ -1,6 +1,7 @@
 const _ = require('builders/utils')
 const getEntityByUri = require('../get_entity_by_uri')
 const resolveExternalIds = require('./resolve_external_ids')
+const { isWdEntityUri } = require('lib/boolean_validations')
 
 const resolveSeedsByExternalIds = (seeds, expectedEntityType) => {
   return Promise.all(seeds.map(seed => resolveSeed(seed, expectedEntityType)))
@@ -10,11 +11,28 @@ const resolveSeed = async (seed, expectedEntityType) => {
   if (seed.uri) return seed
   const uris = await resolveExternalIds(seed.claims)
   if (uris == null) return seed
+  let uri
   if (uris.length === 1) {
-    const uri = uris[0]
+    uri = uris[0]
+  } else if (uris.length > 1) {
+    // There is no test for this, as this condition should not be possible to create,
+    // other than by setting an external id already set on existing local entity on a Wikidata entity,
+    // or alternatively, by bypassing local checks and writting directly in the database
+    _.warn({ uris, claims: seed.claims }, 'resolveExternalIds found too many uris, those are likely duplicates')
+    const wdUris = uris.filter(isWdEntityUri)
+    // Use a Wikidata uri in priority, but if there are none,
+    // use any of the remaining uris, rather than not resolving on any entity,
+    // Not resolving would mean letting create_unresolved_entry run and fail
+    // as it will get a "this property value is already used" error
+    uri = (wdUris.length > 0) ? wdUris[0] : uris[0]
+  }
+  if (uri) {
     if (expectedEntityType) {
       const { type } = await getEntityByUri({ uri })
       if (type === expectedEntityType) seed.uri = uri
+      // Else: the seed is not resolved, and create_unresolved_entry might attempt
+      // to create an entity with an existing external id, triggering
+      // an error "this property value is already used"
     } else {
       seed.uri = uri
     }
