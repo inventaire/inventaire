64983e64a57f62587d2259dda67cde4c94addf7d
maxlath <k@maxlath.eu>
2023-01-06 11:49:05 +0100

    convert eslintrc files to cjs
    as it's more practical than json

--- server/db/couchdb/design_docs/.eslintrc.cjs
+++ server/db/couchdb/design_docs/.eslintrc.cjs
@@ -1,21 +1,21 @@
-{
-  "env": {
+module.exports = {
+  env: {
     // env=couchdb does not exist, so taking what's the closest
     // Since CouchDB v3.1.0, views are executed in SpiderMonkey 68,
     // which corresponds to Firefox 68 features on MDN,
     // and is pretty much ES2020, to the notable exceptions of the following missing features:
     // - Optional chaining (arrives in Firefox 74)
     // - Nullish coalescing operator (arrives in Firefox 72)
-    "es2020": true
+    es2020: true,
   },
-  "parserOptions": {
-    "ecmaVersion": 2020
+  parserOptions: {
+    ecmaVersion: 2020,
   },
-  "rules": {
-    "prefer-arrow/prefer-arrow-functions": "off",
-    "no-undef": "off"
+  rules: {
+    'prefer-arrow/prefer-arrow-functions': 'off',
+    'no-undef': 'off',
   },
-  "globals": {
-    "emit": "readonly"
-  }
+  globals: {
+    emit: 'readonly',
+  },
 }
