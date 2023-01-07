c0099728636fb8e308a5e0c3eeb77ce0b166b967
maxlath <k@maxlath.eu>
2023-01-07 14:32:28 +0100

    bne: get_bne_entry_from_isbn: update namespace

--- server/data/bne/get_bne_entry_from_isbn.js
+++ server/data/bne/get_bne_entry_from_isbn.js
@@ -42,7 +42,7 @@ const getQuery = isbn => {
   // Virtuoso 37000 doesn't support GROUP_CONCAT
   const query = `
   PREFIX dcterms: <http://purl.org/dc/terms/>
-  PREFIX bnep: <http://datos.bne.es/def/>
+  PREFIX bnep: <https://datos.bne.es/def/>
   PREFIX owl: <http://www.w3.org/2002/07/owl#>
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
