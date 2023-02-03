a2f6154c5ff134d15f413998bca7c4a3d00050ba
maxlath <k@maxlath.eu>
2023-02-03 16:16:57 +0100

    client config: send an array of suspect keywords

--- server/controllers/config.js
+++ server/controllers/config.js
@@ -1,12 +1,16 @@
 import CONFIG from 'config'
 import { sendStaticJson } from '#lib/responses'
+import { assert_ } from '#lib/utils/assert_types'
 
-const { piwik } = CONFIG
+const { piwik, spam } = CONFIG
+
+assert_.array(spam.suspectKeywords)
 
 const endpoint = piwik.enabled ? piwik.endpoint : null
 
 const clientConfig = JSON.stringify({
   piwik: endpoint && endpoint.replace('/piwik.php', ''),
+  spam,
 })
 
 // A endpoint dedicated to pass configuration parameters to the client
