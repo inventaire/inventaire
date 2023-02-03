inventaire/inventaire

commit fda7e2cb02e107410b23d9923d8193f0d035bf77

remove unnecessary validate parameters

server/lib/sanitize/parameters.js
@@ -140,7 +140,7 @@ const arrayOfNumbers = {
 }
 
 const imgUrl = {
-  format: (value, name, config) => {
+  format: value => {
     let decodedUrl = decodeURIComponent(value)
     if (decodedUrl[0] === '/') decodedUrl = `${origin}${decodedUrl}`
     return decodedUrl
@@ -171,7 +171,7 @@ const allowlistedStrings = {
 
 const lang = {
   default: 'en',
-  validate: (value, name, config) => {
+  validate: (value, config) => {
     if (config.type === 'wikimedia') {
       return isWikimediaLanguageCode(value)
     } else {
@@ -235,7 +235,7 @@ module.exports = {
     format: value => {
       return JSON.parse(value)
     },
-    validate: (bbox, name, config) => {
+    validate: bbox => {
       if (_.typeOf(bbox) !== 'array') return false
       if (bbox.length !== 4) return false
       for (const coordinate of bbox) {
@@ -259,7 +259,7 @@ module.exports = {
     validate: _.isColorHexCode
   },
   context: {
-    validate: (value, name, config) => {
+    validate: value => {
       if (!isVisibilityKey(value)) {
         throw error_.new(`invalid context: ${value}`, 400, { value })
       }

commit d206494a869ef2c81894acc77c0e6ca645421c2e

revert bug introduced by fda7e2c

server/lib/sanitize/parameters.js
@@ -175,7 +175,7 @@ const allowlistedStrings = {
 
 const lang = {
   default: 'en',
-  validate: (value, config) => {
+  validate: (value, name, config) => {
     if (config.type === 'wikimedia') {
       return isWikimediaLanguageCode(value)
     } else {