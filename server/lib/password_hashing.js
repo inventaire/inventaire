7da1f759fb65d9cdc65fd68a23774d67cc0de8f4
maxlath <k@maxlath.eu>
2023-06-05 19:58:35 +0200

    password_hashing: reducing work to get hashing and verification below 5s
    In the long term, we will probably need to get rid of passwords and move to something like WebAuthn,
    but we are not there yet, and this was getting unusable

--- server/lib/password_hashing.js
+++ server/lib/password_hashing.js
@@ -6,7 +6,13 @@ const { useSlowPasswordHashFunction } = CONFIG
 let passwordHashing
 
 if (useSlowPasswordHashFunction) {
-  passwordHashing = credential()
+  passwordHashing = credential({
+    // "As a general rule, calculating a hash should take less than one second."
+    // https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
+    // The 'credential' lib might be too conservative here
+    // work=1 is too slow for the user (as of 2023: >3*10^6 iterations, >10s)
+    work: 0.25,
+  })
 } else {
   // Mimicking 'credential' API, should only be used in test environment
   passwordHashing = {
