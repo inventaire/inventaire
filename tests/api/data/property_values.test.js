dca167cd17dd229fac2db4416be783a1ffa6f77a
maxlath <k@maxlath.eu>
2023-09-06 14:59:43 +0200

    data: simplify property-values endpoint to return a single static json object
    requiring only one request by the client to get all the config its might need.
    The reduced request overhead might be worth the extra bandwidth,
    but even if not, this is mostly a way to simplify the client

--- tests/api/data/property_values.test.js
+++ tests/api/data/property_values.test.js
@@ -1,42 +1,14 @@
 import 'should'
 import allowedValuesPerTypePerProperty from '#controllers/entities/lib/properties/allowed_values_per_type_per_property'
-import { shouldNotBeCalled } from '#tests/unit/utils'
 import { publicReq } from '../utils/utils.js'
 
 const endpoint = '/api/data?action=property-values'
 
 describe('data:property-values', () => {
-  it('should reject without param', async () => {
-    await publicReq('get', endpoint)
-    .then(shouldNotBeCalled)
-    .catch(err => {
-      err.body.status_verbose.should.startWith('missing parameter in query')
-      err.statusCode.should.equal(400)
-    })
-  })
-
-  it('should reject invalid property', async () => {
-    const property = 'bla'
-    await publicReq('get', `${endpoint}&property=${property}`)
-    .then(shouldNotBeCalled)
-    .catch(err => {
-      err.body.status_verbose.should.startWith('invalid property')
-      err.statusCode.should.equal(400)
-    })
-  })
-
-  it('should reject invalid type', async () => {
-    const type = 'blu'
-    await publicReq('get', `${endpoint}&property=wdt:P31&type=${type}`)
-    .then(shouldNotBeCalled)
-    .catch(err => {
-      err.body.status_verbose.should.startWith('invalid type')
-      err.statusCode.should.equal(400)
-    })
-  })
-
   it('should return property values', async () => {
-    const { values } = await publicReq('get', `${endpoint}&property=wdt:P31&type=works`)
-    values.should.deepEqual(allowedValuesPerTypePerProperty['wdt:P31'].works)
+    const { values } = await publicReq('get', endpoint)
+    values['wdt:P31'].works.should.containEql('wd:Q47461344')
+    values['wdt:P437'].editions.should.containEql('wd:Q106833')
+    values.should.deepEqual(allowedValuesPerTypePerProperty)
   })
 })
