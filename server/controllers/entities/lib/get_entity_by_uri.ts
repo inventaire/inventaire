95d11c8ac981dff7cd70ffae8a59d8144561d7c9
maxlath <k@maxlath.eu>
2023-12-04 18:48:21 +0100

    entities: start typing getEntityByUri/getEntitiesByUris functions

---
+++ server/controllers/entities/lib/get_entity_by_uri.ts
@@ -0,0 +1,15 @@
+import { getEntitiesByUris } from './get_entities_by_uris.js'
+
+interface GetEntityByUriArgs {
+  uri: string
+  refresh?: boolean
+  dry?: boolean
+}
+
+// Get only the entity formatted doc you needs instead of an object
+// with entities and redirects
+export async function getEntityByUri ({ uri, refresh, dry }: GetEntityByUriArgs) {
+  const uris = [ uri ]
+  const { entities } = await getEntitiesByUris({ uris, refresh, dry })
+  return Object.values(entities)[0]
+}
