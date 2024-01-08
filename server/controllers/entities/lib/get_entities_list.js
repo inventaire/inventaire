87724728640ea3c7367be0bfa74cf3f0309e0a74
maxlath <k@maxlath.eu>
2024-01-08 09:57:18 +0100

    get_entities_list: compact uris
    to avoid letting through `[ undefined ]`, fixing tests/api/items/export.test.js csv tests

--- server/controllers/entities/lib/get_entities_list.js
+++ server/controllers/entities/lib/get_entities_list.js
@@ -1,9 +1,12 @@
+import { compact } from 'lodash-es'
 import getEntitiesByUris from './get_entities_by_uris.js'
 
 // A convenience function wrapping getEntitiesByUris, typically to be used in a promise chain
 // ex: getSomeUris.then(getEntitiesList)
 
 export async function getEntitiesList (uris) {
-  if (uris == null || uris.length === 0) return []
+  if (uris == null) return []
+  uris = compact(uris)
+  if (uris.length === 0) return []
   return getEntitiesByUris({ uris, list: true })
 }
