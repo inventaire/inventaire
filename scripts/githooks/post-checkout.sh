5568c46339848b304ca9b650915077d2edbc06e0
maxlath <k@maxlath.eu>
2024-02-22 15:42:47 +0100

    githooks: post-checkout: log package.json diff

--- scripts/githooks/post-checkout.sh
+++ scripts/githooks/post-checkout.sh
@@ -3,5 +3,7 @@ CHANGED=$(git diff $1 $2 --stat package-lock.json)
 if [[ -n $CHANGED ]]
 then
     echo -e "\e[0;33m△  Warning: package-lock.json has changed between ${1:0:7} and ${2:0:7}"
+    # Use diff on package.json rather than package-lock.json as the later is too verbose
+    git diff -U0 $1 $2 package.json
     echo -e "\e[0;33m△  You may want to run 'npm install'.\e[0m"
 fi
