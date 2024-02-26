b73b6e93897318f998a9742e79b6ccc4c16212e4
maxlath <k@maxlath.eu>
2024-02-26 19:14:39 +0100

    server_mode: support both .ts and .js extensions
    to work with both ts-node and node running the transpiled server

--- server/lib/server_mode.ts
+++ server/lib/server_mode.ts
@@ -5,15 +5,15 @@ import { error_ } from '#lib/error/error'
 // We assume that serverPath has the right value to avoid having to rely
 // on the server file itself to declare its path, as that would be too light
 // or require circular dependencies
-const serverPath = absolutePath('server', 'server.js')
+const serverPathWithoutExtension = absolutePath('server', 'server.js')
 
 // but this assumption needs to be confirmed, so that when the server root file
 // is renamed, the serverPath value above is updated
 export function confirmServerPath (url) {
   const realServerPath = fileURLToPath(url)
-  if (serverPath !== realServerPath) {
-    throw error_.new('serverPath needs to be updated', 500, { serverPath, realServerPath })
+  if (realServerPath.includes(serverPathWithoutExtension)) {
+    throw error_.new('serverPath needs to be updated', 500, { serverPathWithoutExtension, realServerPath })
   }
 }
 
-export const serverMode = process.argv[1] === serverPath
+export const serverMode = process.argv[1].includes(serverPathWithoutExtension)
