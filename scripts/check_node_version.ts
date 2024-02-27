7fff8a8e341ea7bb2a10be30814ae0388c095d09
maxlath <k@maxlath.eu>
2024-02-27 17:15:27 +0100

    package.json: lock engine.node to v18
    to discourage upgrading to v20, until the issue with ts-node is resolved
    https://github.com/TypeStrong/ts-node/issues/1997

--- scripts/check_node_version.ts
+++ scripts/check_node_version.ts
@@ -6,20 +6,27 @@ import { requireJson } from '#lib/utils/json'
 const { engines } = requireJson(absolutePath('root', 'package.json'))
 const nodejsVersion = process.version
 const [ , actualMajor, actualMinor, actualPatch ] = nodejsVersion.match(/^v(\d+)\.(\d+)\.(\d+)$/).map(num => parseInt(num))
-const [ , requiredMajor, requiredMinor, requiredPatch ] = engines.node.match(/ (\d+)\.(\d+)?\.?(\d+)?$/).map(num => num ? parseInt(num) : 0)
+const parseVersionNumber = num => num ? parseInt(num) : 0
+let [ , comparator, requiredMajor, requiredMinor, requiredPatch ] = engines.node.match(/([=><]+) (\d+)\.?(\d+)?\.?(\d+)?$/)
+requiredMajor = parseVersionNumber(requiredMajor)
+requiredMinor = parseVersionNumber(requiredMinor)
+requiredPatch = parseVersionNumber(requiredPatch)
 
 const normalizeVersion = (...versionParts) => {
   return versionParts.map(num => num.toString().padStart(2, '0')).join('.')
 }
 
-if (normalizeVersion(requiredMajor, requiredMinor, requiredPatch) > normalizeVersion(actualMajor, actualMinor, actualPatch)) {
+const nodeVersionIsTooLow = comparator.startsWith('>') && (normalizeVersion(requiredMajor, requiredMinor, requiredPatch) > normalizeVersion(actualMajor, actualMinor, actualPatch))
+const nodeVersionIsTooHigh = comparator === '=' && actualMajor !== requiredMajor
+
+if (nodeVersionIsTooLow || nodeVersionIsTooHigh) {
   console.error(`Invalid NodeJS version:
   Expected: ${engines.node}
   Found: ${nodejsVersion} (${process.execPath})
 
 If you installed NodeJS from a package manager, you could try to uninstall it and reinstall it with NVM https://github.com/nvm-sh/nvm instead:
 
-   nvm install --lts
+   nvm install ${requiredMajor}
 
 Then rerun the dependency installation:
 
