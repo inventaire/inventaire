fd1edc2e118c8ff730acd90326eab49575cf0087
maxlath <k@maxlath.eu>
2024-03-03 17:00:29 +0100

    transactions rights_verification: fix type errors and add some types

--- server/controllers/transactions/lib/rights_verification.ts
+++ server/controllers/transactions/lib/rights_verification.ts
@@ -1,6 +1,9 @@
 import { newError } from '#lib/error/error'
 import { verifyRightToInteractWithItem } from '#lib/interactions'
 import { transactionIsActive } from '#models/transaction'
+import type { Item } from '#types/item'
+import type { Transaction } from '#types/transaction'
+import type { UserId } from '#types/user'
 
 let getTransactionsByUserAndItem, checkIfItemIsBusy
 const importCircularDependencies = async () => {
@@ -8,43 +11,43 @@ const importCircularDependencies = async () => {
 }
 setImmediate(importCircularDependencies)
 
-const verifyNoExistingTransaction = async (reqUserId, item) => {
+async function verifyNoExistingTransaction (reqUserId: UserId, item: Item) {
   const transactionsDocs = await getTransactionsByUserAndItem(reqUserId, item._id)
   const activeTransactionsDocs = transactionsDocs.filter(transactionIsActive)
   if (activeTransactionsDocs.length > 0) {
     const message = 'user already made a request on this item'
-    throw newError(message, 403, reqUserId, item, activeTransactionsDocs[0])
+    throw newError(message, 403, { reqUserId, item, activeTransaction: activeTransactionsDocs[0] })
   } else {
     return item
   }
 }
 
-export async function verifyRightToRequest (reqUserId, item) {
+export async function verifyRightToRequest (reqUserId: UserId, item: Item) {
   const itemIsBusy = await checkIfItemIsBusy(item._id)
   if (itemIsBusy) {
-    throw newError('item already busy', 403, item)
+    throw newError('item already busy', 403, { item })
   }
   await verifyRightToInteractWithItem({ reqUserId, item, ownerAllowed: false })
   await verifyNoExistingTransaction(reqUserId, item)
 }
 
-export function verifyRightToInteractWithTransaction (userId, transaction) {
+export function verifyRightToInteractWithTransaction (userId: UserId, transaction: Transaction) {
   const { owner, requester } = transaction
   if (!(userId === owner || userId === requester)) {
-    throw newError('wrong user', 403, userId, transaction)
+    throw newError('wrong user', 403, { userId, transaction })
   }
 }
 
-export function verifyIsOwner (userId, transaction) {
+export function verifyIsOwner (userId: UserId, transaction: Transaction) {
   const { owner } = transaction
   if (userId !== owner) {
-    throw newError('wrong user', 403, userId, transaction)
+    throw newError('wrong user', 403, { userId, transaction })
   }
 }
 
-export function verifyIsRequester (userId, transaction) {
+export function verifyIsRequester (userId: UserId, transaction: Transaction) {
   const { requester } = transaction
   if (userId !== requester) {
-    throw newError('wrong user', 403, userId, transaction)
+    throw newError('wrong user', 403, { userId, transaction })
   }
 }
