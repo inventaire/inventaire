f2c1b1e677ab952a78616c763be930e8976df369
maxlath <k@maxlath.eu>
2024-03-03 23:25:42 +0100

    add SerializedWdEntity type

--- types/entity.ts
+++ types/entity.ts
@@ -1,6 +1,7 @@
 import type { allLocallyEditedEntitiesTypes, localPropertiesUris } from '#controllers/entities/lib/properties/properties'
 import type { CouchDoc, CouchUuid, ImageHash, Url } from '#types/common'
-import type { WikimediaLanguageCode } from 'wikibase-sdk'
+import type { OverrideProperties } from 'type-fest'
+import type { WikimediaLanguageCode, SitelinkBadges, Item as WdItem } from 'wikibase-sdk'
 
 export type WdEntityId = `Q${number}`
 export type WdPropertyId = `P${number}`
@@ -17,7 +18,8 @@ export type IsbnEntityUri = `isbn:${Isbn}`
 
 export type EntityUri = WdEntityUri | InvEntityUri | IsbnEntityUri
 
-export type Label = string
+export type Term = string
+export type Label = Term
 export type Labels = Partial<Record<WikimediaLanguageCode, Label>>
 
 export type EntityValue = WdEntityUri | InvEntityUri
@@ -47,9 +49,7 @@ export interface InvEntity extends CouchDoc {
   version: number
 }
 
-export interface RemovedPlaceholderEntity extends Omit<InvEntity, 'type'> {
-  type: 'removed:placeholder'
-}
+export type RemovedPlaceholderEntity = OverrideProperties<InvEntity, { type: 'removed:placeholder' }>
 
 export interface EntityRedirection extends Omit<InvEntity, 'labels' | 'claims'> {
   redirect: EntityUri
@@ -60,4 +60,46 @@ export type InvEntityDoc = InvEntity | RemovedPlaceholderEntity | EntityRedirect
 
 export type EntityImg = `/img/entities/${ImageHash}`
 
+// No `File:` prefix (ex: 'Victor Hugo by Étienne Carjat 1876 - full.jpg')
+export type WikimediaCommonsFilename = string
+
 export type EntityType = typeof allLocallyEditedEntitiesTypes[number]
+
+export interface SerializedInvEntity extends OverrideProperties<InvEntity, { type?: EntityType }> {
+  uri: InvEntityUri | IsbnEntityUri
+  originalLang?: WikimediaLanguageCode
+  image?: {
+    url: EntityImg
+  }
+}
+
+export interface SerializedRemovedPlaceholder extends SerializedInvEntity {
+  _meta_type: 'removed:placeholder'
+}
+
+export type SimplifiedLanguageAliases = Term[]
+export type SimplifiedAliases = Partial<Record<WikimediaLanguageCode, SimplifiedLanguageAliases>>
+export type SimplifiedDescriptions = Partial<Record<WikimediaLanguageCode, Term>>
+
+export interface SimplifiedSitelink {
+  title: string
+  badges?: SitelinkBadges
+}
+export type SimplifiedSitelinks = Partial<Record<WikimediaLanguageCode, SimplifiedSitelink>>
+
+export interface SerializedWdEntity {
+  uri: WdEntityUri
+  type?: EntityType
+  labels: Labels
+  aliases: SimplifiedAliases
+  descriptions: SimplifiedDescriptions
+  claims: Claims
+  sitelinks: SimplifiedSitelinks
+  originalLang?: WikimediaLanguageCode
+  redirects?: WdItem['redirects']
+  image?: {
+    url: WikimediaCommonsFilename
+  }
+}
+
+export type SerializedEntity = SerializedInvEntity | SerializedWdEntity
