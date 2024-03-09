5707c0b504d67dae4f93616c054a564f6523226c
jums <jums@inventaire.io>
2024-03-09 14:18:59 +0100

    activitypub:undo: catch if activity not found
    see test "should repond ok if already not following"

--- server/controllers/activitypub/undo.ts
+++ server/controllers/activitypub/undo.ts
@@ -1,7 +1,7 @@
 import { isPlainObject } from 'lodash-es'
 import { getActivityByExternalId, deleteActivityById } from '#controllers/activitypub/lib/activities'
 import { isNonEmptyString } from '#lib/boolean_validations'
-import { newError } from '#lib/error/error'
+import { catchNotFound, newError } from '#lib/error/error'
 import { trackActor } from '#lib/track'
 
 export default async params => {
@@ -11,6 +11,7 @@ export default async params => {
 
   if (!isNonEmptyString(object)) throw newError('invalid activity object', 400, params)
   const activity = await getActivityByExternalId(object)
+  .catch(catchNotFound)
   if (!activity) return { ok: true, warn: 'No activity was undone' }
   if (activity.actor.uri !== actor) {
     throw newError('request actor and activity actor do not match', 403, { actor, activity })
