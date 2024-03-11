eccdeaefdef53f20cef2b13a33382459813f4868
maxlath <k@maxlath.eu>
2024-03-11 17:52:07 +0100

    deweirdify fixtures

--- tests/api/fixtures/text.ts
+++ tests/api/fixtures/text.ts
@@ -1,22 +1,13 @@
-import { readFileSync } from 'node:fs'
 import { sampleSize } from 'lodash-es'
-import { absolutePath } from '#lib/absolute_path'
+import { someNames } from '#fixtures/names'
+import { someWords } from '#fixtures/words'
 import { getRandomString } from '#lib/utils/random_string'
-
-const getFixtureWords = filename => {
-  return readFileSync(absolutePath('tests', `api/fixtures/${filename}.ts`))
-  .toString()
-  .split('\\\n')
-}
-const lorem = getFixtureWords('words')
-// no name should match on regex /\W/
-const firstNames = getFixtureWords('names')
 
 const capitalize = word => word[0].toUpperCase() + word.slice(1).toLowerCase()
 
-export const randomWords = (numberOfWords = 3, suffix = '') => sampleSize(lorem, numberOfWords).join(' ').concat(suffix)
+export const randomWords = (numberOfWords = 3, suffix = '') => sampleSize(someWords, numberOfWords).join(' ').concat(suffix)
 
-export const firstName = () => sampleSize(firstNames, 1)[0]
+export const firstName = () => sampleSize(someNames, 1)[0]
 
 // Add a random string to prevent creating several users with the same username
 // and be rejected because of it
