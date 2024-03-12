3cfcc2233eac81c4fc63338319edb67ee427f53b
maxlath <k@maxlath.eu>
2024-03-13 00:27:03 +0100

    user delete: fix type error

--- server/controllers/user/delete.ts
+++ server/controllers/user/delete.ts
@@ -1,17 +1,17 @@
 import deleteUserAndCleanup from '#controllers/user/lib/delete_user_and_cleanup'
 import { responses_ } from '#lib/responses'
-import { Track } from '#lib/track'
+import { track } from '#lib/track'
 import { warn } from '#lib/utils/logs'
 
-export default (req, res) => {
+export default async function (req, res) {
   const reqUserId = req.user._id
 
   warn(req.user, 'deleting user')
 
-  return deleteUserAndCleanup(reqUserId)
+  await deleteUserAndCleanup(reqUserId)
   // triggering track before logging out
   // to get access to req.user before it's cleared
-  .then(Track(req, [ 'user', 'delete' ]))
-  .then(req.logout.bind(req))
-  .then(responses_.Ok(res))
+  track(req, [ 'user', 'delete' ])
+  req.logout()
+  responses_.ok(res)
 }
