5f2873636fcf334bcfeb1a4a1bca4fd0d4309ce0
maxlath <k@maxlath.eu>
2024-03-15 21:56:37 +0100

    scripts: update delete_tests_databases

--- scripts/tests/api/delete_tests_databases.sh
+++ scripts/tests/api/delete_tests_databases.sh
@@ -2,16 +2,16 @@
 databases=$(./scripts/print_module_exports.ts server/db/couchdb/databases.ts databases | jq 'keys | join(" ")' -r)
 dbHost=$(node -p "require('config').db.getOrigin()")
 elasticOrigin=$(node -p "require('config').elasticsearch.origin")
-leveldbPathBase=$(node ./server/lib/absolute_path.js root db/leveldb)
+leveldbPathBase=$(ts-node ./server/lib/absolute_path.ts root db/leveldb)
 leveldbPath="${leveldbPathBase}*-tests"
 
 for db in $databases
 do
-  echo "\ndeleting ${db}-tests in couchdb... " &&
+  echo "deleting ${db}-tests in couchdb... " &&
   curl -sXDELETE "${dbHost}/${db}-tests"
-  echo "\ndeleting ${db}-tests in elastic search... " &&
+  echo "deleting ${db}-tests in elastic search... " &&
   curl -sXDELETE "${elasticOrigin}/${db}-tests"
 done
 
-echo "\ndeleting ${leveldbPath}... " &&
+echo "deleting ${leveldbPath}... " &&
 rm -rf ${leveldbPath}
