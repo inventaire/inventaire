a59d9c8876270eb17ea4389fa073c4c4a71e0e6c
maxlath <k@maxlath.eu>
2024-03-19 21:48:51 +0100

    typscript build: update

--- scripts/typescript/build.sh
+++ scripts/typescript/build.sh
@@ -11,9 +11,6 @@ tsc  --project ./tsconfig.build.json --diagnostics
 # Ex: '#lib/requests' needs to resolve to ./dist/server/lib/requests.js
 ln -s "$(realpath package.json)" ./dist
 
-# tsc will have created a build/inventaire-i18n folder with the files imported
-# within the code base, but lacking the i18n json, which are then required by the static server
-rm -rf ./dist/inventaire-i18n
-ln -sf "$(realpath inventaire-i18n)" ./dist
+ln -s "$(realpath config)" ./dist
 
 ln -s "$(realpath client)" ./dist
