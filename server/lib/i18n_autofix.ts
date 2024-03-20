89298e4fdf2edfdd7b177af1a106410f34752429
maxlath <k@maxlath.eu>
2024-03-20 17:58:38 +0100

    i18n_autofix: prevent loggin a warning when autofix=false

--- server/lib/i18n_autofix.ts
+++ server/lib/i18n_autofix.ts
@@ -6,7 +6,7 @@ import { success, info, warn } from '#lib/utils/logs'
 import config from '#server/config'
 import { wait } from './promises.js'
 
-const { srcFolderPath } = config.i18n
+const { autofix, srcFolderPath } = config.i18n
 
 const missingSrcFolderPathMessage = 'Set config.i18n.srcFolderPath in config/local-dev.cjs to automatically add missing i18n keys to your local inventaire-i18n repository'
 export let appendToClientKeys = (keys: string[]) => {
@@ -18,50 +18,52 @@ export let appendToServerKeys = (key: string) => {
 }
 
 let resolvedSrcFolderPath
-try {
-  resolvedSrcFolderPath = resolve(process.cwd(), srcFolderPath)
-  await stat(resolvedSrcFolderPath)
+if (autofix) {
+  try {
+    resolvedSrcFolderPath = resolve(process.cwd(), srcFolderPath)
+    await stat(resolvedSrcFolderPath)
 
-  const clientSourceFile = `${resolvedSrcFolderPath}/client/en.json`
-  const serverSourceFile = `${resolvedSrcFolderPath}/server/en.json`
+    const clientSourceFile = `${resolvedSrcFolderPath}/client/en.json`
+    const serverSourceFile = `${resolvedSrcFolderPath}/server/en.json`
 
-  // Using _ as the convention to identify short keys: ex: awesome_title
-  // (that is, keys with an English value different than the key itself)
-  // the underscore should be surrounded by letters, not spaces
-  const shortKeyPattern = /^\w+_\w+$/
+    // Using _ as the convention to identify short keys: ex: awesome_title
+    // (that is, keys with an English value different than the key itself)
+    // the underscore should be surrounded by letters, not spaces
+    const shortKeyPattern = /^\w+_\w+$/
 
-  appendToClientKeys = keys => appendToI18nKeys(clientSourceFile, keys)
-  appendToServerKeys = key => appendToI18nKeys(serverSourceFile, [ key ])
+    appendToClientKeys = keys => appendToI18nKeys(clientSourceFile, keys)
+    appendToServerKeys = key => appendToI18nKeys(serverSourceFile, [ key ])
 
-  // Don't use 'require' as it will be cached until next start
-  async function appendToI18nKeys (path, newKeys) {
+    // Don't use 'require' as it will be cached until next start
+    async function appendToI18nKeys (path, newKeys) {
     // Add a random pause so that several calls at the same time
     // are unlickly to conflict. Sort of a debounce ersatz
-    await wait(Math.trunc(Math.random() * 1000))
+      await wait(Math.trunc(Math.random() * 1000))
 
-    const keys = await readJsonFile(path)
-    const lengthBefore = objLength(keys)
-    for (const key of newKeys) {
-      if (!keys[key]) {
-        keys[key] = shortKeyPattern.test(key) ? null : key
-        success(`+i18n: '${key}'`)
+      const keys = await readJsonFile(path)
+      const lengthBefore = objLength(keys)
+      for (const key of newKeys) {
+        if (!keys[key]) {
+          keys[key] = shortKeyPattern.test(key) ? null : key
+          success(`+i18n: '${key}'`)
+        } else {
+          info(`i18n: already there '${key}'`)
+        }
+      }
+
+      if (objLength(keys) > lengthBefore) {
+        try {
+          await writeJsonFile(path, keys)
+          success(`i18n:updated ${path}`)
+        } catch (err) {
+          console.log('err', err)
+        }
       } else {
-        info(`i18n: already there '${key}'`)
+        info(`i18n:not:updating ${path}: no new key`)
       }
     }
-
-    if (objLength(keys) > lengthBefore) {
-      try {
-        await writeJsonFile(path, keys)
-        success(`i18n:updated ${path}`)
-      } catch (err) {
-        console.log('err', err)
-      }
-    } else {
-      info(`i18n:not:updating ${path}: no new key`)
-    }
+  } catch (err) {
+    err.context = { resolvedSrcFolderPath, srcFolderPath }
+    warn(err, missingSrcFolderPathMessage)
   }
-} catch (err) {
-  err.context = { resolvedSrcFolderPath, srcFolderPath }
-  warn(err, missingSrcFolderPathMessage)
 }
