22500cad01db7a8455b94337b69350ac56a90e52
jums <jums@inventaire.io>
2024-03-21 16:56:51 +0100

    test api quick: update test server pid grep to new watcher

--- scripts/tests/api/test_api_quick.sh
+++ scripts/tests/api/test_api_quick.sh
@@ -11,7 +11,7 @@
 export NODE_ENV=tests-api NODE_APP_INSTANCE=server
 # Use `toString` to prevent getting colored output
 server_port=$(node -p "require('config').port.toString()")
-watcher_pid=$(pgrep --full "nodemon_server_port_${server_port}")
+watcher_pid=$(pgrep --full "watcher_server_port_${server_port}")
 
 if [ -n "$watcher_pid" ];
 then
