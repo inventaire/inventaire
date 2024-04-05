168f1b251a960a614deb665804b923b26a8c1d1a
maxlath <k@maxlath.eu>
2024-04-05 18:19:23 +0200

    scripts: action_by_input: contextualize log labels

--- scripts/db_actions/lib/action_by_input.ts
+++ scripts/db_actions/lib/action_by_input.ts
@@ -2,7 +2,7 @@ import { LogError, Log } from '#lib/utils/logs'
 
 export default input => action => {
   action(input)
-  .then(Log('ok'))
-  .catch(LogError('err'))
+  .then(Log('action_by_input ok'))
+  .catch(LogError('action_by_input err'))
   .then(() => process.exit(0))
 }
