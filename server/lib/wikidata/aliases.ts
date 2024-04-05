8430400377d97d764650c8827e597334e9bea11f
maxlath <k@maxlath.eu>
2024-04-05 22:58:04 +0200

    wikidata aliases: added young adult novel (wd:Q11825892)

--- server/lib/wikidata/aliases.ts
+++ server/lib/wikidata/aliases.ts
@@ -71,6 +71,7 @@ const wikidataOnlyP31Values = {
     'wd:Q16363676', // manual
     'wd:Q11826511', // work of science
     'wd:Q116476516', // dramatic work
+    'wd:Q11825892', // young adult novel
   ],
   editions: [
     'wd:Q3972943', // publishing
