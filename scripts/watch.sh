249959c40f87d2141bb5ef599f11555ce4d0fe23
maxlath <k@maxlath.eu>
2024-04-05 16:44:13 +0200

    scripts: watch: log timestamp before starting to watch

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -6,6 +6,9 @@ server_port=$(node -p "require('config').port.toString()")
 
 export FORCE_COLOR=true
 
+echo -n 'start watcher '
+date +%T
+
 # Hijack the --ignore argument to declare which server port will be used
 # in order to let other process find the desired nodemon process id with `pkill --full`
 tsx watch \
