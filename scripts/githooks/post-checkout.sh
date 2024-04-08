576a4cb85e782838c8a695d6d29442c38b3e6a51
maxlath <k@maxlath.eu>
2024-04-08 18:17:40 +0200

    post-checkout git hook: disable git pager

--- scripts/githooks/post-checkout.sh
+++ scripts/githooks/post-checkout.sh
@@ -4,6 +4,6 @@ if [[ -n $CHANGED ]]
 then
     echo -e "\e[0;33m△  Warning: package-lock.json has changed between ${1:0:7} and ${2:0:7}"
     # Use diff on package.json rather than package-lock.json as the later is too verbose
-    git diff -U0 $1 $2 package.json
+    git --no-pager diff -U0 $1 $2 package.json
     echo -e "\e[0;33m△  You may want to run 'npm install'.\e[0m"
 fi
