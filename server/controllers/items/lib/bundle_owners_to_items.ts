34977ef2011143a5637b0f50be43c9e516a57c13
maxlath <k@maxlath.eu>
2024-04-24 12:05:14 +0200

    bundle_owners_to_items: fix type error

--- server/controllers/items/lib/bundle_owners_to_items.ts
+++ server/controllers/items/lib/bundle_owners_to_items.ts
@@ -1,15 +1,17 @@
 import { uniq } from 'lodash-es'
 import { getUsersAuthorizedDataByIds } from '#controllers/user/lib/user'
 import { newError } from '#lib/error/error'
+import type { Item } from '#server/types/item'
+import type { UserId } from '#server/types/user'
 
-export default async function (items, reqUserId) {
+export default async function (items: Item[], reqUserId: UserId) {
   if (!(items && items.length > 0)) throw newError('no item found', 404)
   const usersIds = getItemsOwners(items)
   const users = await getUsersAuthorizedDataByIds(usersIds, reqUserId)
   return { items, users }
 }
 
-function getItemsOwners (items) {
+function getItemsOwners (items: Item[]) {
   const users = items.map(item => item.owner)
-  return uniq(users)
+  return uniq(users) as UserId[]
 }
