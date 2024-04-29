9362c306c721792dd1d058e357f53c482f20baea
maxlath <k@maxlath.eu>
2024-04-30 00:56:46 +0200

    get_inv_entities/get_entities_by_isbns: simplify claims

--- server/controllers/entities/lib/format_edition_entity.ts
+++ server/controllers/entities/lib/format_edition_entity.ts
@@ -1,9 +1,12 @@
 import { normalizeIsbn } from '#lib/isbn/isbn'
+import { getClaimValue, simplifyInvClaims } from '#models/entity'
+import type { SerializedInvEntity } from '#server/types/entity'
 import formatEntityCommon from './format_entity_common.js'
 
 export default entity => {
-  const isbn = entity.claims['wdt:P212'][0]
+  const isbn = getClaimValue(entity.claims['wdt:P212'][0])
   entity.uri = `isbn:${normalizeIsbn(isbn)}`
   entity.type = 'edition'
-  return formatEntityCommon(entity)
+  entity.claims = simplifyInvClaims(entity.claims)
+  return formatEntityCommon(entity) as SerializedInvEntity
 }
