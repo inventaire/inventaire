493f380900bcb447a4520c5eccfe483ae036010c
maxlath <k@maxlath.eu>
2024-04-29 19:29:23 +0200

    entities: claims validation and formatting: add support for object claims

--- server/controllers/entities/lib/validate_and_format_claim.ts
+++ server/controllers/entities/lib/validate_and_format_claim.ts
@@ -1,7 +1,7 @@
 import { validateProperty } from './properties/validations.js'
-import { validateAndFormatClaimValue } from './validate_and_format_claim_value.js'
+import { validateAndFormatClaimValue, type ValidateAndFormatClaimValueParams } from './validate_and_format_claim_value.js'
 
-export async function validateAndFormatClaim (params) {
+export async function validateAndFormatClaim (params: ValidateAndFormatClaimValueParams) {
   const { property } = params
   validateProperty(property)
   return validateAndFormatClaimValue(params)
