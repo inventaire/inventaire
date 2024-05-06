83f9a54b7996ecb808264a9476b7577fcc9514ba
maxlath <k@maxlath.eu>
2024-05-06 17:31:01 +0200

    tests utils: add types

--- tests/unit/utils/utils.ts
+++ tests/unit/utils/utils.ts
@@ -1,17 +1,19 @@
 import util from 'node:util'
+import type { ContextualizedError } from '#lib/error/format_error'
 import { warn } from '#lib/utils/logs'
 
-export function shouldNotBeCalled (res) {
+export function shouldNotBeCalled (res?: unknown) {
   warn(util.inspect(res, false, null), 'undesired positive res')
-  const err = new Error('function was expected not to be called')
+  const err: ContextualizedError = new Error('function was expected not to be called')
   // Give 'shouldNotBeCalled' more chance to appear in the red text of the failing test
+  // @ts-expect-error
   err.name = err.statusCode = 'shouldNotBeCalled'
   err.body = { status_verbose: 'shouldNotBeCalled' }
   err.context = { res }
   throw err
 }
 
-export function rethrowShouldNotBeCalledErrors (err) {
+export function rethrowShouldNotBeCalledErrors (err: Error) {
   if (err.name === 'shouldNotBeCalled') throw err
 }
 
