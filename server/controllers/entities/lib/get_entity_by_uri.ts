b982bb4ae07e635b9ce5aba80933df466a024193
maxlath <k@maxlath.eu>
2024-05-09 23:45:28 +0200

    get_entities_by_uris: use dedicated functions with forced output type to work around undeterminated output type

--- server/controllers/entities/lib/get_entity_by_uri.ts
+++ server/controllers/entities/lib/get_entity_by_uri.ts
@@ -1,12 +1,18 @@
 import type { EntityUri } from '#types/entity'
-import { getEntitiesByUris, type EntitiesGetterParams } from './get_entities_by_uris.js'
+import { getEntitiesByUris, getExtendedEntitiesByUris, type EntitiesGetterParams } from './get_entities_by_uris.js'
 
-type GetEntityByUriArgs = Omit<EntitiesGetterParams, 'uris'> & { uri: EntityUri }
+type GetEntityByUriArgs = Omit<EntitiesGetterParams, 'uris' | 'includeReferences'> & { uri: EntityUri }
 
 // Get only the entity formatted doc you needs instead of an object
 // with entities and redirects
-export async function getEntityByUri ({ uri, refresh, dry, includeReferences }: GetEntityByUriArgs) {
+export async function getEntityByUri ({ uri, refresh, dry }: GetEntityByUriArgs) {
   const uris = [ uri ]
-  const { entities } = await getEntitiesByUris({ uris, refresh, dry, includeReferences })
+  const { entities } = await getEntitiesByUris({ uris, refresh, dry })
+  return Object.values(entities)[0]
+}
+
+export async function getExtendedEntityByUri ({ uri, refresh, dry }: GetEntityByUriArgs) {
+  const uris = [ uri ]
+  const { entities } = await getExtendedEntitiesByUris({ uris, refresh, dry })
   return Object.values(entities)[0]
 }
