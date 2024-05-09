0f9fd0a0d659ecd88cedbe54333dc6a09bb77561
maxlath <k@maxlath.eu>
2024-05-10 00:47:52 +0200

    validateSnakValueSync: avoid to throw when the entity type corresponding to an external id is not known

--- server/controllers/entities/lib/properties/properties_config_builders.ts
+++ server/controllers/entities/lib/properties/properties_config_builders.ts
@@ -3,7 +3,9 @@ import { newError } from '#lib/error/error'
 import { formatIsbn } from '#lib/isbn/isbn'
 import { parseIsbn } from '#lib/isbn/parse'
 import { assert_ } from '#lib/utils/assert_types'
+import { objectValues } from '#lib/utils/base'
 import { getPluralType } from '#lib/wikidata/aliases'
+import type { EntityType } from '#server/types/entity'
 import { allowedValuesPerTypePerProperty } from './allowed_values_per_type_per_property.js'
 import { concurrentString, concurrentExternalId, uniqueEntity } from './properties_config_bases.js'
 
@@ -32,17 +34,22 @@ export function externalId (regex) {
   } as const
 }
 
-export function typedExternalId (regexPerType) {
+export function typedExternalId (regexPerType: Partial<Record<EntityType, RegExp>>) {
   return {
     ...concurrentExternalId,
     typeSpecificValidation: true,
     format: trim,
     validate: ({ value, entityType }) => {
-      assert_.string(entityType)
-      if (regexPerType[entityType] == null) {
-        throw newError('unsupported type', 500, { regexPerType, entityType, value })
+      if (entityType) {
+        if (regexPerType[entityType] == null) {
+          throw newError('unsupported type', 500, { regexPerType, entityType, value })
+        }
+        return regexPerType[entityType].test(value)
+      } else {
+        // There are cases where it's hard to pass target the entity type
+        // so we just check that one of the known pattern matches
+        return objectValues(regexPerType).some(regex => regex.test(value))
       }
-      return regexPerType[entityType].test(value)
     },
   } as const
 }
@@ -54,6 +61,7 @@ export function allowedPropertyValues (property) {
     typeSpecificValidation: true,
     format: trim,
     validate: ({ value, entityType }) => {
+      assert_.string(entityType)
       const type = getPluralType(entityType)
       return allowedValuesPerType[type].includes(value)
     },
