bad55ee3a124728092af2472da3c40f5c4bb4ec8
maxlath <k@maxlath.eu>
2024-05-09 15:44:11 +0200

    tests: enrich_and_get_edition_entity_from_isbn: test claim references

--- server/controllers/entities/lib/get_entity_by_uri.ts
+++ server/controllers/entities/lib/get_entity_by_uri.ts
@@ -1,16 +1,12 @@
 import type { EntityUri } from '#types/entity'
-import { getEntitiesByUris } from './get_entities_by_uris.js'
+import { getEntitiesByUris, type EntitiesGetterParams } from './get_entities_by_uris.js'
 
-interface GetEntityByUriArgs {
-  uri: EntityUri
-  refresh?: boolean
-  dry?: boolean
-}
+type GetEntityByUriArgs = Omit<EntitiesGetterParams, 'uris'> & { uri: EntityUri }
 
 // Get only the entity formatted doc you needs instead of an object
 // with entities and redirects
-export async function getEntityByUri ({ uri, refresh, dry }: GetEntityByUriArgs) {
+export async function getEntityByUri ({ uri, refresh, dry, includeReferences }: GetEntityByUriArgs) {
   const uris = [ uri ]
-  const { entities } = await getEntitiesByUris({ uris, refresh, dry })
+  const { entities } = await getEntitiesByUris({ uris, refresh, dry, includeReferences })
   return Object.values(entities)[0]
 }
--- tests/integration/data/enrich_and_get_edition_entity_from_isbn.test.ts
+++ tests/integration/data/enrich_and_get_edition_entity_from_isbn.test.ts
@@ -2,6 +2,7 @@ import 'should'
 import { getEntityByUri } from '#controllers/entities/lib/get_entity_by_uri'
 import { enrichAndGetEditionEntityFromIsbn } from '#data/dataseed/enrich_and_get_edition_entity_from_isbn'
 import { wait } from '#lib/promises'
+import { simpleDay } from '#lib/utils/base'
 
 describe('get resolved seed', () => {
   // Give the time to importCircularDependencies to run
@@ -10,9 +11,19 @@ describe('get resolved seed', () => {
   it('should get an edition entity when only one authority returns a seed', async () => {
     // Expect only BNF to return a seed. If that's not the case, you can find new candidates with
     // https://query.inventaire.io/#SELECT%20%2a%20%7B%0A%20%20%3Fitem%20wdt%3AP268%20%3FbnfId%20.%0A%20%20%3Fitem%20wdt%3AP629%20%3Fwork%20.%0A%20%20FILTER%20NOT%20EXISTS%20%7B%20%3Fwork%20wdt%3AP31%20%3Ftype%20%7D%20.%0A%20%20%3Fitem%20wdt%3AP212%20%3Fisbn%20.%0A%20%20%3Fitem%20wdt%3AP577%20%3Fdate%20.%0A%7D%0AORDER%20BY%20%3Fdate
-    const edition = await enrichAndGetEditionEntityFromIsbn('978-0-316-76953-2')
-    edition.claims['wdt:P629'].should.deepEqual([ 'wd:Q183883' ])
-    edition.claims['wdt:P268'].should.deepEqual([ '37461803r' ])
+    const { uri } = await enrichAndGetEditionEntityFromIsbn('978-0-316-76953-2')
+    const edition = await getEntityByUri({ uri, includeReferences: true })
+    const reference = {
+      'wdt:P854': [ 'https://catalogue.bnf.fr/ark:/12148/cb37461803r' ],
+      'wdt:P813': [ simpleDay() ],
+    }
+    edition.claims['wdt:P629'].should.deepEqual([ { value: 'wd:Q183883' } ])
+    edition.claims['wdt:P268'].should.deepEqual([
+      {
+        value: '37461803r',
+        references: [ reference ],
+      },
+    ])
   })
 
   it('should get an edition entity when multiple authorities return a seed', async () => {
