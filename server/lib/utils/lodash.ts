739df3fe57031d8b0cf49273b15ad8c3d6dfccd7
maxlath <k@maxlath.eu>
2024-05-10 18:40:43 +0200

    typed lodash utils: add map, uniq, groupBy

--- server/lib/utils/lodash.ts
+++ server/lib/utils/lodash.ts
@@ -1,7 +1,23 @@
 // Lodash functions, but preserving types
-
-import { difference as _difference } from 'lodash-es'
+import {
+  difference as _difference,
+  groupBy as _groupBy,
+  map as _map,
+  uniq as _uniq,
+} from 'lodash-es'
 
 export function difference <T> (array1: T[], array2: T[]) {
   return _difference(array1, array2) as T[]
+}
+
+export function groupBy <T, K extends keyof T> (array: T[], attribute: K) {
+  return _groupBy(array, attribute) as (Record<T[K] extends (string | number) ? T[K] : never, T[]>)
+}
+
+export function map <T, K extends keyof T> (array: T[], attribute: K) {
+  return _map(array, attribute) as T[K][]
+}
+
+export function uniq <T> (array: T[]) {
+  return _uniq(array) as T[]
 }
