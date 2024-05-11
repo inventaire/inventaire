inventaire/inventaire

commit a51a6a5d7a1a7b04d8a696ae733033d27e607317

entity model: more TS errors fixed and added types

server/models/entity.ts
@@ -34,211 +34,207 @@ import { validateRequiredPropertiesValues } from './validations/validate_require
 
 const wikimediaLanguageCodes = new Set(Object.keys(wikimediaLanguageCodesByWdId))
 
-const Entity = {
-  create: () => {
-    return {
-      type: 'entity',
-      labels: {},
-      claims: {},
-      created: Date.now(),
-      version: 1,
+export function createBlankEntityDoc () {
+  return {
+    type: 'entity',
+    labels: {},
+    claims: {},
+    created: Date.now(),
+    version: 1,
+  }
+}
+
+export function setEntityDocLabel (doc, lang, value) {
+  assert_.object(doc)
+  assert_.string(lang)
+
+  if (!wikimediaLanguageCodes.has(lang)) {
+    throw newError('invalid lang', 400, { doc, lang, value })
+  }
+
+  preventRedirectionEdit(doc)
+
+  if (value === null) {
+    deleteLabel(doc, lang)
+  } else {
+    assert_.string(value)
+    value = superTrim(value)
+
+    if (doc.labels[lang] === value) {
+      throw newError('already up-to-date', 400, { doc, lang, value })
     }
-  },
 
-  setLabel: (doc, lang, value) => {
-    assert_.object(doc)
-    assert_.string(lang)
+    doc.labels[lang] = value
+  }
 
-    if (!wikimediaLanguageCodes.has(lang)) {
-      throw newError('invalid lang', 400, { doc, lang, value })
+  return doc
+}
+
+export function setEntityDocLabels (doc, labels) {
+  preventRedirectionEdit(doc)
+  for (const lang in labels) {
+    const value = labels[lang]
+    doc = setEntityDocLabel(doc, lang, value)
+  }
+
+  return doc
+}
+
+export function addEntityDocClaims (doc, claims) {
+  preventRedirectionEdit(doc)
+
+  // Pass the list of all edited properties, so that wen trying to infer property
+  // values, we know which one should not be infered at the risk of creating
+  // a conflict
+  doc._allClaimsProps = Object.keys(claims)
+
+  for (const property in claims) {
+    const array = claims[property]
+    for (const value of array) {
+      doc = createBlankEntityDocClaim(doc, property, value)
+    }
+  }
+
+  delete doc._allClaimsProps
+
+  return doc
+}
+
+export function createBlankEntityDocClaim (doc, property, value) {
+  preventRedirectionEdit(doc)
+  return updateEntityDocClaim(doc, property, null, value)
+}
+
+export function updateEntityDocClaim (doc, property, oldVal, newVal) {
+  const context = { doc, property, oldVal, newVal }
+  preventRedirectionEdit(doc)
+  if (oldVal == null && newVal == null) {
+    throw newError('missing old or new value', 400, context)
+  }
+
+  if (isString(oldVal)) oldVal = superTrim(oldVal)
+  if (isString(newVal)) newVal = superTrim(newVal)
+
+  let propArray = get(doc, `claims.${property}`)
+
+  if (propArray && newVal != null && propArray.includes(newVal)) {
+    throw newError('claim property new value already exist', 400, [ propArray, newVal ])
+  }
+
+  if (oldVal != null) {
+    if (propArray != null) {
+      propArray = propArray.map(value => isString(value) ? superTrim(value) : value)
+    }
+    if (propArray == null || !propArray.includes(oldVal)) {
+      throw newError('claim property value not found', 400, context)
     }
 
-    Entity.preventRedirectionEdit(doc, 'setLabel')
-
-    if (value === null) {
-      deleteLabel(doc, lang)
+    if (newVal != null) {
+      const oldValIndex = propArray.indexOf(oldVal)
+      doc.claims[property][oldValIndex] = newVal
     } else {
-      assert_.string(value)
-      value = superTrim(value)
+      // if the new value is null, it plays the role of a removeClaim
+      propArray = without(propArray, oldVal)
 
-      if (doc.labels[lang] === value) {
-        throw newError('already up-to-date', 400, { doc, lang, value })
-      }
-
-      doc.labels[lang] = value
+      setPossiblyEmptyPropertyArray(doc, property, propArray)
     }
+  } else {
+    // if the old value is null, it plays the role of a createClaim
+    if (!doc.claims[property]) doc.claims[property] = []
+    doc.claims[property].push(newVal)
+  }
 
-    return doc
-  },
+  return updateInferredProperties(doc, property, oldVal, newVal)
+}
 
-  setLabels: (doc, labels) => {
-    Entity.preventRedirectionEdit(doc, 'setLabels')
-    for (const lang in labels) {
-      const value = labels[lang]
-      doc = Entity.setLabel(doc, lang, value)
+export function beforeEntityDocSave (doc) {
+  // Do not validate redirections, removed placeholder, etc
+  if (doc.claims != null) {
+    validateRequiredPropertiesValues(doc.claims)
+  }
+  doc.updated = Date.now()
+  doc.version++
+  return doc
+}
+
+// 'from' and 'to' refer to the redirection process which rely on merging
+// two existing document: redirecting from an entity to another entity,
+// only the 'to' doc will survive
+export function mergeEntitiesDocs (fromEntityDoc, toEntityDoc) {
+  preventRedirectionEdit(fromEntityDoc)
+  preventRedirectionEdit(toEntityDoc)
+
+  for (const lang in fromEntityDoc.labels) {
+    const value = fromEntityDoc.labels[lang]
+    if (toEntityDoc.labels[lang] == null) {
+      toEntityDoc.labels[lang] = value
     }
+  }
 
-    return doc
-  },
-
-  addClaims: (doc, claims) => {
-    Entity.preventRedirectionEdit(doc, 'addClaims')
-
-    // Pass the list of all edited properties, so that wen trying to infer property
-    // values, we know which one should not be infered at the risk of creating
-    // a conflict
-    doc._allClaimsProps = Object.keys(claims)
-
-    for (const property in claims) {
-      const array = claims[property]
-      for (const value of array) {
-        doc = Entity.createClaim(doc, property, value)
-      }
-    }
-
-    delete doc._allClaimsProps
-
-    return doc
-  },
-
-  createClaim: (doc, property, value) => {
-    Entity.preventRedirectionEdit(doc, 'createClaim')
-    return Entity.updateClaim(doc, property, null, value)
-  },
-
-  updateClaim: (doc, property, oldVal, newVal) => {
-    const context = { doc, property, oldVal, newVal }
-    Entity.preventRedirectionEdit(doc, 'updateClaim')
-    if (oldVal == null && newVal == null) {
-      throw newError('missing old or new value', 400, context)
-    }
-
-    if (isString(oldVal)) oldVal = superTrim(oldVal)
-    if (isString(newVal)) newVal = superTrim(newVal)
-
-    let propArray = get(doc, `claims.${property}`)
-
-    if (propArray && newVal != null && propArray.includes(newVal)) {
-      throw newError('claim property new value already exist', 400, [ propArray, newVal ])
-    }
-
-    if (oldVal != null) {
-      if (propArray != null) {
-        propArray = propArray.map(value => isString(value) ? superTrim(value) : value)
-      }
-      if (propArray == null || !propArray.includes(oldVal)) {
-        throw newError('claim property value not found', 400, context)
-      }
-
-      if (newVal != null) {
-        const oldValIndex = propArray.indexOf(oldVal)
-        doc.claims[property][oldValIndex] = newVal
-      } else {
-        // if the new value is null, it plays the role of a removeClaim
-        propArray = without(propArray, oldVal)
-
-        setPossiblyEmptyPropertyArray(doc, property, propArray)
-      }
-    } else {
-      // if the old value is null, it plays the role of a createClaim
-      if (!doc.claims[property]) doc.claims[property] = []
-      doc.claims[property].push(newVal)
-    }
-
-    return updateInferredProperties(doc, property, oldVal, newVal)
-  },
-
-  beforeSave: doc => {
-    // Do not validate redirections, removed placeholder, etc
-    if (doc.claims != null) {
-      validateRequiredPropertiesValues(doc.claims)
-    }
-    doc.updated = Date.now()
-    doc.version++
-    return doc
-  },
-
-  // 'from' and 'to' refer to the redirection process which rely on merging
-  // two existing document: redirecting from an entity to another entity,
-  // only the 'to' doc will survive
-  mergeDocs: (fromEntityDoc, toEntityDoc) => {
-    Entity.preventRedirectionEdit(fromEntityDoc, 'mergeDocs (from)')
-    Entity.preventRedirectionEdit(toEntityDoc, 'mergeDocs (to)')
-
-    for (const lang in fromEntityDoc.labels) {
-      const value = fromEntityDoc.labels[lang]
-      if (toEntityDoc.labels[lang] == null) {
-        toEntityDoc.labels[lang] = value
-      }
-    }
-
-    for (const property in fromEntityDoc.claims) {
-      const values = fromEntityDoc.claims[property]
-      if (toEntityDoc.claims[property] == null) { toEntityDoc.claims[property] = [] }
-      for (const value of values) {
-        if (!toEntityDoc.claims[property].includes(value)) {
-          if (toEntityDoc.claims[property].length > 0) {
-            if (properties[property].uniqueValue) {
-              warn(value, `${property} can have only one value: ignoring merged entity value`)
-            } else if (properties[property].hasPlaceholders) {
-              warn(value, `${property} values may be placeholders: ignoring merged entity value`)
-            } else {
-              toEntityDoc.claims[property].push(value)
-            }
+  for (const property in fromEntityDoc.claims) {
+    const values = fromEntityDoc.claims[property]
+    if (toEntityDoc.claims[property] == null) { toEntityDoc.claims[property] = [] }
+    for (const value of values) {
+      if (!toEntityDoc.claims[property].includes(value)) {
+        if (toEntityDoc.claims[property].length > 0) {
+          if (properties[property].uniqueValue) {
+            warn(value, `${property} can have only one value: ignoring merged entity value`)
+          } else if (properties[property].hasPlaceholders) {
+            warn(value, `${property} values may be placeholders: ignoring merged entity value`)
           } else {
             toEntityDoc.claims[property].push(value)
           }
+        } else {
+          toEntityDoc.claims[property].push(value)
         }
       }
     }
+  }
 
-    return toEntityDoc
-  },
-
-  turnIntoRedirection: (fromEntityDoc, toUri, removedPlaceholdersIds) => {
-    const [ prefix, id ] = toUri.split(':')
-
-    if (prefix === 'inv' && id === fromEntityDoc._id) {
-      throw newError('circular redirection', 500, { fromEntityDoc, toUri, removedPlaceholdersIds })
-    }
-
-    const redirection = cloneDeep(fromEntityDoc)
-
-    redirection.redirect = toUri
-    delete redirection.labels
-    delete redirection.claims
-    // the list of placeholders entities to recover if the merge as to be reverted
-    redirection.removedPlaceholdersIds = removedPlaceholdersIds
-
-    return redirection
-  },
-
-  removePlaceholder: entityDoc => {
-    if (entityDoc.redirect) {
-      const message = "can't turn a redirection into a removed placeholder"
-      throw newError(message, 400, entityDoc)
-    }
-
-    const removedDoc = cloneDeep(entityDoc)
-    removedDoc.type = 'removed:placeholder'
-    return removedDoc
-  },
-
-  recoverPlaceholder: entityDoc => {
-    const recoveredDoc = cloneDeep(entityDoc)
-    recoveredDoc.type = 'entity'
-    return recoveredDoc
-  },
-
-  preventRedirectionEdit: (doc, editLabel) => {
-    if (doc.redirect == null) return
-    throw newError(`${editLabel} failed: the entity is a redirection`, 400, { doc, editLabel })
-  },
+  return toEntityDoc
 }
 
-export default Entity
+export function convertEntityDocIntoARedirection (fromEntityDoc, toUri, removedPlaceholdersIds = []) {
+  const [ prefix, id ] = toUri.split(':')
 
-const updateInferredProperties = (doc, property, oldVal, newVal) => {
+  if (prefix === 'inv' && id === fromEntityDoc._id) {
+    throw newError('circular redirection', 500, { fromEntityDoc, toUri, removedPlaceholdersIds })
+  }
+
+  const redirection = cloneDeep(fromEntityDoc)
+
+  redirection.redirect = toUri
+  delete redirection.labels
+  delete redirection.claims
+  // the list of placeholders entities to recover if the merge as to be reverted
+  redirection.removedPlaceholdersIds = removedPlaceholdersIds
+
+  return redirection
+}
+
+export function convertEntityDocToPlaceholder (entityDoc) {
+  if (entityDoc.redirect) {
+    const message = "can't turn a redirection into a removed placeholder"
+    throw newError(message, 400, entityDoc)
+  }
+
+  const removedDoc = cloneDeep(entityDoc)
+  removedDoc.type = 'removed:placeholder'
+  return removedDoc
+}
+
+export function recoverEntityDocFromPlaceholder (entityDoc) {
+  const recoveredDoc = cloneDeep(entityDoc)
+  recoveredDoc.type = 'entity'
+  return recoveredDoc
+}
+
+export function preventRedirectionEdit (doc) {
+  if (doc.redirect == null) return
+  throw newError('entity edit failed: the entity is a redirection', 400, { doc })
+}
+
+function updateInferredProperties (doc, property, oldVal, newVal) {
   const declaredProperties = doc._allClaimsProps || []
   // Use _allClaimsProps to list properties that shouldn't be inferred
   const propInferences = omit(inferences[property], declaredProperties)
@@ -283,7 +279,7 @@ const updateInferredProperties = (doc, property, oldVal, newVal) => {
   return doc
 }
 
-const setPossiblyEmptyPropertyArray = (doc, property, propertyArray) => {
+function setPossiblyEmptyPropertyArray (doc, property, propertyArray) {
   if (propertyArray.length === 0) {
     // if empty, clean the doc from the property
     doc.claims = omit(doc.claims, property)
@@ -292,7 +288,7 @@ const setPossiblyEmptyPropertyArray = (doc, property, propertyArray) => {
   }
 }
 
-const deleteLabel = (doc, lang) => {
+function deleteLabel (doc, lang) {
   if (doc.labels[lang] == null) {
     throw newError('can not delete a non-existant label', 400, { doc, lang })
   }

server/controllers/entities/lib/create_inv_entity.ts
@@ -1,6 +1,6 @@
 import { editInvEntity } from '#controllers/entities/lib/entities'
 import { log } from '#lib/utils/logs'
-import Entity from '#models/entity'
+import { createBlankEntityDoc } from '#models/entity'
 import { prefixifyInv } from './prefix.js'
 import validateEntity from './validate_entity.js'
 
@@ -10,7 +10,7 @@ export default async params => {
 
   await validateEntity({ labels, claims })
 
-  const blankEntityDoc = Entity.create()
+  const blankEntityDoc = createBlankEntityDoc()
 
   const entity = await editInvEntity({
     create: true,

server/controllers/entities/lib/entities.ts
@@ -7,7 +7,7 @@ import { getUrlFromImageHash } from '#lib/images'
 import { toIsbn13h } from '#lib/isbn/isbn'
 import { emit } from '#lib/radio'
 import { assert_ } from '#lib/utils/assert_types'
-import Entity from '#models/entity'
+import { addEntityDocClaims, beforeEntityDocSave, setEntityDocLabels } from '#models/entity'
 import getInvEntityCanonicalUri from './get_inv_entity_canonical_uri.js'
 import createPatch from './patches/create_patch.js'
 import { validateProperty } from './properties/validations.js'
@@ -87,8 +87,8 @@ export async function getInvEntitiesClaimValueCount (value) {
 export async function editInvEntity (params) {
   const { userId, updatedLabels, updatedClaims, currentDoc, batchId, create } = params
   let updatedDoc = cloneDeep(currentDoc)
-  updatedDoc = Entity.setLabels(updatedDoc, updatedLabels)
-  updatedDoc = Entity.addClaims(updatedDoc, updatedClaims)
+  updatedDoc = setEntityDocLabels(updatedDoc, updatedLabels)
+  updatedDoc = addEntityDocClaims(updatedDoc, updatedClaims)
   return putInvEntityUpdate({ userId, currentDoc, updatedDoc, batchId, create })
 }
 
@@ -96,7 +96,7 @@ export async function putInvEntityUpdate (params) {
   const { userId, currentDoc, updatedDoc, create } = params
   assert_.types([ 'string', 'object', 'object' ], [ userId, currentDoc, updatedDoc ])
 
-  Entity.beforeSave(updatedDoc)
+  beforeEntityDocSave(updatedDoc)
 
   // It is to the consumers responsability to check if there is an update:
   // empty patches at this stage will throw 500 errors

server/controllers/entities/lib/merge_entities.ts
@@ -3,7 +3,7 @@ import { getEntitiesByIds, putInvEntityUpdate } from '#controllers/entities/lib/
 import { newError } from '#lib/error/error'
 import { assert_ } from '#lib/utils/assert_types'
 import { info } from '#lib/utils/logs'
-import Entity from '#models/entity'
+import { mergeEntitiesDocs } from '#models/entity'
 import getInvEntityCanonicalUri from './get_inv_entity_canonical_uri.js'
 import turnIntoRedirection from './turn_into_redirection.js'
 
@@ -50,7 +50,7 @@ const mergeInvEntities = async (userId, fromId, toId) => {
   // Transfer all data from the 'fromEntity' to the 'toEntity'
   // if any difference can be found
   const toEntityDocBeforeMerge = cloneDeep(toEntityDoc)
-  const toEntityDocAfterMerge = Entity.mergeDocs(fromEntityDoc, toEntityDoc)
+  const toEntityDocAfterMerge = mergeEntitiesDocs(fromEntityDoc, toEntityDoc)
 
   // If the doc hasn't changed, don't run putInvEntityUpdate
   // as it will throw an 'empty patch' error

server/controllers/entities/lib/placeholders.ts
@@ -9,14 +9,13 @@ import { putInvEntityUpdate } from '#controllers/entities/lib/entities'
 import dbFactory from '#db/couchdb/base'
 import { emit } from '#lib/radio'
 import { warn } from '#lib/utils/logs'
-import Entity from '#models/entity'
+import { convertEntityDocToPlaceholder, recoverEntityDocFromPlaceholder } from '#models/entity'
 
 const db = await dbFactory('entities')
 
-const PlaceholderHandler = actionName => {
-  const modelFnName = `${actionName}Placeholder`
+function PlaceholderHandler (actionName, modelFn) {
   return async (userId, entityId) => {
-    warn(entityId, `${modelFnName} entity`)
+    warn(entityId, `${actionName} placeholder entity`)
     // Using db.get anticipates a possible future where db.byId filters-out
     // non type='entity' docs, thus making type='removed:placeholder' not accessible
     const currentDoc = await db.get(entityId)
@@ -26,7 +25,7 @@ const PlaceholderHandler = actionName => {
     }
     let updatedDoc
     try {
-      updatedDoc = Entity[modelFnName](currentDoc)
+      updatedDoc = modelFn(currentDoc)
     } catch (err) {
       if (err.message === "can't turn a redirection into a removed placeholder") {
         // Ignore this error as the effects of those two states are close
@@ -44,5 +43,5 @@ const PlaceholderHandler = actionName => {
   }
 }
 
-export const removePlaceholder = PlaceholderHandler('remove')
-export const recoverPlaceholder = PlaceholderHandler('recover')
+export const removePlaceholder = PlaceholderHandler('remove', convertEntityDocToPlaceholder)
+export const recoverPlaceholder = PlaceholderHandler('recover', recoverEntityDocFromPlaceholder)

server/controllers/entities/lib/redirect_claims.ts
@@ -2,7 +2,6 @@ import { cloneDeep, keyBy, map } from 'lodash-es'
 import { getInvClaimsByClaimValue, getEntitiesByIds, putInvEntityUpdate } from '#controllers/entities/lib/entities'
 import { retryOnConflict } from '#lib/retry_on_conflict'
 import { log } from '#lib/utils/logs'
-import Entity from '#models/entity'
 
 async function _redirectClaims (userId, fromUri, toUri) {
   const results = await getInvClaimsByClaimValue(fromUri)
@@ -47,7 +46,7 @@ const applyRedirections = (entitiesIndex, fromUri, toUri) => result => {
     newVal = toUri
   }
 
-  entitiesIndex[entity] = Entity.updateClaim(doc, property, fromUri, newVal)
+  entitiesIndex[entity] = updateEntityDocClaim(doc, property, fromUri, newVal)
 }
 
 export const redirectClaims = retryOnConflict({ updateFn: _redirectClaims })

server/controllers/entities/lib/turn_into_redirection.ts
@@ -3,7 +3,7 @@ import { getInvClaimsByClaimValue, getEntityById, putInvEntityUpdate } from '#co
 import { removePlaceholder } from '#controllers/entities/lib/placeholders'
 import { assert_ } from '#lib/utils/assert_types'
 import { log } from '#lib/utils/logs'
-import Entity from '#models/entity'
+import { convertEntityDocIntoARedirection, preventRedirectionEdit } from '#models/entity'
 import propagateRedirection from './propagate_redirection.js'
 
 export default async ({ userId, fromId, toUri, previousToUri, context }) => {
@@ -13,10 +13,10 @@ export default async ({ userId, fromId, toUri, previousToUri, context }) => {
   const fromUri = `inv:${fromId}`
 
   const currentFromDoc = await getEntityById(fromId)
-  Entity.preventRedirectionEdit(currentFromDoc, 'turnIntoRedirection')
+  preventRedirectionEdit(currentFromDoc)
   // If an author has no more links to it, remove it
   const removedIds = await removeObsoletePlaceholderEntities(userId, currentFromDoc)
-  const updatedFromDoc = Entity.turnIntoRedirection(currentFromDoc, toUri, removedIds)
+  const updatedFromDoc = convertEntityDocIntoARedirection(currentFromDoc, toUri, removedIds)
   await putInvEntityUpdate({
     userId,
     currentDoc: currentFromDoc,

commit d5e14c0145b0e369d450fae2e6bfa258c07f8e8b

entity model: fix renaming error
error introduced by a51a6a5

server/models/entity.ts
@@ -94,7 +94,7 @@ export function addEntityDocClaims (doc: CustomInvEntity, claims: Claims) {
 
   for (const [ property, array ] of Object.entries(claims) as Entries<typeof claims>) {
     for (const value of array) {
-      doc = createBlankEntityDocClaim(doc, property, value)
+      doc = createEntityDocClaim(doc, property, value)
     }
   }
 
@@ -103,7 +103,7 @@ export function addEntityDocClaims (doc: CustomInvEntity, claims: Claims) {
   return doc
 }
 
-export function createBlankEntityDocClaim (doc: InvEntity, property: PropertyUri, value: InvClaimValue) {
+export function createEntityDocClaim (doc: InvEntity, property: PropertyUri, value: InvClaimValue) {
   preventRedirectionEdit(doc)
   return updateEntityDocClaim(doc, property, null, value)
 }

tests/unit/models/entity.ts
@@ -1,6 +1,6 @@
 import should from 'should'
 import { superTrim } from '#lib/utils/base'
-import { beforeEntityDocSave, createBlankEntityDoc, mergeEntitiesDocs, setEntityDocLabel, convertEntityDocIntoARedirection, convertEntityDocToPlaceholder, createBlankEntityDocClaim, updateEntityDocClaim } from '#models/entity'
+import { beforeEntityDocSave, createBlankEntityDoc, mergeEntitiesDocs, setEntityDocLabel, convertEntityDocIntoARedirection, convertEntityDocToPlaceholder, createEntityDocClaim, updateEntityDocClaim } from '#models/entity'
 import type { CouchRevId, CouchUuid } from '#server/types/couchdb'
 import type { EntityRedirection, InvEntity } from '#server/types/entity'
 import { shouldNotBeCalled } from '#tests/unit/utils'
@@ -51,32 +51,32 @@ describe('entity model', () => {
 
   describe('create claim', () => {
     it('should add a claim value', () => {
-      const doc = createBlankEntityDocClaim(workDoc(), 'wdt:P50', 'wd:Q42')
+      const doc = createEntityDocClaim(workDoc(), 'wdt:P50', 'wd:Q42')
       doc.claims['wdt:P50'].at(-1).should.equal('wd:Q42')
     })
 
     it('should return a doc with the new value for an existing property', () => {
       const entityDoc = workDoc()
       const lengthBefore = entityDoc.claims['wdt:P50'].length
-      const updatedDoc = createBlankEntityDocClaim(entityDoc, 'wdt:P50', 'wd:Q42')
+      const updatedDoc = createEntityDocClaim(entityDoc, 'wdt:P50', 'wd:Q42')
       updatedDoc.claims['wdt:P50'].length.should.equal(lengthBefore + 1)
-      const updatedDoc2 = createBlankEntityDocClaim(entityDoc, 'wdt:P135', 'wd:Q53121')
+      const updatedDoc2 = createEntityDocClaim(entityDoc, 'wdt:P135', 'wd:Q53121')
       updatedDoc2.claims['wdt:P135'][0].should.equal('wd:Q53121')
     })
 
     it('should return a doc with the new value for a new property', () => {
-      const updatedDoc = createBlankEntityDocClaim(workDoc(), 'wdt:P135', 'wd:Q53121')
+      const updatedDoc = createEntityDocClaim(workDoc(), 'wdt:P135', 'wd:Q53121')
       updatedDoc.claims['wdt:P135'][0].should.equal('wd:Q53121')
     })
 
     it('should return a doc with the new value added last', () => {
-      const updatedDoc = createBlankEntityDocClaim(workDoc(), 'wdt:P50', 'wd:Q42')
+      const updatedDoc = createEntityDocClaim(workDoc(), 'wdt:P50', 'wd:Q42')
       updatedDoc.claims['wdt:P50'].at(-1).should.equal('wd:Q42')
     })
 
     it('should throw if the new value already exist', () => {
       const entityDoc = workDoc()
-      const updater = () => createBlankEntityDocClaim(entityDoc, 'wdt:P50', 'wd:Q1541')
+      const updater = () => createEntityDocClaim(entityDoc, 'wdt:P50', 'wd:Q1541')
       updater.should.throw()
     })
   })
@@ -292,7 +292,7 @@ describe('entity model', () => {
       it('should transfer claims', () => {
         const entityA = workDoc()
         const entityB = workDoc()
-        createBlankEntityDocClaim(entityA, 'wdt:P921', 'wd:Q3')
+        createEntityDocClaim(entityA, 'wdt:P921', 'wd:Q3')
         mergeEntitiesDocs(entityA, entityB)
         entityB.claims['wdt:P921'].should.deepEqual([ 'wd:Q3' ])
       })
@@ -300,8 +300,8 @@ describe('entity model', () => {
       it('should add new claims on already used property', () => {
         const entityA = workDoc()
         const entityB = workDoc()
-        createBlankEntityDocClaim(entityA, 'wdt:P921', 'wd:Q3')
-        createBlankEntityDocClaim(entityB, 'wdt:P921', 'wd:Q1')
+        createEntityDocClaim(entityA, 'wdt:P921', 'wd:Q3')
+        createEntityDocClaim(entityB, 'wdt:P921', 'wd:Q1')
         mergeEntitiesDocs(entityA, entityB)
         entityB.claims['wdt:P921'].should.deepEqual([ 'wd:Q1', 'wd:Q3' ])
       })
@@ -317,8 +317,8 @@ describe('entity model', () => {
       it('should not create duplicated claims', () => {
         const entityA = workDoc()
         const entityB = workDoc()
-        createBlankEntityDocClaim(entityA, 'wdt:P921', 'wd:Q3')
-        createBlankEntityDocClaim(entityB, 'wdt:P921', 'wd:Q3')
+        createEntityDocClaim(entityA, 'wdt:P921', 'wd:Q3')
+        createEntityDocClaim(entityB, 'wdt:P921', 'wd:Q3')
         mergeEntitiesDocs(entityA, entityB)
         entityB.claims['wdt:P921'].should.deepEqual([ 'wd:Q3' ])
       })
@@ -326,8 +326,8 @@ describe('entity model', () => {
       it('should keep the target claim in case of claim uniqueness restrictions', () => {
         const entityA = workDoc()
         const entityB = workDoc()
-        createBlankEntityDocClaim(entityA, 'wdt:P648', 'OL123456W')
-        createBlankEntityDocClaim(entityB, 'wdt:P648', 'OL123457W')
+        createEntityDocClaim(entityA, 'wdt:P648', 'OL123456W')
+        createEntityDocClaim(entityB, 'wdt:P648', 'OL123457W')
         mergeEntitiesDocs(entityA, entityB).claims['wdt:P648'].should.deepEqual([ 'OL123457W' ])
       })
 