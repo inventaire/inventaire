3cb76ca9e5a1981b33f88def3fca9d8aafac5dec
maxlath <k@maxlath.eu>
2024-05-16 22:53:06 +0200

    findOrdinalBetween: fix edge case and add tests

---
+++ tests/unit/libs/find_ordinal.ts
@@ -0,0 +1,45 @@
+import should from 'should'
+import { findOrdinalBetween } from '#lib/find_ordinal'
+import { getRandomString } from '#lib/utils/random_string'
+
+describe('findOrdinalBetween', () => {
+  it('should find an ordinal between two ordinals', () => {
+    const ordinalA = '00000'
+    const ordinalB = 'zzzzzzz'
+    const ordinalC = findOrdinalBetween(ordinalA, ordinalB)
+    should(ordinalC > ordinalA).be.true()
+    should(ordinalC < ordinalB).be.true()
+  })
+
+  it('should find an ordinal when not all characters in the two ordinals are compare in the same direction', () => {
+    const ordinalA = 'azU'
+    const ordinalB = 'ba'
+    const ordinalC = findOrdinalBetween(ordinalA, ordinalB)
+    should(ordinalC > ordinalA).be.true()
+    should(ordinalC < ordinalB).be.true()
+  })
+
+  it('should find an ordinal between two random ordinals', () => {
+    let i = 0
+    while (i++ < 100) {
+      let ordinalA = getRandomString(10).replace(/[_]]g/, '')
+      let ordinalB = getRandomString(10).replace(/[_]]g/, '')
+      if (ordinalA > ordinalB) [ ordinalA, ordinalB ] = [ ordinalB, ordinalA ]
+      const ordinalC = findOrdinalBetween(ordinalA, ordinalB)
+      should(ordinalC > ordinalA).be.true()
+      should(ordinalC < ordinalB).be.true()
+    }
+  })
+
+  it('should always be able to find an ordinal between two very close ordinals', () => {
+    let i = 0
+    let ordinalA = 'abcd'
+    const ordinalB = 'abce'
+    while (i++ < 100) {
+      const ordinalC = findOrdinalBetween(ordinalA, ordinalB)
+      should(ordinalC > ordinalA).be.true()
+      should(ordinalC < ordinalB).be.true()
+      ordinalA = ordinalC
+    }
+  })
+})
