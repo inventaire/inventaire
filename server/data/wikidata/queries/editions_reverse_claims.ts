7adb156a6af9421a962350d5946926a899ce9c8f
maxlath <k@maxlath.eu>
2024-05-30 16:22:23 +0200

    wikidata queries: editions_reverse_claims: improve work filter

--- server/data/wikidata/queries/editions_reverse_claims.ts
+++ server/data/wikidata/queries/editions_reverse_claims.ts
@@ -1,4 +1,7 @@
 import type { SparqlQueryParams } from '#data/wikidata/queries/queries'
+import { typesAliases } from '#lib/wikidata/aliases'
+
+const { works: worksP31Values } = typesAliases
 
 export default {
   parameters: [ 'pid', 'qid' ] as const,
@@ -11,8 +14,8 @@ export default {
   ?item wdt:${pid} wd:${qid} .
   ?item wdt:P31 wd:Q3331189 .
   # Filter-out entities tagged as both work and edition
-  FILTER NOT EXISTS { ?item wdt:P31 wd:Q571 }
-  FILTER NOT EXISTS { ?item wdt:P31 wd:Q47461344 }
+  VALUES (?work_type) { ${worksP31Values.map(uri => `(${uri})`).join(' ')} }
+  FILTER NOT EXISTS { ?item wdt:P31 ?work_type }
 }
 LIMIT 1000`
   },
