ef2a84a80fb16626e09f742d04800ffd83eab0a8
maxlath <k@maxlath.eu>
2024-06-01 18:58:18 +0200

    boolean_validations: add isInvPropertyUri and isWdPropertyUri

--- server/lib/regex.ts
+++ server/lib/regex.ts
@@ -11,12 +11,13 @@ export const Float = /^-?[\d.]+$/
 export const GroupImg = /^\/img\/groups\/[0-9a-f]{40}$/
 export const ImageHash = /^[0-9a-f]{40}$/
 export const Integer = /^-?\d+$/
+export const InvPropertyUri = /^invp:P[1-9]\d*$/
 // Accepting second level languages (like es-AR)
 export const Lang = /^\w{2,3}(-\w{2})?$/
 export const LocalImg = /^\/img\/(users|groups|entities)\/[0-9a-f]{40}$/
 export const PatchId = /^[0-9a-f]{32}:[1-9]\d{0,3}$/
 export const PositiveInteger = /^\d+$/
-export const PropertyUri = /^(wdt|invp):P\d+$/
+export const PropertyUri = /^(wdt|invp):P[1-9]\d*$/
 // A year can't start by a 0
 export const Sha1 = /^[0-9a-f]{40}$/
 export const SimpleDay = /^-?([1-9]{1}[0-9]{0,3}|0)(-\d{2})?(-\d{2})?$/
@@ -26,3 +27,4 @@ export const UserImg = /^\/img\/users\/[0-9a-f]{40}$/
 // all 1 letter strings are reserved for the application
 export const Username = /^[\p{Letter}\p{Number}_]{2,20}$/u
 export const Uuid = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+export const WdPropertyUri = /^wdt:P[1-9]\d*$/
