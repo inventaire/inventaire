840f3ec9f82ff8f8c5e66a92ad107d8338933756
maxlath <k@maxlath.eu>
2024-08-03 16:32:22 +0200

    node version: add support for v20
    which requires to replace --import with --loader, see https://github.com/privatenumber/tsx/issues/349
    
    Hopefully, this would resolve the issues with tsx watch/chokidar not detecting file changes on git checkout/switch
    
    Possibly related https://github.com/privatenumber/tsx/issues/246

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,8 +1,16 @@
+const nodeOptionsBeforeV20 = [
+  'loader=tsx/esm',
+  // Mute node error: (node:29544) ExperimentalWarning: `--experimental-loader` may be removed in the future; instead use `register()`
+  'no-warnings',
+]
+
+const nodeOptionsFromV20 = [
+  'import=tsx/esm',
+]
+
+const nodeVersion = parseInt(process.version.split('.')[0].slice(1))
+
 module.exports = {
   extension: 'ts',
-  'node-option': [
-    'loader=tsx/esm',
-    // Mute node error: (node:29544) ExperimentalWarning: `--experimental-loader` may be removed in the future; instead use `register()`
-    'no-warnings',
-  ],
+  'node-option': nodeVersion >= 20 ? nodeOptionsFromV20 : nodeOptionsBeforeV20,
 }
