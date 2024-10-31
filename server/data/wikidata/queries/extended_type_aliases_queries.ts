6a700b8f01da1458c6ef18756b7a174e82910a49
maxlath <k@maxlath.eu>
2024-10-31 15:38:48 +0100

    extended_type_aliases_queries: add queries for editions, series, and collections
    Some tweaking is still required for works and humans as the requests timeout

--- server/data/wikidata/queries/extended_type_aliases_queries.ts
+++ server/data/wikidata/queries/extended_type_aliases_queries.ts
@@ -1,15 +1,62 @@
-import { typesAliases } from '#lib/wikidata/aliases'
-import type { EntityType } from '#server/types/entity'
+import { typesAliases, type PluralizedEntityType } from '#lib/wikidata/aliases'
 
-const { publishers: publishersP31Values } = typesAliases
+const {
+  editions: editionP31Values,
+  works: workP31Values,
+  series: serieP31Values,
+  humans: humanP31Values,
+  publishers: publisherP31Values,
+  collections: collectionP31Values,
+} = typesAliases
 
-const publishersAliasesQuery = `SELECT DISTINCT ?publisher_type {
-  VALUES (?wellknown_publisher_type) { ${publishersP31Values.map(uri => `(${uri})`).join(' ')} }
-  ?publisher_type wdt:P279* ?wellknown_publisher_type .
-  FILTER NOT EXISTS { ?publisher_type wdt:P31 ?wellknown_publisher_type }
-  FILTER EXISTS { ?publisher wdt:P31 ?publisher_type }
+const editionsAliasesQuery = `SELECT DISTINCT ?type {
+  VALUES (?wellknown_type) { ${editionP31Values.map(uri => `(${uri})`).join(' ')} }
+  ?type wdt:P279+ ?wellknown_type .
+  FILTER NOT EXISTS { ?type wdt:P31 ?wellknown_type }
+  FILTER EXISTS { ?instance wdt:P31 ?type }
+}`
+
+const worksAliasesQuery = `SELECT DISTINCT ?type {
+  VALUES (?wellknown_type) { ${workP31Values.map(uri => `(${uri})`).join(' ')} }
+  ?type wdt:P279+ ?wellknown_type .
+  FILTER NOT EXISTS { ?type wdt:P31 ?wellknown_type }
+  FILTER EXISTS { ?instance wdt:P31 ?type }
+}`
+
+const seriesAliasesQuery = `SELECT DISTINCT ?type {
+  VALUES (?wellknown_type) { ${serieP31Values.map(uri => `(${uri})`).join(' ')} }
+  ?type wdt:P279+ ?wellknown_type .
+  FILTER NOT EXISTS { ?type wdt:P31 ?wellknown_type }
+  FILTER EXISTS { ?instance wdt:P31 ?type }
+}`
+
+// TODO: include collectives
+const humansAliasesQuery = `SELECT DISTINCT ?type {
+  VALUES (?wellknown_type) { ${humanP31Values.map(uri => `(${uri})`).join(' ')} }
+  ?type wdt:P279+ ?wellknown_type .
+  FILTER NOT EXISTS { ?type wdt:P31 ?wellknown_type }
+  FILTER EXISTS { ?instance wdt:P31 ?type }
+}`
+
+const publishersAliasesQuery = `SELECT DISTINCT ?type {
+  VALUES (?wellknown_type) { ${publisherP31Values.map(uri => `(${uri})`).join(' ')} }
+  ?type wdt:P279+ ?wellknown_type .
+  FILTER NOT EXISTS { ?type wdt:P31 ?wellknown_type }
+  FILTER EXISTS { ?instance wdt:P31 ?type }
+}`
+
+const collectionsAliasesQuery = `SELECT DISTINCT ?type {
+  VALUES (?wellknown_type) { ${collectionP31Values.map(uri => `(${uri})`).join(' ')} }
+  ?type wdt:P279+ ?wellknown_type .
+  FILTER NOT EXISTS { ?type wdt:P31 ?wellknown_type }
+  FILTER EXISTS { ?instance wdt:P31 ?type }
 }`
 
 export const extendedAliasesQueries = {
-  publisher: publishersAliasesQuery,
-} satisfies Partial<Record<EntityType, string>>
+  editions: editionsAliasesQuery,
+  // works: worksAliasesQuery,
+  series: seriesAliasesQuery,
+  // humans: humansAliasesQuery,
+  publishers: publishersAliasesQuery,
+  collections: collectionsAliasesQuery,
+} satisfies Partial<Record<PluralizedEntityType, string>>
