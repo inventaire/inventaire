ed4adabba2f7452933a436a0aa4ee93dae1e4aaa
maxlath <k@maxlath.eu>
2024-11-06 14:34:06 +0100

    tests: update_wd_claims: add test on invalid local claim updates

--- tests/api/entities/update_wd_claims.test.ts
+++ tests/api/entities/update_wd_claims.test.ts
@@ -1,5 +1,5 @@
 import should from 'should'
-import { getSomeWdEditionUri, someRandomImageHash } from '#fixtures/entities'
+import { getSomeWdEditionUri, someImageHash, someRandomImageHash } from '#fixtures/entities'
 import { validateP31Update } from '#lib/wikidata/validate_wd_update'
 import type { SimplifiedClaims } from '#server/types/entity'
 import { addClaim, getByUri, removeClaim, updateClaim } from '#tests/api/utils/entities'
@@ -83,13 +83,13 @@ describe('entities:update-claims:wd', () => {
       })
     })
 
-    it('should reject invalid inv properties', async () => {
-      const uri = await getSomeWdEditionUri()
-      await addClaim({ uri, property: 'invp:P20000', value: 'foo' })
+    it('should reject invalid local claim updates', async () => {
+      const remoteWorkUri = 'wd:Q18120925'
+      await addClaim({ uri: remoteWorkUri, property: 'invp:P2', value: someImageHash })
       .then(shouldNotBeCalled)
       .catch(err => {
         err.statusCode.should.equal(400)
-        err.body.status_verbose.should.equal("property isn't allowlisted")
+        err.body.status_verbose.should.equal("works can't have a property invp:P2")
       })
     })
   })
