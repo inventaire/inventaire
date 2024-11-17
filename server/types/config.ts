587908e0240883bc2e3d52cbb823e9b334e6d347
maxlath <k@maxlath.eu>
2024-11-17 09:42:06 +0100

    config: allow to set a remote entities endpoint

--- server/types/config.ts
+++ server/types/config.ts
@@ -74,6 +74,10 @@ export type Config = ReadonlyDeep<{
     minReindexationInterval: number
   }
 
+  federation: {
+    remoteEntitiesOrigin: AbsoluteUrl
+  }
+
   // See server/data/dataseed/dataseed.js
   dataseed: {
     enabled: boolean
