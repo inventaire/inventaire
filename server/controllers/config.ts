f4468a676ff92f46c0e93423e844155e8c5dcfc4
maxlath <k@maxlath.eu>
2024-11-17 10:25:59 +0100

    config controller: send remoteEntitiesOrigin to the client

--- server/controllers/config.ts
+++ server/controllers/config.ts
@@ -2,12 +2,14 @@ import { sendStaticJson } from '#lib/responses'
 import config from '#server/config'
 
 const { piwik, mapTilesAccessToken } = config
+const { remoteEntitiesOrigin } = config.federation
 
 const endpoint = piwik.enabled ? piwik.endpoint : null
 
 const clientConfig = JSON.stringify({
   piwik: endpoint && endpoint.replace('/piwik.php', ''),
   mapTilesAccessToken,
+  remoteEntitiesOrigin,
 })
 
 // A endpoint dedicated to pass configuration parameters to the client
