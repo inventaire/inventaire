c82fc12f2e2cd5e6f1bb4ac68a6719e54ab77420
maxlath <k@maxlath.eu>
2024-11-18 15:18:32 +0100

    + config/federated.cjs and scripts/start_federated_server.sh

---
+++ config/federated.cjs
@@ -0,0 +1,35 @@
+/** @typedef { import('../types/types.ts').Config } Config */
+/** @typedef { import('type-fest').PartialDeep } PartialDeep */
+
+/** @type {PartialDeep<Config>} */
+const config = {
+  env: 'federated',
+  // By convention, federated server port = (equivalent default server port + 10)
+  port: 3016,
+  db: {
+    suffix: 'federated',
+  },
+  federation: {
+    remoteEntitiesOrigin: 'http://localhost:3006',
+  },
+  dataseed: {
+    enabled: false,
+  },
+  mailer: {
+    disabled: true,
+  },
+  jobs: {
+    'inv:deduplicate': {
+      run: true,
+    },
+    'entity:popularity': {
+      run: true,
+    },
+    'wd:entity:indexation': {
+      run: true,
+    },
+  },
+  nice: false,
+}
+
+module.exports = config
