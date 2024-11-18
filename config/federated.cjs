2a4a101ccb4867f603611d1a7f66ce28b2bea9c6
maxlath <k@maxlath.eu>
2024-11-18 15:18:32 +0100

    + config/federated.cjs and scripts/start_federated_server.sh

---
+++ config/federated.cjs
@@ -0,0 +1,39 @@
+// Custom config for the API tests server
+// This config file will be used if: NODE_ENV=tests-api NODE_APP_INSTANCE=server
+// Override locally in ./local-tests-api-server.js
+
+/** @typedef { import('../types/types.ts').Config } Config */
+/** @typedef { import('type-fest').PartialDeep } PartialDeep */
+
+/** @type {PartialDeep<Config>} */
+const config = {
+  env: 'federated',
+  // By convention, federated server port = (equivalent default server port + 10)
+  port: 3016,
+  db: {
+    suffix: 'federated',
+  },
+  federation: {
+    remoteEntitiesOrigin: 'http://localhost:3006',
+  },
+  dataseed: {
+    enabled: false,
+  },
+  mailer: {
+    disabled: true,
+  },
+  jobs: {
+    'inv:deduplicate': {
+      run: true,
+    },
+    'entity:popularity': {
+      run: true,
+    },
+    'wd:entity:indexation': {
+      run: true,
+    },
+  },
+  nice: false,
+}
+
+module.exports = config
