de938f5823b2d467c2af24fe879a7da838715cc4
maxlath <k@maxlath.eu>
2024-11-18 21:57:33 +0100

    couchdb/elasticsearch: create empty entities databases rather than no databases
    It's easier to do it this way for now than to go to every consumer of `dbFactory('entities)` to make a switch

--- server/db/elasticsearch/formatters/formatters.ts
+++ server/db/elasticsearch/formatters/formatters.ts
@@ -1,4 +1,3 @@
-import config from '#server/config'
 import entities from './entity.js'
 import groups from './group.js'
 import items from './item.js'
@@ -6,18 +5,12 @@ import lists from './list.js'
 import shelves from './shelf.js'
 import users from './user.js'
 
-const federatedEntities = config.federation.remoteEntitiesOrigin != null
-
 export default {
   groups,
   items,
   shelves,
   lists,
   users,
-  ...(federatedEntities
-    ? {}
-    : {
-      wikidata: entities,
-      entities,
-    }),
+  wikidata: entities,
+  entities,
 }
