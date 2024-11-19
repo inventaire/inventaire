fd493452d0d660efd213742ce89a000caed545a3
maxlath <k@maxlath.eu>
2024-11-19 23:43:52 +0100

    instance_agnostic_entities: working on naming

--- server/controllers/entities/lib/remote/instance_agnostic_entities.ts
+++ server/controllers/entities/lib/remote/instance_agnostic_entities.ts
@@ -1,44 +1,44 @@
-import { getEntitiesByUris as getEntitiesByUrisFromLocalInstance, type GetEntitiesByUrisParams } from '#controllers/entities/lib/get_entities_by_uris'
-import { getEntitiesList as getEntitiesListFromLocalInstance } from '#controllers/entities/lib/get_entities_list'
-import { getEntityByUri as getEntityByUriFromLocalInstance } from '#controllers/entities/lib/get_entity_by_uri'
-import { getEntitiesByUrisFromRemoteInstance, getEntitiesListFromRemoteInstance, getEntityByUriFromRemoteInstance, getReverseClaimsFromRemoteInstance } from '#controllers/entities/lib/remote/get_remote_entities_by_uris'
-import { getReverseClaims as getReverseClaimsFromLocalInstance, type ReverseClaimsParams } from '#controllers/entities/lib/reverse_claims'
+import { getEntitiesByUris as getLocalEntitiesByUris, type GetEntitiesByUrisParams } from '#controllers/entities/lib/get_entities_by_uris'
+import { getEntitiesList as getLocalEntitiesList } from '#controllers/entities/lib/get_entities_list'
+import { getEntityByUri as getLocalEntityByUri } from '#controllers/entities/lib/get_entity_by_uri'
+import { getRemoteEntitiesByUris, getRemoteEntitiesList, getRemoteEntityByUri, getRemoteReverseClaims } from '#controllers/entities/lib/remote/get_remote_entities_by_uris'
+import { getReverseClaims as getLocalReverseClaims, type ReverseClaimsParams } from '#controllers/entities/lib/reverse_claims'
 import { info } from '#lib/utils/logs'
 import config from '#server/config'
 import type { EntityUri } from '#server/types/entity'
 
 const { remoteEntitiesOrigin } = config.federation
-const federatedEntities = remoteEntitiesOrigin != null
-if (federatedEntities) info({ remoteEntitiesOrigin }, 'federated entities mode')
+const federatedMode = remoteEntitiesOrigin != null
+if (federatedMode) info({ remoteEntitiesOrigin }, 'federated entities mode')
 
 export async function getEntitiesByUris ({ uris }: Pick<GetEntitiesByUrisParams, 'uris'>) {
-  if (federatedEntities) {
-    return getEntitiesByUrisFromRemoteInstance({ uris })
+  if (federatedMode) {
+    return getRemoteEntitiesByUris({ uris })
   } else {
-    return getEntitiesByUrisFromLocalInstance({ uris })
+    return getLocalEntitiesByUris({ uris })
   }
 }
 
 export async function getEntityByUri ({ uri }: { uri: EntityUri }) {
-  if (federatedEntities) {
-    return getEntityByUriFromRemoteInstance({ uri })
+  if (federatedMode) {
+    return getRemoteEntityByUri({ uri })
   } else {
-    return getEntityByUriFromLocalInstance({ uri })
+    return getLocalEntityByUri({ uri })
   }
 }
 
 export async function getEntitiesList (uris: EntityUri[]) {
-  if (federatedEntities) {
-    return getEntitiesListFromRemoteInstance(uris)
+  if (federatedMode) {
+    return getRemoteEntitiesList(uris)
   } else {
-    return getEntitiesListFromLocalInstance(uris)
+    return getLocalEntitiesList(uris)
   }
 }
 
 export async function getReverseClaims (params: ReverseClaimsParams) {
-  if (federatedEntities) {
-    return getReverseClaimsFromRemoteInstance(params)
+  if (federatedMode) {
+    return getRemoteReverseClaims(params)
   } else {
-    return getReverseClaimsFromLocalInstance(params)
+    return getLocalReverseClaims(params)
   }
 }
