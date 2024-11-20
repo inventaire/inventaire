03f38ba13cf9a69642dd113964a42e4f5c188cf2
maxlath <k@maxlath.eu>
2024-11-20 15:14:33 +0100

    setup federated server API tests

---
+++ config/tests-api-federated-server.cjs
@@ -0,0 +1,23 @@
+// Custom config for the API tests server
+// This config file will be used if: NODE_ENV=tests-api NODE_APP_INSTANCE=server
+// Override locally in ./local-tests-api-server.js
+
+/** @typedef { import('../types/types.ts').Config } Config */
+/** @typedef { import('type-fest').PartialDeep } PartialDeep */
+
+const testsApiServerConfig = require('./tests-api-server.cjs')
+
+/** @type {PartialDeep<Config>} */
+const config = {
+  ...testsApiServerConfig,
+  env: 'federated-tests',
+  port: 3013,
+  db: {
+    suffix: 'federated-tests',
+  },
+  federation: {
+    remoteEntitiesOrigin: 'http://localhost:3009',
+  },
+}
+
+module.exports = config
