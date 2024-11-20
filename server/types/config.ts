93351a525451dae206088a9023b176e997061eb0
maxlath <k@maxlath.eu>
2024-11-20 23:01:24 +0100

    federated config: send more customization config elements to the client

--- server/types/config.ts
+++ server/types/config.ts
@@ -75,6 +75,11 @@ export type Config = ReadonlyDeep<{
 
   federation: {
     remoteEntitiesOrigin: AbsoluteUrl
+    instanceClientCustomization: {
+      name: string
+      orgName: string
+      orgUrl: AbsoluteUrl
+    }
   }
 
   // See server/data/dataseed/dataseed.js
