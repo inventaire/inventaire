4cd82b20d75ea110358635aec523b10747757007
maxlath <k@maxlath.eu>
2024-11-20 23:01:24 +0100

    federated config: send more customization config elements to the client

--- server/controllers/config.ts
+++ server/controllers/config.ts
@@ -2,14 +2,15 @@ import { sendStaticJson } from '#lib/responses'
 import config from '#server/config'
 
 const { piwik, mapTilesAccessToken } = config
-const { remoteEntitiesOrigin } = config.federation
+const { remoteEntitiesOrigin, instanceClientCustomization } = config.federation
 
 const endpoint = piwik.enabled ? piwik.endpoint : null
 
 const clientConfig = JSON.stringify({
+  ...instanceClientCustomization,
+  remoteEntitiesOrigin,
   piwik: endpoint && endpoint.replace('/piwik.php', ''),
   mapTilesAccessToken,
-  remoteEntitiesOrigin,
 })
 
 // A endpoint dedicated to pass configuration parameters to the client
