a60de064ff791e81f1e060a0a804b46837a6f057
maxlath <k@maxlath.eu>
2024-11-22 19:44:55 +0100

    resolve_edition: fix case where non-edition entities are found when resolving external ids
    Typically, when an ISBN is set on a work on Wikidata

--- server/controllers/entities/lib/resolver/resolve_edition.ts
+++ server/controllers/entities/lib/resolver/resolve_edition.ts
@@ -1,6 +1,7 @@
-import { getEntityByUri } from '#controllers/entities/lib/get_entity_by_uri'
-import { isWdEntityUri } from '#lib/boolean_validations'
+import { map } from 'lodash-es'
+import { getEntitiesList } from '#controllers/entities/lib/get_entities_list'
 import { toIsbn13h } from '#lib/isbn/isbn'
+import { warn } from '#lib/utils/logs'
 import type { SanitizedResolverEntry } from '#server/types/resolver'
 import { resolveExternalIds } from './resolve_external_ids.js'
 
@@ -16,12 +17,22 @@ export async function resolveEdition (entry: SanitizedResolverEntry) {
   if (!matchingTriples) return
 
   let uri
-  if (matchingTriples.length === 1) {
-    uri = matchingTriples[0].subject
-    if (isWdEntityUri(uri)) {
-      const entity = await getEntityByUri({ uri })
-      // Get the canonical uri and check that the type is correct
-      uri = entity.type === 'edition' ? entity.uri : null
+  if (matchingTriples.length > 0) {
+    const uris = map(matchingTriples, 'subject')
+    // Get the canonical uri
+    const subjectEntities = await getEntitiesList(uris)
+    // and check that the type is correct
+    const matchingEditions = subjectEntities.filter(entity => entity.type === 'edition')
+    if (matchingEditions.length === 1) {
+      const entity = matchingEditions[0]
+      uri = entity.uri
+    } else {
+      const context = {
+        matchingTriples,
+        matchingEditions: map(matchingEditions, 'uri'),
+      }
+      // TODO: create merge tasks
+      warn(context, 'Too many matching editions found')
     }
   }
 
