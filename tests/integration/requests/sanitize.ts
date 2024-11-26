c9cc8e8db29969d1f0adb733771ef98d865b677c
maxlath <k@maxlath.eu>
2024-11-26 16:22:00 +0100

    move url sanitization outside of the request lib
    to not need the request function to accept anything other than absolute urls

--- tests/integration/requests/sanitize.ts
+++ tests/integration/requests/sanitize.ts
@@ -1,10 +1,10 @@
-import { requests_ } from '#lib/requests'
+import { sanitizeUrl } from '#lib/requests'
 import config from '#server/config'
 import { shouldNotBeCalled } from '#tests/unit/utils/utils'
 
 describe('requests:sanitize', () => {
   it('should reject private URLs', async () => {
-    await requests_.get(config.elasticsearch.origin, { sanitize: true })
+    await sanitizeUrl(config.elasticsearch.origin)
     .then(shouldNotBeCalled)
     .catch(err => {
       err.error_name.should.equal('invalid_url')
