05b9466c9fa26ef47fcefbd733343b0974957471
maxlath <k@maxlath.eu>
2024-11-27 01:09:40 +0100

    entities: sign patch with user account uri (aka userAcct)

--- server/controllers/user/lib/user.ts
+++ server/controllers/user/lib/user.ts
@@ -10,7 +10,9 @@ import { assert_ } from '#lib/utils/assert_types'
 import { toLowerCase } from '#lib/utils/base'
 import { setUserDocOauthTokens, addUserDocRole, removeUserDocRole, setUserDocStableUsername } from '#models/user'
 import userValidations from '#models/validations/user'
+import { publicHost } from '#server/config'
 import type { ImageHash } from '#types/image'
+import type { AccountUri } from '#types/server'
 import type { DocWithUsernameInUserDb, Email, User, UserId, UserRole, Username } from '#types/user'
 import { omitPrivateData, type UserExtraAttribute } from './authorized_user_data_pickers.js'
 import { byEmail, byEmails, findOneByEmail } from './shared_user_handlers.js'
@@ -164,4 +166,8 @@ export async function stopAllUserEmailNotifications (email) {
   return db.update(user._id, doc => {
     return set(doc, 'settings.notifications.global', false)
   })
+}
+
+export function getLocalUserAcct (userId: UserId) {
+  return `${userId}@${publicHost}` as AccountUri
 }
