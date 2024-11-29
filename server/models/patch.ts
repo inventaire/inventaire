0716c5c20ec5cdf0a77a7063e6f46ca8afe828c3
maxlath <k@maxlath.eu>
2024-11-29 13:10:43 +0100

    patch model: update to use user account uri and add types

--- server/models/patch.ts
+++ server/models/patch.ts
@@ -1,13 +1,15 @@
 import jiff from 'jiff'
 import { cloneDeep, get, isArray, pick } from 'lodash-es'
+import { isUserAcct } from '#lib/boolean_validations'
 import { newError } from '#lib/error/error'
-import { assertNumber, assertObject, assertString } from '#lib/utils/assert_types'
+import { assertNumber, assertObject } from '#lib/utils/assert_types'
 import { createBlankEntityDoc } from '#models/entity'
-import type { InvEntityDoc, NewInvEntity } from '#types/entity'
-import type { BatchId, Patch, PatchContext } from '#types/patch'
+import type { EntityRedirection, InvEntity, InvEntityDoc, NewInvEntity, RemovedPlaceholderEntity } from '#types/entity'
+import type { BatchId, OperationPath, Patch, PatchContext, PatchOperation, PatchWithSnapshot } from '#types/patch'
 import type { UserAccountUri } from '#types/server'
 import { versioned } from './attributes/entity.js'
 import validations from './validations/common.js'
+import type { SetOptional } from 'type-fest'
 
 interface CreatePatchDocParams {
   userAcct: UserAccountUri
@@ -19,7 +21,7 @@ interface CreatePatchDocParams {
 
 export function createPatchDoc (params: CreatePatchDocParams) {
   const { userAcct, currentDoc, updatedDoc, context, batchId } = params
-  assertString(userAcct)
+  if (!isUserAcct(userAcct)) throw newError('invalid user acct', 500, { userAcct })
   assertObject(currentDoc)
   assertObject(updatedDoc)
   validations.pass('couchUuid', updatedDoc._id)
@@ -68,14 +70,18 @@ export function createPatchDoc (params: CreatePatchDocParams) {
   return patch as Omit<Patch, '_rev'>
 }
 
-export function getPatchDiff (currentDoc, updatedDoc) {
-  currentDoc = pick(currentDoc, versioned)
-  updatedDoc = pick(updatedDoc, versioned)
-  return jiff.diff(currentDoc, updatedDoc)
+type VersionedInvEntity = Pick<InvEntity | RemovedPlaceholderEntity, 'type' | 'labels' | 'claims'>
+type VersionedRedirection = Pick<EntityRedirection, 'type' | 'redirect'>
+type VersionedInvEntityDoc = VersionedInvEntity | VersionedRedirection
+
+export function getPatchDiff (currentDoc: NewInvEntity | InvEntityDoc, updatedDoc: InvEntityDoc) {
+  const versionedCurrentDoc = pick(currentDoc, versioned) as VersionedInvEntityDoc
+  const versionedUpdatedDoc = pick(updatedDoc, versioned) as VersionedInvEntityDoc
+  return jiff.diff(versionedCurrentDoc, versionedUpdatedDoc)
 }
 
 // Reverts the effects of a patch on a entity doc
-export function revertPatch (currentDoc, patch) {
+export function revertPatch (currentDoc: InvEntityDoc, patch: Patch) {
   if (patch._id.split(':')[0] !== currentDoc._id) {
     throw newError('entity and patch ids do not match', 500, { currentDoc, patch })
   }
@@ -90,21 +96,21 @@ export function revertPatch (currentDoc, patch) {
   }
 }
 
-export function addVersionsSnapshots (patchesDocs) {
-  let previousVersion = getEntityHistoryBase()
+export function addVersionsSnapshots (patchesDocs: SetOptional<PatchWithSnapshot, 'snapshot'>[]) {
+  let previousVersion = getEntityHistoryBase() as InvEntityDoc | NewInvEntity
 
   // Assumes that patchesDocs are ordered from oldest to newest
   for (const patchDoc of patchesDocs) {
-    patchDoc.snapshot = jiff.patch(patchDoc.operations, previousVersion)
+    patchDoc.snapshot = jiff.patch(patchDoc.operations, previousVersion) as InvEntityDoc
     // jiff.patch is non-mutating: we get a new object
     // without modifying the previous snapshot
     previousVersion = patchDoc.snapshot
   }
 
-  return patchesDocs
+  return patchesDocs as PatchWithSnapshot[]
 }
 
-function applyInverseOperations (currentDoc, inverseOperations) {
+function applyInverseOperations (currentDoc: InvEntityDoc, inverseOperations: PatchOperation[]) {
   currentDoc = cloneDeep(currentDoc)
   inverseOperations.forEach(fixOperation(currentDoc, inverseOperations))
 
@@ -121,15 +127,18 @@ function applyInverseOperations (currentDoc, inverseOperations) {
 
 // Make the required modification for the jiff.patch to success
 // to work around cases known to crash
-const fixOperation = (currentDoc, inverseOperations) => (op, index) => {
-  const opFn = operationFix[op.op]
-  if (opFn != null) return opFn(currentDoc, inverseOperations, op, index)
+function fixOperation (currentDoc: InvEntityDoc, inverseOperations: PatchOperation[]) {
+  return function (op: PatchOperation, index: number) {
+    const opFn = operationFix[op.op]
+    if (opFn != null) return opFn(currentDoc, inverseOperations, op, index)
+  }
 }
 
 const operationFix = {
-  add: (currentDoc, inverseOperations, op) => {
-    const { path, value } = op
-    if (isArray(value)) {
+  add: (currentDoc: InvEntityDoc, inverseOperations: PatchOperation[], op: PatchOperation) => {
+    const { path } = op
+    if ('value' in op && isArray(op.value)) {
+      const { value } = op
       if (value.length === 1) {
         const currentArray = getFromPatchPath(currentDoc, path)
         // Case when the 'add' operation tries to add an array with a value in it,
@@ -149,41 +158,43 @@ const operationFix = {
         const valueIndex = parseInt(path.split('/').at(-1))
         // Avoid "InvalidPatchOperationError: target of add outside of array bounds"
         if (valueIndex > currentArray.length) {
-          op.path = `${parentObjectPath}/${currentArray.length}`
+          op.path = `${parentObjectPath}/${currentArray.length}` as OperationPath
         }
       }
     }
   },
 
-  test: (currentDoc, inverseOperations, op, index) => {
-    const { value } = op
+  test: (currentDoc: InvEntityDoc, inverseOperations: PatchOperation[], op: PatchOperation, index: number) => {
     const nextOp = inverseOperations[index + 1]
     if ((nextOp.path === op.path) && (nextOp.op === 'remove')) {
       // Case when the 'remove' operation tries to remove an array with a value
       // in it, while this array might now contain more values
-      if (isArray(value) && (value.length === 1)) {
+      if ('value' in op && isArray(op.value) && (op.value.length === 1)) {
+        const { value } = op
         op.value = value[0]
-        op.path = (nextOp.path = `${op.path}/0`)
+        op.path = (nextOp.path = `${op.path}/0` as OperationPath)
       }
 
-      const arrayPath = op.path.replace(/\/\d+$/, '')
+      const arrayPath = op.path.replace(/\/\d+$/, '') as OperationPath
       const patchedArray = getFromPatchPath(currentDoc, arrayPath)
 
       if (!patchedArray) {
         throw newError('unhandled patch case', 500, { currentDoc, inverseOperations, op, index, arrayPath })
       }
 
-      const currentValueIndex = patchedArray.indexOf(op.value)
-      // Update the operation path to the current value index
-      // to avoid removing the wrong value if changes messed with the value index
-      op.path = nextOp.path = op.path.replace(/\/\d+$/, `/${currentValueIndex}`)
+      if ('value' in op) {
+        const currentValueIndex = patchedArray.indexOf(op.value)
+        // Update the operation path to the current value index
+        // to avoid removing the wrong value if changes messed with the value index
+        op.path = nextOp.path = op.path.replace(/\/\d+$/, `/${currentValueIndex}`) as OperationPath
+      }
     }
   },
 }
 
-function getFromPatchPath (obj, path) {
+function getFromPatchPath (doc: InvEntityDoc, path: string) {
   const key = path.slice(1).replaceAll('/', '.')
-  return get(obj, key)
+  return get(doc, key)
 }
 
 function getEntityHistoryBase () {
