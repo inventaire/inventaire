b7686f430c903cc694b19f55e6ad7d7a1f2f6f7d
maxlath <k@maxlath.eu>
2024-11-29 18:39:16 +0100

    couchdb_views_context: add log function

--- server/db/couchdb/couchdb_views_context.ts
+++ server/db/couchdb/couchdb_views_context.ts
@@ -6,3 +6,15 @@ export function emit (key: ViewKey, value?: unknown) {
   console.error('emit arguments', { key, value })
   throw new Error('This function should never actually be called by the server')
 }
+
+/** This function mimicks the `log` function available in CouchDB views environment
+ * As view functions will be stringified, they will keep no reference to this function other than its name.
+ *
+ * From [CouchDB's documentation](https://docs.couchdb.org/en/stable/best-practices/jsdevel.html):
+ *   *"The log() function will log output to the CouchDB log file or stream.
+ *    You can log strings, objects, and arrays directly, without first converting to JSON"*
+ */
+export function log (...args: unknown[]) {
+  console.error('log arguments', { args })
+  throw new Error('This function should never actually be called by the server')
+}
