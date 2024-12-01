6f6c5a9ca00b81128ad3fe2bb2b0b1a5200e938b
maxlath <k@maxlath.eu>
2024-12-02 00:15:10 +0100

    controller_wrapper: try to get the remote req user only when the remote user header is set
    to not crash activitypub requests, just because they have a signature header but no remote user header

--- server/lib/controller_wrapper.ts
+++ server/lib/controller_wrapper.ts
@@ -1,6 +1,6 @@
 import { isAuthentifiedReq } from '#lib/boolean_validations'
 import { errorHandler } from '#lib/error/error_handler'
-import { getReqRemoteUser } from '#lib/federation/remote_user'
+import { getReqRemoteUser, remoteUserHeader } from '#lib/federation/remote_user'
 import { someMatch } from '#lib/utils/base'
 import validateObject from '#lib/validate_object'
 import { newUnauthorizedApiAccessError } from './error/pre_filled.js'
@@ -24,7 +24,7 @@ export async function controllerWrapper (controllerParams, req, res) {
     if (req.user) {
       roles.push('authentified')
       if (req.user.roles) roles.push(...req.user.roles)
-    } else if ('signature' in req.headers) {
+    } else if ('signature' in req.headers && remoteUserHeader in req.headers) {
       req.remoteUser = await getReqRemoteUser(req)
       roles.push('authentified')
     }
