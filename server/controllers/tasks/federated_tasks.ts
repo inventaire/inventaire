9686e23f263daa4de86a4292e5cd0f122decb14f
maxlath <k@maxlath.eu>
2024-12-13 14:04:26 +0100

    reorganize federated controllers and introduce federated /api/data controllers

--- server/controllers/tasks/federated_tasks.ts
+++
@@ -1,4 +0,0 @@
-import { localTasksControllersParams } from '#controllers/tasks/tasks'
-import { buildProxiedControllers } from '#lib/federation/proxied_controllers'
-
-export const federatedTasksControllers = buildProxiedControllers('/api/tasks', localTasksControllersParams)
