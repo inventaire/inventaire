d3c6d4f0fa61c3506f034afa1dcca4a3bc4c7344
maxlath <k@maxlath.eu>
2024-12-14 14:12:15 +0100

    images upload: move form parsing to the controller
    preparing for the federated mode where the form data is proxied instead of parsed

--- server/controllers/images/upload.ts
+++ server/controllers/images/upload.ts
@@ -1,10 +1,12 @@
 import type { ParsedForm } from '#controllers/images/lib/parse_form'
+import { parseReqForm } from '#controllers/images/lib/parse_form'
 import { isNonEmptyPlainObject } from '#lib/boolean_validations'
 import { newError } from '#lib/error/error'
 import { assertArray, assertObject, assertString } from '#lib/utils/assert_types'
 import { objectEntries } from '#lib/utils/base'
 import { Log } from '#lib/utils/logs'
-import type { FormReq } from '#types/server'
+import type { SanitizedParameters } from '#types/controllers_input_sanitization_parameters'
+import type { Req } from '#types/server'
 import { containers, uploadContainersNames } from './lib/containers.js'
 
 const sanitization = {
@@ -15,14 +17,13 @@ const sanitization = {
   },
 }
 
-async function controller (params, req: FormReq) {
+async function controller (params: SanitizedParameters, req: Req) {
   const { container } = params
 
   const { putImage } = containers[container]
 
-  if (!req.form) throw newError('missing form data', 400)
-
-  const files = getFilesFromFormData(req.form)
+  const form = await parseReqForm(req)
+  const files = getFilesFromFormData(form)
 
   return Promise.all(files.map(putImage))
   .then(indexUrlById)
