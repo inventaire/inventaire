0cd8c86633b3c8192736897806f39432eadc5b08
maxlath <k@maxlath.eu>
2024-12-14 16:15:14 +0100

    image upload: accept federated requests

--- server/controllers/images/upload.ts
+++ server/controllers/images/upload.ts
@@ -1,11 +1,22 @@
-import { parseReqForm } from '#controllers/images/lib/parse_form'
+import { request as httpRequest, type IncomingMessage } from 'node:http'
+import { request as httpsRequest } from 'node:https'
+import { pick } from 'lodash-es'
+import { multipartFormContentType, parseReqForm } from '#controllers/images/lib/parse_form'
 import { isNonEmptyPlainObject } from '#lib/boolean_validations'
 import { newError } from '#lib/error/error'
+import { errorHandler } from '#lib/error/error_handler'
+import { forwardRemoteError } from '#lib/federation/federated_requests'
+import { getSignedFederatedRequestHeaders } from '#lib/federation/signed_federated_request'
 import { assert_ } from '#lib/utils/assert_types'
 import { Log } from '#lib/utils/logs'
-import type { SanitizedParameters } from '#types/controllers_input_sanitization_parameters'
-import type { Req } from '#types/server'
+import { buildUrl } from '#lib/utils/url'
+import config, { federatedMode } from '#server/config'
+import type { AbsoluteUrl, HttpHeaders } from '#types/common'
+import type { AuthentifiedReq, RemoteUserAuthentifiedReq, Req, Res } from '#types/server'
 import { containers, uploadContainersNames } from './lib/containers.js'
+
+const { remoteEntitiesOrigin } = config.federation
+const streamRequest = remoteEntitiesOrigin?.startsWith('https') ? httpsRequest : httpRequest
 
 const sanitization = {
   nonJsonBody: true,
@@ -15,8 +26,16 @@ const sanitization = {
   },
 }
 
-async function controller (params: SanitizedParameters, req: Req) {
+async function controller (params, req: AuthentifiedReq | RemoteUserAuthentifiedReq, res: Res) {
+  const contentType = req.headers['content-type']
+  if (!contentType?.startsWith(multipartFormContentType)) {
+    throw newError('invalid content-type', 400, { contentType })
+  }
   const { container } = params
+
+  if (federatedMode && container === 'entities') {
+    return proxyRequestToUploadEntityImage(req as AuthentifiedReq, res)
+  }
 
   const { putImage } = containers[container]
 
@@ -52,6 +71,42 @@ function indexUrlById (collection) {
     index[id] = url
   })
   return index
+}
+
+const method = 'post'
+const transferedHeaders = [
+  'content-type',
+  'transfer-encoding',
+  'connection',
+]
+
+async function proxyRequestToUploadEntityImage (req: AuthentifiedReq, res: Res) {
+  const url = buildUrl('/api/images', {
+    action: 'upload',
+    container: 'entities',
+  })
+  const extraHeaders = pick(req.headers, transferedHeaders) as HttpHeaders
+  const headers = await getSignedFederatedRequestHeaders(req, method, url, undefined, extraHeaders)
+  const remoteUrl = `${remoteEntitiesOrigin}${url}` as AbsoluteUrl
+  const forwardedReq = streamRequest(remoteUrl, { method: 'post', headers }, forwardedRes => handleRemoteResponse(req, res, remoteUrl, forwardedRes))
+  req.pipe(forwardedReq)
+}
+
+function handleRemoteResponse (req: Req, res: Res, remoteUrl: AbsoluteUrl, forwardedRes: IncomingMessage) {
+  const { statusCode } = forwardedRes
+  let forwardedResText = ''
+  forwardedRes.on('data', buf => { forwardedResText += buf.toString() })
+  forwardedRes.on('close', () => {
+    res.status(statusCode)
+    const resBody = JSON.parse(forwardedResText)
+    if (statusCode >= 400) {
+      const err = newError('request error', statusCode, { method, url: remoteUrl, statusCode, resBody })
+      err.body = resBody
+      errorHandler(req, res, forwardRemoteError(err, remoteUrl))
+    } else {
+      res.json(resBody)
+    }
+  })
 }
 
 export default { sanitization, controller }
