965d13f94fc6c73ed3010377ea58222c61439ed6
maxlath <k@maxlath.eu>
2024-12-19 18:36:46 +0100

    users: fix special user issues

--- server/controllers/user/lib/anonymizable_user.ts
+++ server/controllers/user/lib/anonymizable_user.ts
@@ -2,12 +2,12 @@ import { get, pick } from 'lodash-es'
 import { dbFactory } from '#db/couchdb/base'
 import type { RemoteUserWithAcct } from '#lib/federation/remote_user'
 import type { UserAccountUri } from '#types/server'
-import type { AnonymizableUserId, DeletedUser, User, UserRole } from '#types/user'
+import type { AnonymizableUserId, DeletedUser, DocWithAnonymizableId, SpecialUser, User, UserRole } from '#types/user'
 
 const db = await dbFactory('users')
 
 export async function getUsersByAnonymizedIds (anonymizableIds: AnonymizableUserId[]) {
-  return db.getDocsByViewKeys<User>('byAnonymizableId', anonymizableIds)
+  return db.getDocsByViewKeys<DocWithAnonymizableId>('byAnonymizableId', anonymizableIds)
 }
 
 export const deanonymizedAttributes = [ 'username', 'bio', 'picture', 'created', 'roles' ] as const
@@ -26,6 +26,7 @@ export interface AnonymizedUser {
 
 export interface DeanonymizedUser extends Pick<User, DeanonymizedAttribute> {
   anonymizableId: AnonymizableUserId
+  special?: boolean
   // Turn the user deleted timestamp into a boolean for consistency with AnonymizedUser
   deleted?: boolean
   settings: {
@@ -39,7 +40,7 @@ export interface AnonymizeUserOptions {
   reqUserHasAdminAccess?: boolean
 }
 
-export function anonymizeUser (user: User | DeletedUser | RemoteUserWithAcct, options: AnonymizeUserOptions = {}) {
+export function anonymizeUser (user: User | DeletedUser | SpecialUser | RemoteUserWithAcct, options: AnonymizeUserOptions = {}) {
   const { reqUserHasAdminAccess } = options
   const anonymizeSetting = get(user, 'settings.contributions.anonymize', true)
   if (anonymizeSetting && !reqUserHasAdminAccess) {
@@ -47,7 +48,7 @@ export function anonymizeUser (user: User | DeletedUser | RemoteUserWithAcct, op
   } else {
     return {
       anonymizableId: user.anonymizableId,
-      special: 'special' in user ? user.special : undefined,
+      special: 'type' in user ? user.type === 'special' : undefined,
       deleted: 'deleted' in user ? true : undefined,
       settings: {
         contributions: {
