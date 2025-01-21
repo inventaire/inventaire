48f33969427928c3ef7e75c4ccca8ee050b3ea05
maxlath <k@maxlath.eu>
2025-01-21 12:19:27 +0100

    server startup: log env variables

--- server/lib/startup.ts
+++ server/lib/startup.ts
@@ -9,6 +9,7 @@ export function beforeStartup () {
   logErrorsCount()
   log(`node: ${process.version}`)
   log(`pid: ${process.pid}`)
+  log(`NODE_ENV=${process.env.NODE_ENV || '<none>'} NODE_APP_INSTANCE=${process.env.NODE_APP_INSTANCE || '<none>'}`)
   log(`env: ${config.env}`)
   log(`local origin: ${getLocalOrigin()}`)
   log(`public origin: ${publicOrigin}`)
