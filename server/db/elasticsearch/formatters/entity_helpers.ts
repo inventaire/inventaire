f82cc31bb037a54c2e50718f7ede4914d7559bd1
maxlath <k@maxlath.eu>
2025-01-30 22:17:50 +0100

    elasticsearch formatters: fix getEntityId helper for wikidata entities with isbn uris

--- server/db/elasticsearch/formatters/entity_helpers.ts
+++ server/db/elasticsearch/formatters/entity_helpers.ts
@@ -1,9 +1,15 @@
-export function getEntityId (entity) {
+import type { InvEntity, SerializedEntity } from '#types/entity'
+import type { Item as WdEntity } from 'wikibase-sdk'
+
+export function getEntityId (entity: SerializedEntity | WdEntity | InvEntity) {
+  if ('wdId' in entity) return entity.wdId
+  if ('invId' in entity) return entity.invId
   // Working around differences in entities formatting between
   // - Wikidata entities from a dump or from Wikidata API (entity.id)
   // - Wikidata entities with inventaire formatting (entity.uri)
   //   (returned in case of Inventaire entity redirection)
   // - Inventaire entities (entity.uri)
-  if (entity.uri) return entity.uri.split(':')[1]
-  else return entity.id || entity._id
+  if ('uri' in entity && typeof entity.uri === 'string') return entity.uri.split(':')[1]
+  // @ts-expect-error
+  return entity.id || entity._id
 }
