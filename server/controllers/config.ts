f921010a0c12cacbc3b7f2998642cd5345df9f3c
maxlath <k@maxlath.eu>
2025-01-30 19:41:23 +0100

    config controller: export client config type

--- server/controllers/config.ts
+++ server/controllers/config.ts
@@ -4,16 +4,20 @@ import config, { publicHost } from '#server/config'
 const { instanceName, orgName, orgUrl, mapTilesAccessToken } = config
 const { remoteEntitiesOrigin } = config.federation
 
-const clientConfig = JSON.stringify({
+const clientConfig = {
   instanceName,
   orgName,
   orgUrl,
   remoteEntitiesOrigin,
   mapTilesAccessToken,
   publicHost,
-})
+} as const
+
+const stringifiedClientConfig = JSON.stringify(clientConfig)
 
 // A endpoint dedicated to pass configuration parameters to the client
 export default {
-  get: (req, res) => sendStaticJson(res, clientConfig),
+  get: (req, res) => sendStaticJson(res, stringifiedClientConfig),
 }
+
+export type ClientConfig = typeof clientConfig
