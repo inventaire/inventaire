f74b512a58856b1db2465b989e041a8cdc92a351
maxlath <k@maxlath.eu>
2025-02-10 18:11:34 +0100

    images resize: modernize getApproximativeRootDomain implementation

--- server/controllers/images/resize.ts
+++ server/controllers/images/resize.ts
@@ -111,7 +111,7 @@ function getApproximativeRootDomain (hostname) {
   if (parts.length <= 2) return hostname
 
   parts = parts.slice(-3)
-  if ([ 'co', 'com' ].indexOf(parts[1]) > -1) return parts.join('.')
+  if ([ 'co', 'com' ].includes(parts[1])) return parts.join('.')
 
   return parts.slice(-2).join('.')
 }
