f8ba7aa79227aa422f0cdff51edeea7333cf14b3
maxlath <k@maxlath.eu>
2025-03-01 01:08:26 +0100

    items snapshots: bundle items related entities requests, to reduce requests overhead
    With a hot entity cache, a requests of 20 items was seen to drop from ~150ms to ~55ms with this optimization.
    It is particularly justified as its a very hot path

--- server/controllers/items/lib/snapshot/refresh_snapshot.ts
+++ server/controllers/items/lib/snapshot/refresh_snapshot.ts
@@ -1,5 +1,10 @@
+import { pick } from 'lodash-es'
+import { getAggregatedPropertiesValues, getEntitiesAggregatedPropertiesValues } from '#controllers/entities/lib/entities'
 import { getEntityByUri } from '#controllers/entities/lib/federation/instance_agnostic_entities'
-import type { EntityUri } from '#types/entity'
+import { getClaimValue } from '#controllers/entities/lib/inv_claims_utils'
+import { editionAuthorRelationsProperties, workAuthorRelationsProperties } from '#controllers/entities/lib/properties/properties'
+import { objectValues } from '#lib/utils/base'
+import type { EntityUri, SerializedEntitiesByUris, SerializedEntity } from '#types/entity'
 import buildSnapshot from './build_snapshot.js'
 import { getWorkAuthorsAndSeries, getEditionGraphEntities } from './get_entities.js'
 
@@ -14,6 +19,27 @@ export const getSnapshotByType = {
   work: async (uri: EntityUri) => {
     const work = await getEntityByUri({ uri })
     const [ authors, series ] = await getWorkAuthorsAndSeries(work)
+    return buildSnapshot.work(work as SerializedEntity, authors, series)
+  },
+}
+
+export const buildSnapshotFromEntitiesByType = {
+  edition: (edition: SerializedEntity, entitiesByUris: SerializedEntitiesByUris) => {
+    const worksUris = edition.claims['wdt:P629'].map(getClaimValue) as EntityUri[]
+    const works = objectValues(pick(entitiesByUris, worksUris))
+    const worksAuthorsUris = getEntitiesAggregatedPropertiesValues(works, workAuthorRelationsProperties)
+    const worksSeriesUris = getEntitiesAggregatedPropertiesValues(works, [ 'wdt:P179' ])
+    const editionsAuthorsUris = getAggregatedPropertiesValues(edition.claims, editionAuthorRelationsProperties) as EntityUri[]
+    const authors = objectValues(pick(entitiesByUris, worksAuthorsUris.concat(editionsAuthorsUris)))
+    const series = objectValues(pick(entitiesByUris, worksSeriesUris))
+    return buildSnapshot.edition(edition, works, authors, series)
+  },
+
+  work: (work: SerializedEntity, entitiesByUris: SerializedEntitiesByUris) => {
+    const worksAuthorsUris = getAggregatedPropertiesValues(work.claims, workAuthorRelationsProperties)
+    const worksSeriesUris = getAggregatedPropertiesValues(work.claims, [ 'wdt:P179' ])
+    const authors = objectValues(pick(entitiesByUris, worksAuthorsUris))
+    const series = objectValues(pick(entitiesByUris, worksSeriesUris))
     return buildSnapshot.work(work, authors, series)
   },
 }
