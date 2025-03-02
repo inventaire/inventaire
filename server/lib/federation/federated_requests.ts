b94d9cfec517712a18da08dd5d4df7d3e83f6025
maxlath <k@maxlath.eu>
2025-03-02 21:26:58 +0100

    federated_requests: fix forwardRemoteError for cases where err.body has already been parsed

--- server/lib/federation/federated_requests.ts
+++ server/lib/federation/federated_requests.ts
@@ -1,3 +1,4 @@
+import { isString } from 'lodash-es'
 import { isRelativeUrl } from '#lib/boolean_validations'
 import { newError } from '#lib/error/error'
 import type { ContextualizedError } from '#lib/error/format_error'
@@ -23,8 +24,8 @@ export async function federatedRequest <Response = unknown> (method: HttpMethod,
 export function forwardRemoteError (err: ContextualizedError, remoteUrl: AbsoluteUrl) {
   if (!('body' in err)) throw err
   const { statusCode } = err
-  // @ts-expect-error
-  const { status_verbose: message, context } = JSON.parse(err.body)
+  const body = isString(err.body) ? JSON.parse(err.body) : err.body
+  const { status_verbose: message, context } = body
   const repackedError = newError(message, statusCode, context)
   repackedError.forwardedFrom = remoteUrl
   return repackedError
