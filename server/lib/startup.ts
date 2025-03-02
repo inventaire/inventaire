395e28a9b287d6451ba20490a0c69b759880c329
maxlath <k@maxlath.eu>
2025-03-03 00:55:18 +0100

    startup: log the server version

--- server/lib/startup.ts
+++ server/lib/startup.ts
@@ -1,5 +1,6 @@
 import { red } from 'tiny-chalk'
 import { initEmailServices } from '#lib/emails/mailer'
+import { gitHeadRev, version } from '#lib/package'
 import { log, logErrorsCount } from '#lib/utils/logs'
 import config, { localOrigin, publicOrigin } from '#server/config'
 
@@ -7,6 +8,7 @@ export function beforeStartup () {
   initUncaughtExceptionCatcher()
 
   logErrorsCount()
+  log(`server: ${version} (${gitHeadRev})`)
   log(`node: ${process.version}`)
   log(`pid: ${process.pid}`)
   log(`NODE_ENV=${process.env.NODE_ENV || '<none>'} NODE_APP_INSTANCE=${process.env.NODE_APP_INSTANCE || '<none>'}`)
