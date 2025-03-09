3a134a280417641f87c2662d0f9a1ccf1f2b029d
maxlath <k@maxlath.eu>
2025-03-09 13:21:28 +0100

    memory_cache_public_controller: implement debounce behavior
    Keeping the cached value alive for as long as it is being used

--- server/lib/memory_cache_public_controller.ts
+++ server/lib/memory_cache_public_controller.ts
@@ -7,17 +7,28 @@ interface MemoryCachePublicControllerParams <T> {
   cacheTtl: number
 }
 
-const cache = {}
+const cache: Record<string, { body: string, timeout: NodeJS.Timeout }> = {}
 
 export function memoryCachePublicController <T> ({ before, controller, getCacheKey, cacheTtl }: MemoryCachePublicControllerParams<T>) {
   return async function cachedController (params: T, req: Req, res: Res) {
     if (before != null) before(res)
+    let stringifiedBody
     const cacheKey = getCacheKey(params)
     if (cache[cacheKey] == null) {
       const body = await controller(params)
-      cache[cacheKey] = JSON.stringify(body)
-      setTimeout(() => delete cache[cacheKey], cacheTtl)
+      stringifiedBody = JSON.stringify(body)
+    } else {
+      const { body, timeout } = cache[cacheKey]
+      // Reset the timeout on cache hit, making it behave like a debounce
+      clearTimeout(timeout)
+      stringifiedBody = body
     }
-    res.send(cache[cacheKey])
+    setCache(cacheKey, stringifiedBody, cacheTtl)
+    res.send(stringifiedBody)
   }
+}
+
+function setCache (cacheKey: string, body: string, cacheTtl: number) {
+  const timeout = setTimeout(() => delete cache[cacheKey], cacheTtl)
+  cache[cacheKey] = { body, timeout }
 }
