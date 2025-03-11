9d8ed77a35c815c99cf3992c2427c4e17ccf2259
maxlath <k@maxlath.eu>
2025-03-11 13:25:50 +0100

    memory_cache_public_controller: prevent parallel calls to the underlying controller

--- server/lib/memory_cache_public_controller.ts
+++ server/lib/memory_cache_public_controller.ts
@@ -2,33 +2,39 @@ import type { Req, Res } from '#types/server'
 
 interface MemoryCachePublicControllerParams <T> {
   before?: (res: Res) => unknown
-  controller: (params: T) => unknown
+  controller: (params: T) => Promise<unknown>
   getCacheKey: (params: T) => string
   cacheTtl?: number
 }
 
-const cache: Record<string, { body: string, timeout: NodeJS.Timeout }> = {}
+const cache: Record<string, { stringifiedBodyPromise: Promise<string>, timeout: NodeJS.Timeout }> = {}
 
 export function memoryCachePublicController <T> ({ before, controller, getCacheKey, cacheTtl = 30 * 1000 }: MemoryCachePublicControllerParams<T>) {
   return async function cachedController (params: T, req: Req, res: Res) {
     if (before != null) before(res)
-    let stringifiedBody
+    let stringifiedBodyPromise
     const cacheKey = getCacheKey(params)
     if (cache[cacheKey] == null) {
-      const body = await controller(params)
-      stringifiedBody = JSON.stringify(body)
+      stringifiedBodyPromise = getStringifiedBody(controller(params))
     } else {
-      const { body, timeout } = cache[cacheKey]
+      ;({ stringifiedBodyPromise } = cache[cacheKey])
       // Reset the timeout on cache hit, making it behave like a debounce
-      clearTimeout(timeout)
-      stringifiedBody = body
+      clearTimeout(cache[cacheKey].timeout)
     }
-    setCache(cacheKey, stringifiedBody, cacheTtl)
-    res.send(stringifiedBody)
+    // Cache the promise, so that parallel initial requests do not trigger
+    // parallel calls to the controller function
+    setCache(cacheKey, stringifiedBodyPromise, cacheTtl)
+    const body = await stringifiedBodyPromise
+    res.send(body)
   }
 }
 
-function setCache (cacheKey: string, body: string, cacheTtl: number) {
+async function getStringifiedBody (bodyPromise: Promise<unknown>) {
+  const body = await bodyPromise
+  return JSON.stringify(body)
+}
+
+function setCache (cacheKey: string, stringifiedBodyPromise: Promise<string>, cacheTtl: number) {
   const timeout = setTimeout(() => delete cache[cacheKey], cacheTtl)
-  cache[cacheKey] = { body, timeout }
+  cache[cacheKey] = { stringifiedBodyPromise, timeout }
 }
