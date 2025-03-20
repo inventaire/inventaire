b0353dba12650bccb9a5cbfbd354ac5bd0b47e25
maxlath <k@maxlath.eu>
2025-03-20 11:41:27 +0100

    get_user_groups: export response type

--- server/controllers/groups/get_user_groups.ts
+++ server/controllers/groups/get_user_groups.ts
@@ -1,9 +1,14 @@
+import type { SanitizedParameters } from '#types/controllers_input_sanitization_parameters'
 import { getGroupsWhereUserIsAdminOrMemberOrInvited } from './lib/groups.js'
+
+async function controller ({ reqUserId }: SanitizedParameters) {
+  const groups = await getGroupsWhereUserIsAdminOrMemberOrInvited(reqUserId)
+  return { groups }
+}
 
 export default {
   sanitization: {},
-  controller: async ({ reqUserId }) => {
-    const groups = await getGroupsWhereUserIsAdminOrMemberOrInvited(reqUserId)
-    return { groups }
-  },
+  controller,
 }
+
+export type GetUserGroupsResponse = Awaited<ReturnType<typeof controller>>
