c1de987a2d4689400533f9e7689665b5f8b49551
maxlath <k@maxlath.eu>
2025-03-31 17:43:48 +0200

    wikidata aliases: add more genre aliases
    addressing https://github.com/inventaire/inventaire/issues/829

--- server/lib/wikidata/aliases.ts
+++ server/lib/wikidata/aliases.ts
@@ -101,6 +101,8 @@ const wikidataOnlyP31Values = {
     'wd:Q108368282', // literary genre by form
     'wd:Q108317211', // novel genre
     'wd:Q20076756', // speculative fiction genre
+    'wd:Q20644555', // fantasy genre
+    'wd:Q11796169', // literary subgenre
   ],
   publishers: [
     'wd:Q1320047', // book publishing company
